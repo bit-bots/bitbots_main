c540d790c3eb8ac73c98f9be204b1a92de044f58
Martin Poppinga <1popping@informatik.uni-hamburg.de>
2017-01-13 00:33:14 +0100

    Added visual horizon

--- bitbots_dummyvision/src/bitbots_dummyvision/bitbots_dummyvision.py
+++ bitbots_dummyvision/src/bitbots_dummyvision/bitbots_dummyvision.py
@@ -27,9 +27,20 @@ class DummyVision:
         ra = self.bridge.imgmsg_to_cv2(img, "bgr8")
         #bimg = cv2.bilateralFilter(ra, 14, 100, 100)
         bimg = cv2.GaussianBlur(ra, (9, 9), 0)
-        # mask = cv2.inRange(img, (0, 120, 0), (160, 255, 160))
+        mask = cv2.inRange(ra, (0, 120, 0), (160, 255, 160))
         # mask = cv2.erode(mask, None, iterations = 2)
         # mask = 255 - mask
+
+        horizonbase = np.zeros((ra.shape[0] / 30, ra.shape[1] / 30))
+        for x in range(ra.shape[0] / 30):
+            for y in range(ra.shape[1] / 30):
+                horizonbase[x, y] = self.check_img(mask[x * 30:(x + 1) * 30, y * 30:(y + 1) * 30])
+
+        horizon = []
+        for col in range(horizonbase.shape[1]):
+            horizon.append(list(horizonbase[:, col]).index(1) * 30)
+
+
         b, g, r = cv2.split(bimg)
         circles = cv2.HoughCircles(g, cv2.HOUGH_GRADIENT, 1, 100,
                                    param1=50, param2=43, minRadius=15, maxRadius=200)
@@ -42,8 +53,12 @@ class DummyVision:
             circles = np.uint16(np.around(circles))
 
             for i in circles[0, :]:
+                if not horizon[i[0] // 30] < i[1]:
+                    #cv2.circle(bimg, (i[0], i[1]), i[2], (0, 0, 255))
+                    continue
                 #corp = ra[i[1] - i[2]:i[1] + i[2], i[0] - i[2]:i[0] + i[2]]
                 can = BallInImage()
+                #cv2.circle(bimg, (i[0], i[1]), i[2], (255,0,0))
                 can.center.x = i[0]
                 can.center.y = i[1]
                 can.diameter = (i[2] * 2) + 3
@@ -51,7 +66,19 @@ class DummyVision:
                 can.header.stamp = img.header.stamp
                 msg.candidates.append(can)
 
+        #for x in range(len(horizon)-1):
+        #    cv2.line(bimg, (30*x, horizon[x]),(30*(x+1), horizon[x+1]), color=(0,255,0))
+
+        #cv2.imshow("Image", bimg)
+        #cv2.waitKey(1)
+
         self.pub_balls.publish(msg)
+
+    @staticmethod
+    def check_img(subim):
+        m = np.average(np.average(subim, axis=0), axis=0)
+        return m > 100
+
 
     def _image_callback(self, img):
         self.work(img)
