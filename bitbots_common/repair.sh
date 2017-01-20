38f566bc0fa9f656908baa732334ca79311e0a58
Marc Bestmann <0bestman@informatik.uni-hamburg.de>
2017-01-20 12:32:57 +0100

    repair script for making __init__ files and so on

---
+++ bitbots_common/repair.sh
@@ -0,0 +1,18 @@
+touch devel/lib/python2.7/dist-packages/bitbots_cm730/lowlevel/controller/__init__.py
+
+touch devel/lib/python2.7/dist-packages/bitbots_cm730/lowlevel/__init__.py
+
+touch devel/lib/python2.7/dist-packages/bitbots_cm730/__init__.py
+
+touch devel/lib/python2.7/dist-packages/bitbots_walking/__init__.py
+
+touch devel/lib/python2.7/dist-packages/bitbots_common/pose/__init__.py
+
+touch devel/lib/python2.7/dist-packages/bitbots_common/utilCython/__init__.py
+
+cp src/bitbots_misc/bitbots_speaker/src/bitbots_speaker/cfg/speaker_paramsConfig.py* ./devel/lib/python2.7/dist-packages/bitbots_speaker/cfg
+
+cp src/bitbots_lowlevel/bitbots_motion/src/bitbots_motion/cfg/motion_paramsConfig.py* ./devel/lib/python2.7/dist-packages/bitbots_motion/cfg
+
+cp src/bitbots_vision/bitbots_dummyvision/src/bitbots_dummyvision/cfg/dummyvision_paramsConfig.py* ./devel/lib/python2.7/dist-packages/bitbots_dummyvision/cfg
+
