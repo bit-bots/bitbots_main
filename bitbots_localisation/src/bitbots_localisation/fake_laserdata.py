5dc097a9c1a9992bf3e89dac315180350c7ec899
Martin Poppinga <git-me@poppinga.xyz>
2017-01-21 02:01:53 +0100

    added tf transform for fake laser data

--- bitbots_localisation/src/bitbots_localisation/fake_laserdata.py
+++ bitbots_localisation/src/bitbots_localisation/fake_laserdata.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 import math
 from cv2 import norm
 
@@ -6,17 +6,42 @@ import numpy as np
 from numpy import dot
 
 import rospy
+from geometry_msgs.msg import PointStamped
 from humanoid_league_msgs.msg import LineInformationInImage
 from sensor_msgs.msg import LaserScan
 
+import tf
+from tf.listener import TransformListener
 
-class FakeLaser:
+
+def angle_b(a):
+    b = (1, 0)
+    arccos_input = dot(a, b) / norm(a) / norm(b)
+    arccos_input = 1.0 if arccos_input > 1.0 else arccos_input
+    arccos_input = -1.0 if arccos_input < -1.0 else arccos_input
+    return math.acos(arccos_input)
+
+
+class FakeLaser(object):
     def __init__(self):
-        self.sub_line = rospy.Subscriber("/lines_relative", self._callback_lines)
+        self.sub_line = rospy.Subscriber("/line_in_image", LineInformationInImage, self._callback_lines)
         self.pub_laser = rospy.Publisher("/laser_scan_data", LaserScan)
+        rospy.init_node("bitbots_Fakelaser")
+        self.tfl = TransformListener()
 
-    def _callback_lines(self, lines: LineInformationInImage):
+        rospy.spin()
+
+    def _callback_lines(self, lines):
         linepoints = [(x.start.x, x.start.y) for x in lines.segments]
+        relp =[]
+        for x, y in linepoints:
+            ps = PointStamped()
+            ps.point.x = x
+            ps.point.y = y
+            ps.header.frame_id = "L_HEAD_TILT"
+            l = self.tfl.transformPoint("/base_link", ps)
+            relp.append((l.point.x, l.point.y))
+
         for deg in range(360):
             ls = LaserScan()
             ls.header.frame_id = lines.header.frame_id
@@ -28,14 +53,6 @@ class FakeLaser:
             for scan in filter(angle_b, linepoints):
                 ls.ranges.append(math.sqrt(scan[0] ** 2 * scan[1] ** 2))
             self.pub_laser.publish(ls)
-
-
-def angle_b(a):
-    b = (1, 0)
-    arccos_input = dot(a, b) / norm(a) / norm(b)
-    arccos_input = 1.0 if arccos_input > 1.0 else arccos_input
-    arccos_input = -1.0 if arccos_input < -1.0 else arccos_input
-    return math.acos(arccos_input)
 
 
 if __name__ == "__main__":
