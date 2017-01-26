b0560c7c016c6749716c07b5f4f6788201471ff0
Martin Poppinga <git-me@poppinga.xyz>
2017-01-26 22:54:03 +0100

    transform_lines now uses tf2 & python2 & pointcloud2

--- bitbots_transform/src/bitbots_transform/transform_lines.py
+++ bitbots_transform/src/bitbots_transform/transform_lines.py
@@ -1,36 +1,36 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python2.7
 import rospy
-from tf.listener import TransformListener
-from sensor_msgs.msg import PointCloud2, CameraInfo
+import tf2_ros
+from geometry_msgs.msg import Point32
+from sensor_msgs.msg import PointCloud2, CameraInfo, PointCloud, PointField
+from sensor_msgs.point_cloud2 import create_cloud
 from humanoid_league_msgs.msg import LineInformationInImage
 import image_geometry
+from std_msgs.msg import Header
 
-class Transform_Lines:
+
+class TransformLines(object):
     def __init__(self):
         rospy.Subscriber("/line_in_image", LineInformationInImage, self._callback_lines)
-        rospy.Subscriber("/camera_info", CameraInfo, self._callback_camera_info)
-        self.line_pointcloud = rospy.Publisher("/cloud_in", PointCloud2)
+        rospy.Subscriber("/minibot/camera/camera_info", CameraInfo, self._callback_camera_info)
+        self.line_pointcloud = rospy.Publisher("/cloud_in", PointCloud2, queue_size=10)
 
         self.caminfo = None  # type:CameraInfo
 
         rospy.init_node("transform_lines")
+        rospy.spin()
 
-    def _callback_lines(self, lineinfo:LineInformationInImage):
+    def _callback_lines(self, lineinfo):
+        """
+        :type lineinfo: LineInformationInImage
+        :return:
+        """
         if not self.caminfo:
-            return # No camaraInfo available
+            return  # No camaraInfo available
 
-        pc = PointCloud2()
-        pc.header.stamp = lineinfo.header.stamp
-        pc.header.frame_id = "/base_link"
-        pc.fields = [float, float]
-        pc.height = 10000
-        pc.width = 10000
-        pc.point_step = 1
-        pc.row_step = 1
-
-        tfl = TransformListener()
-        tfl.waitForTransform("/base_link", lineinfo.header.frame_id, rospy.Time.now(), rospy.Duration(4))
-        (trans, rot) = tfl.lookupTRansform("/base_link", lineinfo.header.frame_id, lineinfo.header.stamp)
+        tfbuffer = tf2_ros.Buffer()
+        tfl = tf2_ros.TransformListener(tfbuffer)
+        trans = tfbuffer.lookup_transform("base_link", "L_CAMERA", rospy.Time.now(), rospy.Duration(1))
         points = []
         for seg in lineinfo.segments:
             lineu = seg.start.x
@@ -42,13 +42,20 @@ class Transform_Lines:
 
             ray = cam.projectPixelTo3dRay((lineu, linev))
 
-            points.append((trans[0] + ray[0] * ray[2], trans[1] + ray[1] * ray[3]))
-        pc.data = bytearray(points)
+            point = [trans.transform.translation.x + ray[0] * ray[2], trans.transform.translation.y + ray[1] * ray[2], 0]
+            points.append(point)
 
-        self.line_pointcloud.publish(pc)
+        header = Header()
+        header.stamp = lineinfo.header.stamp
+        header.frame_id = "base_link"
+        pfs = [PointField.FLOAT32, PointField.FLOAT32, PointField.FLOAT32]
+        pc2 = create_cloud(header, pfs, points)
+
+        self.line_pointcloud.publish(pc2)
 
     def _callback_camera_info(self, camerainfo):
         self.caminfo = camerainfo
 
+
 if __name__ == "__main__":
-    Transform_Lines()
+    TransformLines()
