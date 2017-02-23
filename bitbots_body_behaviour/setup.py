adbb9871dd04a76a69610d74297cda56d33acd11
Martin Poppinga <1popping@informatik.uni-hamburg.de>
2017-02-23 22:30:34 +0100

    changed name form bitbots_behavior to bitbots_body_behavior in files

--- bitbots_body_behaviour/setup.py
+++ bitbots_body_behaviour/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['bitbots_behaviour'],
+    packages=['bitbots_body_behaviour'],
     #scripts=['bin/myscript'],
     package_dir={'': 'src'}
 )
