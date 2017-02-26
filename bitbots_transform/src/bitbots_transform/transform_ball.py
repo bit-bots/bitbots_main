4f3a2e7cb895fb048bc7db92505c16e61c01bca2
Martin Poppinga <1popping@informatik.uni-hamburg.de>
2017-02-26 19:47:34 +0100

    transform added for balls

---
+++ bitbots_transform/src/bitbots_transform/transform_ball.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2.7
+import rospy
+from bitbots_transform.transform_helper import transf
+from humanoid_league_msgs.msg import BallRelative, BallInImage
+from sensor_msgs.msg import CameraInfo
+
+
+class TransformLines(object):
+    def __init__(self):
+        rospy.Subscriber("ball_in_image", BallInImage, self._callback_ball, queue_size=1)
+        rospy.Subscriber("camera/camera_info", CameraInfo, self._callback_camera_info)
+        self.line_relative_pub = rospy.Publisher("ball_relative", BallRelative, queue_size=10)
+        self.caminfo = None  # type:CameraInfo
+
+        rospy.init_node("transform_ball")
+
+        rospy.spin()
+
+    def _callback_ball(self, ballinfo):
+        if not self.caminfo:
+            return  # No camaraInfo available
+
+        self.work(ballinfo)
+
+    def work(self, ballinfo):
+        p = transf(ballinfo.center.x, ballinfo.center.y, self.caminfo)
+
+        br = BallRelative()
+        br.header.stamp = ballinfo.header.stamp
+        br.header.frame_id = "base_link"
+
+        br.ball_relative.x = p[0]
+        br.ball_relative.y = p[1]
+        br.ball_relative.z = p[2]
+
+        self.line_relative_pub.publish(br)
+
+    def _callback_camera_info(self, camerainfo):
+        self.caminfo = camerainfo
+
+
+if __name__ == "__main__":
+    TransformLines()
