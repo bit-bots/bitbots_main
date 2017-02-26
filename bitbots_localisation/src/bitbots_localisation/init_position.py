4dd7eabdeb48b3da625455c9f6b708d675a5eee8
Martin Poppinga <1popping@informatik.uni-hamburg.de>
2017-02-26 21:28:39 +0100

    Fixed init_pose pose position information

--- bitbots_localisation/src/bitbots_localisation/init_position.py
+++ bitbots_localisation/src/bitbots_localisation/init_position.py
@@ -11,14 +11,16 @@ class InitPosition:
 
     def init_pose(self):
         pose = PoseWithCovarianceStamped()
-        pose.pose.pose.position = (0, 0, 0)
+        pose.pose.pose.position.x = 0
+        pose.pose.pose.position.y = 0
+        pose.pose.pose.position.z = 0
         self.pose.publish(pose)
 
 
 if __name__ == "__main__":
     ip = InitPosition()
 
-    rate = rospy.Rate(0.01)
+    rate = rospy.Rate(1)
 
     while not rospy.is_shutdown():
         ip.init_pose()
