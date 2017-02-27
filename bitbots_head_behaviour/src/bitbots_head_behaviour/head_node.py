a79d8251d4e1f6e4b6340335741933edf1aa578b
Martin Poppinga <1popping@informatik.uni-hamburg.de>
2017-02-27 03:23:21 +0100

    fixed starting issue for headbehaviour, moves pub and sub to mainclass

--- bitbots_head_behaviour/src/bitbots_head_behaviour/head_node.py
+++ bitbots_head_behaviour/src/bitbots_head_behaviour/head_node.py
@@ -4,6 +4,8 @@ from bitbots_common.connector.connector import HeadConnector
 from bitbots_head_behaviour.decisions.head_duty_decider import HeadDutyDecider
 from bitbots_stackmachine.stack_machine_module import StackMachineModule
 from humanoid_league_msgs.msg import HeadMode, BallRelative, ObstacleRelative
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory
 
 
 class HeadNode(StackMachineModule):
@@ -12,16 +14,22 @@ class HeadNode(StackMachineModule):
         self.connector = HeadConnector()
         self.set_start_module(HeadDutyDecider)
 
+        self.connector.config = rospy.get_param("Behaviour")
+        self.connector.head.position_publisher = rospy.Publisher("head_motor_goals", JointTrajectory, queue_size=10)
+        rospy.Subscriber("joint_states", JointState, self.connector.head.joint_state_cb)
         rospy.Subscriber("head_duty", HeadMode, self.connector.head.cb_headmode, queue_size=10)
         rospy.Subscriber("ball_relative", BallRelative, self.connector.vision.ball_callback)
         rospy.Subscriber("obstacle_relative", ObstacleRelative, self.connector.vision.obstacle_callback)
 
-        self.connector.config = rospy.get_param("Behaviour")
         rospy.init_node("Headbehaviour")
-        self.run()
 
     def run(self):
         rate = rospy.Rate(15)
         while not rospy.is_shutdown():
             self.update()
             rate.sleep()
+
+if __name__ == "__main__":
+    hb = HeadNode()
+    hb.run()
+
