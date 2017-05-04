36aa0882de75179a715329c9bd07ed1d095fd1c4
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2017-05-04 10:28:57 +0200

    fixes on boot scripts

--- bitbots_common/scripts/boot-behaviour.sh
+++ bitbots_common/scripts/boot-behaviour.sh
@@ -2,7 +2,7 @@
 
 while true ; do
     (
-        . /home/odroid/boot-defaults.sh
+        . ~/catkin_ws/src/bitbots_meta/bitbots_misc/bitbots_common/scripts/boot-defaults.sh
 
         if $START_BEHAVIOUR ; then
             roslaunch bitbots_body_behaviour start_behaviour_standalone.launch
---
+++ bitbots_common/scripts/bootup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$TERM" != "screen" ] ; then
+    echo "this script must be run with screen"
+    exit 1
+fi
+
+source ~/catkin_ws/devel/setup.sh
+
+screen ~/catkin_ws/src/bitbots_meta/bitbots_misc/bitbots_common/scripts/boot-robot-control.sh
+screen ~/catkin_ws/src/bitbots_meta/bitbots_misc/bitbots_common/scripts/boot-behaviour.sh
