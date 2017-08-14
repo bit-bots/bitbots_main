e56008b9b4eb5f410f7b0ff58c2395660c7753b7
Martin Poppinga <1popping@informatik.uni-hamburg.de>
2017-08-14 21:43:43 +0200

    explicit setting the desired behaviour in bootupscript

--- bitbots_common/scripts/boot-highlevel.sh
+++ bitbots_common/scripts/boot-highlevel.sh
@@ -7,7 +7,7 @@ while true ; do
         . ~/catkin_ws/src/bitbots_meta/bitbots_misc/bitbots_common/scripts/boot-defaults.sh
 
         if $START_BEHAVIOUR ; then
-            roslaunch bitbots_common start_robocup_teamplayer.launch hcm:=false
+            roslaunch bitbots_common start_robocup_teamplayer.launch hcm:=false duty:=TeamPlayer
         fi
     )
     echo "You have two seconds to kill this script."
