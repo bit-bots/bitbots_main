a8adb00af3e98047cbed54030c83279566067c2c
Martin Poppinga <1popping@informatik.uni-hamburg.de>
2017-08-19 16:19:33 +0200

    added pathfinding from old behaviour

--- bitbots_body_behaviour/src/bitbots_body_behaviour/body/actions/go_to_ball_pathfinding.py
+++ bitbots_body_behaviour/src/bitbots_body_behaviour/body/actions/go_to_ball_pathfinding.py
@@ -5,7 +5,13 @@ GotToBallPathfinding
 .. moduleauthor:: Martin Poppinga <1popping@informatik.uni-hamburg.de>
 
 """
-from geometry_msgs.msg import Pose2D
+import math
+
+import time
+
+from bitbots_common.connector.connector import BodyConnector
+from bitbots_pathfinding.potential_field import PotentialMap
+
 
 from bitbots_stackmachine.abstract_action_module import AbstractActionModule
 
@@ -14,7 +20,87 @@ class GoToBallPathfinding(AbstractActionModule):
     """
     Goes to the ball
     """
-    def perform(self, connector, reevaluate=False):
-        p = Pose2D()
-        p.x, p.y = connector.vision.get_ball_relative()
-        connector.walking.pub_walking_objective.publish(p)
+
+    def __init__(self, _, connector: BodyConnector):
+        super().__init__(connector)
+        self.align_to_goal = None
+        self.toggle_track_both = None
+        self.focus_ball_distance = None
+        self.f = 0
+        self.t = 0
+        self.s = 0
+        self.last_f = 0
+        self.last_t = 0
+        self.last_s = 0
+        self.last_iteration = time.time()
+
+    def perform(self, connector: BodyConnector, reevaluate=False):
+        self.align_to_goal = connector.config["Body"]["Toggles"]["Fieldie"]["alignToGoal"]
+        self.toggle_track_both = connector.config["Body"]["Toggles"]["Fieldie"]["trackBoth"]
+        self.focus_ball_distance = connector.config["Body"]["Fieldie"]["focusBallDistance"]
+
+        # Get ball data
+        bu, bv = connector.vision.get_ball_relative()
+        bdist = math.sqrt(bu ** 2 + bv ** 2)
+
+        # Look to the Ball
+        if self.toggle_track_both and bdist > self.focus_ball_distance:
+            connector.blackboard.schedule_both_tracking()
+        else:
+            connector.blackboard.schedule_ball_tracking()
+
+        # The Ball is an obstacle, the Obstacles also and please avoid them
+        obstacles = [
+            [bu, bv]
+        ]
+
+        # use the value from the config
+        align_to_goal = self.align_to_goal
+
+        self.go_to_pos(connector, bu, bv, obstacles, align_to_goal)
+
+    def go_to_pos(self, connector: BodyConnector, pu, pv, additional_obstacles, align_to_goal):
+
+        # only run approx. 3 times a second:
+
+        if time.time() - self.last_iteration > 0.33:
+            network = connector.blackboard.get_pathfinding_net()
+            self.last_iteration = time.time()
+
+            if False and align_to_goal:
+                pass
+                # gu, gv = connector.vision.get_local_goal_model_opp_goal()
+            else:
+                gu, gv = 0, 0
+
+            # Slow down if near to position
+            distancefactor = 1 if ((pu ** 2 + pv ** 2) ** 0.5) > 200 else 0.4
+
+            # Neuronal net computes parameters for walking
+            walk_parameters = network.compute([pu, pv, gu, gv, self.f, self.t, self.s])
+
+            obstacles = []
+            #obstacles.extend(connector.vision.get_obstacle_info())
+            obstacles.extend(additional_obstacles)
+
+            potential_map = PotentialMap((len(obstacles), 0))
+            self.f, self.t, self.s = potential_map.compute(obstacles)
+
+            # put values together and take some factors
+            tres = lambda x: 0.0 if abs(x) < 0.75 else x
+            sig = lambda x: x / (1 + abs(x))
+            turn = tres(walk_parameters[1] * 5.0)
+            forward = ((tres(((walk_parameters[0]) + (self.f * 0.5)) * distancefactor)) * 5) + abs(turn / 4)
+
+            side = sig(tres(((walk_parameters[2]) + self.s) * 0.3)) * 2.5
+
+            self.last_f = forward
+            self.last_t = turn
+            self.last_s = side
+
+        else:
+            forward = self.last_f
+            turn = self.last_t
+            side = self.last_s
+        # start walking
+        connector.walking.start_walking_plain(forward, turn, side)
