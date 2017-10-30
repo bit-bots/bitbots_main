87ffd350f798457aeaaff8c66735b4524df660c9
5hagge <5hagge@informatik.uni-hamburg.de>
2017-10-30 16:13:23 +0100

    added quick horizon search

--- bitbots_soccer_vision/scripts/vision_modules/horizon.py
+++ bitbots_soccer_vision/scripts/vision_modules/horizon.py
@@ -1,28 +1,47 @@
 import numpy as np
+from.color import ColorDetector
 
 class HorizonDetector:
 
-    def __init__(self, image, color_detector):
-        self.image = image
-        self.color_detector = color_detector
+    def __init__(self, image, color_detector: ColorDetector):
+        self._image = image
+        self._color_detector = color_detector
         self._horizon_points = None
         self._horizon_full = None
         self._x_steps = 30
         self._y_steps = 30
 
-    def get_horizon_points(self):
+    def get_horizon_points(self) -> list:
         if self._horizon_points is None:
-            y_stepsize = self.image.shape[0] / (self._y_steps-1)
-            x_stepsize = self.image.shape[1] / (self._x_steps-1)
-            self._horizon_points = []
-            for x_step in range(self._x_steps):
-                x = round(x_step * x_stepsize)
-                for y_step in range(self._y_steps):
-
-                                        pass
+            self._horizon_points = self._fast_horizon()
         return self._horizon_points
 
-    def get_full_horizon(self):
+    def _fast_horizon(self) -> list:
+        y_stepsize = self._image.shape[0] / (self._y_steps - 1)
+        x_stepsize = self._image.shape[1] / (self._x_steps - 1)
+        horizon_points = []
+        for x_step in range(self._x_steps):
+            x = round(x_step * x_stepsize)
+            temp_horizon_y = round(self._y_steps * y_stepsize)
+            for y_step in range(self._y_steps):
+                y = round(y_step * y_stepsize)
+                # y,x because opencv image format
+                if self._color_detector.match_pixel(self._image[y, x]):
+                    temp_horizon_y = y
+                    # going back half the step size if horizon was found
+                    # doubles the precision at almost no cost
+                    if y_step > 0:
+                        half_y_step = round(y - (0.5 * y_stepsize))
+                        if self._color_detector.match_pixel(self._image[half_y_step, x]):
+                            temp_horizon_y = [half_y_step, x]
+                    break
+            horizon_points.append((x, temp_horizon_y))
+        return horizon_points
+
+
+
+
+    def get_full_horizon(self) -> list:
         if self._horizon_full is None:
             xp, fp = zip(*self.get_horizon_points())
             x = list(range(len(fp)+1))
