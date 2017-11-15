a4bf2f9a3dd77c4e49aca9daa30013d8faaf3861
5hagge <5hagge@informatik.uni-hamburg.de>
2017-11-15 09:55:18 +0100

    added documentation for debug_image.py

--- bitbots_soccer_vision/scripts/vision_modules/debug_image.py
+++ bitbots_soccer_vision/scripts/vision_modules/debug_image.py
@@ -7,14 +7,28 @@ class DebugImage:
         self.raw_image = image[:]
 
     def draw_horizon(self, horizon_points):
+        """
+        draws a line where the horizon algorithm found the horizon
+        :param horizon_points list of coordinates of the horizon:
+        :return void:
+        """
         for i in range(len(horizon_points) - 1):
             cv2.line(self.raw_image, horizon_points[i], horizon_points[i+1], 255)
 
     def draw_ball_candidates(self, ball_candidates):
+        """
+        draws a circle around every coordinate where a ball candidate was found
+        :param ball_candidates list of cooordinates of ball candidates:
+        :return void:
+        """
         for candidate in ball_candidates:
             cv2.circle(self.raw_image, (candidate[0] + candidate[2]//2, candidate[1] + candidate[3] // 2), candidate[3] // 2, 1023)
 
     def imshow(self):
+        """
+        saves the image in current directory as "img.png" with edits made to the image from the other functions
+        :return void:
+        """
         cv2.imwrite('img.png', self.raw_image)
         # cv2.imshow('Debug Image', self.raw_image)
         # cv2.waitKey(0)
