926f7169d627e54ff473ae9aaed4b59ae1d1132e
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2017-12-08 17:04:22 +0100

    line_detection params to visionparams config-file

--- bitbots_vision/scripts/vision_modules/lines.py
+++ bitbots_vision/scripts/vision_modules/lines.py
@@ -7,6 +7,7 @@ from.horizon import HorizonDetector
 import math
 import numpy as np
 import cv2
+import rospy
 
 
 class LineDetector:
@@ -18,7 +19,7 @@ class LineDetector:
         self._linepoints = None
         self._white_detector = white_detector
         self._horizon_detector = horizon_detector
-        self._horizon_offset = 10
+        self._horizon_offset = rospy.get_param('visionparams/line_detector/horizon_offset')
 
     def set_candidates(self, candidates):
         # type: (list) -> None
@@ -26,7 +27,7 @@ class LineDetector:
 
     def get_linepoints(self):
         if self._linepoints is None:
-            for x in range(1000):
+            for x in range(rospy.get_param('visionparams/line_detector/linepoints_range')):
                 # point (x, y)
                 p = tuple((randint(0, self._blurred_image.shape[1] - 1),
                            randint(self._horizon_detector.get_upper_bound(self._horizon_offset),
@@ -51,5 +52,5 @@ class LineDetector:
 
     def _get_blurred_image(self):
         if self._blurred_image is None:
-            self._blurred_image = cv2.GaussianBlur(self._image, (9, 9), 0)
+            self._blurred_image = cv2.blur(self._image, rospy.get_param('visionparams/line_detector/blur_kernel_size'))
         return self._blurred_image
