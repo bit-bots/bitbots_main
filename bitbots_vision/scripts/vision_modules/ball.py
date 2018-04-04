29cf69dd747061d2f5a9964a73d0702941fcab18
Niklas Fiedler <git@nfiedler.de>
2018-04-04 15:37:15 +0200

    alternative ball representation

--- bitbots_vision/scripts/vision_modules/ball.py
+++ bitbots_vision/scripts/vision_modules/ball.py
@@ -4,6 +4,44 @@ import numpy as np
 import cv2
 
 
+class Ball:
+    def __init__(self, x1=0, y1=0, width=0, height=0):
+        self._x1 = x1
+        self._y1 = y1
+        self._width = width
+        self._height = height
+
+    def get_width(self):
+        return self._width
+
+    def get_height(self):
+        return self._height
+
+    def get_center_x(self):
+        return self._x1 + int(self._width // 2)
+
+    def get_center_y(self):
+        self._y1 + int(self._height // 2)
+
+    def get_center_point(self):
+        return self.get_center_x(), self.get_center_y()
+
+    def get_diameter(self):
+        return int((self._height + self._width) // 2)
+
+    def get_radius(self):
+        return int(self.get_diameter() // 2)
+
+    def get_upper_left_point(self):
+        return self._x1, self._y1
+
+    def get_upper_left_x(self):
+        return self._x1
+
+    def get_upper_left_y(self):
+        return self._y1
+
+
 class BallFinder:
     def __init__(self, image, cascade, config):
         # type: (np.matrix, cv2.CascadeClassifier, dict) -> BallFinder
