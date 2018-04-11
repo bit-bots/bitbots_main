98cd8288befe5b67ce0058004dd7915e0f974e37
Jasper Güldenstein <jasper.guel@hotmail.de>
2018-04-11 11:01:18 +0200

    tester now warns when doing something stupid and is also keyboard interruptable

--- bitbots_transform/scripts/transform_tester.py
+++ bitbots_transform/scripts/transform_tester.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python2.7
 import rospy
 from humanoid_league_msgs.msg import BallsInImage, BallInImage
+import sys
+import signal
 
 
 
-rospy.init_node("transform_tester")
-pub = rospy.Publisher("ball_in_image", BallsInImage,queue_size=10)
+def signal_term_handler(signal, frame):
+  rospy.logerr('User Keyboard interrupt')
+  sys.exit(0)
 
-while True:
-    x = int(raw_input("x:"))
-    y = int(raw_input("y:"))
+if __name__ == "__main__":
+    # handle keyboard interrupts
+    signal.signal(signal.SIGINT, signal_term_handler)
+    
+    rospy.init_node("transform_tester")
+    pub = rospy.Publisher("ball_in_image", BallsInImage,queue_size=10)
+
+    while True:
+        x_str = raw_input("x:")
+        y_str = raw_input("y:")
 
 
-    bi = BallsInImage()
-    bi.header.stamp = rospy.get_rostime() - rospy.Duration(0.2)
-    ball = BallInImage()
-    ball.confidence = 1
-    ball.center.x = x
-    ball.center.y = y
-    ball.diameter = 0.13
+        try:
+            x = int(x_str)
+            y = int(y_str)
+        except ValueError:
+            print("try again, without fucking this time up please")
+            continue
 
-    bi.candidates.append(ball)
+        bi = BallsInImage()
+        bi.header.stamp = rospy.get_rostime() - rospy.Duration(0.2)
+        ball = BallInImage()
+        ball.confidence = 1
+        ball.center.x = x
+        ball.center.y = y
+        ball.diameter = 0.13
 
-    pub.publish(bi)
+        bi.candidates.append(ball)
+
+        pub.publish(bi)
+
