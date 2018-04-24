2df510bc34032943d6836d9097c3e9e236eb0d21
Judith Hartfill <2hartfil@informatik.uni-hamburg.de>
2018-04-24 16:38:44 +0200

    launch files for amcl

--- bitbots_transform/scripts/line_tester.py
+++ bitbots_transform/scripts/line_tester.py
@@ -17,36 +17,37 @@ if __name__ == "__main__":
     rospy.init_node("line_tester")
     pub = rospy.Publisher("line_in_image", LineInformationInImage, queue_size=10)
 
+    x_start_str = raw_input("x_start:")
+    try:
+        x_start = int(x_start_str)
+    except ValueError:
+        print("try again, without fucking this time up please")
+        #continue
+
+    y_start_str = raw_input("y_start:")
+    try:
+        y_start = int(y_start_str)
+    except ValueError:
+        print("try again, without fucking this time up please")
+       # continue
+
+    x_end_str = raw_input("x_end:")
+    try:
+        x_end = int(x_end_str)
+    except ValueError:
+        print("try again, without fucking this time up please")
+       # continue
+
+    y_end_str = raw_input("y_end:")
+    try:
+        y_end = int(y_end_str)
+    except ValueError:
+        print("try again, without fucking this time up please")
+       # continue
+
     while True:
-        x_start_str = raw_input("x_start:")
-        try:
-            x_start = int(x_start_str)
-        except ValueError:
-            print("try again, without fucking this time up please")
-            continue
-
-        y_start_str = raw_input("y_start:")
-        try:
-            y_start = int(y_start_str)
-        except ValueError:
-            print("try again, without fucking this time up please")
-            continue
-
-        x_end_str = raw_input("x_end:")
-        try:
-            x_end = int(x_end_str)
-        except ValueError:
-            print("try again, without fucking this time up please")
-            continue
-
-        y_end_str = raw_input("y_end:")
-        try:
-            y_end = int(y_end_str)
-        except ValueError:
-            print("try again, without fucking this time up please")
-            continue
-
         li = LineInformationInImage()
+        # press play in simulation first!
         li.header.stamp = rospy.get_rostime() - rospy.Duration(0.2)
         seg = LineSegmentInImage()
         seg.start.x = x_start
