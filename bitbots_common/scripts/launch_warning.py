5da5e14a997da4ae6174a1a0a0a93a8307dee5e9
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2018-05-04 15:33:38 +0200

    added davros to launch files

--- bitbots_common/scripts/launch_warning.py
+++ bitbots_common/scripts/launch_warning.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 import rospy
 
-rospy.logerr("###\n###\n###\n###\n###\nYou didn't specifiy which robot you want to start!\nPlease add minibot:=true or woflgang:=true behind you roslaunch.\n###\n###\n###\n###\n###")
+rospy.logerr("###\n###\n###\n###\n###\nYou didn't specifiy which robot you want to start!\nPlease add minibot:=true or woflgang:=true or davros:=true behind you roslaunch.\n###\n###\n###\n###\n###")
