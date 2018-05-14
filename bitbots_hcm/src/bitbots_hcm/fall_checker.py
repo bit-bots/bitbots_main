8e22af161b98615912ccadb689c1f0e6de0be8a6
Florian Vahl (17vahl) <florian@flova.de>
2018-05-14 16:47:23 +0200

    Changed Axis

--- bitbots_hcm/src/bitbots_hcm/fall_checker.py
+++ bitbots_hcm/src/bitbots_hcm/fall_checker.py
@@ -121,12 +121,12 @@ class FallChecker(object):
         
         t0 = +2.0 * (w * x + y * z)
         t1 = +1.0 - 2.0 * (x * x + ysqr)
-        X = math.atan2(t0, t1)
+        Y = math.atan2(t0, t1)
         
         t2 = +2.0 * (w * y - z * x)
         t2 = +1.0 if t2 > +1.0 else t2
         t2 = -1.0 if t2 < -1.0 else t2
-        Y = math.asin(t2)
+        X = math.asin(t2)
         
         t3 = +2.0 * (w * z + x * y)
         t4 = +1.0 - 2.0 * (ysqr + z * z)
