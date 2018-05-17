4fe0ac631a918f2302244ba828ed7b20d3164cf6
Jasper Güldenstein <5guelden@informatik.uni-hamburg.de>
2018-05-17 11:55:54 +0200

    added teleop node and launch

--- bitbots_joy/src/bitbots_joy/joy_node.py
+++ bitbots_joy/src/bitbots_joy/joy_node.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2.7
 # -*- coding:utf-8 -*-
 import rospy
 
-from sensor_msgs import Joy
-from geometry_msgs import Twist
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
 
 class JoyNode(object):
     """ This node handles pressing of buttons on the robot. It should be used to call services on other nodes,
@@ -12,14 +12,14 @@ class JoyNode(object):
 
     def __init__(self):
         log_level = rospy.DEBUG if rospy.get_param("/debug_active", False) else rospy.INFO
-        rospy.init_node("joy_node", log_level=log_level, anonymous=False)
+        rospy.init_node("joy_to_twist", log_level=log_level, anonymous=False)
 
         # --- Initialize Topics ---
         rospy.Subscriber("/joy", Joy, self.joy_cb)
         self.walk_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
 
         self.walk_msg = Twist()
-        """
+
         self.walk_msg.linear.x = 0.0;
         self.walk_msg.linear.y = 0.0;
         self.walk_msg.linear.z = 0.0;
@@ -27,7 +27,7 @@ class JoyNode(object):
         self.walk_msg.angular.x = 0.0;
         self.walk_msg.angular.y = 0.0;
         self.walk_msg.angular.z = 0.0;
-        """
+
 
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
@@ -35,21 +35,15 @@ class JoyNode(object):
             r.sleep()
 
     def joy_cb(self, msg):
-
-        if msg.axes[1] > 0.5:
-            self.walk_msg.linear.x = 1.0
-        elif msg.axes[1] < -0.5:
-            self.walk_msg.linear.x = 1.0
+        if msg.axes[1] > 0:
+            self.walk_msg.linear.x = 0.08 * msg.axes[1]
         else:
-            self.walk_msg = 0.0
+            self.walk_msg.linear.x = 0.03 * msg.axes[1]
 
-        if msg.axes[0] > 0.5:
-            self.walk_msg.angular.z = 1.0
-        elif msg.axes[0] < -0.5:
-            self.walk_msg.angular.z = -1.0
-        else:
-            self.walk_msg.angular.z = 0.0
+        self.walk_msg.linear.y = 0.03 * msg.axes[0]
 
+
+        self.walk_msg.angular.z = 0.1 * msg.axes[2]
 
 if __name__ == "__main__":
     joy = JoyNode()
