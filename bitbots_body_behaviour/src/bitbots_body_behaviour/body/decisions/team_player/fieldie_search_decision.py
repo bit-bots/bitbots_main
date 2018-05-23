484c8348fdbfe6c46fb7fc0bb7016f7b02738d86
Timon Engelke <timon.engelke@online.de>
2018-05-23 21:27:06 +0200

    Fix fixes in fieldie_search_decision

--- bitbots_body_behaviour/src/bitbots_body_behaviour/body/decisions/team_player/fieldie_search_decision.py
+++ bitbots_body_behaviour/src/bitbots_body_behaviour/body/decisions/team_player/fieldie_search_decision.py
@@ -15,7 +15,7 @@ History:
 """
 import rospy
 
-from bitbots_body_behaviour.body.actions.search import StopAndSearch
+from bitbots_body_behaviour.body.actions.search import Search
 from bitbots_body_behaviour.body.actions.go_to import GoToCenterpoint, GoToRelativePosition
 
 from bitbots_stackmachine.abstract_decision_module import AbstractDecisionModule
@@ -32,17 +32,16 @@ class FieldieSearchDecision(AbstractDecisionModule):
         self.at_centerpoint = False
 
     def perform(self, connector, reevaluate=False):
-        # if we have looked too long in the same direktion without seeing we want to walk
-        if rospy.get_time() - self.start_time > self.turn_wait_time:
-            # reset the timer and increment counter
-            if not connector.pathfinding.is_walking_active():
-                self.start_time = rospy.get_time()
-                self.counter += 1
-                return self.push(GoToRelativePosition, (0, 0, self.turn_angle))
-        else:
+        # if we have looked too long in the same direction without seeing we want to walk
+        if rospy.get_time() - connector.pathfinding.arrive_time > self.turn_wait_time:
             if self.counter >= self.turns_before_going_to_center_point and not self.at_centerpoint:
+                self.push(GoToCenterpoint)
                 self.at_centerpoint = True
-                return self.push(GoToCenterpoint)
             else:
-                # Just Search
-                return self.push(StopAndSearch)
+                # reset the timer and increment counter
+                if not connector.pathfinding.is_walking_active():
+                    self.counter += 1
+                self.push(GoToRelativePosition, (0, 0, self.turn_angle))
+        else:
+            # Just Search
+            self.push(Search)
--- bitbots_connector/src/bitbots_connector/capsules/pathfinding_capsule.py
+++ bitbots_connector/src/bitbots_connector/capsules/pathfinding_capsule.py
@@ -7,13 +7,16 @@ class PathfindingCapsule:
     def __init__(self):
         self.action_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self.active = False
+        self.arrive_time = 0
         self.counter = 0
+        self.timeout = 30
 
     def call_action(self, action_msg):
         if self.active or self.counter == 0:
             self.counter += 1
             return
         self.active = True
+        self.arrive_time = rospy.get_time() + self.timeout
         self.counter = 0
         print("New Goal!")
         goal = MoveBaseGoal()
@@ -21,9 +24,9 @@ class PathfindingCapsule:
         self.action_client.send_goal(goal, done_cb=self.done_cb)
 
     def done_cb(self, id, msg):
-        rospy.loginfo(msg)
-        rospy.loginfo(id)
+        rospy.loginfo("Arrived at goal!")
         self.active = False
+        self.arrive_time = rospy.get_time()
 
     def is_walking_active(self):
         return self.active
