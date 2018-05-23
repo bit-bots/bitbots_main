6f9db394339c88ee637d6b3acbd023609dbf2761
Timon Engelke <timon.engelke@online.de>
2018-05-23 16:03:15 +0200

    Fixes in go_to

--- bitbots_body_behaviour/src/bitbots_body_behaviour/body/actions/go_to.py
+++ bitbots_body_behaviour/src/bitbots_body_behaviour/body/actions/go_to.py
@@ -10,7 +10,7 @@ Goes to a position or an object
 
 import math
 import rospy
-import tf2_ros
+import tf2_ros as tf2
 from tf.transformations import quaternion_from_euler
 from geometry_msgs.msg import PoseStamped, Quaternion
 from bitbots_stackmachine.abstract_action_module import AbstractActionModule
@@ -25,8 +25,8 @@ class GoToRelativePosition(AbstractActionModule):
                      of rotation in degrees
         """
         super(GoToRelativePosition, self).__init__(connector)
-        self.tf_buffer = tf2_ros.Buffer(cache_time=rospy.Duration(5.0))
-        tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.tf_buffer = tf2.Buffer(cache_time=rospy.Duration(5.0))
+        tf_listener = tf2.TransformListener(self.tf_buffer)
         self.point = args
 
     def perform(self, connector, reevaluate=False):
@@ -41,9 +41,30 @@ class GoToRelativePosition(AbstractActionModule):
         rotation = quaternion_from_euler(0, 0, math.radians(self.point[2]))
         pose_msg.pose.orientation = Quaternion(*rotation)
 
-        absolute_pose = self.tf_buffer.transform(pose_msg, 'map')
+        try:
+            absolute_pose = self.tf_buffer.transform(pose_msg, 'map', timeout=rospy.Duration(0.3))
+        except (tf2.LookupException, tf2.ConnectivityException, tf2.ExtrapolationException):
+            rospy.logdebug("Waiting for transform...")
+            return
 
+        # To have the object we are going to in front of us, go to a point behind it
+        absolute_pose.pose.position.x -= 0.2
         connector.pathfinding_publisher.publish(absolute_pose)
+        return self.pop()
+
+
+class Stand(AbstractActionModule):
+    def perform(self, connector, reevaluate=False):
+        pose_msg = PoseStamped()
+        pose_msg.header.stamp = rospy.Time.now()
+        pose_msg.header.frame_id = 'base_link'
+
+        pose_msg.pose.position.x = 0
+        pose_msg.pose.position.y = 0
+        pose_msg.pose.position.z = 0
+
+        pose_msg.pose.orientation = Quaternion(0, 0, 0, 1)
+        connector.pathfinding_publisher.publish(pose_msg)
         return self.pop()
 
 
@@ -76,15 +97,41 @@ class GoToAbsolutePosition(AbstractActionModule):
 
 
 class GoToBall(GoToRelativePosition):
-    def __init__(self, connector, args=0):
+    def __init__(self, connector, args=0.0):
         """Go to the ball
 
         :param args: the angle of relative rotation in degrees
         """
-        point = (connector.personal_model.get_ball_relative()[0],
-                 connector.personal_model.get_ball_relative()[1],
-                 args)
+        ball_u, ball_v = connector.world_model.get_ball_position_uv()
+        point = (ball_u, ball_v, connector.world_model.get_opp_goal_angle_from_ball())
+        print("Going to ball", point)
         super(GoToBall, self).__init__(connector, point)
+
+    def perform(self, connector, reevaluate=False):
+        pose_msg = PoseStamped()
+        pose_msg.header.stamp = rospy.Time.now()
+        pose_msg.header.frame_id = 'base_link'
+
+        pose_msg.pose.position.x = self.point[0]
+        pose_msg.pose.position.y = self.point[1]
+        pose_msg.pose.position.z = 0
+
+        try:
+            absolute_pose = self.tf_buffer.transform(pose_msg, 'map', timeout=rospy.Duration(0.3))
+        except (tf2.LookupException, tf2.ConnectivityException, tf2.ExtrapolationException) as e:
+            rospy.loginfo("Waiting for transform...")
+            return
+
+        # To have the object we are going to in front of us, go to a point behind it
+        absolute_pose.pose.position.x -= 0.2
+
+        print(self.point)
+        rotation = quaternion_from_euler(0, 0, self.point[2])
+        absolute_pose.pose.orientation = Quaternion(*rotation)
+
+        connector.pathfinding_publisher.publish(absolute_pose)
+        return self.pop()
+
 
 
 class GoToOwnGoal(GoToAbsolutePosition):
