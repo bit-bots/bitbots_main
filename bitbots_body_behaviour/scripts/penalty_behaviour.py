675dcf16ed7b2328a1119bfd45227b8b4f6596d6
Timon Engelke <timon.engelke@online.de>
2018-06-20 09:53:26 -0400

    Fixes in PenaltyBehaviour

--- bitbots_body_behaviour/scripts/penalty_behaviour.py
+++ bitbots_body_behaviour/scripts/penalty_behaviour.py
@@ -9,7 +9,7 @@ class PenaltyBehaviour:
     def __init__(self):
         rospy.init_node('penalty_behaviour')
         self.connected = False
-        self.allowed_to_move = False
+        self.penalty_kick_allowed = False
         self.animation_running = False
         self.button_pressed = False
         self.pressed_time = 0
@@ -20,22 +20,22 @@ class PenaltyBehaviour:
         rospy.Subscriber('gamestate', GameState, self.gamestate_callback, queue_size=1)
 
         rospy.loginfo('Playing walkready animation')
-        self.play_animation('walkreadyduplicated') # TODO
+        self.play_animation('walkreadyduplicated')  # TODO
         rate = rospy.Rate(10)
 
         while not rospy.is_shutdown():
-            rospy.logdebug(self.connected, self.allowed_to_move, self.animation_running)
-            if (self.connected and self.allowed_to_move and not self.animation_running or
-                    not self.connected and self.button_pressed and rospy.get_time() - self.pressed_time > 10) and not self.animation_running:
+            if ((self.connected and self.penalty_kick_allowed and not self.animation_running or
+                    not self.connected and self.button_pressed and rospy.get_time() - self.pressed_time > 10)
+                    and not self.animation_running):
                 rospy.loginfo('Playing kick animation')
-                self.play_animation('penaltykickleft') # TODO
+                self.play_animation('penaltykickleft')  # TODO
             rate.sleep()
 
     def wifi_callback(self, msg: Bool):
         self.connected = msg.data
 
     def gamestate_callback(self, msg: GameState):
-        self.allowed_to_move = msg.allowedToMove
+        self.penalty_kick_allowed = msg.allowedToMove and msg.gameState == GameState.GAMESTATE_PLAYING
 
     def button_callback(self, msg: Bool):
         self.pressed_time = rospy.get_time()
@@ -49,7 +49,8 @@ class PenaltyBehaviour:
         self.animation_running = True
 
     def animation_done(self, arg1, arg2):
-       self.animation_running = False
+        self.animation_running = False
+
 
 if __name__ == '__main__':
     PenaltyBehaviour()
