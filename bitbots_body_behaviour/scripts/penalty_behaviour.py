5fa8e6e7644d7a0b30cd657f5e870ccae7b6814d
Timon Engelke <7engelke@informatik.uni-hamburg.de>
2018-06-20 18:41:16 +0200

    Fixes in penalty behaviour

--- bitbots_body_behaviour/scripts/penalty_behaviour.py
+++ bitbots_body_behaviour/scripts/penalty_behaviour.py
@@ -13,14 +13,17 @@ class PenaltyBehaviour:
         self.animation_running = False
         self.button_pressed = False
         self.pressed_time = 0
+        self.kick_animation = 'penalty_kick_left_kamikaze_higher6_actually_working_standing_end'
+        self.last_animation = None
         self.animation_client = actionlib.SimpleActionClient('animation', PlayAnimationAction)
 
         rospy.Subscriber('wifi_connected', Bool, self.wifi_callback, queue_size=1)
         rospy.Subscriber('shoot_button', Bool, self.button_callback, queue_size=1)
         rospy.Subscriber('gamestate', GameState, self.gamestate_callback, queue_size=1)
 
-        rospy.loginfo('Playing walkready animation')
-        self.play_animation('walkreadyduplicated')  # TODO
+        rospy.sleep(1)
+        self.play_animation('walkready')
+
         rate = rospy.Rate(10)
 
         while not rospy.is_shutdown():
@@ -28,7 +31,8 @@ class PenaltyBehaviour:
                     not self.connected and self.button_pressed and rospy.get_time() - self.pressed_time > 10)
                     and not self.animation_running):
                 rospy.loginfo('Playing kick animation')
-                self.play_animation('penaltykickleft')  # TODO
+                self.play_animation(self.kick_animation)
+                self.button_pressed = False
             rate.sleep()
 
     def wifi_callback(self, msg: Bool):
@@ -38,10 +42,12 @@ class PenaltyBehaviour:
         self.penalty_kick_allowed = msg.allowedToMove and msg.gameState == GameState.GAMESTATE_PLAYING
 
     def button_callback(self, msg: Bool):
+        rospy.loginfo('Button pressed, kick in 10 seconds')
         self.pressed_time = rospy.get_time()
         self.button_pressed = msg.data
 
     def play_animation(self, name):
+        self.last_animation = name
         goal = PlayAnimationGoal()
         goal.animation = name
         goal.hcm = False
@@ -50,6 +56,9 @@ class PenaltyBehaviour:
 
     def animation_done(self, arg1, arg2):
         self.animation_running = False
+        if self.last_animation == self.kick_animation:
+            rospy.sleep(10)
+            self.play_animation('walkready')
 
 
 if __name__ == '__main__':
