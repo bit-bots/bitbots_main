286e554ebd7cfe3d289cb77c7647fe5974bc112e
Niklas Fiedler <git@nfiedler.de>
2018-09-14 10:58:55 +0200

    added include dirs to python2 extension setup.py

--- python2_extensions/setup.py
+++ python2_extensions/setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup, Extension
+import numpy
 
 module1 = Extension('VisionExtensions',
                     sources=['vision_extensions.cpp'],
                     extra_compile_args=["-std=c++14"],
+                    include_dirs=[numpy.get_include()],
                     # extra_compile_args=["-O0", "-g"],
                     )
 
