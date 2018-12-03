d54466324680669e4c6d9692e9bb0ac59242ca8c
Timon Engelke <timon.engelke@online.de>
2018-12-03 12:16:14 +0100

    Clip pan and tilt position to range that does not kill the robot

--- bitbots_connector/src/bitbots_connector/capsules/head_capsule.py
+++ bitbots_connector/src/bitbots_connector/capsules/head_capsule.py
@@ -27,9 +27,19 @@ class HeadCapsule:
     # Head position #
     #################
 
-    def send_motor_goals(self, pan_position, tilt_position, pan_speed=3, tilt_speed=3):
+    def send_motor_goals(self, pan_position, tilt_position, pan_speed=1.5, tilt_speed=1.5):
+        """
+
+        :param pan_position: pan in radians
+        :param tilt_position: tilt in radians
+        :param pan_speed:
+        :param tilt_speed:
+        :return:
+        """
         # 3 is slower than maximum, maybe it is good
         print('{}/{}'.format(pan_position, tilt_position))
+        pan_position = min(max(pan_position, -1), 1)
+        tilt_position = min(max(tilt_position, -1.2), 0)
         self.pos_msg.positions = pan_position, tilt_position
         self.pos_msg.velocities = [pan_speed, tilt_speed]
         self.pos_msg.header.stamp = rospy.Time.now()
