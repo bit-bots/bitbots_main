3b251612119a70a337fa76e080f800ca6a4288b0
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2018-12-13 18:36:23 +0100

    updated package definitions

--- bitbots_live_tool_rqt/setup.py
+++ bitbots_live_tool_rqt/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rqt_live_tool'],
+    packages=['bitbots_live_tool_rqt'],
     package_dir={'': 'src'},
     #scripts=['scripts/motion_viz']
 )
--- bitbots_teleop/setup.py
+++ bitbots_teleop/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['bitbots_joy'],
+    packages=['bitbots_teleop'],
     #scripts=['bin/myscript'],
     package_dir={'': 'src'}
 )
