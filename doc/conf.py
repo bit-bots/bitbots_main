f1e874475f78cb2cd4b1b510eb4cfecd5a808699
Finn Sell <contact@finn-thorben.me>
2018-12-15 10:45:23 +0100

    Configure documentation to hide parent classes doc

--- doc/conf.py
+++ doc/conf.py
@@ -110,6 +110,9 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
+# Dont write documentation inherited from parent classes into the current class
+autodoc_inherit_docstrings = False
+
 
 # -- Options for HTML output ----------------------------------------------
 
@@ -293,10 +296,9 @@ texinfo_documents = [
 
 
 def skip(app, what, name, obj, skip, options):
-    #if name == "__init__":
-    #    return False
-    #return skip
-    return false
+    if name == "__init__":
+        return False
+    return skip
 
 def setup(app):
     app.connect("autodoc-skip-member", skip)
