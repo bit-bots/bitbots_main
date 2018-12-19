ac2cb899e2fc3faa8a11d8102629a290232cfe47
Finn Sell <contact@finn-thorben.me>
2018-12-19 12:31:36 +0100

    Change goals_to_joint_states to be its own node

--- bitbots_bringup/src/bitbots_bringup/goals_to_joint_states.py
+++ bitbots_bringup/src/bitbots_bringup/goals_to_joint_states.py
@@ -4,7 +4,8 @@ import rospy
 from bitbots_msgs.msg import JointCommand
 from sensor_msgs.msg import JointState
 
-def goal_cb(msg):    
+
+def goal_cb(msg):
     state_msg = JointState()
     state_msg.name = msg.joint_names
     state_msg.position = msg.positions
@@ -13,9 +14,10 @@ def goal_cb(msg):
 
     state_publisher.publish(state_msg)
 
+
 if __name__ == "__main__":
-    rospy.init_node('goals_to_states')    
-    state_publisher = rospy.Publisher("/joint_states", JointState, queue_size=1)    
-    sub = rospy.Subscriber("/DynamixelController/command", JointCommand, callback=goal_cb, queue_size=1, tcp_nodelay=True)
+    rospy.init_node('goals_to_joint_states')
+    state_publisher = rospy.Publisher("/joint_states", JointState, queue_size=1)
+    sub = rospy.Subscriber("/DynamixelController/command", JointCommand, callback=goal_cb, queue_size=1,
+                           tcp_nodelay=True)
     rospy.spin()
-    
