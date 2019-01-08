4f71277ace43972dbed2b39869e15c5db3e0e47b
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2019-01-08 15:57:46 +0100

    Bringup: added new simple physics simulation mode with fake walking

--- bitbots_bringup/scripts/motion_in_gazebo.py
+++ bitbots_bringup/scripts/motion_in_gazebo.py
@@ -9,22 +9,28 @@ from std_msgs.msg import Float64MultiArray
 from bitbots_msgs.msg import JointCommand
 
 
-def map_data(d_msg):
-    msg = Float64MultiArray()
-    positions = []
-    d_joint_order = d_msg.joint_names
-    for joint in joint_list:
-        positions.append(d_msg.positions[d_joint_order.index(joint)])
 
-    msg.data = positions
-    goal_publisher.publish(msg)
-
-
-if __name__ == "__main__":
+if __name__ == "__main__":    
     rospy.init_node('pub_goals_sim')
     joint_list = ['HeadPan', 'HeadTilt', 'LShoulderPitch', 'LShoulderRoll', 'LElbow', 'RShoulderPitch',
                   'RShoulderRoll', 'RElbow', 'LHipYaw', 'LHipRoll', 'LHipPitch', 'LKnee', 'LAnklePitch',
                   'LAnkleRoll', 'RHipYaw', 'RHipRoll', 'RHipPitch', 'RKnee', 'RAnklePitch', 'RAnkleRoll']
+
+    positions = [0] * 20
+    
+    def map_data(d_msg):
+        msg = Float64MultiArray()    
+        d_joint_order = d_msg.joint_names
+        i = 0
+        # only update newly transmitted positions and retransmit all previos positions
+        # Gazebo expects us to always send everything
+        for joint in joint_list:
+            if joint in d_joint_order:
+                positions[i] =d_msg.positions[d_joint_order.index(joint)]
+            i += 1
+
+        msg.data = positions
+        goal_publisher.publish(msg)
 
     goal_subscriber = rospy.Subscriber("DynamixelController/command", JointCommand, map_data)
     goal_publisher = rospy.Publisher('JointGroupController/command', Float64MultiArray,queue_size=10)
