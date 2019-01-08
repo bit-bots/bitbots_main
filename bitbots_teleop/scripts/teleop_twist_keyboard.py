1a8ca44089742020cd1ad0d3f762a2783719d3b0
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2019-01-08 15:56:36 +0100

    Teleop: added new functionalities to keyboard teleop

--- bitbots_teleop/scripts/teleop_twist_keyboard.py
+++ bitbots_teleop/scripts/teleop_twist_keyboard.py
@@ -6,6 +6,9 @@
 import rospy
 
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import JointState
+from bitbots_msgs.msg import JointCommand
+
 
 import sys, select, termios, tty
 
@@ -44,6 +47,24 @@ moveBindings = {
 		'Q':(0,0,10),
 		'E':(0,0,-10),
 	       }
+headBindings = {
+	'u':(1, 1),	
+	'i':(1, 0),
+	'o':(1, -1),
+	'j':(0, 1),
+	'l':(0, -1),
+	'm':(-1, 1),
+	',':(-1, 0),
+	'.':(-1, -1),		
+	'U':(10, 10),
+	'I':(10,0),
+	'O':(10, -10),	
+	'J':(0, 10),
+	'L':(0, -10),
+	'M':(-10, 10),
+	';':(-10, 0),
+	':':(-10, -10)
+}
 
 def getKey():
 	tty.setraw(sys.stdin.fileno())
@@ -52,12 +73,23 @@ def getKey():
 	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
 	return key
 
+head_pan_pos = 0
+head_tilt_pos = 0
+def joint_state_cb(msg):
+	global head_pan_pos
+	global head_tilt_pos
+
+	i = 0	
+	head_pan_pos = msg.position[msg.name.index("HeadPan")]
+	head_tilt_pos = msg.position[msg.name.index("HeadTilt")]
 
 if __name__=="__main__":
 	settings = termios.tcgetattr(sys.stdin)
+
+	rospy.init_node('teleop_twist_keyboard')	
 	
-	pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1, latch=True)
-	rospy.init_node('teleop_twist_keyboard')
+	# Walking Part
+	pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1, latch=True)	
 
 	x_speed_step = 0.01
 	y_speed_step = 0.01
@@ -67,6 +99,22 @@ if __name__=="__main__":
 	y = 0
 	th = 0
 	status = 0
+
+	# Head Part			
+	rospy.Subscriber("joint_states", JointState, joint_state_cb, queue_size=1)
+	if rospy.get_param("sim_active", default=False):
+		head_pub = rospy.Publisher("head_motor_goals", JointCommand, queue_size=1)
+	else:
+		head_pub = rospy.Publisher("DynamixelController/command", JointCommand, queue_size=1)
+	head_msg = JointCommand()
+	head_msg.max_currents = [-1] * 2
+	head_msg.velocities = [5] * 2
+	head_msg.accelerations = [40] * 2
+	head_msg.joint_names = ['HeadPan', 'HeadTilt']
+	head_msg.positions = [0] * 2
+
+	head_pan_step = 0.05
+	head_tilt_step = 0.05
 
 	try:
 		print(msg)
@@ -80,6 +128,15 @@ if __name__=="__main__":
 				y = round(y, 2)
 				th += moveBindings[key][2] * turn_speed_step			
 				th = round(th, 2)
+			elif key in headBindings.keys():
+				head_msg.positions[0] = head_pan_pos + headBindings[key][1] * head_pan_step
+				head_msg.positions[1] = head_tilt_pos + headBindings[key][0] * head_tilt_step
+				head_pub.publish(head_msg)
+			elif key == 'k' or key == 'K':
+				# put head back in init
+				head_msg.positions[0] = 0
+				head_msg.positions[1] = 0
+				head_pub.publish(head_msg)
 			else:
 				x = 0
 				y = 0
