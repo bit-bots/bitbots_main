1499e5c9b896b2e0c0099c54a5dc58417829264f
Florian <florian@flova.de>
2019-01-17 00:09:11 +0100

    Fixes dummy_ballfinder in paralel vision.

--- bitbots_vision/src/bitbots_vision/vision_modules/dummy_ballfinder.py
+++ bitbots_vision/src/bitbots_vision/vision_modules/dummy_ballfinder.py
@@ -13,5 +13,8 @@ class DummyClassifier(CandidateFinder):
     def get_candidates(self):
         return self._classified_candidates
 
+    def compute_top_candidate(self):
+        pass
+
     def get_top_candidates(self, count=1):
         return self._sorted_candidates
