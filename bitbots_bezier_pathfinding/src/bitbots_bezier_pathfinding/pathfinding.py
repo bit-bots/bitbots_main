2a4cacf253f4b65e0961f6579ec354af1cf2f072
Timon Engelke <timon.engelke@online.de>
2019-03-02 17:23:18 +0000

    Bezier Pathfinding: Add the possibility to publish goals in map frame

--- bitbots_bezier_pathfinding/src/bitbots_bezier_pathfinding/pathfinding.py
+++ bitbots_bezier_pathfinding/src/bitbots_bezier_pathfinding/pathfinding.py
@@ -2,11 +2,12 @@
 import math
 import numpy
 import rospy
-import tf_conversions
+import tf2_ros
 
 from dynamic_reconfigure.server import Server
 from geometry_msgs.msg import Pose2D, Pose, PoseStamped, Twist
 from nav_msgs.msg import Path
+from tf2_geometry_msgs import PoseStamped as TFPoseStamped
 
 from bitbots_bezier_pathfinding.cfg import PathfindingConfig
 from bitbots_bezier_pathfinding.bezier import Bezier
@@ -15,6 +16,8 @@ class BezierPathfinding:
     def __init__(self):
         rospy.init_node('bezier_pathfinding')
         Server(PathfindingConfig, self.dynamic_reconfigure_callback)
+        self.tf_buffer = tf2_ros.Buffer(cache_time=rospy.Duration(5))
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
         rospy.spin()
 
     def dynamic_reconfigure_callback(self, config, level):
@@ -32,16 +35,43 @@ class BezierPathfinding:
         return config
 
     def goal_callback(self, goal_msg):
+        # goal_msg must be either in base_footprint or map frame
         goal_pose = Pose2D()
         goal_pose.x = goal_msg.pose.position.x
         goal_pose.y = goal_msg.pose.position.y
-        angle_quaternion = (goal_msg.pose.orientation.x,
-                            goal_msg.pose.orientation.y,
-                            goal_msg.pose.orientation.z,
-                            goal_msg.pose.orientation.w)
-        angle_euler = tf_conversions.transformations.euler_from_quaternion(angle_quaternion)
-        goal_pose.theta = angle_euler[2]  # Yaw of the euler angle
-        curve = Bezier.from_pose(Pose2D(0, 0, 0), goal_pose, self.straightness)
+        # Quaternion -> Euler (a lot simpler because x and y are guaranteed to be 0)
+        yaw = math.atan2(2 * goal_msg.pose.orientation.z * goal_msg.pose.orientation.w,
+                         1 - 2 * goal_msg.pose.orientation.z * goal_msg.pose.orientation.z)
+        goal_pose.theta = yaw
+        if goal_msg.header.frame_id == 'base_footprint':
+            self.frame_id = 'base_footprint'
+            own_pose = Pose2D(0, 0, 0)
+        elif goal_msg.header.frame_id == 'map':
+            self.frame_id = 'map'
+            # Get own position from tf to map frame
+            own_pose_relative = TFPoseStamped()
+            own_pose_relative.header.frame_id = 'base_footprint'
+            own_pose_relative.header.stamp = rospy.Time.now()
+            own_pose_relative.pose.orientation.w = 1
+            try:
+                own_pose_3d = self.tf_buffer.transform(own_pose_relative, 'map', timeout=rospy.Duration(0.3))
+            except tf2_ros.LookupException:
+                rospy.logwarn('A goal was published in map frame but no map frame exists')
+                return
+            except tf2_ros.ExtrapolationException:
+                rospy.logwarn('A goal was published in map frame but the map frame is no longer published')
+                return
+            own_pose = Pose2D()
+            own_pose.x = own_pose_3d.pose.position.x
+            own_pose.y = own_pose_3d.pose.position.y
+            # Quaternion -> Euler again
+            yaw = - math.atan2(2 * own_pose_3d.pose.orientation.z * own_pose_3d.pose.orientation.w,
+                               1 - 2 * own_pose_3d.pose.orientation.z * own_pose_3d.pose.orientation.z)
+            own_pose.theta = yaw
+        else:
+            raise AssertionError('goal_msg must be in base_footprint of map frame')
+
+        curve = Bezier.from_pose(own_pose, goal_pose, self.straightness)
         self.publish_curve_as_path(curve)
         self.publish_cmd_vel(curve)
 
@@ -49,12 +79,12 @@ class BezierPathfinding:
         ts = numpy.linspace(0, 1, 50)
         path_msg = Path()
         path_msg.header.stamp = rospy.Time.now()
-        path_msg.header.frame_id = 'base_link'
+        path_msg.header.frame_id = self.frame_id
         path_poses = []
         for t in ts:
             pose = PoseStamped()
             pose.header.stamp = rospy.Time.now()
-            pose.header.frame_id = 'base_link'
+            pose.header.frame_id = self.frame_id
             pose.pose.position.x, pose.pose.position.y = bezier_curve.get_xy(t)
             pose.pose.orientation.w = 1
             path_poses.append(pose)
@@ -65,7 +95,7 @@ class BezierPathfinding:
         # Publish the pose where the next cmd_vel is going
         pose = PoseStamped()
         pose.header.stamp = rospy.Time.now()
-        pose.header.frame_id = 'base_link'
+        pose.header.frame_id = self.frame_id
         pose.pose.position.x = x
         pose.pose.position.y = y
         # Euler -> Quaternion (a lot simpler without roll and pitch)
