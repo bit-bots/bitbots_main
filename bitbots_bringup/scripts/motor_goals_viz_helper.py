30ed7d3e1980965c3b064369e644740e492da9c9
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2019-03-04 08:23:38 +0100

    Bringup: improved helper script

--- bitbots_bringup/scripts/motor_goals_viz_helper.py
+++ bitbots_bringup/scripts/motor_goals_viz_helper.py
@@ -10,16 +10,16 @@ from humanoid_league_msgs.msg import Animation
 class MotorVizHelper:
     def __init__(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument("--w", help="Directly get walking motor goals", action="store_true")
-        parser.add_argument("--a", help="Directly get animation motor goals", action="store_true")
+        parser.add_argument("--walking", "-w", help="Directly get walking motor goals", action="store_true")
+        parser.add_argument("--animation", "-a", help="Directly get animation motor goals", action="store_true")
         args = parser.parse_args()
 
         rospy.init_node("motor_viz_helper", anonymous=False)
         self.joint_state_msg = JointState()
         self.joint_publisher = rospy.Publisher('joint_states', JointState, queue_size=1)
-        if args.w:
+        if args.walking:
             rospy.Subscriber("walking_motor_goals", JointCommand, self.joint_command_cb, queue_size=1)
-        elif args.a:
+        elif args.animation:
             rospy.Subscriber("animation_motor_goals", JointCommand, self.animation_cb, queue_size=1)
         else:
             rospy.Subscriber("/DynamixelController/command", JointCommand, self.joint_command_cb, queue_size=1)
