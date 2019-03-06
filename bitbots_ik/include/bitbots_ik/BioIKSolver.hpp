6a0b3b15b316eb9edf026af441afad73747d3424
Timon Engelke <timon.engelke@online.de>
2019-03-06 21:58:11 +0100

    Revert "BitbotsIK: tf->tf2"
    This reverts commit b30cb9041233140d6be5b2bea7c216dcae524733.

--- bitbots_ik/include/bitbots_ik/BioIKSolver.hpp
+++ bitbots_ik/include/bitbots_ik/BioIKSolver.hpp
@@ -5,8 +5,6 @@
 #include <tf/transform_datatypes.h>
 #include <tf/transform_broadcaster.h>
 
-#include <tf2/LinearMath/Vector3.h>
-
 #include "bio_ik/bio_ik.h"
 #include <moveit/kinematics_base/kinematics_base.h>
 #include <moveit/robot_state/conversions.h>
@@ -23,7 +21,7 @@ class BioIKSolver{
     public:
         BioIKSolver();
         BioIKSolver(const robot_state::JointModelGroup &all_joints_group, const robot_state::JointModelGroup &lleg_joints_group, const robot_state::JointModelGroup &rleg_joints_group);
-        bool solve(tf2::Transform& trunk_to_support_foot, tf2::Transform& trunk_to_flying_foot, bool is_left_support, robot_state::RobotStatePtr &robot_state_ptr);
+        bool solve(tf::Transform& trunk_to_support_foot, tf::Transform& trunk_to_flying_foot, bool is_left_support, robot_state::RobotStatePtr &robot_state_ptr);
         void set_use_approximate(bool use_approximate);
         void set_bioIK_timeout(double timeout);
     private:
