f66972562647fd0a070ba44ab7d9a5b4c2c39f8e
Niklas Fiedler <git@nfiedler.de>
2019-03-13 19:40:35 +0100

    initial implementation of BallDangerous

--- bitbots_body_behavior/src/bitbots_body_behavior/decisions/ball_dangerous.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/ball_dangerous.py
@@ -2,4 +2,32 @@ from dynamic_stack_decider.abstract_decision_element import AbstractDecisionElem
 
 
 class BallDangerous(AbstractDecisionElement):
-    pass
+    def __init__(self, blackboard, dsd, parameters=None):
+        super(BallDangerous, self).__init__(blackboard, dsd, parameters)
+        self.ball_dangerous_goal_radius = self.blackboard.config['ball_dangerous_goal_radius']
+
+    def perform(self, reevaluate=False):
+        if self._in_dangerous_area(self.blackboard.world_model.get_ball_position_xy()):
+            return 'YES'
+        return 'NO'
+
+    def _in_dangerous_area(self, position):
+        """"
+        returns whether the position is in the dangerous area (close to the goal)
+        """
+
+        # close enough on the x axis
+        if position[0] > -(self.blackboard.field_length / 2) + self.ball_dangerous_goal_radius:
+            return False
+
+        if (-self.blackboard.goal_width / 2) - self.ball_dangerous_goal_radius <= \
+                position[1] <= \
+                (self.blackboard.goal_width / 2) + self.ball_dangerous_goal_radius:
+            return True
+        return False
+
+
+
+    def get_reevaluate(self):
+        return True
+
