ce7698b8b6005daf0066b189f1ea54871fd03c6b
Niklas Fiedler <git@nfiedler.de>
2019-03-13 18:24:36 +0100

    initial implementation of GoToDefensePosition

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to_defense_position.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to_defense_position.py
@@ -1,5 +1,39 @@
 from dynamic_stack_decider.abstract_action_element import AbstractActionElement
+from tf2_geometry_msgs import PoseStamped
+import rospy
 
 
 class GoToDefensePosition(AbstractActionElement):
-    pass
+    def __init__(self, blackboard, dsd, parameters=None):
+        super(GoToDefensePosition, self).__init__(blackboard, dsd, parameters)
+
+    def perform(self, reevaluate=False):
+        # the defense position should be a position between the ball and the own goal.
+        # TODO: in the case of two defensive players, they should use an offset,
+        #  so that they are not targeting the same position
+
+        #      y
+        #      ^       ______________________
+        #      |    M  |          |          |  O
+        #      |    Y  |_ -x, y   |   x, y  _|  P
+        #      |    G  | |        |        | |  P
+        # 0    +    O  | |       ( )       | |  G
+        #      |    A  |_|        |        |_|  O
+        #      |    L  |  -x,-y   |   x,-y   |  A
+        #      |       |__________|__________|  L
+        #      |
+        #      +------------------+--------------> x
+        #                         0
+
+        goal_position = (0, -self.blackboard.field_length / 2)  # position of the own goal
+        ball_position = self.blackboard.world_model.get_ball_position_xy()
+
+        pose_msg = PoseStamped()
+        pose_msg.header.stamp = rospy.Time.now()
+        pose_msg.header.frame_id = 'map'
+
+        pose_msg.pose.position.x = abs(goal_position[0]) + ball_position[0]
+        pose_msg.pose.position.y = ball_position[1] / 2
+        pose_msg.pose.orientation.w = 1
+
+        self.blackboard.pathfinding.publish(pose_msg)
