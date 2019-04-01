866fa9d905e1acdc4c6dce798e8cf1cfd5efaf29
Timon Engelke <timon.engelke@online.de>
2019-04-01 13:23:42 +0200

    Fixed exit code in pull all script

--- scripts/pull_all.sh
+++ scripts/pull_all.sh
@@ -5,4 +5,4 @@
     git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; [ "$branch" = "" ] && git checkout master || git checkout $branch' &&
     git submodule sync &&
     git submodule foreach git pull
-} || echo -e "\033[91m\033[1m########## Pull failed! ##########\033[0m" && exit 1
+} || { echo -e "\033[91m\033[1m########## Pull failed! ##########\033[0m" && exit 1; }
