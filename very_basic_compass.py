eeef09855b87334cb60dce9e8c31c7fb57973f13
Florian <florian@flova.de>
2019-04-04 18:32:26 +0200

    Adds visualisation

--- very_basic_compass.py
+++ very_basic_compass.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-import numpy
+import numpy as np
 import cv2
 import silx
 import time
@@ -15,15 +15,22 @@ class DemoSift():
         self.sift_ocl = sift.SiftPlan(template=initframe, devicetype="CPU")
         self.mp = sift.MatchPlan()
         # print("Device used for calculation: ", self.sift_ocl.ctx.devices[0].name)
+        self.debug_image = None
+        self.state = -1
 
     def compare(self, image):
         kp = self.sift_ocl(image)
 
-        matches = (self.mp(kp, self.side_key_points[0]).shape[0], 
-                   self.mp(kp, self.side_key_points[1]).shape[0])
+        matches = (self.mp(kp, self.side_key_points[0]), 
+                   self.mp(kp, self.side_key_points[1]))
+
+        # Debug
+        image_with_matches = self.plot(image, self.convert_match(matches[0][:,0]), (255,0,0),6)
+        image_with_matches = self.plot(image_with_matches, self.convert_match(matches[1][:,0]), (0,255,0),4)
+        self.debug_image = self.plot(image_with_matches, kp, (0,0,255),2)
 
         # (Anzahl Keypoints Image1, Anzahl Keypoints Image2, Matched Keypoints)
-        return matches
+        return matches[0].shape[0], matches[1].shape[0]
 
 
     def workloop(self):
@@ -31,11 +38,11 @@ class DemoSift():
         while True:
             ret, frame = self.cap.read()
 
-            cv2.imshow("Video", frame)
-
             k = cv2.waitKey(1)
 
             if side < 2:
+                self.debug_image = frame
+
                 # Wurde SPACE gedrueckt
                 if k%256 == 32:
                     self.initside(side, frame)
@@ -47,11 +54,15 @@ class DemoSift():
 
                 if common_keys[0] > common_keys[1]:
                     visible_side = 0
-                    print("SEITE 1")
                 else:
                     visible_side = 1
-                    print("SEITE 2")
+
+                if self.state != visible_side:
+                    self.state = visible_side
+                    print("SEITE {}".format(self.state))
                 
+            cv2.imshow("Video", self.debug_image)
+
             # Abbrechen mit ESC
             if k%256 == 27 or 0xFF == ord('q'):
                 break
@@ -62,8 +73,14 @@ class DemoSift():
         print("Taken!!")
         self.side_key_points[side] = self.sift_ocl(frame)
 
-
-
+    def convert_match(self, kps):
+        d = np.dtype((np.record, [('x', '<f4'), ('y', '<f4'), ('scale', '<f4'), ('angle', '<f4'), ('desc', 'u1', (128,))]))
+        return kps.astype(d)
+    
+    def plot(self, image, kp, color, size):
+        for i in range(kp.shape[0]):
+            cv2.circle(image, (kp[i].x, kp[i].y), size, color, thickness=-1)
+        return image
 
 if __name__ == "__main__":
     demo = DemoSift()
