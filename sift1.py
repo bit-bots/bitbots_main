bf58a6485f38574944fab3867f3f335403fd2d76
Florian <florian@flova.de>
2019-04-04 15:20:03 +0200

    Refactoring

--- sift1.py
+++ sift1.py
@@ -2,32 +2,45 @@
 import numpy
 import cv2
 import silx
+import time
 from silx.image import sift
 
-def show(image):
-    cv2.imshow('image',image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+class DemoSift():
+    def __init__(self):
+        self.demoimage = [cv2.imread('frame1.png'),
+                          cv2.imread('frame2.png'),
+                          cv2.imread('frame3.png'),
+                          cv2.imread('frame4.png'),]
 
-def compare(image1, image2):
-    sift_ocl = sift.SiftPlan(template=image1, devicetype="CPU")
-    #print("Device used for calculation: ", sift_ocl.ctx.devices[0].name)
+        self.sift_ocl = sift.SiftPlan(template=self.demoimage[0], devicetype="CPU")
+        #print("Device used for calculation: ", self.sift_ocl.ctx.devices[0].name)
 
-    kp = sift_ocl(image1)
-    kp1 = sift_ocl(image2)
+    def compare(self, image1, image2):
+        kp = self.sift_ocl(image1)
+        kp1 = self.sift_ocl(image2)
 
-    mp = sift.MatchPlan()
-    match = mp(kp, kp1)
+        mp = sift.MatchPlan()
+        match = mp(kp, kp1)
 
-    # (Anzahl Keypoints Image1, Anzahl Keypoints Image2, Matched Keypoints)
-    return (len(kp), len(kp1), match.shape[0])
+        # (Anzahl Keypoints Image1, Anzahl Keypoints Image2, Matched Keypoints)
+        return (len(kp), len(kp1), match.shape[0])
 
-imgs = [cv2.imread('frame1.png'),
-        cv2.imread('frame2.png'),
-        cv2.imread('frame3.png'),
-        cv2.imread('frame4.png'),]
+    def test(self):
+        print("Frame 1 + 2: {}".format(self.compare(self.demoimage[0], self.demoimage[1])))
+        print("Frame 1 + 4: {}".format(self.compare(self.demoimage[0], self.demoimage[3])))
+        print("Frame 3 + 4: {}".format(self.compare(self.demoimage[2], self.demoimage[3])))
 
-print("Frame 1 + 2: {}".format(compare(imgs[0], imgs[1])))
-print("Frame 1 + 4: {}".format(compare(imgs[0], imgs[3])))
-print("Frame 3 + 4: {}".format(compare(imgs[2], imgs[3])))
 
+    def show(self, image):
+        cv2.imshow('image',image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def time(self, f):
+        start = time.time()
+        f()
+        return (time.time() - start)
+
+if __name__ == "__main__":
+    demo = DemoSift()
+    print(demo.time(demo.test))
