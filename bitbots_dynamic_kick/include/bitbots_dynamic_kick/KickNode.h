47980ebb55652bda75b83553b2e8240b47fdfcd4
Finn-Thorben Sell <dev@finn-thorben.me>
2019-04-10 19:09:45 +0000

    node: add documentation (docstrings and comments)

--- bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickNode.h
+++ bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickNode.h
@@ -11,11 +11,24 @@
 
 typedef actionlib::SimpleActionServer<bitbots_msgs::KickAction> ActionServer;
 
+/**
+ * KickNode is that part of bitbots_dynamic_kick which takes care of interacting with ROS and utilizes KickEngine
+ * to calculate actual kick behavior.
+ *
+ * It provides an ActionServer for the bitbots_msgs::KickAction
+ */
 class KickNode {
 public:
     KickNode();
+
+    /** Callback for dynamic reconfigure */
     void reconfigure_callback(bitbots_dynamic_kick::DynamicKickConfig &config, uint32_t level);
-    void execute(const bitbots_msgs::KickGoalConstPtr& goal);
+
+    /**
+     * Callback that gets executed whenever m_server receives a new goal.
+     * @param goal New goal to process
+     */
+    void execute_cb(const bitbots_msgs::KickGoalConstPtr& goal);
 private:
     ros::NodeHandle m_node_handle;
     ActionServer m_server;
@@ -24,8 +37,7 @@ private:
 
     /**
      * Do main loop in which engine.tick() get called repeatedly.
-     *
-     * The ActionServer is taken into account meaning that a cancelled goal should no longer get processed.
+     * The ActionServers state is taken into account meaning that a cancelled goal no longer get processed.
      */
     void loop_engine();
 };
--- bitbots_dynamic_kick/src/KickNode.cpp
+++ bitbots_dynamic_kick/src/KickNode.cpp
@@ -1,33 +1,33 @@
 #include "bitbots_dynamic_kick/KickNode.h"
 
 KickNode::KickNode() :
-        m_server(m_node_handle, "dynamic_kick", boost::bind(&KickNode::execute, this, _1), false) {
+        m_server(m_node_handle, "dynamic_kick", boost::bind(&KickNode::execute_cb, this, _1), false) {
     m_server.start();
 }
 
-void KickNode::reconfigure_callback(bitbots_dynamic_kick::DynamicKickConfig& config, uint32_t level) {
+void KickNode::reconfigure_callback(bitbots_dynamic_kick::DynamicKickConfig &config, uint32_t level) {
     m_engine_rate = config.engine_rate;
 }
 
-void KickNode::execute(const bitbots_msgs::KickGoalConstPtr& goal) {
-    std::cout << "Received new goal" << std::endl;
-
+void KickNode::execute_cb(const bitbots_msgs::KickGoalConstPtr &goal) {
+    /* Setup engine for new goal and start calculating */
     m_engine.reset();
     m_engine.set_goal(goal);
-
     loop_engine();
 
+    /* Figure out the reason why loop_engine() returned and act accordingly */
     if (m_server.isPreemptRequested()) {
+        /* Confirm that we canceled processing of the current goal */
         m_server.setPreempted();
     } else {
+        /* Publish results */
         m_server.setSucceeded();
     }
 }
 
 void KickNode::loop_engine() {
+    /* Do the loop as long as nothing cancels it */
     while (m_server.isActive() && !m_server.isPreemptRequested()) {
-        ros::Rate loop_rate(m_engine_rate);
-
         bitbots_msgs::KickFeedback feedback = m_engine.tick();
         m_server.publishFeedback(feedback);
 
@@ -35,17 +35,23 @@ void KickNode::loop_engine() {
             break;
         }
 
+        /* Let ROS do some important work of its own sleep afterwards */
         ros::spinOnce();
+        ros::Rate loop_rate(m_engine_rate);
         loop_rate.sleep();
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
+    /* Setup ROS node */
     ros::init(argc, argv, "dynamic_kick");
     KickNode node;
+
+    /* Setup dynamic_reconfigure */
     dynamic_reconfigure::Server<bitbots_dynamic_kick::DynamicKickConfig> dyn_reconf_server;
     dynamic_reconfigure::Server<bitbots_dynamic_kick::DynamicKickConfig>::CallbackType f;
     f = boost::bind(&KickNode::reconfigure_callback, &node, _1, _2);
     dyn_reconf_server.setCallback(f);
+
     ros::spin();
 }
