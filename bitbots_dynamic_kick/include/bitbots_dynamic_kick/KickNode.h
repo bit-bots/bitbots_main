6c4c38e1a55859d569c333718e0acef741c8972b
Timon Engelke <timon.engelke@online.de>
2019-04-10 14:33:40 +0200

    node: first draft

--- bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickNode.h
+++ bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickNode.h
@@ -1,6 +1,15 @@
 #ifndef BITBOTS_DYNAMIC_KICK_KICK_NODE_H
 #define BITBOTS_DYNAMIC_KICK_KICK_NODE_H
 
-// Code here
+#include <ros/ros.h>
+#include <dynamic_reconfigure/server.h>
+#include <actionlib/server/simple_action_server.h>
+#include <bitbots_dynamic_kick/DynamicKickConfig.h>
+
+class KickNode {
+public:
+    KickNode();
+    void reconfigure_callback(bitbots_dynamic_kick::DynamicKickConfig &config, uint32_t level);
+};
 
 #endif  // BITBOTS_DYNAMIC_KICK_KICK_NODE_H
--- bitbots_dynamic_kick/src/KickNode.cpp
+++ bitbots_dynamic_kick/src/KickNode.cpp
@@ -1,4 +1,18 @@
 #include "bitbots_dynamic_kick/KickNode.h"
 
+KickNode::KickNode() {
+
+}
+
+void KickNode::reconfigure_callback(bitbots_dynamic_kick::DynamicKickConfig &config, uint32_t level) {
+
+}
+
 int main(int argc, char* argv[]) {
+    ros::init(argc, argv, "dynamic_kick");
+    KickNode node;
+    dynamic_reconfigure::Server<bitbots_dynamic_kick::DynamicKickConfig> server;
+    dynamic_reconfigure::Server<bitbots_dynamic_kick::DynamicKickConfig>::CallbackType f;
+    f = boost::bind(&KickNode::reconfigure_callback, &node, _1, _2);
+    server.setCallback(f);
 }
