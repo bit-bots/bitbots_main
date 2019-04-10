b775a65487f5238864668c59b16c4077d83f1d33
Timon Engelke <timon.engelke@online.de>
2019-04-10 17:30:48 +0200

    engine: create empty engine definition

--- bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickEngine.h
+++ bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickEngine.h
@@ -1,6 +1,20 @@
 #ifndef BITBOTS_DYNAMIC_KICK_KICK_ENGINE_H
 #define BITBOTS_DYNAMIC_KICK_KICK_ENGINE_H
 
-// Code here
+#include <bitbots_splines/TrajectoryUtils.h>
+#include <bitbots_msgs/KickGoal.h>
+#include <bitbots_msgs/KickFeedback.h>
+
+class KickEngine {
+public:
+    KickEngine();
+    void set_goal(const bitbots_msgs::KickGoalConstPtr& goal);
+    void reset();
+    bitbots_msgs::KickFeedback tick();
+private:
+    bitbots_msgs::KickGoal m_current_goal;
+    bitbots_splines::Trajectories m_trajectories;
+    void calc_splines();
+};
 
 #endif  // BITBOTS_DYNAMIC_KICK_KICK_ENGINE_H
--- bitbots_dynamic_kick/src/KickEngine.cpp
+++ bitbots_dynamic_kick/src/KickEngine.cpp
@@ -1,1 +1,13 @@
 #include "bitbots_dynamic_kick/KickEngine.h"
+
+KickEngine::KickEngine() {}
+
+void KickEngine::reset() {}
+
+void KickEngine::set_goal(const bitbots_msgs::KickGoalConstPtr &goal) {}
+
+bitbots_msgs::KickFeedback KickEngine::tick() {
+    return bitbots_msgs::KickFeedback();
+}
+
+void KickEngine::calc_splines() {}
