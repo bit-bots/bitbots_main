2f6c81d2e3e2f560fd7e556f63c3d0a9b53c98ed
Timon Engelke <timon.engelke@online.de>
2019-04-10 13:57:04 +0000

    node: implement SimpleActionServer

--- bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickNode.h
+++ bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickNode.h
@@ -5,11 +5,19 @@
 #include <dynamic_reconfigure/server.h>
 #include <actionlib/server/simple_action_server.h>
 #include <bitbots_dynamic_kick/DynamicKickConfig.h>
+#include <bitbots_msgs/KickAction.h>
+
+typedef actionlib::SimpleActionServer<bitbots_msgs::KickAction> ActionServer;
 
 class KickNode {
 public:
     KickNode();
     void reconfigure_callback(bitbots_dynamic_kick::DynamicKickConfig &config, uint32_t level);
+    void execute(const bitbots_msgs::KickGoalConstPtr& goal);
+    void run();
+private:
+    ros::NodeHandle nh;
+    ActionServer server;
 };
 
 #endif  // BITBOTS_DYNAMIC_KICK_KICK_NODE_H
--- bitbots_dynamic_kick/src/KickNode.cpp
+++ bitbots_dynamic_kick/src/KickNode.cpp
@@ -1,11 +1,19 @@
 #include "bitbots_dynamic_kick/KickNode.h"
+#include <iostream>
 
-KickNode::KickNode() {
+KickNode::KickNode() : server(nh, "dynamic_kick", boost::bind(&KickNode::execute, this, _1), false) {
+    server.start();
+}
 
+void KickNode::run() {
 }
 
 void KickNode::reconfigure_callback(bitbots_dynamic_kick::DynamicKickConfig &config, uint32_t level) {
+}
 
+void KickNode::execute(const bitbots_msgs::KickGoalConstPtr& goal) {
+    std::cout << "Received action! Speed: " << goal->foot_speed << std::endl;
+    server.setSucceeded();
 }
 
 int main(int argc, char* argv[]) {
@@ -15,4 +23,6 @@ int main(int argc, char* argv[]) {
     dynamic_reconfigure::Server<bitbots_dynamic_kick::DynamicKickConfig>::CallbackType f;
     f = boost::bind(&KickNode::reconfigure_callback, &node, _1, _2);
     server.setCallback(f);
+    node.run();
+    ros::spin();
 }
