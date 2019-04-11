5646bbc1ee2937ca8b27e3c73133f8c654ff375a
Finn-Thorben Sell <dev@finn-thorben.me>
2019-04-11 14:31:24 +0000

    dummy_client: improve client so that it is usable

--- bitbots_dynamic_kick/scripts/dummy_client.py
+++ bitbots_dynamic_kick/scripts/dummy_client.py
@@ -1,37 +1,52 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import sys
 
+from time import sleep
+import random
 import rospy
 import actionlib
 
 from bitbots_msgs.msg import KickGoal, KickAction, KickFeedback
 
-rospy.init_node('dynamic_kick_dummy_client')
+showing_feedback = False
 
-client = actionlib.SimpleActionClient('dynamic_kick', KickAction)
+if __name__ == "__main__":
+    print("[..] Initializing node", end='')
+    rospy.init_node('dynamic_kick_dummy_client', anonymous=True)
+    print("\r[OK] Initializing node")
 
-print('Waiting for server...')
-if not client.wait_for_server():
-    exit(1)
-print('done')
 
-goal = KickGoal()
-goal.foot_speed = 0.5
+    def done_cb(state, result):
+        print("Action completed with code " + str(state))
+        print(str(result))
 
-def done_cb(state, result):
-    print('Done!')
-    print(state)
-    print(result)
 
-def active_cb():
-    print('Active!')
+    def active_cb():
+        print("Server accepted action")
 
-def feedback_cb(feedback):
-    print('Feedback')
-    print(feedback)
+
+    def feedback_cb(feedback):
+        if len(sys.argv) > 1 and sys.argv[1] == '--feedback':
+            print('Feedback')
+            print(feedback)
+            print()
+
+
+    print('[..] Connecting to action server \'dynamic_kick\'', end='')
+    sys.stdout.flush()
+    client = actionlib.SimpleActionClient('dynamic_kick', KickAction)
+    if not client.wait_for_server():
+        exit(1)
+    print('\r[OK] Connecting to action server \'dynamic_kick\'')
     print()
 
-client.send_goal(goal, done_cb, active_cb, feedback_cb)
-
-client.wait_for_result()
+    goal = KickGoal()
+    goal.foot_speed = random.randrange(1000, 5000) / 1000  # random between 1.0 and 5.0
+    client.send_goal(goal)
+    client.done_cb = done_cb
+    client.feedback_cb = feedback_cb
+    client.active_cb = active_cb
+    print("Sent new goal. Waiting for result")
+    client.wait_for_result()
