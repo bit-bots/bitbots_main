f023f5eb1f369208d637039789e8fd871629fcb9
Finn-Thorben Sell <dev@finn-thorben.me>
2019-04-21 16:08:39 +0000

    engine: reformat documentation into /* */

--- bitbots_dynamic_kick/src/KickEngine.cpp
+++ bitbots_dynamic_kick/src/KickEngine.cpp
@@ -18,20 +18,21 @@ void KickEngine::set_goal(const geometry_msgs::Pose& target_pose, double speed,
 }
 
 bool KickEngine::tick(double dt, JointGoals& goals) {
-    // Get should-be pose from planned splines (every axis) at current time
+    /* Get should-be pose from planned splines (every axis) at current time */
     geometry_msgs::Pose foot_pose;
     foot_pose.position.x = m_trajectories.get("foot_pos_x").pos(m_time);
     foot_pose.position.y = m_trajectories.get("foot_pos_y").pos(m_time);
     foot_pose.position.z = m_trajectories.get("foot_pos_z").pos(m_time);
     foot_pose.orientation.w = 1;
 
-    // Stabilize and return result
+    /* Stabilize and return result */
     bool success = m_stabilizer.stabilize(/* is_left_kick */ false, foot_pose, goals);
     m_time += dt;
     return success;
 }
 
 void KickEngine::calc_splines(const geometry_msgs::Pose &target_pose, const geometry_msgs::Pose &r_foot_pose) {
+    /* Add neccessary points to all splines so that they become smooth */
     m_trajectories.get("foot_pos_x").addPoint(0, r_foot_pose.position.x);
     m_trajectories.get("foot_pos_x").addPoint(1, m_goal_pose.position.x);
 
