de0f3262de0624aeff2f7da7a64dc7e3f35668a5
Finn-Thorben Sell <dev@finn-thorben.me>
2019-04-21 11:48:04 +0000

    engine: add documentation

--- bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickEngine.h
+++ bitbots_dynamic_kick/include/bitbots_dynamic_kick/KickEngine.h
@@ -13,10 +13,37 @@ typedef bitbots_splines::SplineContainer<bitbots_splines::SmoothSpline> Trajecto
 class KickEngine {
 public:
     KickEngine();
+
+    /**
+     * Set new goal which the engine tries to kick at. This will remove the old goal completely and reset all splines.
+     * @param target_pose Pose in base_link frame which should be reached.
+     *      Ideally this is also where the ball lies and a kick occurs.
+     * @param speed Speed with which to reach the target // TODO document scale of speed (m/s, km/h, 0-1,...)
+     * @param l_foot_pose Current pose of left foot in base_link frame
+     * @param r_foot_pose Current pos of right foot in base_link frame
+     */
     void set_goal(const geometry_msgs::Pose& target_pose, double speed,
             const geometry_msgs::Pose& l_foot_pose, const geometry_msgs::Pose& r_foot_pose);
+
+    /**
+     * Reset this KickEngine completely, removing the goal, all splines and thereby stopping all output
+     */
     void reset();
+
+    /**
+     * Do one iteration of spline-progress-updating. This means that whenever tick() is called,
+     *      new motor goals are retrieved from previously calculated splines, stabilized and set to goals
+     * @param dt Passed delta-time between last call to tick() and now. Measured in seconds
+     * @param goals Output motor positions which need to be reached to advance the overall kick movement
+     * @return Whether calculating new motor positions was successful or not.
+     *      This could fail because no goal is set, more time has passed then the spline was long or bio_ik was not
+     *      able to calculate motor_positions from splines.
+     */
     bool tick(double dt, JointGoals& goals);
+
+    /**
+     * Is the currently performed kick with the left foor or not
+     */
     bool is_left_kick();
 private:
     geometry_msgs::Pose m_goal_pose;
@@ -25,7 +52,12 @@ private:
     Trajectories m_trajectories;
     Stabilizer m_stabilizer;
 
+    /**
+     * Construct m_trajectories and add all required splines with their respective keys
+     */
     void init_trajectories();
+
+    // TODO Add docstring to calc_splines once we resolve issue #2
     void calc_splines(const geometry_msgs::Pose& target_pose, const geometry_msgs::Pose& r_foot_pose);
 };
 
--- bitbots_dynamic_kick/src/KickEngine.cpp
+++ bitbots_dynamic_kick/src/KickEngine.cpp
@@ -2,24 +2,30 @@
 
 KickEngine::KickEngine() {}
 
-void KickEngine::reset() {}
+void KickEngine::reset() {
+    m_time = 0;
+    // TODO Reset all other goal variables (depends on whether we use std::option)
+}
 
 void KickEngine::set_goal(const geometry_msgs::Pose& target_pose, double speed,
         const geometry_msgs::Pose& l_foot_pose, const geometry_msgs::Pose& r_foot_pose) {
     m_goal_pose = target_pose;
     m_speed = speed;
     m_time = 0;
+
     init_trajectories();
     calc_splines(l_foot_pose, r_foot_pose);
 }
 
 bool KickEngine::tick(double dt, JointGoals& goals) {
+    // Get should-be pose from planned splines (every axis) at current time
     geometry_msgs::Pose foot_pose;
     foot_pose.position.x = m_trajectories.get("foot_pos_x").pos(m_time);
     foot_pose.position.y = m_trajectories.get("foot_pos_y").pos(m_time);
     foot_pose.position.z = m_trajectories.get("foot_pos_z").pos(m_time);
     foot_pose.orientation.w = 1;
 
+    // Stabilize and return result
     bool success = m_stabilizer.stabilize(/* is_left_kick */ false, foot_pose, goals);
     m_time += dt;
     return success;
