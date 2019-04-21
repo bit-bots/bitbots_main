99481df4bcbea721127445768817b906eb350768
Finn-Thorben Sell <dev@finn-thorben.me>
2019-04-21 16:00:14 +0000

    stabilizer: use std::optional

--- bitbots_dynamic_kick/src/Stabilizer.cpp
+++ bitbots_dynamic_kick/src/Stabilizer.cpp
@@ -1,9 +1,8 @@
 #include "bitbots_dynamic_kick/Stabilizer.h"
 
-Stabilizer::Stabilizer() {
+Stabilizer::Stabilizer() {}
 
-}
-
-bool Stabilizer::stabilize(bool is_left_kick, geometry_msgs::Pose foot_goal, JointGoals& goals) {
-    return false;
+std::optional<JointGoals> Stabilizer::stabilize(bool is_left_kick, geometry_msgs::Pose foot_goal) {
+    // TODO Implement stabilization
+    return std::nullopt;
 }
