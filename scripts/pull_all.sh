471f09b76024a5e2a2f2c4848050a617b57cba9d
Timon Engelke <timon.engelke@online.de>
2019-04-25 15:27:38 +0200

    add pull_init script for initial pull and actual pull all

--- scripts/pull_all.sh
+++ scripts/pull_all.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-
-{
-    git submodule update --recursive --remote --init &&
-    git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; [ "$branch" = "" ] && git checkout master || git checkout $branch' &&
-    git submodule sync &&
-    git submodule foreach git pull
-} || { echo -e "\033[91m\033[1m########## Pull failed! ##########\033[0m" && exit 1; }
+git submodule foreach git pull
---
+++ scripts/pull_init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+{
+    git submodule update --recursive --remote --init &&
+    git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; [ "$branch" = "" ] && git checkout master || git checkout $branch' &&
+    git submodule sync &&
+    git submodule foreach git pull
+} || { echo -e "\033[91m\033[1m########## Pull failed! ##########\033[0m" && exit 1; }
