b3cd18bcf9b7de4611ff09a246d3bccb4bc7b083
lenimitoundie <7kurz@informatik.uni-hamburg.de>
2019-04-25 17:53:12 +0200

    added track_post, post_seen and config

---
+++ bitbots_head_behavior/src/bitbots_head_behavior/actions/track_post.py
@@ -0,0 +1,32 @@
+import rospy
+from geometry_msgs.msg import PointStamped
+
+from bitbots_head_behavior.actions.look_at import AbstractLookAt
+
+
+class TrackPost(AbstractLookAt):
+    """
+    This action follows the seen post so that the camera always points towards it.
+    We try to do this so that the post doesnt get lost as easily
+    """
+
+    def __init__(self, dsd, blackboard, parameters=None):
+        super(TrackPost, self).__init__(dsd, blackboard, parameters)
+        self.ball_tracking_min_pan_delta = self.blackboard.config['ball_tracking_min_pan_delta']
+        self.ball_tracking_min_tilt_delta = self.blackboard.config['ball_tracking_min_tilt_delta']
+
+    def perform(self, reevaluate=False):
+        """
+        Call look_at to look at the point which our world-model determines to be the post
+        if both posts are seen, look in between
+
+        :param reevaluate: No effect here
+        """
+
+        # Get last post position
+        point = self.blackboard.world_model.get_ball_stamped()
+        # TODO change into post left or right or goal when both
+
+        # Call internal look-at to turn head to this point (when necessary)
+        self._look_at(point, self.ball_tracking_min_pan_delta, self.ball_tracking_min_tilt_delta)
+
