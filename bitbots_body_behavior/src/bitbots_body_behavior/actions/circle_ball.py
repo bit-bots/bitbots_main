d701c03632ac3c9e0d5d6ef240af92458a412381
lenimitoundie <7kurz@informatik.uni-hamburg.de>
2019-04-26 10:49:52 +0200

    comments

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/circle_ball.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/circle_ball.py
@@ -14,10 +14,16 @@ class CircleBall(AbstractActionElement):
         tf_listener = tf2.TransformListener(self.tf_buffer)
 
     def perform(self, reevaluate=False):
+        """
+        Walks around the ball to get a post into the field of sight to make
+        a kick towards the goal possible
+
+        :param reevaluate:
+        :return:
+        """
         goal_position = self.blackboard.world_model.get_detection_based_goal_position_uv()
         if not goal_position:
             return
-        # goal_u, goal_v = goal_position
 
         ball_position = self.blackboard.world_model.get_ball_position_uv()
         ball_u, ball_v = ball_position
@@ -30,11 +36,10 @@ class CircleBall(AbstractActionElement):
         # ball position
         pose_msg.pose.position = Point(point[0], point[1], 0)
 
-        rotation = quaternion_from_euler(0, 0, 45)  # 45 degrees
+        rotation = quaternion_from_euler(0, 0, 45)  # (hopefully) 45 degrees to the left
         pose_msg.pose.orientation = Quaternion(*rotation)
-        pose_msg.pose.position.x -= 0.2  # little bit before the ball
-        pose_msg.pose.position.y -= 0.2  # little bit right of the ball
+        pose_msg.pose.position.x -= 0.2  # 20 cm before the ball
+        pose_msg.pose.position.y -= 0.2  # 20 cm to the right of the ball
 
         self.blackboard.pathfinding.publish(pose_msg)
 
-        # TODO walk around ball but without getting out of good kicking distance
