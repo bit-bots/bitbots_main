54336d87fd72713938efd5a5d68423f229aefc32
Nils Rokita <0rokita@informatik.uni-hamburg.de>
2019-05-02 11:02:54 +0200

    Add script to use ros with direct network between laptop and robot

---
+++ scripts/direct_robot.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && echo "Please use \"source ${BASH_SOURCE[0]}\"" && exit 1
+export ROS_IP=`ip a | grep 'inet 192.168.17' | awk '{print $2}' | cut -d '/' -f 1`
+echo "Set \$ROS_IP to ${ROS_IP}"
+echo "To deactivate use \"unset ROS_IP\""
