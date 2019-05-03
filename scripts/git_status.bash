b06df709e371a32c8673c20a7916c510c9e9bd94
Timon Engelke <timon.engelke@online.de>
2019-05-03 13:12:23 +0200

    git status script: more compact information

--- scripts/git_status.bash
+++ scripts/git_status.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git submodule foreach git status --short | sed "s/Entering '\(.*\)'$/\1:/"
+git submodule --quiet foreach 'echo -e "\033[1m$(basename $(realpath .)):\033[0m branch \033[1m$(git rev-parse --abbrev-ref HEAD)\033[0m, $(git status --porcelain | grep -c " M ") modified, $(git status --porcelain | grep -c "?? ") untracked"'
