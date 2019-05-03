532b31d7ff2b96dfc1628c74b3b106d4c5751239
Timon Engelke <timon.engelke@online.de>
2019-05-03 16:40:25 +0200

    git_status: add number of unpushed commits

--- scripts/git_status.bash
+++ scripts/git_status.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git submodule --quiet foreach 'echo -e "\033[1m$(basename $(realpath .)):\033[0m branch \033[1m$(git rev-parse --abbrev-ref HEAD)\033[0m, $(git status --porcelain | grep -c " M ") modified, $(git status --porcelain | grep -c "?? ") untracked"'
+git submodule --quiet foreach 'unpushed=$(git log --oneline origin/HEAD..HEAD | wc -l); echo -ne "\033[1m$(basename $(realpath .)):\033[0m branch \033[1m$(git rev-parse --abbrev-ref HEAD)\033[0m, $(git status --porcelain | grep -c " M ") modified, $(git status --porcelain | grep -c "?? ") untracked"; [[ $unpushed != 0 ]] && echo -e ", \033[1m${unpushed} unpushed commit(s)\033[0m" || echo'
