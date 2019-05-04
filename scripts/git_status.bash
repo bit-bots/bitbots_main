e0687d26048a108c0eefca333341b0b9d5c2fc60
Timon Engelke <timon.engelke@online.de>
2019-05-04 12:28:12 +0200

    git_status: make output more beautiful

--- scripts/git_status.bash
+++ scripts/git_status.bash
@@ -1,3 +1,40 @@
 #!/bin/bash
 
-git submodule --quiet foreach 'unpushed=$(git log --oneline origin/HEAD..HEAD | wc -l); echo -ne "\033[1m$(basename $(realpath .)):\033[0m branch \033[1m$(git rev-parse --abbrev-ref HEAD)\033[0m, $(git status --porcelain | grep -c " M ") modified, $(git status --porcelain | grep -c "?? ") untracked"; [[ $unpushed != 0 ]] && echo -e ", \033[1m${unpushed} unpushed commit(s)\033[0m" || echo'
+function echo_bold() {
+    echo -ne "\033[1m$@\033[0m"
+}
+
+folders=$(git submodule --quiet foreach 'pwd')
+
+longest=0
+for folder in $folders; do
+    length=$(wc -c <<< $(basename $folder))
+    if [[ $(( $length > $longest )) == 1 ]]; then
+        longest=$length
+    fi
+done
+
+for folder in $folders; do
+    module=$(basename $folder)
+    length=$(wc -c <<< $(basename $folder))
+    branch=$(git -C $folder rev-parse --abbrev-ref HEAD)
+    modified=$(git -C $folder status --porcelain | grep -c "^ M ")
+    untracked=$(git -C $folder status --porcelain | grep -c "^?? ")
+    unpushed=$(git -C $folder log --oneline origin/$branch..$branch | wc -l)
+    echo_bold $module
+    echo -n ":  "
+    spaces=$(( $longest - $length ))
+    while [[ $(( $spaces > 0 )) == 1 ]]; do
+        echo -n " "
+        spaces=$(( $spaces - 1 ))
+    done
+    echo -n "On branch $branch"
+    [[ $modified != 0 ]] && echo -n ", $modified modified"
+    [[ $untracked != 0 ]] && echo -n ", $untracked untracked"
+    if [[ $unpushed == 1 ]]; then
+        echo_bold ", 1 unpushed commit"
+    elif [[ $unpushed != 0 ]]; then
+        echo_bold ", $unpushed unpushed commits"
+    fi
+    echo
+done
