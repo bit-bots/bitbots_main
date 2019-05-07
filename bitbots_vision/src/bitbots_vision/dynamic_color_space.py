55d9aad684bf54c8b3b088da1e38378d0c65379d
Hendrik Brandt <45638790+hendrikbrandt@users.noreply.github.com>
2019-05-07 08:16:52 +0200

    Update dynamic_color_space.py

--- bitbots_vision/src/bitbots_vision/dynamic_color_space.py
+++ bitbots_vision/src/bitbots_vision/dynamic_color_space.py
@@ -302,8 +302,8 @@ class Heuristic:
     def __init__(self, debug_printer):
         # type: (DebugPrinter) -> None
         """
-        Filters new color space colors according to their position relative to the field_boundary.
-        Only colors that occur under the field_boundary and have no occurrences over the field_boundary get picked.
+        Filters new color space colors according to their position relative to the field boundary.
+        Only colors that occur under the field boundary and have no occurrences over the field boundary get picked.
 
         :param DebugPrinter debug_printer: Debug-printer
         :return: None
@@ -313,11 +313,11 @@ class Heuristic:
     def run(self, color_list, image, mask):
         # type: (np.array, np.array, np.array) -> np.array
         """
-        This method filters a given list of colors using the original image and a field_boundary-mask.
+        This method filters a given list of colors using the original image and a field-boundary-mask.
 
         :param np.array color_list: list of color values, that need to be filtered
         :param np.array image: raw vision image
-        :param np.array mask: binary field_boundary-mask
+        :param np.array mask: binary field-boundary-mask
         :return np.array: filtered list of colors
         """
         # Simplifies the handling by merging the three color channels
@@ -334,10 +334,10 @@ class Heuristic:
     def recalculate(self, image, mask):
         # type: (np.array, np.array) -> set
         """
-        Generates a whitelist of allowed colors using the original image and the field_boundary-mask.
+        Generates a whitelist of allowed colors using the original image and the field-boundary-mask.
 
         :param np.array image: image
-        :param np.array mask: field_boundary-mask
+        :param np.array mask: field-boundary-mask
         :return set: whitelist
         """
         # Generates whitelist
@@ -350,9 +350,9 @@ class Heuristic:
         Masks picture and returns the unique colors that occurs in both mask partitions.
 
         :param np.array image: image
-        :param np.array mask: field_boundary-mask
-        :return np.array: colors over the field_boundary
-        :return np.array: colors under the field_boundary
+        :param np.array mask: field-boundary-mask
+        :return np.array: colors over the field boundary
+        :return np.array: colors under the field boundary
         """
         # Calls a function to calculate the number of occurrences of all colors in the image
         return (self.get_unique_colors(cv2.bitwise_and(image, image, mask=255 - mask)),
