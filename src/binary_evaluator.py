916be437c79a9aeab4d391a6c6959c09e97850f2
Florian <florian@flova.de>
2019-05-07 13:32:54 +0200

    Updates evaluator

--- src/binary_evaluator.py
+++ src/binary_evaluator.py
@@ -37,8 +37,8 @@ class BinaryEvaluator(object):
         self.evaluateAllImages()
 
     def setTruth(self):
-        angle_1 = float(1)/4*2*math.pi
-        angle_2 = float(3)/4*2*math.pi
+        angle_1 = math.radians(90)
+        angle_2 = math.radians(180+90)
         side_1_image = self.loader.getImage(4, 3, angle_1)
         side_2_image = self.loader.getImage(4, 3, angle_2)
         self.vc.set_truth(0, side_1_image)
@@ -57,39 +57,52 @@ class BinaryEvaluator(object):
         confidences = list()
         fail = 0.0
         unsave = 0.0
+        confidence_threshold = 0.5
         for row in range(self.dimensions[0]):
             confidences.append(list())
             for checkpoint in range(self.dimensions[1]):
                 confidences[row].append(list())
+                fails = list()
                 for angle in [4,12]: #,12]:# range(self.angle_steps):
                     image = self.loader.getImage(row, checkpoint, float(angle)/16*2*math.pi)
                     # self.show_img(image)
                     ground_truth = float(angle - 4)/16*2*math.pi
-                    print(ground_truth)
-                    compass_result = self.vc.process_image(image)#, debugCB=self.debug_image_callback)
+                    compass_result = self.vc.process_image(image)
                     confidence = compass_result[1]
-                    print(compass_result[0])
-                    if (abs(ground_truth - compass_result[0]) > 0.0001 and compass_result[1] > 0.5):
+                    fail_val = 0.0
+                    if (abs(ground_truth - compass_result[0]) > 0.0001 and compass_result[1] > confidence_threshold):
                         print("Bad detection", compass_result[0], ground_truth)
                         fail += 1
-                        confidence = 0.0
-                    if compass_result[1] <= 0.5:
+                        fail_val = 1.0
+                    if compass_result[1] <= confidence_threshold:
                         # self.show_img(image)
                         unsave += 1
                         confidence = 0.0
                         print("Confidence too low")
                     confidences[row][checkpoint].append(confidence)
+                    fails.append(fail_val)
+                for fail_obj in fails:
+                    confidences[row][checkpoint].append(fail_obj)
         count = (self.dimensions[0] * self.dimensions[1]) * 2
-        print(confidences)
+        plt.title('Binary evaluation')
+        plt.subplot(2, 2, 1)
+        plt.title('Confidence values torwards goal 1')
         self.plot_confidence(confidences,0)
+        plt.subplot(2, 2, 2)
+        plt.title('Confidence values torwards goal 2')
         self.plot_confidence(confidences,1)
+        plt.subplot(2, 2, 3)
+        plt.title('False positives torwards goal 1')
+        self.plot_confidence(confidences,2)
+        plt.subplot(2, 2, 4)
+        plt.title('False positives torwards goal 2')
+        self.plot_confidence(confidences,3)
         plt.show()
         print("Fail {}%".format(fail/count*100))
         print("Unsave {}%".format(unsave/count*100))
 
     def plot_confidence(self, confidences, side):
         a = np.zeros((10, 7))
-        plt.subplot(2, 1, side + 1)
         for row_index, row in enumerate(confidences):
             for checkpoint_index, checkpoint in enumerate(row):
                 a[row_index, checkpoint_index] = float(checkpoint[side])
