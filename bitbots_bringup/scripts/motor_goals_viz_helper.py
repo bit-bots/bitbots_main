55c2409faac17b0676c98eef18219724b20c8904
Timon Engelke <timon.engelke@online.de>
2019-05-13 13:26:27 +0200

    motor_goals_helper: fix animation

--- bitbots_bringup/scripts/motor_goals_viz_helper.py
+++ bitbots_bringup/scripts/motor_goals_viz_helper.py
@@ -18,15 +18,16 @@ class MotorVizHelper:
         parser.add_argument("--walking", "-w", help="Directly get walking motor goals", action="store_true")
         parser.add_argument("--animation", "-a", help="Directly get animation motor goals", action="store_true")
         parser.add_argument("--head", "-k", help="Directly get head motor goals", action="store_true")
+        parser.add_argument("--all", help="Directly get all motor goals", action="store_true")
         args = parser.parse_args(args0[1:])
 
         rospy.init_node("motor_viz_helper", anonymous=False)
         self.joint_publisher = rospy.Publisher('joint_states', JointState, queue_size=1, tcp_nodelay=True)
-        if args.walking:
+        if args.walking or args.all:
             rospy.Subscriber("walking_motor_goals", JointCommand, self.joint_command_cb, queue_size=1, tcp_nodelay=True)
-        if args.animation:
-            rospy.Subscriber("animation_motor_goals", JointCommand, self.animation_cb, queue_size=1, tcp_nodelay=True)
-        if args.head:
+        if args.animation or args.all:
+            rospy.Subscriber("animation", Animation, self.animation_cb, queue_size=1, tcp_nodelay=True)
+        if args.head or args.all:
             rospy.Subscriber("head_motor_goals", JointCommand, self.animation_cb, queue_size=1, tcp_nodelay=True)
         rospy.Subscriber("/DynamixelController/command", JointCommand, self.joint_command_cb, queue_size=1, tcp_nodelay=True)
 
@@ -51,8 +52,8 @@ class MotorVizHelper:
 
     def animation_cb(self, msg: Animation):
         self.joint_state_msg.header = msg.header
-        self.joint_state_msg.name = msg.position.joint_names
-        self.joint_state_msg.position = msg.position.points[0].positions
+        self.joint_state_msg.name = list(msg.position.joint_names)
+        self.joint_state_msg.position = list(msg.position.points[0].positions)
 
 helper = MotorVizHelper()
 
