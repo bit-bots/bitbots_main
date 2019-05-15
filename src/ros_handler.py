c46c062c439ef8e517cc6edcbf86f2ad6a0514ec
JanGut <jgutsche@ymail.com>
2019-05-15 16:44:44 +0200

    added action to trigger visual compass

--- src/ros_handler.py
+++ src/ros_handler.py
@@ -1,11 +1,13 @@
 #! /usr/bin/env python2
 import rospy
 import rospkg
+import actionlib
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from dynamic_reconfigure.server import Server
 from humanoid_league_msgs.msg import VisualCompassRotation
 from bitbots_visual_compass.cfg import VisualCompassConfig
+from bitbots_msgs.msg import VisualCompassSetGroundTruthAction
 from worker import VisualCompass
 
 # TODO define message for behavior
@@ -16,7 +18,6 @@ class VisualCompassROSHandler():
     # type: () -> None
     """
     TODO docs
-    Subscribes to 'vision_config'-message
     Subscribes to raw image
 
     Trigger: 'trigger_visual_compass'-trigger
@@ -43,6 +44,7 @@ class VisualCompassROSHandler():
         self.bridge = CvBridge()
 
         self.config = {}
+        self.image_dict = {}
         self.compass = None
 
         # Register publisher of 'visual_compass'-messages
@@ -52,25 +54,31 @@ class VisualCompassROSHandler():
             queue_size=1)
 
         # Register VisualCompassConfig server for dynamic reconfigure and set callback
-        Server(VisualCompassConfig, self._dynamic_reconfigure_callback)
+        Server(VisualCompassConfig, self.dynamic_reconfigure_callback)
 
         rospy.spin()
 
-    def _dynamic_reconfigure_callback(self, config, level):
+    def dynamic_reconfigure_callback(self, config, level):
         # type: (dict, TODO) -> None
         """
         TODO docs
         """
         self.compass = VisualCompass(config)
 
-        if 'compass_multiple_sample_count' not in self.config or \
-            config['compass_multiple_sample_count'] != self.vision_config['compass_multiple_sample_count']:
+        if self.changed_config_param(config, 'compass_multiple_sample_count'):
             self.sample_count = 0
-            self.ready_for_loop = False # TODO change name
+            self.ready_for_loop = False
+
+        # Register VisualCompassSetGroundTruthAction server
+        if self.changed_config_param(config, 'ROS_handler_action_server_name'):
+            self.actionServer = actionlib.SimpleActionServer(config['ROS_handler_action_server_name'],
+                                                            VisualCompassSetGroundTruthAction,
+                                                            execute_cb=self.action_callback,
+                                                            auto_start = False)
+            self.actionServer.start()
 
         # Subscribe to Image-message
-        if 'ROS_handler_img_msg_topic' not in self.config or \
-                self.config['ROS_handler_img_msg_topic'] != config['ROS_handler_img_msg_topic']:
+        if self.changed_config_param(config, 'ROS_handler_img_msg_topic'):
             if hasattr(self, 'sub_image_msg'):
                 self.sub_image_msg.unregister()
             self.sub_image_msg = rospy.Subscriber(
@@ -84,9 +92,21 @@ class VisualCompassROSHandler():
 
         self.config = config
 
-        self.update_sample_count()
+        self.check_sample_count()
 
         return self.config
+
+    def action_callback(self, goal):
+        if self.image_dict:
+            # TODO: check timestamps
+            self.compass.set_truth(goal.orientation, self.image_dict['image'])
+            self.actionServer.set_succeeded()
+            self.sample_count += 1
+            self.check_sample_count()
+
+        else:
+            self.actionServer.set_aborted(text="No image received yet")
+            rospy.logwarn('No image received yet')
 
     def image_callback(self, image_msg):
         # type: (Image) -> None
@@ -94,41 +114,45 @@ class VisualCompassROSHandler():
         TODO docs
         """
         # Drops old images
-        image_age = rospy.get_rostime() - image_msg.header.stamp 
-        if image_age.to_sec() > 0.1:
-            print("Visual Compass: Dropped Image-message")  # TODO debug printer
-            return
+        # TODO: fix
+        # image_age = rospy.get_rostime() - image_msg.header.stamp 
+        # if image_age.to_sec() > 0.1:
+        #     print("Visual Compass: Dropped Image-message")  # TODO debug printer
+        #     return
+
+        # Converting the ROS image message to CV2-image
+        self.image_dict['image'] = self.bridge.imgmsg_to_cv2(image_msg, 'bgr8')
+        self.image_dict['header_frame_id'] = image_msg.header.frame_id
+        self.image_dict['header_stamp'] = image_msg.header.stamp
 
         if self.ready_for_loop:
-            self.handle_image(image_msg)
+            self.loop_over_image(self.image_dict)
 
-    def handle_image(self, image_msg):
-        # type: (Image) -> None
+    def loop_over_image(self, image_dict):
+        # type: (dict) -> None
         """
         TODO docs
         """
-        # Converting the ROS image message to CV2-image
-        image = self.bridge.imgmsg_to_cv2(image_msg, 'bgr8')
-
         # Set image
-        self.compass.process_image(image)
+        # TODO: Set y-axis orientation of IMU
+        self.compass.process_image(image_dict['image'])
 
         # Get angle and certainty from compass
         result = self.compass.get_side()
 
         # Publishes the 'visual_compass'-message
-        self.publish(image_msg, result[0], result[1])
+        self.publish_rotation(image_dict['header_frame_id'], image_dict['header_stamp'], result[0], result[1])
     
-    def publish(self, image_msg, orientation, confidence):
-        # type: (Image, float, float) -> None
+    def publish_rotation(self, header_frame_id, header_stamp, orientation, confidence):
+        # type: (TODO, TODO, float, float) -> None
         """
         TODO docs
         """
         msg = VisualCompassRotation()
 
         # Create VisualCompassRotation-message
-        msg.header.frame_id = image_msg.header.frame_id
-        msg.header.stamp = image_msg.header.stamp
+        msg.header.frame_id = header_frame_id
+        msg.header.stamp = header_stamp
 
         msg.orientation = orientation
         msg.confidence = confidence
@@ -136,7 +160,7 @@ class VisualCompassROSHandler():
         # Publish VisualCompassMsg-message
         self.pub_compass.publish(msg)
 
-    def update_sample_count(self):
+    def check_sample_count(self):
         # type: () -> None
         """
         TODO docs
@@ -151,7 +175,12 @@ class VisualCompassROSHandler():
                 rospy.loginfo('Visual compass: Initiating finished. Now Ready for processing loop.')
             self.ready_for_loop = True
             
-
+    def changed_config_param(self, config, param_name):
+        # type: (dict, str) -> bool
+        """
+        TODO
+        """
+        return param_name not in self.config or config[param_name] != self.config[param_name]
 
 if __name__ == '__main__':
     VisualCompassROSHandler()
