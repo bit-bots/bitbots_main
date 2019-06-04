6fc77d34824efe3e11001cac5c443f3e896906b4
Timon Engelke <timon.engelke@online.de>
2019-06-04 12:31:23 +0200

    git status: add meta

--- scripts/git_status.bash
+++ scripts/git_status.bash
@@ -4,22 +4,23 @@ function echo_bold() {
     echo -ne "\033[1m$@\033[0m"
 }
 
-folders=$(git submodule --quiet foreach 'pwd')
+submodules=$(git submodule --quiet foreach 'pwd')
+folders=". $submodules"
 
 longest=0
 for folder in $folders; do
-    length=$(wc -c <<< $(basename $folder))
+    length=$(wc -c <<< $(basename $(realpath $folder)))
     if [[ $(( $length > $longest )) == 1 ]]; then
         longest=$length
     fi
 done
 
 for folder in $folders; do
-    module=$(basename $folder)
-    length=$(wc -c <<< $(basename $folder))
+    module=$(basename $(realpath $folder))
+    length=$(wc -c <<< $module)
     branch=$(git -C $folder rev-parse --abbrev-ref HEAD)
-    modified=$(git -C $folder status --porcelain | grep -c "^ M ")
-    untracked=$(git -C $folder status --porcelain | grep -c "^?? ")
+    modified=$(git -C $folder status --ignore-submodules --porcelain | grep -c "^ M ")
+    untracked=$(git -C $folder status --ignore-submodules --porcelain | grep -c "^?? ")
     unpushed=$(git -C $folder log --oneline origin/$branch..$branch | wc -l)
     echo_bold $module
     echo -n ":  "
@@ -32,9 +33,11 @@ for folder in $folders; do
     [[ $modified != 0 ]] && echo -n ", $modified modified"
     [[ $untracked != 0 ]] && echo -n ", $untracked untracked"
     if [[ $unpushed == 1 ]]; then
-        echo_bold ", 1 unpushed commit"
+        echo -n ", "
+        echo_bold "1 unpushed commit"
     elif [[ $unpushed != 0 ]]; then
-        echo_bold ", $unpushed unpushed commits"
+        echo -n ", "
+        echo_bold "$unpushed unpushed commits"
     fi
     echo
 done
