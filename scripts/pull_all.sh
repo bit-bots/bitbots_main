8deb66f20800c6a3afb7ad84c38a8c6ab1addde8
Finn-Thorben Sell <dev@finn-thorben.me>
2019-06-12 14:19:15 +0000

    add vision_opencv as lib
    this is needed because we compile it ourselves on our jetsons

--- scripts/pull_all.sh
+++ scripts/pull_all.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 git submodule update --init lib/dwa_local_planner
 git -C lib/dwa_local_planner checkout master
+git submodule update --init lib/vision_opencv
+git -C lib/vision_opencv checkout master
 git submodule foreach git pull
