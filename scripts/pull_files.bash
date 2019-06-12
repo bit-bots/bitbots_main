3abf31b9d039abf4343aff35a5adff38344a6cf0
Timon Engelke <timon.engelke@online.de>
2019-06-12 11:31:49 +0200

    add fcnn032 to pull files script

--- scripts/pull_files.bash
+++ scripts/pull_files.bash
@@ -10,3 +10,6 @@ wget -r -N -np -nd -P $META/bitbots_vision/bitbots_vision/models/fcnn03 --reject
 
 mkdir -p $META/bitbots_vision/bitbots_vision/models/fcnn031
 wget -r -N -np -nd -P $META/bitbots_vision/bitbots_vision/models/fcnn031 --reject "index.html*" "http://data.bit-bots.de/models/fcnn031/"
+
+mkdir -p $META/bitbots_vision/bitbots_vision/models/fcnn032
+wget -r -N -np -nd -P $META/bitbots_vision/bitbots_vision/models/fcnn032 --reject "index.html*" "http://data.bit-bots.de/models/fcnn032/"
