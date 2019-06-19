02944abd4c6d58d015c1c048aba47fe7bbed8e61
Timon Engelke <timon.engelke@online.de>
2019-06-19 17:21:35 +0000

    pull_all: add pull of basler drivers

--- scripts/pull_all.sh
+++ scripts/pull_all.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 git submodule update --init lib/dwa_local_planner
 git -C lib/dwa_local_planner checkout master
 git submodule update --init lib/vision_opencv
 git -C lib/vision_opencv checkout melodic
 git submodule foreach git pull
+if [[ -d basler_drivers ]]; then
+    git -C basler_drivers pull
+else
+    echo -e "\n\n\033[91m\033[1mNo basler drivers found! Use 'make basler' to install them\033[0m\n"
+    exit 1
+fi
