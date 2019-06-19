54f8ef36807374f1df2b219e8095af9736f53976
Florian <florian@flova.de>
2019-06-19 18:14:07 +0200

    Fix bug

--- src/worker/matcher/sift_matcher.py
+++ src/worker/matcher/sift_matcher.py
@@ -23,8 +23,10 @@ class SiftMatcher(Matcher):
                                                 keypoint.size,
                                                 keypoint.angle), kp))
         features1 = np.zeros((desc1.shape[0],), dtype=self.feature_dtype)
+
         for index, keypoint in enumerate(keypoints):
-            features1[index]['angle'] = keypoints[3]
+            features1[index]['angle'] = keypoint[3]
+        
         features1['desc'] = desc1
         features2 = np.zeros((desc2.shape[0],), dtype=self.feature_dtype)
         features2['desc'] = desc2
