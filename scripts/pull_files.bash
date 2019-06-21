e307db5bb0c9a740d7bac9682ae908fbbe132681
Timon Engelke <timon.engelke@online.de>
2019-06-21 22:41:58 +0200

    pull_files: adapt to always pull all models

--- scripts/pull_files.bash
+++ scripts/pull_files.bash
@@ -2,14 +2,4 @@
 
 META=$(dirname $(dirname $(realpath $0)))
 
-mkdir -p $META/bitbots_vision/bitbots_vision/models/classifier_01
-wget -r -N -np -nd -P $META/bitbots_vision/bitbots_vision/models/classifier_01 --reject "index.html*" "http://data.bit-bots.de/models/classifier_01/"
-
-mkdir -p $META/bitbots_vision/bitbots_vision/models/fcnn03
-wget -r -N -np -nd -P $META/bitbots_vision/bitbots_vision/models/fcnn03 --reject "index.html*" "http://data.bit-bots.de/models/fcnn03/"
-
-mkdir -p $META/bitbots_vision/bitbots_vision/models/fcnn031
-wget -r -N -np -nd -P $META/bitbots_vision/bitbots_vision/models/fcnn031 --reject "index.html*" "http://data.bit-bots.de/models/fcnn031/"
-
-mkdir -p $META/bitbots_vision/bitbots_vision/models/fcnn032
-wget -r -N -np -nd -P $META/bitbots_vision/bitbots_vision/models/fcnn032 --reject "index.html*" "http://data.bit-bots.de/models/fcnn032/"
+wget -r -N -np -nH -P $META/bitbots_vision/bitbots_vision --reject "index.html*" "http://data.bit-bots.de/models/"
