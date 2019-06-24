941ae7bafa1210ed9b7316142b5adc3c56e4d544
Timon Engelke <timon.engelke@online.de>
2019-06-24 17:35:55 +0200

    body behavior: small fix

--- bitbots_body_behavior/src/bitbots_body_behavior/decisions/ball_in_defensive_area.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/ball_in_defensive_area.py
@@ -7,7 +7,7 @@ class BallInDefensiveArea(AbstractDecisionElement):
         self.defensive_area = self.blackboard.config['defensive_area']
 
     def perform(self, reevaluate=False):
-        ball_position = self.blackboard.world_model.get_ball_position_xy
+        ball_position = self.blackboard.world_model.get_ball_position_xy()
         # calculate the x value of the boundary of the defensive area
         defensive_x = (self.defensive_area * self.blackboard.field_length) - (self.blackboard.field_length / 2)
         if ball_position[0] <= defensive_x:
