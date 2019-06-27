095cd83d1744cb283735345a3eead36a69e95a24
Timon Engelke <timon.engelke@online.de>
2019-06-27 20:52:03 +0200

    body behavior: improve stand/stop actions
    Closes #18

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/look_at.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/look_at.py
@@ -5,12 +5,16 @@ from humanoid_league_msgs.msg import HeadMode
 class LookAtBall(AbstractActionElement):
     def perform(self):
         self.blackboard.blackboard.set_head_duty(HeadMode.BALL_MODE)
+        self.pop()
+
 
 class LookAtBallPenalty(AbstractActionElement):
     def perform(self):
         self.blackboard.blackboard.set_head_duty(HeadMode.BALL_MODE_PENALTY)
+        self.pop()
 
 
 class LookAtFieldFeatures(AbstractActionElement):
     def perform(self):
         self.blackboard.blackboard.set_head_duty(HeadMode.FIELD_FEATURES)
+        self.pop()
--- bitbots_body_behavior/src/bitbots_body_behavior/actions/stand.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/stand.py
@@ -4,18 +4,6 @@ from tf2_geometry_msgs import PoseStamped
 from dynamic_stack_decider.abstract_action_element import AbstractActionElement
 
 
-class SendStop(AbstractActionElement):
-    """ This stops the robot's walking and directly pops itself """
-
-    def perform(self, reevaluate=False):
-        stand_pose = PoseStamped()
-        stand_pose.header.stamp = rospy.Time.now()
-        stand_pose.header.frame_id = 'base_footprint'
-        stand_pose.pose.orientation.w = 1
-        self.blackboard.pathfinding.publish(stand_pose)
-        self.pop()
-
-
 class Stop(AbstractActionElement):
     """ This stops the robot's walking and pops itself when the robot stands """
 
@@ -29,7 +17,7 @@ class Stop(AbstractActionElement):
             self.pop()
 
 
-class Stand(AbstractActionElement):
+class StandAndWait(AbstractActionElement):
     """ This stops the robots walking and keeps standing """
 
     def perform(self, reevaluate=False):
