b0fd02f33b118f1d4cb9be71fcc0d60e409e4ab7
Florian <florian@flova.de>
2019-06-28 10:34:52 +0200

    Test stuff

--- src/filter.py
+++ src/filter.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import math
 import rospy
+import rospkg
 import tf2_ros as tf2
 from tf.transformations import euler_from_quaternion
 import numpy as np
@@ -10,34 +11,35 @@ class VisualCompassFilter:
     def __init__(self, config):
         self.config = config
 
+        # TODO remove next lines
+        rospack = rospkg.RosPack()
+        self.package_path = rospack.get_path('bitbots_visual_compass')
+
+        rospy.init_node('bitbots_visual_compass_setup')
+
         # TODO config file
         self.base_frame = 'base_footprint'
         self.camera_frame = 'camera_optical_frame'
+        self.odom_frame = "odom"
 
-        """
-        self.tf_buffer = tf2.Buffer(cache_time=rospy.Duration(20))
+        self.tf_buffer = tf2.Buffer(cache_time=rospy.Duration(120))
         self.listener = tf2.TransformListener(self.tf_buffer)
-        """
 
         # Filter constant
         self.K = 0.8
 
         self.threshold = 0.3
 
-        self.filteredVector = None #TODO
+        self.filteredVector = None
 
         currentTimestamp = None
 
         lastTimestamp = None
 
     def _getYawOrientationInBasefootprintFrame(self, visualCompassAngle, header):
-        return visualCompassAngle
-        orientation = self.tf_buffer.lookup_transform(self.camera_frame, self.base_frame, header.stamp, timeout=rospy.Duration(0.5)).transform.rotation
-        camera_yaw_angle = (euler_from_quaternion(( orientation.x, 
-                                                    orientation.y, 
-                                                    orientation.z, 
-                                                    orientation.w))[2] + 0.5 * math.pi) % (2 * math.pi)
-        correctedAngle = visualCompassAngle + camera_yaw_angle
+        # TODO stamp
+        camera_yaw_angle = self._getYawFromTf(self.camera_frame, self.base_frame, header) + 0.5 * math.pi
+        correctedAngle = (visualCompassAngle + camera_yaw_angle) % (2 * math.pi)
         return correctedAngle
 
     def _initFilter(self, vector):
@@ -51,7 +53,7 @@ class VisualCompassFilter:
         relativeYawAngle = self._getYawOrientationInBasefootprintFrame(angle, header)
         relativeYawVector = self._angleToVector(relativeYawAngle, limitedConfidence)
 
-        self.currentTimestamp = header # TODO set stamp
+        self.currentTimestamp = header
 
         if self.filteredVector is None:
             self._initFilter(relativeYawVector)
@@ -63,6 +65,8 @@ class VisualCompassFilter:
         # Set new timestamp
         self.lastTimestamp = self.currentTimestamp
 
+        return math.degrees(self.getFilteredValue()[0]), self.getFilteredValue()[1]
+
     def _cutAtThreshold(self, confidence):
         if confidence <= self.threshold:
             confidence = 0
@@ -70,7 +74,11 @@ class VisualCompassFilter:
 
     def _odomOffset(self, vector, lastTimestamp, currentTimestamp):
         # reset filter if timestamps are too fare off.
-        odomOffsetYawAngle = 0 # TODO get odom rotation
+        oldOrientation = self._getYawFromTf(self.odom_frame, self.base_frame, lastTimestamp)
+        currentOrientation = self._getYawFromTf(self.odom_frame, self.base_frame, currentTimestamp)
+
+        orientationDelta = (currentOrientation - oldOrientation)
+        odomOffsetYawAngle = orientationDelta
         odomCorrectedVector = self._rotateVectorByAngle(vector, odomOffsetYawAngle)
         return odomCorrectedVector
     
@@ -91,6 +99,16 @@ class VisualCompassFilter:
 
     def _runFilter(self, oldVectorWithOdom, currentVector):
         return oldVectorWithOdom * self.K + currentVector * (1 - self.K)
+
+    def _getYawFromTf(self, frame1, frame2, stamp, timeout=0.5):
+        orientation = self.tf_buffer.lookup_transform(frame1, frame2, stamp, timeout=rospy.Duration(timeout)).transform.rotation
+        yaw_angle = (euler_from_quaternion((orientation.x, 
+                                            orientation.y, 
+                                            orientation.z, 
+                                            orientation.w))[2]) % (2 * math.pi)
+        return yaw_angle
     
     def getFilteredValue(self):
-        return self._vectorToAngle(self.filteredVector)
+        now = rospy.Time.now()
+        vector = self._odomOffset(self.filteredVector, self.currentTimestamp, now)
+        return self._vectorToAngle(vector)
