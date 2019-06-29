a8971e7e5eddf85bf7b11ab293b6b0085522bc69
Timon Engelke <timon.engelke@online.de>
2019-06-29 22:39:11 +1000

    pull_files: remove legacy vision models

--- scripts/pull_files.bash
+++ scripts/pull_files.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 META=$(dirname $(dirname $(realpath $0)))
-
+rm -rf $META/bitbots_vision/bitbots_vision/models/fcnn03 $META/bitbots_vision/bitbots_vision/models/fcnn031 $META/bitbots_vision/bitbots_vision/models/fcnn032 $META/bitbots_vision/bitbots_vision/models/classifier01
 wget -r -N -np -nH -P $META/bitbots_vision/bitbots_vision --reject "index.html*" "http://data.bit-bots.de/models/"
