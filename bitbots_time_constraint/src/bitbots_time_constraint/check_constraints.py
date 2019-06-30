149e33488442b2a06d155f369815d74e536e394e
Florian <florian@flova.de>
2019-06-30 10:33:21 +1000

    Add basic functions (tested)

--- bitbots_time_constraint/src/bitbots_time_constraint/check_constraints.py
+++ bitbots_time_constraint/src/bitbots_time_constraint/check_constraints.py
@@ -3,7 +3,9 @@
 
 import rospy
 import rostopic
+import rospkg
 import yaml
+import os
 
 class AutoSubscriber:
     """
@@ -45,54 +47,44 @@ class AutoSubscriber:
         return self.latest_timestamp
 
     def __message_callback(self, data):
-        self.latest_timestamp = rospy.Timer.now()
+        self.latest_timestamp = rospy.Time.now()
 
-
-class DummySubscriber:
-    """
-    Returns only the current ros time function
-    """
-
-    def __init__(self):
-        pass
-    
-    def get_timestamp(self):
-        return rospy.Time.now()
 
 class ConstraintChecker:
     def __init__(self):
-        with open("../config.yaml", 'r') as stream:
+        rospack = rospkg.RosPack()
+        self.package_path = rospack.get_path('bitbots_time_constraint')
+
+        rospy.init_node("bitbots_time_constraint", anonymous=False)
+
+        config_path = os.path.join(self.package_path, "config/constraints.yaml")
+
+        with open(config_path, 'r') as stream:
             try:
                 constraint_config = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 rospy.roserr("Config not found!!!!")
                 rospy.signal_shutdown("")
 
-        rate = constraint_config['check_rate']
-
-        self.r = rospy.Rate(rate)
+        self.rate = constraint_config['check_rate']
 
         self.constraints = constraint_config['constraints']
 
-        self._check_min_time(rate)
+        self._check_min_time(self.rate)
 
-        source_topics = set([element['source_topic'] for element in self.constraints])
-        destination_topic = set([element['destination_topic'] for element in self.constraints])
-
-        self.topics = source_topics.union(destination_topic).discard("NOW")
+        self.topics = set([element['source_topic'] for element in self.constraints])
 
         self.topic_auto_subscriber_dict = dict()
         for topic in self.topics:
             self.topic_auto_subscriber_dict[topic] = AutoSubscriber(topic)
-        
-        self.topic_auto_subscriber_dict["NOW"] = DummySubscriber()
 
         self._main_loop()
     
     def _main_loop(self):
+        r = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
             self._check_constraints()
-            self.r.sleep()
+            r.sleep()
 
     def _check_min_time(self, freq):
         min_time = 1/freq
@@ -101,26 +93,27 @@ class ConstraintChecker:
 
         if any(too_small_warn_times + too_small_error_times):
             rospy.logwarn("Warn delays too small for check freq.")
-
-    def _check_timestamps(self, source_timestamp, destination_timestamp, now, threshold):
-        return not ((source_timestamp is not None) and (now - source_timestamp >= rospy.Duration(threshold)) and not (now >= destination_timestamp) and (destination_timestamp >= source_timestamp))
     
     def _check_constraints(self):
         for constraint in self.constraints:
             source_topic = constraint['source_topic']
-            destination_topic = constraint['destination_topic']
             warn_delay = constraint['warn_delay']
             error_delay = constraint['error_delay']
-
+            now = rospy.Time.now()
+            
             source_cb = self.topic_auto_subscriber_dict[source_topic]
-            destination_cb = self.topic_auto_subscriber_dict[destination_topic]
 
             source_timestamp = source_cb.get_timestamp()
-            destination_timestamp = destination_cb.get_timestamp()
-
-            if not self._check_timestamps(source_timestamp, destination_timestamp, rospy.Time.now(), error_delay):
-                rospy.logerr("Time constraint not satisfied! {} is required {} Seconds after {}, but timestamps where {} and {}.".format(destination_topic, warn_delay, source_topic,destination_timestamp.to_sec(), source_timestamp.to_sec()))
-            elif not self._check_timestamps(source_timestamp, destination_timestamp, rospy.Time.now(), warn_delay):
-                rospy.logwarn("Time constraint not satisfied! {} is required {} Seconds after {}, but timestamps where {} and {}.".format(destination_topic, warn_delay, source_topic,destination_timestamp.to_sec(), source_timestamp.to_sec()))
 
 
+            if source_timestamp is not None:
+                delay = (now -  source_timestamp).to_sec()
+                if delay >= error_delay:
+                    rospy.logerr_throttle(5, "Time constraint not satisfied! {} Seconds passed after last {} Message!".format(delay, source_topic))
+                elif delay >= warn_delay:
+                    rospy.logwarn_throttle(5, "Time constraint not satisfied! {} Seconds passed after last {} Message!".format(delay, source_topic))
+            else:
+                rospy.logwarn_throttle(5, "Never received {}!".format(source_topic))
+
+if __name__ == "__main__":
+    checker = ConstraintChecker()
