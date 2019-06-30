4d42a9219c6df1d4912d0bfdf78f560df064ecca
Finn-Thorben Sell <dev@finn-thorben.me>
2019-06-30 10:18:30 +0000

    implement kick-action which uses bitboty_dynamic_kick

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/kick_ball.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/kick_ball.py
@@ -1,10 +1,14 @@
 import rospy
+from bitbots_msgs.msg import KickGoal
+from geometry_msgs.msg import Quaternion
+from tf.transformations import quaternion_from_euler
+
 from dynamic_stack_decider.abstract_action_element import AbstractActionElement
 
 
-class KickBall(AbstractActionElement):
+class KickBallStatic(AbstractActionElement):
     def __init__(self, blackboard, dsd, parameters=None):
-        super(KickBall, self).__init__(blackboard, dsd, parameters)
+        super(KickBallStatic, self).__init__(blackboard, dsd, parameters)
         if 'foot' not in parameters.keys():
             # usually, we kick with the right foot
             self.kick = 'kick_right'  # TODO get actual name of parameter from some config
@@ -18,6 +22,44 @@ class KickBall(AbstractActionElement):
     def perform(self, reevaluate=False):
         if not self.blackboard.animation.is_animation_busy():
             self.blackboard.animation.play_animation(self.kick)
+
+
+class KickBallDynamic(AbstractActionElement):
+    """
+    Kick the ball using bitbots_dynamic_kick
+    """
+    def __init__(self, blackboard, dsd, parameters=None):
+        super(KickBallDynamic, self).__init__(blackboard, dsd, parameters)
+        if 'foot' not in parameters.keys():
+            # usually, we kick with the right foot
+            self.kick = 'kick_right'  # TODO get actual name of parameter from some config
+        elif 'right' == parameters['foot']:
+            self.kick = 'kick_right'  # TODO get actual name of parameter from some config
+        elif 'left' == parameters['foot']:
+            self.kick = 'kick_left'  # TODO get actual name of parameter from some config
+        else:
+            rospy.logerr('The parameter \'{}\' could not be used to decide which foot should kick'.format(parameters['foot']))
+
+        self._goal_sent = False
+
+    def perform(self, reevaluate=False):
+        self.do_not_reevaluate()
+
+        if not self.blackboard.kick.is_currently_kicking:
+            if not self._goal_sent:
+                goal = KickGoal()
+                goal.header.stamp = rospy.Time.now()
+                goal.header.frame_id = "base_footprint" # get_ball_uv() always returns ball in base_footprint
+                goal.ball_position.x, goal.ball_position.y = self.blackboard.world_model.get_ball_position_uv()
+                goal.ball_position.z = 0
+                goal.kick_direction = Quaternion(*quaternion_from_euler(0, 0, -3.14/2))
+                goal.kick_speed = 1
+
+                self.blackboard.kick.kick(goal)
+                self._goal_sent = True
+
+            else:
+                self.pop()
 
 
 class KickBallVeryHard(AbstractActionElement):
