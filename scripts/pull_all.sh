6cfb30927cf2abeffe0fed27a4f5609a068039fd
Timon Engelke <timon.engelke@online.de>
2019-06-30 12:40:08 +1000

    pull_all: more helpful basler drivers error message

--- scripts/pull_all.sh
+++ scripts/pull_all.sh
@@ -9,6 +9,6 @@ git submodule foreach git pull
 if [[ -d basler_drivers ]]; then
     git -C basler_drivers pull
 else
-    echo -e "\n\n\033[91m\033[1mNo basler drivers found! Use 'make basler' to install them\033[0m\n"
+    echo -e "\n\n\033[91m\033[1mNo basler drivers found! Use 'make basler' to install them and restart this script afterwards\033[0m\n"
     exit 1
 fi
