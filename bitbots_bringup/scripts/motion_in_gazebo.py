bd0f8a76f989b1fcadb1bf9a43380ea095e0c12c
Timon Engelke <timon.engelke@online.de>
2019-07-01 11:26:02 +1000

    motion_in_gazebo: do not ignore command velocities

--- bitbots_bringup/scripts/motion_in_gazebo.py
+++ bitbots_bringup/scripts/motion_in_gazebo.py
@@ -9,36 +9,57 @@ from std_msgs.msg import Float64MultiArray
 from bitbots_msgs.msg import JointCommand
 
 
-
-if __name__ == "__main__":    
+if __name__ == "__main__":
     rospy.init_node('pub_goals_sim')
     joint_list = ['HeadPan', 'HeadTilt', 'LShoulderPitch', 'LShoulderRoll', 'LElbow', 'RShoulderPitch',
                   'RShoulderRoll', 'RElbow', 'LHipYaw', 'LHipRoll', 'LHipPitch', 'LKnee', 'LAnklePitch',
                   'LAnkleRoll', 'RHipYaw', 'RHipRoll', 'RHipPitch', 'RKnee', 'RAnklePitch', 'RAnkleRoll']
 
     positions = [0] * 20
-    
-    def map_data(d_msg):
-        msg = Float64MultiArray()    
-        d_joint_order = d_msg.joint_names
-        i = 0
-        # only update newly transmitted positions and retransmit all previos positions
-        # Gazebo expects us to always send everything
-        for joint in joint_list:
-            if joint in d_joint_order:
-                positions[i] =d_msg.positions[d_joint_order.index(joint)]
-            i += 1
+    update_time = rospy.Time.now()
+    joint_command = JointCommand()
+    joint_command.joint_names = joint_list
+    joint_command.positions = [0] * 20
+    joint_command.velocities = [-1] * 20
 
+    def update_gazebo_command(command):
+        # type: (JointCommand) -> Float64MultiArray
+        global update_time
+        msg = Float64MultiArray()
+        for name, position, velocity in zip(command.joint_names, command.positions, command.velocities):
+            if velocity == -1:
+                positions[joint_list.index(name)] = position
+            else:
+                old_pos = positions[joint_list.index(name)]
+                time_delta = (rospy.Time.now() - update_time).to_sec()
+                max_rad = time_delta * velocity
+                if position - old_pos > max_rad:
+                    new_pos = old_pos + max_rad
+                elif position - old_pos < -max_rad:
+                    new_pos = old_pos - max_rad
+                else:
+                    new_pos = position
+                positions[joint_list.index(name)] = new_pos
+        update_time = rospy.Time.now()
         msg.data = positions
-        goal_publisher.publish(msg)
+        return msg
 
-    goal_subscriber = rospy.Subscriber("DynamixelController/command", JointCommand, map_data, tcp_nodelay=True)
-    goal_publisher = rospy.Publisher('JointGroupController/command', Float64MultiArray,queue_size=10, tcp_nodelay=True)
+    def joint_command_cb(msg):
+        # type: (JointCommand) -> None
+        for i in range(len(msg.joint_names)):
+            name = msg.joint_names[i]
+            joint_command.positions[joint_list.index(name)] = msg.positions[i]
+            joint_command.velocities[joint_list.index(name)] = msg.velocities[i]
+
+
+    goal_subscriber = rospy.Subscriber("DynamixelController/command", JointCommand, joint_command_cb, tcp_nodelay=True)
+    goal_publisher = rospy.Publisher('JointGroupController/command', Float64MultiArray, queue_size=10, tcp_nodelay=True)
     rate = rospy.Rate(20)
-    rospy.sleep(1)
     while not rospy.is_shutdown():
         try:
             # catch exeption of moving backwarts in time, when restarting simulator
+            msg = update_gazebo_command(joint_command)
+            goal_publisher.publish(msg)
             rate.sleep()
         except rospy.exceptions.ROSTimeMovedBackwardsException:
             rospy.logwarn(
