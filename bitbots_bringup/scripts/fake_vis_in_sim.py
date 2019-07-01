5f4fec3ab0b47253ce7d0704a8e261478b4e353c
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2019-07-01 13:17:28 +1000

    fake viz in sim

--- bitbots_bringup/scripts/fake_vis_in_sim.py
+++ bitbots_bringup/scripts/fake_vis_in_sim.py
@@ -4,19 +4,18 @@ from gazebo_msgs.msg import ModelStates, ModelState
 import tf2_ros
 import rospy
 from sensor_msgs.msg import CameraInfo
-from geometry_msgs.msg import PoseStamped
+from tf2_geometry_msgs import PoseStamped
 import numpy as np
 
-rospy.init_node("fake_vision")
+rospy.init_node("fake_vis_in_sim")
 
-tf_buffer = tf2_ros.Buffer(10)
+tf_buffer = tf2_ros.Buffer(rospy.Duration(10))
 tf_listener = tf2_ros.TransformListener(tf_buffer)
 
 
 ball_pose = None
 goal1_pose = None
 goal2_pose = None
-
 cam_info = None
 
 def state_update(state_msg):
@@ -24,11 +23,14 @@ def state_update(state_msg):
     global goal1_pose
     global goal2_pose
     global tf_buffer
-    global  cam_info
+    global cam_info
+
+    if not cam_info:
+        return
 
     ball_index = 0
     for name in state_msg.name:
-        if name == "ball":
+        if name == "teensize_ball":
             break
         ball_index += 1
 
@@ -44,20 +46,22 @@ def state_update(state_msg):
         goal_index2 += 1
 
     ball_pose = state_msg.pose[ball_index]
+    rospy.logerr_throttle(1, state_msg.pose[ball_index])
     goal1_pose = state_msg.pose[goal_index1]
     goal2_pose = state_msg.pose[goal_index2]
 
     ball_pose_stamped = PoseStamped()
     ball_pose_stamped.header.stamp = rospy.Time.now()
-    ball_pose_stamped.header.frame_id = "world"
+    ball_pose_stamped.header.frame_id = "map"
     ball_pose_stamped.pose = ball_pose
 
-    ball_pose_stamped = tf_buffer.transform(ball_pose_stamped, cam_info.header.frame_id, rospy.Duration(0.1))
-    rospy.loginfo(ball_pose_stamped)
+    ball_pose_stamped = tf_buffer.transform(ball_pose_stamped, cam_info.header.frame_id, timeout=rospy.Duration(0.1))
 
-    p = [ball_pose_stamped.pose.x,ball_pose_stamped.pose.x,ball_pose_stamped.pose.x]
-    p_pixel = np.matmul(cam_info.K, p)
-    rospy.logwarn(p_pixel)
+    p = [ball_pose_stamped.pose.position.x, ball_pose_stamped.pose.position.y, ball_pose_stamped.pose.position.z]
+    k = np.reshape(cam_info.K, (3,3))
+    p_pixel = np.matmul(k, p)
+    p_pixel = p_pixel * (1/p_pixel[2])
+    rospy.logwarn_throttle(1, p_pixel)
 
 
 
@@ -68,14 +72,13 @@ def cam_info_cb(msg):
 
 
 if __name__ == "__main__":
-    rospy.init_node("fake_vis_in_sim")
 
     model_subscriber = rospy.Subscriber("/gazebo/model_states", ModelStates, state_update, tcp_nodelay=True)
-    cam_info_sub = rospy.Subscriber("/camera/camera_info", CameraInfo, cam_info_cb)
+    cam_info_sub = rospy.Subscriber("/camera_info", CameraInfo, cam_info_cb)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         try:
-
+            rospy.spin()
             rate.sleep()
         except rospy.exceptions.ROSTimeMovedBackwardsException:
             rospy.logwarn(
