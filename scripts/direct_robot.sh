94493b1fce3ba350451815f5779ba1eca856c519
Judith <2hartfil@informatik.uni-hamburg.de>
2019-07-02 22:19:46 +1000

    direct robot: allow IPs in 192.168.0.0./22

--- scripts/direct_robot.sh
+++ scripts/direct_robot.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && echo "Please use \"source ${BASH_SOURCE[0]}\"" && exit 1
-export ROS_IP=`ip a | grep -F 'inet 192.168.1.' | awk '{print $2}' | cut -d '/' -f 1`
+export ROS_IP=`ip a | grep 'inet 192\.168\.[123]\.' | awk '{print $2}' | cut -d '/' -f 1`
 echo "Set \$ROS_IP to ${ROS_IP}"
 echo "To deactivate use \"unset ROS_IP\""
