ef0325e904bd10e12c2b071fe546d1a159e088df
Judith <2hartfil@informatik.uni-hamburg.de>
2019-07-02 22:27:31 +1000

    fix for not starting to walk and fix for positioning in ready state

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to.py
@@ -45,6 +45,7 @@ class GoToRelativePosition(AbstractActionElement):
 
             # To have the object we are going to in front of us, go to a point behind it
             self.blackboard.pathfinding.publish(pose_msg)
+            rospy.sleep(0.25)
         if not self.blackboard.blackboard.is_currently_walking():
             self.pop()
 
