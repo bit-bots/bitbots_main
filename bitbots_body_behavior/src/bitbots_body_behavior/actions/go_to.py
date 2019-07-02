f2bab72cb86cac37b18ac891b57acdffe279ece6
Tanja <5flemmin@informatik.uni-hamburg.de>
2019-07-02 08:50:36 +0200

    added pop in got to relative position to reevaluate the unpenalized decision when goal is reached

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to.py
@@ -45,6 +45,8 @@ class GoToRelativePosition(AbstractActionElement):
 
             # To have the object we are going to in front of us, go to a point behind it
             self.blackboard.pathfinding.publish(pose_msg)
+        if not self.blackboard.blackboard.is_currently_walking():
+            self.pop()
 
 
 class GoToAbsolutePosition(AbstractActionElement):
