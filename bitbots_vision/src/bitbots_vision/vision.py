1975ec9e7862fe54c6d203d1ec26fe727eb5b993
Hendrik Brandt <baw4626@studium.uni-hamburg.de>
2019-07-29 08:11:07 +0200

    merged master on branch

--- bitbots_vision/src/bitbots_vision/vision.py
+++ bitbots_vision/src/bitbots_vision/vision.py
@@ -13,7 +13,7 @@ from dynamic_reconfigure.encoding import Config as DynamicReconfigureConfig
 from sensor_msgs.msg import Image, JointState
 from humanoid_league_msgs.msg import BallInImage, BallsInImage, LineInformationInImage, \
     LineSegmentInImage, ObstaclesInImage, ObstacleInImage, ImageWithRegionOfInterest, GoalPartsInImage, PostInImage, \
-    GoalInImage, FieldBoundaryInImage
+    GoalInImage, FieldBoundaryInImage, Speak
 from bitbots_vision.vision_modules import lines, field_boundary, color, debug, live_classifier, \
     classifier, ball, fcnn_handler, live_fcnn_03, dummy_ballfinder, obstacle, evaluator, yolo_handler
 from bitbots_vision.cfg import VisionConfig
