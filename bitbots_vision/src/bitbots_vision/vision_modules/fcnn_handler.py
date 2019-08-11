7f72352bcb75d1b25d28a583032070f8146ea759
Florian Vahl <florian@flova.de>
2019-08-11 15:45:34 +0200

    Refactoring the fcnn handler

--- bitbots_vision/src/bitbots_vision/vision_modules/fcnn_handler.py
+++ bitbots_vision/src/bitbots_vision/vision_modules/fcnn_handler.py
@@ -17,7 +17,7 @@ class FcnnHandler(BallDetector):
     Example configuration (put these values into the config dictionary or just
     copy them into the vision package config):
 
-    ball_fcnn:
+    ball_fcnn: TODO fcnn config improvement
         publish_debug_img: false  # toggles publishing of the fcnn heatmap image for debug purposes
         model_path: '/models/2019_05_timon_basler'
         threshold: .6  # minimal value for a candidate to be considered
@@ -31,6 +31,11 @@ class FcnnHandler(BallDetector):
     """
 
     def __init__(self, fcnn, field_boundary_detector, config):
+        """
+        Inits the fcnn handler.
+        :param fcnn: a fcnn model
+        :param config: TODO
+        """
         self._image = None
         self._fcnn = fcnn
         self._field_boundary_detector = field_boundary_detector
@@ -44,6 +49,10 @@ class FcnnHandler(BallDetector):
 
 
     def set_image(self, image):
+        """
+        Set a image for the fcnn. This also resets the caches.
+        :param image: current vision image
+        """
         self._image = image
         self._rated_candidates = None
         self._sorted_rated_candidates = None
@@ -52,6 +61,7 @@ class FcnnHandler(BallDetector):
 
 
     def set_config(self, config):
+        # TODO new
         self._debug = config['debug']
         self._threshold = config['threshold']  # minimal activation
         self._expand_stepsize = config['expand_stepsize']  #
@@ -66,13 +76,18 @@ class FcnnHandler(BallDetector):
 
     def get_candidates(self):
         """
-        candidates are a list of tuples ((candidate),rating)
-        :return:
+        Returns all ball candidates. This method is cached.
+        :return: all ball candidates
         """
+        # Check if a cached value exists
         if self._rated_candidates is None:
+            # Create candidate list
             self._rated_candidates = list()
+            # Run neural network and clustering and iterate over the given candidates
             for candidate in self._get_raw_candidates_cpp():
+                # Get the fcnn heatmap
                 out = self.get_fcnn_output()
+                # Calculate the mean in the ROI in the heatmap
                 candidate.rating = np.mean(
                     out[
                         candidate.get_upper_left_y():
@@ -80,11 +95,18 @@ class FcnnHandler(BallDetector):
                         candidate.get_upper_left_x():
                         candidate.get_upper_left_x() + candidate.get_width()]
                 ) / 255.0
+                # Check if candidate is in rating threshold and size bounds
                 if self.inspect_candidate(candidate):
+                    # Add candidate to list
                     self._rated_candidates.append(candidate)
         return self._rated_candidates
 
     def inspect_candidate(self, candidate):
+        """
+        Checks if candidates is in threshold. And in min/max diameter bounds.
+        :param candidate: a Ball candidate
+        :return: a boolean if the candidate satisfies these conditions
+        """
         # type: (Candidate) -> bool
         return candidate.rating >= self._threshold \
                and self._min_candidate_diameter \
@@ -100,144 +122,79 @@ class FcnnHandler(BallDetector):
 
     def get_top_candidates(self, count=1):
         """
-        Returns the count best candidates. When you use this, using
-        get_top_candidate is wrong. use it always, when you use it.
+        Returns the 'count' best candidates.
         :param count: Number of top-candidates to return
         :return: the count top candidates
         """
         if count < 1:
             raise ValueError('the count must be equal or greater 1!')
+        # Check if cached
         if self._sorted_rated_candidates is None:
-            self._sorted_rated_candidates = sorted(self.get_candidates(), key=lambda x: x.rating)
+            # Sort candidates by rating
+            self._sorted_rated_candidates = sorted(self.get_candidates(), key=lambda x: x.get_rating())
         return self._sorted_rated_candidates[0:count]
 
     def get_fcnn_output(self):
+        """
+        Calculates the fcnn heatmap. The output gets cached.
+        :return: fcnn output
+        """
+        # Check if a cached one exists
         if self._fcnn_output is None:
+            # Resize image for fcnn
             in_img = cv2.resize(self._image, (self._fcnn.input_shape[1], self._fcnn.input_shape[0]))
+            # Convert image to floats
             in_img = cv2.cvtColor(in_img, cv2.COLOR_BGR2RGB).astype(np.float32) / 255.0
+            # Predict
             out = self._fcnn.predict(list([in_img]))
+            # Reshape fcnn output
             out = out.reshape(self._fcnn.output_shape[0], self._fcnn.output_shape[1])
+            # Convert back to uint8 dtype
             out = (out * 255).astype(np.uint8)
+            # Resize the heatmap to match the resolution of the in comming image
             self._fcnn_output = cv2.resize(out, (self._image.shape[1], self._image.shape[0]))
         return self._fcnn_output
 
     def _get_raw_candidates_cpp(self):
-
+        """
+        Runs the fcnn heatmap clustering candidate detection.
+        :return: ball candidates
+        """
         start = cv2.getTickCount()
+        # Get fcnn output
         out = self.get_fcnn_output()
         end = cv2.getTickCount()
         rospy.logdebug('Vision FCNN handler: Net:' + str((end - start) / cv2.getTickFrequency()))
         start = cv2.getTickCount()
+        # Mask the heatmap with a threshold
         r, out_bin = cv2.threshold(out, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # Run cpp vision extention to find clusters in the heatmap
         tuple_candidates = VisionExtensions.findSpots(out_bin, self._pointcloud_stepsize, self._expand_stepsize, self._candidate_refinement_iteration_count)
         candidates = list()
         rospy.logdebug('Vision FCNN handler: ' + str(len(tuple_candidates)))
+        # Convert output tuples to candidates
         for candidate in tuple_candidates:
-            # calculate final width and height
+            # Calculate final width and height
             width, height = candidate[0] - candidate[1], candidate[3] - candidate[2]
             candidates.append(Candidate(candidate[1], candidate[2], width, height))
         end = cv2.getTickCount()
         rospy.logdebug('Vision FCNN handler: Cluster:' + str((end - start) / cv2.getTickFrequency()))
         return candidates
 
-    def _get_raw_candidates(self):
-        """
-        returns a list of candidates [(Candidate), ...]
-        :return: a list of candidates [(Candidate), ...]
-        """
-        out = self.get_fcnn_output()
-        r, out_bin = cv2.threshold(out, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        candidates = list()
-        # creating points
-        # x shape
-        xshape = self._image.shape[1]
-        xlist = []
-        x = 0
-        while x < xshape:
-            xlist.append(x)
-            x += self._pointcloud_stepsize
-        # y shape
-        yshape = self._image.shape[0]
-        ylist = []
-        y = 0
-        while y < yshape:
-            ylist.append(y)
-            y += self._pointcloud_stepsize
-        # generate carthesian product of list
-        points = list(itertools.product(xlist, ylist))
-        if self._shuffle_candidate_list:
-            random.shuffle(points)
-        # expand points
-        while points:
-            point = points[-1]
-            lx, uy = point
-            rx, ly = point
-            # expand to the left
-            if not out_bin[point[1]][point[0]]:
-                points.remove(point)
-                continue
-            next_lx = max(lx - self._expand_stepsize, 0)
-            while next_lx > 0 and out_bin[point[1]][next_lx]:
-                lx = next_lx
-                next_lx = max(lx - self._expand_stepsize, 0)
-            # expand to the right
-            next_rx = min(rx + self._expand_stepsize, out_bin.shape[1] - 1)
-            while next_rx < out_bin.shape[1] - 1 and out_bin[point[1]][next_rx]:
-                rx = next_rx
-                next_rx = min(rx + self._expand_stepsize, out_bin.shape[1] - 1)
-            # expand upwards
-            next_uy = max(uy - self._expand_stepsize, 0)
-            while next_uy > 0 and out_bin[next_uy][point[0]]:
-                uy = next_uy
-                next_uy = max(uy - self._expand_stepsize, 0)
-            # expand downwards (the lowest y is the highest number for y)
-            next_ly = min(ly + self._expand_stepsize, out_bin.shape[0] - 1)
-            while next_ly < out_bin.shape[0] - 1 and out_bin[next_ly][point[0]]:
-                ly = next_ly
-                next_ly = min(ly + self._expand_stepsize, out_bin.shape[0] - 1)
-            for i in range(self._candidate_refinement_iteration_count):
-                # expand from the middle of the borders of the found candidate
-                width, height = rx - lx, ly - uy
-
-                buffer_x = lx + width // 2
-                buffer_y = uy + height // 2
-
-                # expand to the left
-                next_lx = max(lx - self._expand_stepsize, 0)
-                while next_lx > 0 and out_bin[buffer_y][next_lx]:
-                    lx = next_lx
-                    next_lx = max(lx - self._expand_stepsize, 0)
-
-                # expand to the right
-                next_rx = min(rx + self._expand_stepsize, out_bin.shape[1] - 1)
-                while next_rx < out_bin.shape[1] - 1 and out_bin[buffer_y][next_rx]:
-                    rx = next_rx
-                    next_rx = min(rx + self._expand_stepsize, out_bin.shape[1] - 1)
-
-                # expand upwards
-                next_uy = max(uy - self._expand_stepsize, 0)
-                while next_uy > 0 and out_bin[next_uy][buffer_x]:
-                    uy = next_uy
-                    next_uy = max(uy - self._expand_stepsize, 0)
-
-                # expand downwards
-                next_ly = min(ly + self._expand_stepsize, out_bin.shape[0] - 1)
-                while next_ly < out_bin.shape[0] - 1 and out_bin[next_ly][buffer_x]:
-                    ly = next_ly
-                    next_ly = min(ly + self._expand_stepsize, out_bin.shape[0] - 1)
-
-            # calculate final width and height
-            width, height = rx - lx, ly - uy
-            candidates.append(Candidate(lx, uy, width, height))
-            points.remove(point)
-            points = [other_point for other_point in points if not (lx <= other_point[0] <= rx and uy <= other_point[1] <= ly)]
-        return candidates
-
     def get_debug_image(self):
+        """
+        Returns the fcnn heatmap as ros image message if debug is enabled.
+        :return: fcnn heatmap
+        """
         if self._debug:
+            # Create image message with fcnn heatmap
             return self.bridge.cv2_to_imgmsg(self.get_fcnn_output(), "mono8")
 
     def get_cropped_msg(self):
+        """
+        Returns a region of interest with the fcnn heatmap under the field boundary.
+        :return: fcnn heatmap under the field boundary
+        """
         msg = ImageWithRegionOfInterest()
         msg.header.frame_id = 'camera'
         msg.header.stamp = rospy.get_rostime()
