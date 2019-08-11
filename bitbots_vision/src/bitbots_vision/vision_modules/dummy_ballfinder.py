dfe11c02eb41daea8a8132d58747d9f37393abf3
Florian Vahl <florian@flova.de>
2019-08-11 14:39:23 +0200

    Add docu

--- bitbots_vision/src/bitbots_vision/vision_modules/dummy_ballfinder.py
+++ bitbots_vision/src/bitbots_vision/vision_modules/dummy_ballfinder.py
@@ -2,19 +2,39 @@ from .candidate import CandidateFinder
 
 
 class DummyClassifier(BallDetector):
-    def __init__(self, classifier, stuff):
-        self._classified_candidates = []
+    """
+    Dummy ball detector that we use if we want the run vision to without neural network e.g..
+    """
         self._sorted_candidates = []
         self._top_candidate = None
 
     def set_image(self, image):
+        """
+        Method to satisfy the interface.
+        Actually does nothing.
+        :param image: current vision image
+        """
         pass
 
     def compute_top_candidate(self):
+        """
+        Method to satisfy the interface.
+        Actually does nothing, except the extrem complicated command 'pass'.
+        """
         pass
 
     def get_candidates(self):
-        return self._classified_candidates
+        """
+        Method to satisfy the interface.
+        Actually does something. It returns an empty list.
+        :return: a empty list
+        """
 
     def get_top_candidates(self, count=1):
+        """
+        Method to satisfy the interface.
+        It returns an empty list.
+        :param count: how many of zero top candidates do you want?
+        :return: a empty list
+        """
         return self._sorted_candidates
