7251ae9bf20f2766a96e36fa1ec125594f66faca
Florian Vahl <florian@flova.de>
2019-08-11 14:41:34 +0200

    Style docu

--- bitbots_vision/src/bitbots_vision/vision_modules/evaluator.py
+++ bitbots_vision/src/bitbots_vision/vision_modules/evaluator.py
@@ -8,8 +8,8 @@ class RuntimeEvaluator:
     def __init__(self, name="Runtime", queue_size=100):
         # type: (str, int) -> None
         """
-        calculates the average time a method (e.g. get_candidates) takes to work on an image
-        allows improved evaluation and comparison of different methods
+        Calculates the average time a method (e.g. get_candidates) takes to work on an image.
+        Allows improved evaluation and comparison of different methods.
         :param name: name of the evaluator, allow the identification of the printed results
         :param queue_size: amount of measurements used to calculate the average
         """
@@ -28,7 +28,7 @@ class RuntimeEvaluator:
     def set_image(self, image=None):
         # type: (None) -> None
         """
-        resets all variable once the time should be measured for a new picture
+        Resets all variable once the time should be measured for a new picture.
         :param image: we don't use this, but every set_image method of other classes has this parameter
         """
         self.timer_running = False
@@ -39,7 +39,7 @@ class RuntimeEvaluator:
     def start_timer(self):  # -> call this method before the method that should be measured
         # type: () -> None
         """
-        starts the timer if the timer isn't running already
+        Starts the timer if the timer isn't running already.
         """
         if not self.timer_running:  # failsafe in case start_timer is called multiple times before stop_timer
             self.start_time = cv2.getTickCount()
@@ -48,8 +48,8 @@ class RuntimeEvaluator:
     def stop_timer(self):  # -> call this method after the method that should be measured
         # type: () -> None
         """
-        stops the timer and calculates the past time since the start of the timer
-        adds this measurement of time to the queue
+        Stops the timer and calculates the past time since the start of the timer.
+        Adds this measurement of time to the queue.
         """
         self.stop_time = cv2.getTickCount()
         self.timer_running = False
@@ -61,7 +61,7 @@ class RuntimeEvaluator:
     def _append_queue(self):
         # type: () -> None
         """
-        adds a new runtime measurement to the queue while considering the max queue_size
+        Adds a new runtime measurement to the queue while considering the max queue_size.
         """
         if self.last_measurement is not None:  # failsafe
             q = self.queue
@@ -73,15 +73,14 @@ class RuntimeEvaluator:
     def reset_queue(self):
         # type: () -> None
         """
-        resets the queue by creating a new empty queue
+        Resets the queue by creating a new empty queue.
         """
         self.queue = deque()
 
     def print_timer(self):
         # type: () -> None
         """
-        calculates the average of all measurements in the queue once enough measurements are collected
-         and prints the result
+        Calculates the average of all measurements in the queue once enough measurements are collected and prints the result.
         """
         # the results are only printed out after we collected enough measurements:
         rospy.loginfo("Vision runtime evaluator: {} Progress:".format(self.name)+str(self.count+1)+"/"+str(self.queue_size))
