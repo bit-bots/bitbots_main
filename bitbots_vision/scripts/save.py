74b407104ff2b561acd6ca6fe59af61044769826
Flova <florian@flova.de>
2019-09-13 15:01:39 +0200

    Add ignoring debug params (#85)

--- bitbots_vision/scripts/save.py
+++ bitbots_vision/scripts/save.py
@@ -2,6 +2,7 @@
 import os
 import rospy
 import rospkg
+import argparse
 # Use ruamel.yaml to be comment/order persistent
 from ruamel.yaml import YAML
 
@@ -9,11 +10,24 @@ from ruamel.yaml import YAML
 A script to save dynamic reconfigured params into the visionparams.yaml
 """
 
+# Parameters for debug purposes that should be ignored if the -id or --ignore-debug flag is set
+debug_params = [
+    'ball_fcnn_publish_debug_img',
+    'vision_publish_debug_image',
+    'vision_publish_field_mask_image',
+    'dynamic_color_space_publish_field_mask_image',
+]
+
 # Compatibility
 try:
     input = raw_input
 except:
     pass
+
+# Argument parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--ignore-debug", help="Ignore debug params", action='store_true')
+args = parser.parse_args()
 
 # Set yaml stuff
 yaml = YAML()
@@ -34,8 +48,8 @@ changed_params = 0
 for key in data.keys():
     # Get the current key value from the parameter server
     new_value = rospy.get_param("/bitbots_vision/{}".format(str(key)))
-    # Check if param changed
-    if new_value != data[key]:
+    # Check if param changed or if it's a debug parameter that should be ignored
+    if new_value != data[key] and (not args.ignore_debug or not key in debug_params):
         data[key] = new_value
         changed_params += 1
         print("{}:{}".format(key, new_value))
