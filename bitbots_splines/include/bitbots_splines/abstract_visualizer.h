5865172ca547e60c86ea8fc37db72b472703a1d1
Timon Engelke <timon.engelke@online.de>
2019-09-18 17:41:19 +0200

    bitbots_splines: add getPath function to AbstractVisualizer

--- bitbots_splines/include/bitbots_splines/abstract_visualizer.h
+++ bitbots_splines/include/bitbots_splines/abstract_visualizer.h
@@ -10,7 +10,7 @@ namespace bitbots_splines {
 class AbstractVisualizer {
  protected:
   /**
-   * Utility function to create a visualization marker with default properties, i.e. spheric, 3cm wide, white.
+   * Utility function to create a visualization marker for a position with default properties.
    * @param position The position of the marker
    * @param frame The frame in which the position is given
    * @return The visualization marker
@@ -32,6 +32,49 @@ class AbstractVisualizer {
     marker.scale.y = 0.03;
     marker.scale.z = 0.03;
     marker.color.a = 1;
+
+    return marker;
+  }
+
+  /**
+   * Utility function to create a visualization marker for a trajectory with default properties.
+   * @param splines A container containing the splines to be displayed
+   * @param frame The frame in which the splines are given
+   * @param key_x The key to access the x spline from the splines parameter
+   * @param key_y The key to access the y spline from the splines parameter
+   * @param key_z The key to access the z spline from the splines parameter
+   * @param smoothness The smoothness of the splines
+   * @return The visualization marker
+   */
+  visualization_msgs::Marker getPath(const Trajectories &splines,
+                                     const std::string &frame,
+                                     const std::string &key_x,
+                                     const std::string &key_y,
+                                     const std::string &key_z,
+                                     const double smoothness) {
+    visualization_msgs::Marker marker;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.type = visualization_msgs::Marker::LINE_STRIP;
+    marker.lifetime = ros::Duration(1000);
+    marker.frame_locked = false;
+    marker.header.frame_id = frame;
+    marker.header.stamp = ros::Time::now();
+    marker.pose.orientation.w = 1;
+    marker.scale.x = 0.01;
+    marker.color.a = 1;
+
+    std::vector<double> times = splines.getTimes();
+    double first_time = times[0];
+    double last_time = times[times.size() - 1];
+
+    for (double i = first_time; i <= last_time; i += (last_time - first_time) / smoothness) {
+      geometry_msgs::Point point;
+      point.x = splines.get(key_x).pos(i);
+      point.y = splines.get(key_y).pos(i);
+      point.z = splines.get(key_z).pos(i);
+
+      marker.points.push_back(point);
+    }
 
     return marker;
   }
