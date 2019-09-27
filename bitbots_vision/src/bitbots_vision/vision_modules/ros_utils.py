589a6396007a806e7eb7b2b976408a0362d9d149
Florian Vahl <florian@flova.de>
2019-09-27 15:49:00 +0200

    Vision: Remove last occurencies of  the sim path

--- bitbots_vision/src/bitbots_vision/vision_modules/ros_utils.py
+++ bitbots_vision/src/bitbots_vision/vision_modules/ros_utils.py
@@ -171,7 +171,6 @@ def add_color_space_enums(cfg_type, package_path):
 
     # Add enums to configuration
     _change_enum_items(cfg_type, 'field_color_detector_path', field_color_space_enum)
-    _change_enum_items(cfg_type, 'field_color_detector_path_sim', field_color_space_enum)
 
 def create_or_update_publisher(old_config, new_config, publisher_object, topic_key, data_class, subscriber_listener=None, tcp_nodelay=False, latch=False, headers=None, queue_size=1):
     """
