9dc4f94f6374875254c3c5402bf705ee8713e585
Timon Engelke <timon.engelke@online.de>
2019-09-30 14:36:55 +0200

    direct_robot: more helpful error messages

--- scripts/direct_robot.sh
+++ scripts/direct_robot.sh
@@ -1,5 +1,19 @@
 #! /bin/bash
-[[ "${BASH_SOURCE[0]}" == "${0}" ]] && echo -e "This script can be used to connect directly to the robot.\nIt sets the \$ROS_IP variable. See http://wiki.ros.org/ROS/NetworkSetup for more information.\n\n\033[1m\033[91mUse \"source ${BASH_SOURCE[0]}\" instead of executing the script directly.\033[0m" && exit 1
-export ROS_IP=`ip a | grep 'inet 192\.168\.[123]\.' | awk '{print $2}' | cut -d '/' -f 1`
-echo "Set \$ROS_IP to ${ROS_IP}"
-echo "To deactivate use \"unset ROS_IP\""
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+   echo "This script can be used to connect directly to the robot."
+   echo "It sets the \$ROS_IP variable. See http://wiki.ros.org/ROS/NetworkSetup for more information."
+   echo
+   echo -e "\033[1m\033[91mUse \"source ${BASH_SOURCE[0]}\" instead of executing the script directly.\033[0m"
+   exit 1
+fi
+
+_IP=$(ip a | grep 'inet 192\.168\.[123]\.' | awk '{print $2}' | cut -d '/' -f 1)
+
+if [[ -z $_IP ]]; then
+    echo "No network available"
+    unset _IP
+else
+    export ROS_IP=$_IP
+    echo "Set \$ROS_IP to ${ROS_IP}"
+    echo "To deactivate use \"unset ROS_IP\""
+fi
