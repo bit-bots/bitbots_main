537222dcbda69133e5cb1b675535c0ae6490d96c
Florian Vahl <florian@flova.de>
2019-10-01 18:46:45 +0200

    Bringup: Improve launch on remote

--- bitbots_bringup/scripts/launch_on_remote.sh
+++ bitbots_bringup/scripts/launch_on_remote.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ssh -t -t jetson 'export ROS_MASTER_URI=http://nuc:11311/; zsh -o HUP -c "roslaunch bitbots_vision vision_startup.launch debug:=true"'
+# 1 param is the remote device
+# 2 param is the ros master
+# 3 param is the ros_package
+# 4 param is the launch file
+# Following params are the roslaunch arguments
+
+ssh -t -t $0 'export ROS_MASTER_URI=http://$1:11311/; zsh -o HUP -c "roslaunch ${@:2}"'
