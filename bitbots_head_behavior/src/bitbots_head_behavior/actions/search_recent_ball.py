9d3765065bd609322012fb60efe08329bbc9d1f6
Florian Vahl <florian@flova.de>
2019-10-15 12:24:45 +0200

    Head Bahvior: Rework recent ball search

--- bitbots_head_behavior/src/bitbots_head_behavior/actions/search_recent_ball.py
+++ bitbots_head_behavior/src/bitbots_head_behavior/actions/search_recent_ball.py
@@ -14,31 +14,47 @@ class SearchRecentBall(AbstractLookAt):
 
     def __init__(self, dsd, blackboard, parameters=None):
         super(SearchRecentBall, self).__init__(dsd, blackboard, parameters)
-        self.config = self.blackboard.config['search_recent_ball']
+        self._config = self.blackboard.config['search_recent_ball']
 
-        self.pan_speed = self.config['pan_speed']
-        self.tilt_speed = self.config['tilt_speed']
+        self._pan_speed = self._config['pan_speed']
+        self._tilt_speed = self._config['tilt_speed']
 
-        self.offset_pattern = self.config['offset_pattern']
+        self._ball_time_out = rospy.Duration.from_sec(self._config['ball_search_time'])
 
-        self.threshold = self.blackboard.config['position_reached_threshold']
+        self._offset_pattern = self._config['offset_pattern']
 
-    def perform(self, reevaluate=False):
+        self._threshold = self.blackboard.config['position_reached_threshold']
+
+        # Get the coresponding motor goals for the ball position
+        self._recent_ball_motor_goals = self._get_head_goals_for_recent_ball()
+
+        # Check if a ball exists
+        if self._recent_ball_motor_goals is None:
+            rospy.loginfo("No ball seen. So we are not able to seaarch for it.", logger_name="search_recent_ball")
+            return self.pop()
+
+        # Check if the ball is too old
+        if rospy.Time.now() - self.blackboard.world_model.ball_last_seen() > self._ball_time_out):
+            rospy.loginfo("Ball is too old to search for it. Let's forget it.", logger_name="search_recent_ball")
+            return self.pop()
+
+        # Init pattern index
+        self.index = 0
+
+    def _get_head_goals_for_recent_ball(self):
         """
-        Call look_at to look at the point which our world-model determines to be the ball
+        Returns the head motor goals to look at the most recent ball position.
 
-        :param reevaluate: No effect here
+        :retruns tuple(head_pan, head_tilt): The head motor goals
         """
-        # Get last ball position
+        # Check if Ball has been seen
+        if not self.blackboard.world_model.ball_seen:
+            return
+
+         # Get last ball position
         point = self.blackboard.world_model.get_ball_stamped()
 
-        index, ball_id = self.blackboard.head_capsule.ball_specific_pattern_index
-
-        if ball_id != hash(point):
-            self.blackboard.head_capsule.ball_specific_pattern_index = (0, hash(point))
-            index = 0
-
-        # transform the points reference frame to be the head
+        # Transform the points reference frame to be the head
         try:
             point = self.blackboard.head_capsule.tf_buffer.transform(point, self.head_tf_frame, timeout=rospy.Duration(0.9))
         except tf2.LookupException as e:
@@ -51,17 +67,31 @@ class SearchRecentBall(AbstractLookAt):
             rospy.logwarn('The transform {} is currently not available (ExtrapolationException)'.format(self.head_tf_frame))
             return
 
-        head_pan, head_tilt = self.get_motor_goals_from_point(point.point)
+        motor_goals = self.get_motor_goals_from_point(point.point)
+        return motor_goals
+
+    def perform(self, reevaluate=False):
+        """
+        Call look_at to look at the point which our world-model determines to be the ball
+
+        :param reevaluate: No effect here
+        """
 
         current_head_pan, current_head_tilt = self.blackboard.head_capsule.get_head_position()
 
-        current_head_pan += self.offset_pattern[index]
-        current_head_tilt += self.offset_pattern[index]
+        # Add offset pattern to last ball position
+        head_motor_goal_pan = self._recent_ball_motor_goals + self._offset_pattern[self.index][0]
+        head_motor_goal_tilt = self._recent_ball_motor_goals + self._offset_pattern[self.index][1]
 
-        self.blackboard.head_capsule.send_motor_goals(current_head_pan, current_head_tilt, pan_speed=self.pan_speed, tilt_speed=self.tilt_speed)
+        self.blackboard.head_capsule.send_motor_goals(head_motor_goal_pan, head_motor_goal_tilt, pan_speed=self._pan_speed, tilt_speed=self._tilt_speed)
 
-        distance = math.sqrt((current_head_pan - head_pan) ** 2 + (current_head_tilt - head_tilt) ** 2)
+        # Distance between the current and the goal position
+        distance = math.sqrt((current_head_pan - head_motor_goal_pan) ** 2 + (current_head_tilt - head_motor_goal_tilt) ** 2)
+
+        # Exit action if pattern is finished
+        if self.index >= len(self._offset_pattern):
+            return self.pop()
 
         # Increment index when position is reached
-        if distance < math.radians(self.threshold) and index < len(self.offset_pattern):
-            self.blackboard.head_capsule.pattern_index = index + 1
+        if distance < math.radians(self._threshold):
+            self.index += 1
