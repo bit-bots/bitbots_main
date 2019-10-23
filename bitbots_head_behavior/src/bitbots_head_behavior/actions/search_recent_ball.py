61130497536150cc76875d2e98cb5e9ac7f4ec15
Florian Vahl <florian@flova.de>
2019-10-23 13:52:08 +0200

    Remove TODO

--- bitbots_head_behavior/src/bitbots_head_behavior/actions/search_recent_ball.py
+++ bitbots_head_behavior/src/bitbots_head_behavior/actions/search_recent_ball.py
@@ -19,7 +19,6 @@ class SearchRecentBall(AbstractLookAt):
         self._pan_speed = self._config['pan_speed']
         self._tilt_speed = self._config['tilt_speed']
 
-        # TODO param
         self._ball_time_out = rospy.Duration.from_sec(self._config['ball_search_time'])
 
         self._offset_pattern = self._config['offset_pattern']
