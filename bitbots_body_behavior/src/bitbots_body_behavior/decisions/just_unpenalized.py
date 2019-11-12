ecf523266256c466f5db002f292f83bc86c70313
Finn-Thorben Sell <dev@finn-thorben.me>
2019-11-12 18:31:50 +0100

    body_behavior: fix just_unpenalized visualization bug

--- bitbots_body_behavior/src/bitbots_body_behavior/decisions/just_unpenalized.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/just_unpenalized.py
@@ -4,7 +4,7 @@ from dynamic_stack_decider.abstract_decision_element import AbstractDecisionElem
 
 class JustUnpenalized(AbstractDecisionElement):
     def perform(self, reevaluate=False):
-        self._debug_data = self.blackboard.gamestate.get_seconds_since_unpenalized()
+        self.publish_debug_data("Seconds since unpenalized", self.blackboard.gamestate.get_seconds_since_unpenalized())
         if self.blackboard.gamestate.get_seconds_since_unpenalized() < 1:
             return 'YES'
         else:
