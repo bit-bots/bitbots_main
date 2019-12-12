eb49668d2c28013fd54a34c83dbd06db70f3decd
Florian Vahl <florian@flova.de>
2019-12-12 13:00:36 +0100

    Colorpicker: Rework colorpicker as class

--- bitbots_vision_tools/scripts/colorpicker.py
+++ bitbots_vision_tools/scripts/colorpicker.py
@@ -10,220 +10,243 @@ import VisionExtensions
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 
-rospy.init_node('bitbots_colorpicker')
-rospy.loginfo('Initializing colorpicker...', logger_name="colorpicker")
-_cv_bridge = CvBridge()
 
-
-def click_and_crop(event, x, y, flags, param):
+class Colorpicker(object):
     """
-    Callback for the OpenCV cursor.
-
-    :param event: Event type
-    :param x: Mouse x position
-    :param y: Mouse y position
-    :param flags: Callback flags
-    :param param: Some unused parameter
+    The bitbots_colorpicker node. Which is used to select colors that occur in the field.
     """
-    global coord, left_click, undo_click
+    def __init__(self):
+        rospy.init_node('bitbots_colorpicker')
+        rospy.loginfo('Initializing colorpicker...', logger_name="colorpicker")
+        self._cv_bridge = CvBridge()
+        self._image = np.zeros((300,300,3), dtype=np.uint8)
 
-    if flags == (cv2.EVENT_FLAG_SHIFTKEY + cv2.EVENT_FLAG_LBUTTON):
-        # Substracts colors
-        undo_click = True
-    elif event == cv2.EVENT_LBUTTONUP:
-        # Adds colors
-        left_click = True
+        # Subscribe to Image-message
+        self._sub_image_msg = rospy.Subscriber(
+            "image_raw",
+            Image,
+            self._image_callback,
+            queue_size=1,
+            buff_size=60000000,
+            tcp_nodelay=False)
 
-    # Set coordinates
-    coord = (x, y)
+        # Init
+        self._init_text()
+        cv2.namedWindow("Colorpicker")
+        cv2.setMouseCallback("Colorpicker", self._mouse_callback)
+        self._mouse_coord = (0,0)
+        self._left_click, self._undo_click = False, False
+        color_space = np.zeros((256, 256, 256), dtype=np.uint8)
 
+        # Default box size
+        self._box_size = 50
 
-def draw_mask(image, mask, color, opacity=0.5):
-    """
-    Draws the mask on an image
-
-    :param image: The image canvas
-    :param mask: The binary mask
-    :param color: Color bgr tuple
-    :param opacity: Opacity of the mask
-    """
-    # Make a colored image
-    colored_image = np.zeros_like(image)
-    colored_image[:, :] = tuple(np.multiply(color, opacity).astype(np.uint8))
-
-    # Compose debug image with lines
-    return cv2.add(cv2.bitwise_and(image, image, mask=255-mask),
-            cv2.add(colored_image*opacity, image*(1-opacity), mask=mask).astype(np.uint8))
-
-
-def init_text():
-    """
-    Prints some usage information.
-    """
-    rospy.loginfo( "Welcome to the Colorpicker\n\
-Click to select the colors in the OpenCV window.\n\
-Use <shift> and <left click> to remove colors.\n\
-To undo a step press 'u'.\n\
-To save the color space press <s> followed by the path and <enter>.\n\
-Exit using <esc>.\n\n\n", logger_name="colorpicker")
+        # Init history
+        self._history = [color_space]
 
 
-def image_callback(msg):
-    """
-    Image message callback
+    def _mouse_callback(self, event, x, y, flags, param):
+        """
+        Callback for the OpenCV cursor.
 
-    :param msg: Image message
-    """
-    global image
-    image = _cv_bridge.imgmsg_to_cv2(msg, 'bgr8')
+        :param event: Event type
+        :param x: Mouse x position
+        :param y: Mouse y position
+        :param flags: Callback flags
+        :param param: Some unused parameter
+        """
+        if flags == (cv2.EVENT_FLAG_SHIFTKEY + cv2.EVENT_FLAG_LBUTTON):
+            # Substracts colors
+            self._undo_click = True
+        elif event == cv2.EVENT_LBUTTONUP:
+            # Adds colors
+            self._left_click = True
 
-
-image = np.zeros((300,300,3), dtype=np.uint8)
-
-# Subscribe to Image-message
-sub_image_msg = rospy.Subscriber(
-    "image_raw",
-    Image,
-    image_callback,
-    queue_size=1,
-    buff_size=60000000,
-    tcp_nodelay=False)
-
-# Init
-init_text()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
-coord = (0,0)
-left_click, undo_click = False, False
-color_space = np.zeros((256, 256, 256), dtype=np.uint8)
-
-# Default box size
-box_size = 50
-
-# Init history
-history = [color_space]
-
-while not rospy.is_shutdown():
-    # Copy image for the canvas
-    canvas = image.copy()
-    # Get the mouse coordinates
-    x, y = coord
-
-    # Get the values for the selection box
-    box_min_x = max(int(x - box_size / 2), 0)
-    box_min_y = max(int(y - box_size / 2), 0)
-    box_max_x = min(int(x + box_size / 2), image.shape[1] - 1)
-    box_max_y = min(int(y + box_size / 2), image.shape[0] - 1)
-
-    # Check for click event
-    if left_click or undo_click:
-        # Get pixels in the selction
-        selected_matrix = image[
-                                    box_min_y : box_max_y,
-                                    box_min_x : box_max_x
-                                ]
-        # Serialize the colors
-        selected_matrix = selected_matrix.reshape(-1,3)
-        # Get the unique colors
-        selected_matrix = np.unique(selected_matrix, axis=0)
-
-        # Copy the latest color space
-        color_space = history[-1].copy()
-
-        # Check if we do or or undo
-        if undo_click:
-            # Remove values in color space
-            color_space[
-                selected_matrix[:,0],
-                selected_matrix[:,1],
-                selected_matrix[:,2]] = 0
-        # Check if we add stuff
-        elif left_click:
-            # Set values for colors in color space
-            color_space[
-                selected_matrix[:,0],
-                selected_matrix[:,1],
-                selected_matrix[:,2]] = 1
-        else:
-            print("This should never happen")
-
-        # Append new color space to history
-        history.append(color_space)
-
-        # Reset events
-        left_click, undo_click = False, False
-
-    # Mask the image with the current color space
-    mask = VisionExtensions.maskImg(image, history[-1])
-
-    # Draw the mask on the canvas
-    canvas = draw_mask(canvas, mask, (255,0,255), opacity=0.8)
-
-    # Draw selection area
-    cv2.rectangle(canvas,
-        (
-            box_min_x,
-            box_min_y
-        ), (
-            box_max_x,
-            box_max_y,),
-        (0, 255, 0), 2)
-
-    # Show canvas
-    cv2.imshow("image", canvas)
+        # Set self._mouse_coordinates
+        self._mouse_coord = (x, y)
 
 
-    # Key checks for UI events
-    key = cv2.waitKey(1) & 0xFF
+    def _draw_mask(self, image, mask, color, opacity=0.5):
+        """
+        Draws the mask on an image
 
-    # Increase selection size
-    if key == ord("+"):
-        box_size += int(box_size * 0.2) + 1
-    # Reduce selection size
-    if key == ord("-") and box_size > 1:
-        box_size -= int(box_size * 0.2) - 1
-    # Undo
-    if key == ord("u") and len(history) > 1:
-        del history[-1]
-    # Quit exit
-    elif key%256 == 27 or 0xFF == ord('q'):
+        :param image: The image canvas
+        :param mask: The binary mask
+        :param color: Color bgr tuple
+        :param opacity: Opacity of the mask
+        """
+        # Make a colored image
+        colored_image = np.zeros_like(image)
+        colored_image[:, :] = tuple(np.multiply(color, opacity).astype(np.uint8))
+
+        # Compose debug image with lines
+        return cv2.add(cv2.bitwise_and(image, image, mask=255-mask),
+                cv2.add(colored_image*opacity, image*(1-opacity), mask=mask).astype(np.uint8))
+
+
+    def _init_text(self):
+        """
+        Prints some usage information.
+        """
+        rospy.loginfo(
+            "Welcome to the Colorpicker\n"
+            "Click to select the colors in the OpenCV window.\n"
+            "Use <shift> and <left click> to remove colors.\n"
+            "To undo a step press 'u'.\n"
+            "To save the color space press 's' followed by the path and <enter>.\n"
+            "Exit using <esc>.\n\n\n", logger_name="colorpicker")
+
+
+    def _image_callback(self, msg):
+        """
+        Image message callback
+
+        :param msg: Image message
+        """
+        self._image = self._cv_bridge.imgmsg_to_cv2(msg, 'bgr8')
+
+    def ui_loop(self):
+        """
+        Runs the UI window.
+
+        :return: Does the user exit the loop with a save flag set?
+        """
         save = False
-        break
-    # Save exit
-    elif key == ord("s") or key == ord("w"):
-        save = True
-        break
+        while not rospy.is_shutdown():
+            # Copy image for the canvas
+            canvas = self._image.copy()
+            # Get the mouse self._mouse_coordinates
+            x, y = self._mouse_coord
 
-    time.sleep(0.01)
+            # Get the values for the selection box
+            box_min_x = max(int(x - self._box_size / 2), 0)
+            box_min_y = max(int(y - self._box_size / 2), 0)
+            box_max_x = min(int(x + self._box_size / 2), self._image.shape[1] - 1)
+            box_max_y = min(int(y + self._box_size / 2), self._image.shape[0] - 1)
+
+            # Check for click event
+            if self._left_click or self._undo_click:
+                # Get pixels in the selction
+                selected_matrix = self._image[
+                                            box_min_y : box_max_y,
+                                            box_min_x : box_max_x
+                                        ]
+                # Serialize the colors
+                selected_matrix = selected_matrix.reshape(-1,3)
+                # Get the unique colors
+                selected_matrix = np.unique(selected_matrix, axis=0)
+
+                # Copy the latest color space
+                color_space = self._history[-1].copy()
+
+                # Check if we do or or undo
+                if self._undo_click:
+                    # Remove values in color space
+                    color_space[
+                        selected_matrix[:,0],
+                        selected_matrix[:,1],
+                        selected_matrix[:,2]] = 0
+                # Check if we add stuff
+                elif self._left_click:
+                    # Set values for colors in color space
+                    color_space[
+                        selected_matrix[:,0],
+                        selected_matrix[:,1],
+                        selected_matrix[:,2]] = 1
+                else:
+                    print("This should never happen")
+
+                # Append new color space to self._history
+                self._history.append(color_space)
+
+                # Reset events
+                self._left_click, self._undo_click = False, False
+
+            # Mask the image with the current color space
+            mask = VisionExtensions.maskImg(self._image, self._history[-1])
+
+            # Draw the mask on the canvas
+            canvas = self._draw_mask(canvas, mask, (255,0,255), opacity=0.8)
+
+            # Draw selection area
+            cv2.rectangle(canvas,
+                (
+                    box_min_x,
+                    box_min_y
+                ), (
+                    box_max_x,
+                    box_max_y,),
+                (0, 255, 0), 2)
+
+            # Show canvas
+            cv2.imshow("Colorpicker", canvas)
 
 
-# Close all open windows
-cv2.destroyAllWindows()
+            # Key checks for UI events
+            key = cv2.waitKey(1) & 0xFF
 
-if save:
-    # Get user input
-    input_path = input("Enter color space path: ")
+            # Increase selection size
+            if key == ord("+"):
+                self._box_size += int(self._box_size * 0.2) + 1
+            # Reduce selection size
+            if key == ord("-") and self._box_size > 1:
+                self._box_size -= int(self._box_size * 0.2) - 1
+            # Undo
+            if key == ord("u") and len(self._history) > 1:
+                del self._history[-1]
+            # Quit exit
+            elif key%256 == 27 or 0xFF == ord('q'):
+                save = False
+                break
+            # Save exit
+            elif key == ord("s") or key == ord("w"):
+                save = True
+                break
 
-    # Check if the user entered something
-    if input_path == "":
-        # Set to default
-        input_path = "~/.ros/color_space.pickle"
+            time.sleep(0.01)
 
-    # Create abs path
-    input_path = os.path.abspath(os.path.expanduser(input_path))
+        # Close all open windows
+        cv2.destroyAllWindows()
+        return save
 
-    # Get the color values
-    color_indices = np.where(history[-1] == 1)
+    def _save(self):
+        # Get user input
+        input_path = input("Enter color space path: ")
 
-    # Create data structure for the file
-    data = dict(
-        red=color_indices[0].tolist(),
-        green=color_indices[1].tolist(),
-        blue=color_indices[2].tolist()
-    )
+        # Check if the user entered something
+        if input_path == "":
+            # Set to default
+            input_path = "~/.ros/color_space.pickle"
 
-    # Save
-    with open(input_path, 'wb') as outfile:
-        pickle.dump(data, outfile, protocol=2)
+        # Create abs path
+        input_path = os.path.abspath(os.path.expanduser(input_path))
 
-    rospy.loginfo("Output saved to '{}'.".format(input_path), logger_name="colorpicker")
+        # Get the color values
+        color_indices = np.where(self._history[-1] == 1)
+
+        # Create data structure for the file
+        data = dict(
+            red=color_indices[0].tolist(),
+            green=color_indices[1].tolist(),
+            blue=color_indices[2].tolist()
+        )
+
+        # Save
+        with open(input_path, 'wb') as outfile:
+            pickle.dump(data, outfile, protocol=2)
+
+        rospy.loginfo("Output saved to '{}'.".format(input_path), logger_name="colorpicker")
+
+    def run(self):
+        """
+        This function runs the program
+        """
+        save = self.ui_loop()
+
+        if save: # TODO
+            self._save()
+
+
+if __name__ == "__main__":
+    colorpicker = Colorpicker()
+    colorpicker.run()
