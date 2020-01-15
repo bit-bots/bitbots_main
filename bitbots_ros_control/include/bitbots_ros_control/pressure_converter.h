ad1dbb18927cc0e2007dc2b279b7038cec5fde93
Jasper Güldenstein <5guelden@informatik.uni-hamburg.de>
2020-01-15 19:19:00 +0100

    pressure_converter: config for no robot (testing), h file, and launch

---
+++ bitbots_ros_control/include/bitbots_ros_control/pressure_converter.h
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <bitbots_msgs/FootPressure.h>
+#include <numeric>
+#include <bitbots_msgs/FootScale.h>
+#include <std_srvs/Empty.h>
+#include <ros/package.h>
+#include <yaml-cpp/emitter.h>
+#include <yaml-cpp/yaml.h>
+#include <iostream>
+#include <fstream>
+
+class PressureConverter
+{
+private:
+    ros::Publisher pub_;
+    ros::Subscriber sub_;
+    ros::NodeHandle pnh_;
+    std::vector<double> zero_, scale_;
+    std::vector<std::vector<double>> previous_values_, zero_and_scale_values_;
+    bool save_zero_and_scale_values_;
+    int current_index_;
+    int average_, scale_and_zero_average_;
+    char side_;
+    std::string scale_lr_, zero_lr_;
+    ros::ServiceServer zero_service_, scale_service_;
+    void pressureCallback(const bitbots_msgs::FootPressureConstPtr &pressure_raw);
+    void resetZeroAndScaleValues();
+    bool zeroCallback(std_srvs::EmptyRequest &req, std_srvs::EmptyResponse &resp);
+    bool scaleCallback(bitbots_msgs::FootScaleRequest &req, bitbots_msgs::FootScaleResponse &resp);
+    void collectMessages();
+    void saveYAML();
+public:
+    PressureConverter(ros::NodeHandle &nh, char side);
+};
+
+
