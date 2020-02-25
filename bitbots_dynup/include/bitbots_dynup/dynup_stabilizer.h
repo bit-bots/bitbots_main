2f2399d3d072799f9a35e96f5f806f6e99ad5a91
16stelter <6stelter@informatik.uni-hamburg.de>
2020-02-25 16:06:31 +0100

    use control toolbox library for pid controller and use imu data

--- bitbots_dynup/include/bitbots_dynup/dynup_stabilizer.h
+++ bitbots_dynup/include/bitbots_dynup/dynup_stabilizer.h
@@ -9,7 +9,7 @@
 #include "dynup_utils.h"
 #include <moveit/robot_state/robot_state.h>
 #include <tf2_ros/transform_listener.h>
-
+#include <control_toolbox/pid.h>
 
 namespace bitbots_dynup {
 
@@ -21,29 +21,20 @@ class Stabilizer : public bitbots_splines::AbstractStabilizer<DynupResponse> {
   void init(moveit::core::RobotModelPtr kinematic_model);
   DynupResponse stabilize(const DynupResponse &response, const ros::Duration &dt) override;
   void setStabilizeNow(bool now);
-  void setCoP(geometry_msgs::Point cop);
-  void setPFactor(double factor_x, double factor_y);
-  void setIFactor(double factor_x, double factor_y);
-  void setDFactor(double factor_x, double factor_y);
   void setTransforms(geometry_msgs::TransformStamped to_trunk, geometry_msgs::TransformStamped from_trunk);
   void useStabilizing(bool use);
   void useMinimalDisplacement(bool use);
   void setStabilizingWeight(double weight);
   void setRobotModel(moveit::core::RobotModelPtr model); 
   void reset() override;
- private:
-
-
-  double cop_x_error_sum_, cop_y_error_sum_, cop_x_error_, cop_y_error_, p_x_factor_, p_y_factor_, i_x_factor_,
-         i_y_factor_, d_x_factor_, d_y_factor_;
-
-  robot_state::RobotStatePtr goal_state_;
-
-  robot_model::RobotModelPtr kinematic_model_;
-
-  geometry_msgs::TransformStamped to_trunk_, from_trunk_;
-
   geometry_msgs::Point cop_;
+  control_toolbox::Pid pid_trunk_pitch_;
+  control_toolbox::Pid pid_trunk_roll_;
+  
+ private:
+  robot_state::RobotStatePtr goal_state_;
+  robot_model::RobotModelPtr kinematic_model_;
+  geometry_msgs::TransformStamped to_trunk_, from_trunk_;
 
   bool stabilize_now_;
 
