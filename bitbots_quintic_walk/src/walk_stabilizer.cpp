c99f6b963b5dd4f4218612f4a0adf303adae4277
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2020-06-15 14:48:45 +0200

    fix smaller issues for making walking work with optimization

--- bitbots_quintic_walk/src/walk_stabilizer.cpp
+++ bitbots_quintic_walk/src/walk_stabilizer.cpp
@@ -3,8 +3,8 @@
 namespace bitbots_quintic_walk {
 
 WalkStabilizer::WalkStabilizer() {
-  pid_trunk_fused_pitch_.init(ros::NodeHandle("/walking/pid_trunk_fused_pitch"), false);
-  pid_trunk_fused_roll_.init(ros::NodeHandle("/walking/pid_trunk_fused_roll"), false);
+  pid_trunk_fused_pitch_.init(ros::NodeHandle("walking/pid_trunk_fused_pitch"), false);
+  pid_trunk_fused_roll_.init(ros::NodeHandle("walking/pid_trunk_fused_roll"), false);
 
   reset();
 }
