2f472525c72ccc9a8d167fbceaff88199be6ed2e
Finn-Thorben Sell <dev@finn-thorben.me>
2020-07-30 17:39:51 +0200

    add melody to robot_compile definitions

--- scripts/robot_compile.py
+++ scripts/robot_compile.py
@@ -112,12 +112,14 @@ class Target:
         jack = "wolfgang_ws"
         donna = "wolfgang_ws"
         davros = "davros_ws"
+        melody = "wolfgang_ws"
 
     class RobotComputers:
         amy = [c + "1" for c in ["nuc", "odroid", "jetson"]]
         rory = [c + "2" for c in ["nuc", "odroid", "jetson"]]
         jack = [c + "3" for c in ["nuc", "odroid", "jetson"]]
         donna = [c + "4" for c in ["nuc", "odroid", "jetson"]]
+        melody = [c + "5" for c in ["nuc", "odroid", "jetson"]]
         davros = ["davros"]
 
     class IPs:
@@ -134,7 +136,11 @@ class Target:
         nuc4 = __prefix__ + "14"
         odroid4 = __prefix__ + "24"
         jetson4 = __prefix__ + "34"
+        nuc5 = __prefix__ + "15"
+        odroid5 = __prefix__ + "25"
+        jetson5 = __prefix__ + "35"
         davros = __prefix__ + "25"
+        # TODO davros and odroid5 have the same IP
 
     def __init__(self, ip, ssh_target):
         """
