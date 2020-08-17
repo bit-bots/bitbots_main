663d9219072b84f0cda7e4c9e71678eddcf05139
Florian Vahl <florian@flova.de>
2020-08-17 12:45:30 +0200

    Ball Tester: Implement new HLM

--- humanoid_league_transform/src/humanoid_league_transform/ball_tester.py
+++ humanoid_league_transform/src/humanoid_league_transform/ball_tester.py
@@ -3,7 +3,7 @@
 Command line tool to publish balls on the /ball_in_image topic
 """
 import rospy
-from humanoid_league_msgs.msg import BallsInImage, BallInImage
+from humanoid_league_msgs.msg import BallInImage, BallInImageArray
 import sys
 import signal
 
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, _signal_term_handler)
 
     rospy.init_node("ball_tester")
-    pub = rospy.Publisher("balls_in_image", BallsInImage,queue_size=10)
+    pub = rospy.Publisher("balls_in_image", BallInImageArray, queue_size=10)
 
     while True:
         x_str = raw_input("x:")
@@ -35,15 +35,15 @@ if __name__ == "__main__":
             print("try again")
             continue
 
-        bi = BallsInImage()
-        bi.header.stamp = rospy.get_rostime() - rospy.Duration(0.2)
+        ba = BallInImageArray()
+        ba.header.stamp = rospy.get_rostime() - rospy.Duration(0.2)
         ball = BallInImage()
         ball.confidence = 1
         ball.center.x = x
         ball.center.y = y
         ball.diameter = 0.13
 
-        bi.candidates.append(ball)
+        ba.candidates.append(ball)
 
-        pub.publish(bi)
+        pub.publish(ba)
 
