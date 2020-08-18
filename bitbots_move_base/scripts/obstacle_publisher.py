a15461e70cbef3fcb752368b75ca2c4bf97f4481
Florian Vahl <florian@flova.de>
2020-08-18 14:56:22 +0200

    Obstacle Publisher: Refactor Obstacle Message

--- bitbots_move_base/scripts/obstacle_publisher.py
+++ bitbots_move_base/scripts/obstacle_publisher.py
@@ -6,7 +6,7 @@ ObstaclePublisher
 This node publishes the ball and other obstacles as an obstacle to avoid walking through it
 """
 import rospy
-from humanoid_league_msgs.msg import BallRelative, ObstaclesRelative
+from humanoid_league_msgs.msg import BallRelative, ObstacleRelativeArray
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs.point_cloud2 import create_cloud_xyz32
 from std_srvs.srv import Empty
@@ -22,7 +22,7 @@ class ObstaclePublisher:
         rospy.logwarn("Found Service clear_costmap")
 
         rospy.Subscriber("ball_relative", BallRelative, self._ball_callback, queue_size=1)
-        rospy.Subscriber("obstacles_relative", ObstaclesRelative, self._obstacle_callback, queue_size=1)
+        rospy.Subscriber("obstacles_relative", ObstacleRelativeArray, self._obstacle_callback, queue_size=1)
 
         self.obstacle_publisher = rospy.Publisher("obstacles", PointCloud2, queue_size=10)
 
@@ -32,7 +32,8 @@ class ObstaclePublisher:
         self.obstacle_publisher.publish(create_cloud_xyz32(msg.header, [[msg.ball_relative.x, msg.ball_relative.y, msg.ball_relative.z]]))
 
     def _obstacle_callback(self, msg):
-        self.obstacle_publisher.publish(create_cloud_xyz32(msg.header, [[o.position.x, o.position.y, o.position.z] for o in msg.obstacles]))
+        self.obstacle_publisher.publish(create_cloud_xyz32(msg.header, \
+            [[o.pose.pose.pose.position.x, o.pose.pose.pose.position.y, o.pose.pose.pose.position.z] for o in msg.obstacles]))
 
 if __name__ == "__main__":
     ObstaclePublisher()
