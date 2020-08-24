fcb8b3be1b744ac5cce541576ed7c3ec3ba9f5cc
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2020-08-24 17:45:45 +0200

    walk: fix ik guess for rotated knees

--- bitbots_quintic_walk/src/walk_ik.cpp
+++ bitbots_quintic_walk/src/walk_ik.cpp
@@ -67,7 +67,7 @@ void WalkIK::reset() {
   // we have to set some good initial position in the goal state, since we are using a gradient
   // based method. Otherwise, the first step will be not correct
   std::vector<std::string> names_vec = {"LHipPitch", "LKnee", "LAnklePitch", "RHipPitch", "RKnee", "RAnklePitch"};
-  std::vector<double> pos_vec = {0.7, -1.0, -0.4, -0.7, 1.0, 0.4};
+  std::vector<double> pos_vec = {0.7, 1.0, -0.4, -0.7, -1.0, 0.4};
   for (long i = 0; i < names_vec.size(); i++) {
     // besides its name, this method only changes a single joint position...
     goal_state_->setJointPositions(names_vec[i], &pos_vec[i]);
