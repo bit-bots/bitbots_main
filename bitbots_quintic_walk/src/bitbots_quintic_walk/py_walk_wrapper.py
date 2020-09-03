469cf84e54688b0bbd8b162962fecd1c512b4e78
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2020-09-03 11:22:34 +0200

    walk: darwin small fixes

--- bitbots_quintic_walk/src/bitbots_quintic_walk/py_walk_wrapper.py
+++ bitbots_quintic_walk/src/bitbots_quintic_walk/py_walk_wrapper.py
@@ -45,7 +45,7 @@ class PyWalk(object):
     def step(self, dt: float, cmdvel_msg: Twist, imu_msg, jointstate_msg):
         if dt == 0.0:
             # preventing wierd spline interpolation errors on edge case
-            dt = 0.0001
+            dt = 0.001
         stepi = self.py_walk_wrapper.step(
             dt,
             self._to_cpp(cmdvel_msg),
