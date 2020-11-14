c6ab0e48b4ec2098c4c25e5339fe42d74425d82f
Niklas Fiedler <git@nfiedler.de>
2020-11-14 15:20:50 +0100

    added timer functionality in blackboard capsule

--- bitbots_blackboard/src/bitbots_blackboard/capsules/blackboard_capsule.py
+++ bitbots_blackboard/src/bitbots_blackboard/capsules/blackboard_capsule.py
@@ -21,6 +21,7 @@ class BlackboardCapsule:
 
         self.tf_buffer = tf2.Buffer(cache_time=rospy.Duration(30.0))
         self.tf_listener = tf2.TransformListener(self.tf_buffer)
+        self.timers = dict()
 
     #####################
     # ## Tracking Part ##
@@ -43,3 +44,37 @@ class BlackboardCapsule:
             return self.state.state == RobotControlState.WALKING
         else:
             return False
+
+    #############
+    # ## Timer ##
+    #############
+
+    def start_timer(self, timer_name, duration_secs):
+        """
+        Starts a timer
+        :param timer_name: Name of the timer
+        :param duration_secs: Duration of the timer in seconds
+        :return: None
+        """
+        self.timers[timer_name] = rospy.Time.now() + rospy.Duration.from_sec(duration_secs)
+
+    def timer_running(self, timer_name):
+        """
+        Returns whether the timer is running
+        :param timer_name: Name of the timer
+        :return: Whether the timer is running. False if the timer doesn't exist.
+        """
+        if timer_name not in self.timers:
+            return False
+        return rospy.Time.now() < self.timers[timer_name]
+
+    def timer_ended(self, timer_name):
+        """
+        Returns whether the timer has ended
+        :param timer_name: Name of the timer
+        :return: Whether the timer has ended. Also true, if timer doesn't exist.
+        """
+        if timer_name not in self.timers:
+            return True  # Don't wait for a non-existing Timer
+        return rospy.Time.now() > self.timers[timer_name]
+
