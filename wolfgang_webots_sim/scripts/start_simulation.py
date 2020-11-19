5c8a5e8be39ee084e82b5770ad1e5379516be5b5
Marc <0bestman@informatik.uni-hamburg.de>
2020-11-19 14:32:42 +0100

    webots: improved launching

--- wolfgang_webots_sim/scripts/start_simulation.py
+++ wolfgang_webots_sim/scripts/start_simulation.py
@@ -6,6 +6,8 @@ import subprocess
 import rospkg
 import rospy
 
+import argparse
+
 from wolfgang_webots_sim.utils import fix_webots_folder
 try:
     from wolfgang_webots_sim.webots_controller import WebotsController
@@ -13,18 +15,29 @@ except:
     print(f'Please execute "source {os.path.dirname(os.path.realpath(__file__))}/setenvs.sh" first')
     exit(0)
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--nogui', help="Deactivate gui", action='store_true')
+args, unknown = parser.parse_known_args()
+
 rospack = rospkg.RosPack()
 path = rospack.get_path("wolfgang_webots_sim")
 
+if args.nogui:
+    mode = "fast"
+    batch = "--batch"
+else:
+    mode = "normal"
+    batch = ""
+
 arguments = ["webots",
-             "--batch",
+             batch,
              path +"/worlds/flat_world.wbt"]
 sim_proc = subprocess.Popen(arguments)
 
 os.environ["WEBOTS_PID"] = str(sim_proc.pid)
 fix_webots_folder(sim_proc.pid)
 
-robot_controller = WebotsController('', True)
+robot_controller = WebotsController('', True, mode=batch)
 
 while not rospy.is_shutdown():
     robot_controller.step()
