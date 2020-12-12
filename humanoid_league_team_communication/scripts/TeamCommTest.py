8ae07363dacf671d88654fa39817b30554672c50
Tanja <5flemmin@informatik.uni-hamburg.de>
2020-12-12 16:54:59 +0100

    TeamComm: updated dummy test script to the changed msg definitions

--- humanoid_league_team_communication/scripts/TeamCommTest.py
+++ humanoid_league_team_communication/scripts/TeamCommTest.py
@@ -1,30 +1,48 @@
 #!/usr/bin/env python3
 
+"""
+This script publishes dummy values for ball, goalpost, position and obstacles for testing the team communication.
+"""
+
 import rospy
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from humanoid_league_msgs.msg import ObstacleRelativeArray, ObstacleRelative
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseWithCovariance
+from humanoid_league_msgs.msg import ObstacleRelativeArray, ObstacleRelative, PoseWithCertaintyArray, PoseWithCertainty
 
 
 if __name__ == '__main__':
     rospy.init_node("TeamCommTest")
-    ball_pub = rospy.Publisher("ball_relative", PoseWithCovarianceStamped, queue_size=1)
-    goal_post_pub = rospy.Publisher("/goal_post_relative", PoseWithCovarianceStamped, queue_size=1)
+    ball_pub = rospy.Publisher("/balls_relative", PoseWithCertaintyArray, queue_size=1)
+    goal_post_pub = rospy.Publisher("/goal_post_relative", PoseWithCertaintyArray, queue_size=1)
     position_pub = rospy.Publisher("/position", PoseWithCovarianceStamped, queue_size=1)
     obstacle_pub = rospy.Publisher("/obstacle_relative", ObstacleRelativeArray, queue_size=1)
-    msg = PoseWithCovarianceStamped()
-    msg.pose.pose.position.x = 2
+    position_msg = PoseWithCovarianceStamped()
+    position_msg.pose.pose.position.x = 2
     obstacle_msg = ObstacleRelativeArray()
     obstacle = ObstacleRelative()
-    obstacle.pose.pose.position.x = 2
+    obstacle.pose.pose.pose.position.x = 4
     obstacle.type = 2
     obstacle_msg.obstacles.append(obstacle)
-
-
+    ball_msg = PoseWithCertaintyArray()
+    ball = PoseWithCertainty()
+    ball.confidence = 1.0
+    ball_position = PoseWithCovariance()
+    ball_position.pose.position.x = 3
+    ball.pose = ball_position
+    ball_msg.poses.append(ball)
+    goal_msg = PoseWithCertaintyArray()
+    goalpost = PoseWithCertainty()
+    goalpost.confidence = 1.0
+    goalpost_position = PoseWithCovariance()
+    goalpost_position.pose.position.x = 10
+    goalpost.pose = goalpost_position
+    goal_msg.poses.append(goalpost)
 
     while not rospy.is_shutdown():
-        msg.header.stamp = rospy.Time.now()
+        position_msg.header.stamp = rospy.Time.now()
         obstacle_msg.header.stamp = rospy.Time.now()
-        ball_pub.publish(msg)
-        goal_post_pub.publish(msg)
-        position_pub.publish(msg)
+        ball_msg.header.stamp = rospy.Time.now()
+        goal_msg.header.stamp = rospy.Time.now()
+        ball_pub.publish(ball_msg)
+        goal_post_pub.publish(goal_msg)
+        position_pub.publish(position_msg)
         obstacle_pub.publish(obstacle_msg)
