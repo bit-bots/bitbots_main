e8a93a57f077231f014a86727ad941adea1f3b1b
Niklas Fiedler <git@nfiedler.de>
2021-01-07 17:17:56 +0100

    initial ball filter

---
+++ bitbots_ball_filter/scripts/ball_sim.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python3
+import rospy
+import numpy as np
+
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+
+class SimBall:
+    def __init__(self):
+        rospy.init_node('ball_sim')
+        rospy.sleep(1)
+        self.dt = 0.05
+
+        self.max_acceleration = .8
+        self.max_error = np.array([2, 4])
+
+        self.velocity = np.zeros((2))
+        self.position = np.zeros((2))
+        self.p_pub = rospy.Publisher('position', PoseWithCovarianceStamped, queue_size=1)
+        self.p_err_pub = rospy.Publisher('ball_relative', PoseWithCovarianceStamped, queue_size=1)
+        while not rospy.is_shutdown():
+            self.position += self.velocity * self.dt
+            self.velocity = np.clip(self.velocity + self.gen_acceleration(), -3, 3)
+            p_err = self.position + self.gen_error()
+            self.p_pub.publish(self.gen_msg(self.position[0], self.position[1]))
+            self.p_err_pub.publish(self.gen_msg(p_err[0], p_err[1]))
+            rospy.sleep(self.dt)
+
+    def gen_error(self):
+        return np.multiply(np.random.rand(2) * 2 - 1, self.max_error)
+
+    def gen_acceleration(self):
+        return np.multiply(np.random.randn(2), self.max_acceleration) * self.dt
+
+    def gen_msg(self, x, y):
+        msg = PoseWithCovarianceStamped()
+        msg.header.frame_id = 'world'
+        msg.pose.pose.position.x = x
+        msg.pose.pose.position.y = y
+        msg.pose.pose.orientation.w = 1
+        return msg
+
+SimBall()
+
+
+
---
+++ bitbots_ball_filter/src/bitbots_ball_filter/ball_filter.py
@@ -0,0 +1,96 @@
+#! /usr/bin/env python3
+
+import rospy
+import numpy as np
+from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
+from geometry_msgs.msg import PoseWithCovarianceStamped, TwistWithCovarianceStamped
+
+
+class BallFilter:
+    def __init__(self):
+        rospy.init_node('ball_filter')
+
+        self.kf = KalmanFilter(dim_x=4, dim_z=2, dim_u=0)
+        self.filter_init = False
+        self.ball = None  # type:PoseWithCovarianceStamped
+
+        # setup subscriber
+        self.subscriber = rospy.Subscriber(
+            rospy.get_param('/ball_filter/ball_subscribe_topic'),
+            PoseWithCovarianceStamped,
+            self.ball_callback,
+            queue_size=1
+        )
+
+        self.ball_pose_publisher = rospy.Publisher(
+            rospy.get_param('/ball_filter/ball_position_publish_topic'),
+            PoseWithCovarianceStamped,
+            queue_size=1
+        )
+        self.ball_movement_publisher = rospy.Publisher(
+            rospy.get_param('/ball_filter/ball_movement_publish_topic'),
+            TwistWithCovarianceStamped,
+            queue_size=1
+        )
+
+        self.filter_timer = rospy.Timer(rospy.Duration(0.1), self.filter_step)
+        rospy.spin()
+
+    def ball_callback(self, msg):
+        self.ball = msg
+
+    def filter_step(self, event):
+        if self.ball:
+            if self.filter_init:
+                self.kf.predict()
+                self.kf.update(self.get_ball_measurement())
+                state = self.kf.get_update()
+            else:
+                self.init_filter(*self.get_ball_measurement())
+                self.kf.predict()
+                self.kf.update(self.get_ball_measurement())
+                state = self.kf.get_update()
+                self.filter_init = True
+            self.publish_data(*state)
+
+    def get_ball_measurement(self):
+        return np.array([self.ball.pose.pose.position.x, self.ball.pose.pose.position.y])
+
+    def init_filter(self, x, y):
+        self.kf.x = np.array([x, y, 0, 0])
+        self.kf.F = np.array([[1.0, 0.0, 1.0, 0.0],
+                             [0.0, 1.0, 0.0, 1.0],
+                             [0.0, 0.0, 1.0, 0.0],
+                             [0.0, 0.0, 0.0, 1.0]])
+        self.kf.H = np.array([[1.0, 0.0 , 0.0, 0.0],
+                              [0.0, 1.0 , 0.0, 0.0]])
+        self.kf.P = np.eye(4) * 1000
+        self.kf.R = np.array([[1, 0],
+                             [0, 1]]) * 0.5
+        self.kf.Q = Q_discrete_white_noise(dim=4, dt=0.1, var=0.01)
+
+    def publish_data(self, state: np.array, cov_mat: np.array) -> None:
+        pose_msg = PoseWithCovarianceStamped()
+        pose_msg.header = self.ball.header
+        pose_msg.pose.pose.position.x = state[0]
+        pose_msg.pose.pose.position.y = state[1]
+        pose_msg.pose.covariance[0] = cov_mat[0][0]
+        pose_msg.pose.covariance[1] = cov_mat[0][1]
+        pose_msg.pose.covariance[6] = cov_mat[1][0]
+        pose_msg.pose.covariance[7] = cov_mat[1][1]
+        pose_msg.pose.pose.orientation.w = 1
+        self.ball_pose_publisher.publish(pose_msg)
+        movement_msg = TwistWithCovarianceStamped()
+        movement_msg.header = self.ball.header
+        movement_msg.twist.twist.linear.x = state[2]
+        movement_msg.twist.twist.linear.y = state[3]
+        movement_msg.twist.covariance[0] = cov_mat[2][2]
+        movement_msg.twist.covariance[1] = cov_mat[2][3]
+        movement_msg.twist.covariance[6] = cov_mat[3][2]
+        movement_msg.twist.covariance[7] = cov_mat[3][3]
+        self.ball_movement_publisher.publish(movement_msg)
+
+
+if __name__ == "__main__":
+    BallFilter()
