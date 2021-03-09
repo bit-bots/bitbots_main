2cde28029c38f1fceb084961f7d7d039fe97062c
Florian Vahl <git@flova.de>
2021-03-09 19:00:16 +0100

    Add first version of local planner

---
+++ bitbots_local_planner/include/bitbots_local_planner/transform_global_plan.h
@@ -0,0 +1,32 @@
+#ifndef FTC_LOCAL_PLANNER_TRANSFORM_GLOBAL_PLAN_H_
+#define FTC_LOCAL_PLANNER_TRANSFORM_GLOBAL_PLAN_H_
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Point.h>
+
+#include <string>
+#include <cmath>
+
+#include <angles/angles.h>
+#include <costmap_2d/costmap_2d.h>
+namespace bitbots_local_planner
+{
+    /**
+    * @brief  Returns X pose in plan
+    * @param tf A reference to a transform buffer
+    * @param global_plan The plan being followed
+    * @param global_frame The global frame of the local planner
+    * @param goal_pose the pose to copy into
+    * @return True if achieved, false otherwise
+    */
+    bool getXPose(const tf2_ros::Buffer& tf,
+                  const std::vector<geometry_msgs::PoseStamped>& global_plan,
+                  const std::string& global_frame,
+                  tf::Stamped<tf::Pose> &goal_pose, int plan_point);
+};
+#endif
---
+++ bitbots_local_planner/src/ftc_planner.cpp
@@ -0,0 +1,175 @@
+#include <bitbots_local_planner/ftc_planner.h>
+
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(bitbots_local_planner::BBPlanner, nav_core::BaseLocalPlanner)
+
+namespace bitbots_local_planner
+{
+
+    BBPlanner::BBPlanner()
+    {
+    }
+
+    void BBPlanner::initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* costmap_ros)
+    {
+        ros::NodeHandle private_nh("~/" + name);
+        local_plan_publisher_ = private_nh.advertise<nav_msgs::Path>("local_plan", 1);
+        tf_ = tf;
+        first_setPlan_ = true;
+        goal_reached_ = false;
+        stand_at_goal_ = false;
+        costmap_ros_ = costmap_ros;
+
+        //Parameter for dynamic reconfigure
+        dsrv_ = new dynamic_reconfigure::Server<BBPlannerConfig>(private_nh);
+        dynamic_reconfigure::Server<BBPlannerConfig>::CallbackType cb = boost::bind(&BBPlanner::reconfigureCB, this, _1, _2);
+        dsrv_->setCallback(cb);
+
+        ROS_INFO("BBPlanner: Version 2 Init.");
+    }
+
+    void BBPlanner::reconfigureCB(BBPlannerConfig &config, uint32_t level)
+    {
+        config_ = config;
+    }
+
+    bool BBPlanner::setPlan(const std::vector<geometry_msgs::PoseStamped>& plan)
+    {
+        global_plan_ = plan;
+
+        if (global_plan_.size())
+        {
+            //ROS_INFO("%d\n" ,global_plan_.size());
+        }
+
+        //First start of the local plan. First global plan.
+        bool first_use = false;
+        if(first_setPlan_)
+        {
+            first_setPlan_ = false;
+            first_use = true;
+            bitbots_local_planner::getXPose(*tf_,global_plan_, costmap_ros_->getGlobalFrameID(),old_goal_pose_,global_plan_.size()-1);
+            first_use = true;
+        }
+
+        bitbots_local_planner::getXPose(*tf_,global_plan_, costmap_ros_->getGlobalFrameID(),goal_pose_,global_plan_.size()-1);
+
+        //Have the new global plan an new goal, reset. Else dont reset.
+        if(std::abs(std::abs(old_goal_pose_.getOrigin().getX())-std::abs(goal_pose_.getOrigin().getX())) <= config_.position_accuracy &&
+                std::abs(std::abs(old_goal_pose_.getOrigin().getY())-std::abs(goal_pose_.getOrigin().getY())) <= config_.position_accuracy && !first_use
+                && std::abs(angles::shortest_angular_distance(tf::getYaw(old_goal_pose_.getRotation()), tf::getYaw(goal_pose_.getRotation()))) <= config_.rotation_accuracy)
+        {
+            ROS_DEBUG("BBPlanner: Old Goal == new Goal.");
+        }
+        else
+        {
+            goal_reached_ = false;
+            stand_at_goal_ = false;
+            ROS_INFO("BBPlanner: New Goal. Start new routine.");
+        }
+
+        old_goal_pose_ = goal_pose_;
+
+        return true;
+    }
+
+    bool BBPlanner::computeVelocityCommands(geometry_msgs::Twist& cmd_vel)
+    {
+        ros::Time begin = ros::Time::now();
+
+        tf::Stamped<tf::Pose> current_pose;
+        geometry_msgs::PoseStamped msg;
+        costmap_ros_->getRobotPose(msg);
+        current_pose.setData(tf::Transform(
+                                tf::Quaternion(msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w),
+                             tf::Vector3(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z)));
+
+        double walk_angle = std::fmod(
+                std::atan2(
+                    goal_pose_.getOrigin().y() - current_pose.getOrigin().y(),
+                    goal_pose_.getOrigin().x() - current_pose.getOrigin().x()),
+            2* M_PI);
+
+        double distance = sqrt(
+                    pow(goal_pose_.getOrigin().y() - current_pose.getOrigin().y(), 2) +
+                    pow(goal_pose_.getOrigin().x() - current_pose.getOrigin().x(), 2)
+                );
+
+        double walk_vel = std::min(distance * 0.3, config_.max_x_vel);
+
+
+        double diff = 0;
+        if (distance > 1)
+        {
+            diff = walk_angle - tf::getYaw(current_pose.getRotation());
+        } else
+        {
+            diff = tf::getYaw(goal_pose_.getRotation()) - tf::getYaw(current_pose.getRotation());
+        }
+
+
+        double min_angle = (std::fmod(diff + M_PI, 2* M_PI) - M_PI);
+        double vel = std::max(std::min(0.2*min_angle, config_.max_rotation_vel), -config_.max_rotation_vel);
+
+        if(distance < 0.05 && abs(min_angle) < 0.1)
+        {
+            cmd_vel.linear.x = 0;
+            cmd_vel.linear.y = 0;
+            cmd_vel.angular.z = 0;
+            goal_reached_ = true;
+        } else {
+            cmd_vel.linear.x = std::cos(walk_angle - tf::getYaw(current_pose.getRotation())) * walk_vel;
+            cmd_vel.linear.y = std::sin(walk_angle - tf::getYaw(current_pose.getRotation())) * walk_vel;
+            cmd_vel.angular.z = vel;
+            goal_reached_ = false;
+        }
+
+        int max_point = 0;
+        //Second part of the routine. Drive alonge the global plan.
+
+        publishPlan(max_point);
+
+        ros::Time end = ros::Time::now();
+        ros::Duration duration = end - begin;
+        ROS_DEBUG("BBPlanner: Calculation time: %f seconds", duration.toSec());
+        return true;
+    }
+
+    bool BBPlanner::isGoalReached()
+    {
+        if(goal_reached_)
+        {
+            ROS_INFO("BBPlanner: Goal reached.");
+        }
+        return goal_reached_;
+    }
+
+    void BBPlanner::publishPlan(int max_point)
+    {
+        std::vector<geometry_msgs::PoseStamped> path;
+        path = transformed_global_plan_;
+
+        //given an empty path we won't do anything
+        if(path.empty())
+            return;
+
+        //create a path message
+        nav_msgs::Path gui_path;
+        gui_path.poses.resize(path.size());
+        gui_path.header.frame_id = path[0].header.frame_id;
+        gui_path.header.stamp = path[0].header.stamp;
+
+        // Extract the plan in world co-ordinates, we assume the path is all in the same frame
+        for(unsigned int i=0; i < path.size(); i++)
+        {
+            gui_path.poses[i] = path[i];
+        }
+
+        local_plan_publisher_.publish(gui_path);
+    }
+
+    BBPlanner::~BBPlanner()
+    {
+    }
+}
