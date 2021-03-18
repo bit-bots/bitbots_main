a801f22d24c5089afd144b547dc454e906c0ed67
Finn-Thorben Sell <dev@finn-thorben.me>
2021-03-18 14:24:08 +0100

    implement tag merging between different @tag appliances

--- bitbots_test/src/bitbots_test/decorators.py
+++ bitbots_test/src/bitbots_test/decorators.py
@@ -1,26 +1,33 @@
 """Useful decorators for modifying the behavior of test functions as well as :class:`unittest.TestCase` classes."""
 import typing
-import sys
 import os
-from unittest import skipIf, skipUnless
+from unittest import skipIf, skipUnless, skip
 from bitbots_test.test_case import TestCase
 
 
 def tag(*tags: str) -> typing.Callable:
-    """Apply a set of tags to a test function or subclass of :class:`unittest.TestCase`
-
-    This will result in the singular test function or TestCase to only execute when one of the given tags is requested
-    to be run by the user.
-
-    The user can specify a set of tags to run by defining the environment variable *TEST_TAGS* as a comma separated
-    list of tags to run. Additionally a tag prefixed by '!' will forbid tests with that tag to run.
-
-    Combination of tags:
-
-    - When multiple tags are given to this decorator, requesting one of them will result the test to be run.
-    - When this decorator is applied multiple times, all decorators must allow execution in order for the test to be run.
     """
+    Apply a set of tags to a test function or subclass of :class:`unittest.TestCase`
+
+    When multiple @tag decorators are defined, their tags will be merged together.
+
+    This will enable the user to filter test execution to a subset of tests by specifying a list of tags to run as well
+    as a list of tags to forbid. This is done defining the environment variable *TEST_TAGS* as a comma separated list.
+
+    Behavior when specifying tags on test execution:
+
+    - When no tags are specified by the user, all tests will be run and this decorator has no effect at all.
+    - When only a simple list of tags is specified by the user, only tests which have this tag will be run.
+    - When only forbidden tags are specified by the user, tests with these tags will not run but all other tests will.
+    - When specifying a list of tags as well as forbidden tags, first tests will be filtered to the allowed tags and then
+        again all tests with forbidden tags will be removed.
+    """
+    # sanity checks
+    if len(tags) == 0:
+        raise ValueError("at least one tag must be specified")
     for t in tags:
+        if not isinstance(t, str):
+            raise TypeError(f"tags must be of type str and not {type(t).__name__}")
         if t.startswith("!"):
             raise ValueError(f"tag {t} starts with forbidden character '!'")
         if "," in t:
@@ -28,39 +35,75 @@ def tag(*tags: str) -> typing.Callable:
         if t == "":
             raise ValueError(f"an empty string is not a valid tag")
 
-    def is_test_requested() -> bool:
-        requested_tags = [
-            t
-            for t in os.environ.get("TEST_TAGS", "").split(",")
-            if not t.startswith("!")
-        ]
-        matching_tags = [t for t in tags if t in requested_tags]
+    def get_requested_tags() -> typing.Set[str]:
+        return set(
+            [
+                t
+                for t in os.environ.get("TEST_TAGS", "").split(",")
+                if not t.startswith("!") and t != ""
+            ]
+        )
+
+    def get_forbidden_tags() -> typing.Set[str]:
+        return set(
+            [
+                t[1:]
+                for t in os.environ.get("TEST_TAGS", "").split(",")
+                if t.startswith("!")
+            ]
+        )
+
+    def is_test_requested(func: typing.Callable) -> bool:
+        matching_tags = [t for t in func._test_tags_ if t in get_requested_tags()]
         return len(matching_tags) > 0
 
-    def is_test_forbidden() -> bool:
-        forbidden_tags = [
-            t[1:]
-            for t in os.environ.get("TEST_TAGS", "").split(",")
-            if t.startswith("!")
-        ]
-        matching_tags = [t for t in tags if t in forbidden_tags]
+    def is_test_forbidden(func: typing.Callable) -> bool:
+        matching_tags = [t for t in func._test_tags_ if t in get_forbidden_tags()]
         return len(matching_tags) > 0
 
     def func_decorator(func: typing.Callable) -> typing.Callable:
-        return skipIf(
-            is_test_forbidden(), f"one of the tags {tags} is forbidden to be run"
-        )(
-            skipUnless(
-                is_test_requested(), f"none of the tags {tags} are requested to be run"
-            )(func)
-        )
+        if not hasattr(func, "_test_tags_"):
+            func._test_tags_ = set(tags)
+        else:
+            func._test_tags_ = func._test_tags_.union(set(tags))
+
+        # When no tags are specified by the user, all tests will be run and this decorator has no effect at all
+        if len(get_requested_tags()) == 0 and len(get_forbidden_tags()) == 0:
+            return func
+
+        # When only a simple list of tags is specified by the user, only tests which have this tag will be run
+        if len(get_requested_tags()) > 0 and len(get_forbidden_tags()) == 0:
+            if is_test_requested(func):
+                return func
+            else:
+                return skip(f"none of the tags {func._test_tags_} are requested to be run")(func)
+
+        # When only forbidden tags are specified by the user, tests with these tags will not run but all other tests will
+        if len(get_requested_tags()) == 0 and len(get_forbidden_tags()) > 0:
+            if is_test_forbidden(func):
+                return skip(f"one of the tags {func._test_tags_} are forbidden from running")(func)
+            else:
+                return func
+
+        # When specifying a list of tags as well as forbidden tags, first tests will be filtered to the allowed tags
+        # and then again all tests with forbidden tags will be removed.
+        if len(get_requested_tags()) > 0 and len(get_forbidden_tags()) > 0:
+            if is_test_requested(func):
+                if not is_test_forbidden(func):
+                    return func
+                return skip(f"one of the tags {func._test_tags_} are forbidden from running")(func)
+            return skip(f"none of the tags {func._test_tags_} are requested to be run")(func)
 
     def class_decorator(cls: typing.Type) -> typing.Type:
         if not issubclass(cls, TestCase):
-            raise ValueError(f"@tag decorator can only be used on TestCase subclasses and not {cls.__name__}")
+            raise ValueError(
+                f"@tag decorator can only be used on TestCase subclasses and not {cls.__name__}"
+            )
 
         # manually apply @tag() decorator to all test functions in this TestCase
-        test_funcs = [t for t in dir(cls) if t.startswith("test_") and callable(getattr(cls, t))]
+        test_funcs = [
+            t for t in dir(cls) if t.startswith("test_") and callable(getattr(cls, t))
+        ]
         for t in test_funcs:
             original_func = getattr(cls, t)
             decorated_func = tag(*tags)(original_func)
--- bitbots_test/test/unit_tests/test_decorators.py
+++ bitbots_test/test/unit_tests/test_decorators.py
@@ -3,16 +3,21 @@ import os
 import typing
 from unittest.mock import Mock
 from unittest.case import SkipTest
-from hypothesis import given, strategies as st
+from hypothesis import given, assume, strategies as st, example
 from hypothesis.errors import UnsatisfiedAssumption
 from bitbots_test.test_case import TestCase
 from bitbots_test.decorators import tag
+
+
+class CustomStrategies:
+    tags = st.text(min_size=1).filter(lambda t: "," not in t and not t.startswith("!"))
 
 
 class TagTestCase(TestCase):
     @staticmethod
     def _getTestTestCase() -> typing.Tuple[typing.Type[TestCase], Mock]:
         test_func = Mock()
+        test_func.configure_mock(**{"_test_tags_": set()})
 
         class TestTestCase(TestCase):
             pass
@@ -26,133 +31,135 @@ class TagTestCase(TestCase):
         try:
             os.environ["TEST_TAGS"] = ",".join(user_tags)
             return tag(*test_tags)
-        except ValueError:
+        except ValueError as e:
             raise UnsatisfiedAssumption()
 
-    @given(st.sets(st.text(), min_size=1), st.sets(st.text()))
+    def _run(
+        self,
+        test_tags: typing.Set[str],
+        user_tags: typing.Set[str],
+        should_have_run: bool,
+    ):
+        TestTestCase, test_func = self._getTestTestCase()
+        decorator = self._initDecorator(test_tags, set(user_tags))
+
+        with self.subTest("decorated function"):
+            # execution
+            try:
+                decorator(test_func)()
+            except SkipTest:
+                pass
+
+            # verification
+            if should_have_run:
+                test_func.assert_called()
+            else:
+                test_func.assert_not_called()
+
+        test_func.reset_mock()
+        with self.subTest("decorated class"):
+            # execution
+            try:
+                decorator(TestTestCase)().test_func()
+            except SkipTest:
+                pass
+
+            # verification
+            if should_have_run:
+                test_func.assert_called()
+            else:
+                test_func.assert_not_called()
+
+    @given(st.sets(CustomStrategies.tags, min_size=1))
+    def test_tagged_test_gets_run_if_no_tags_are_requested(
+        self, test_tags: typing.Set[str]
+    ):
+        self._run(test_tags, set(), True)
+
+    @given(st.sets(CustomStrategies.tags, min_size=1), st.sets(CustomStrategies.tags))
     def test_tagged_test_gets_run_if_tag_is_requested(
         self, test_tags: typing.Set[str], requested_tags: typing.Set[str]
     ):
-        # setup
         requested_tags.add(list(test_tags)[0])
-        TestTestCase, test_func = self._getTestTestCase()
-        decorator = self._initDecorator(test_tags, requested_tags)
+        self._run(test_tags, requested_tags, True)
 
-        with self.subTest("decorated function"):
-            # execution
-            try:
-                decorator(test_func)()
-            except SkipTest:
-                pass
-
-            # verification
-            test_func.assert_called_once()
-
-        test_func.reset_mock()
-        with self.subTest("decorated class"):
-            # execution
-            try:
-                decorator(TestTestCase)().test_func()
-            except SkipTest:
-                pass
-
-            # verification
-            test_func.assert_called_once()
-
-    @given(st.sets(st.text(), min_size=1), st.sets(st.text()))
+    @given(
+        st.sets(CustomStrategies.tags, min_size=1),
+        st.sets(CustomStrategies.tags, min_size=1),
+    )
     def test_tagged_test_does_not_get_run_if_tag_is_not_requested(
         self, test_tags: typing.Set[str], requested_tags: typing.Set[str]
     ):
-        # setup
         requested_tags.difference_update(test_tags)
-        TestTestCase, test_func = self._getTestTestCase()
-        decorator = self._initDecorator(test_tags, requested_tags)
+        assume(len(requested_tags) > 0)
 
-        with self.subTest("decorated function"):
-            # execution
-            try:
-                decorator(test_func)()
-            except SkipTest:
-                pass
+        self._run(test_tags, requested_tags, False)
 
-            # verification
-            test_func.assert_not_called()
-
-        test_func.reset_mock()
-        with self.subTest("decorated class"):
-            # execution
-            try:
-                decorator(TestTestCase)().test_func()
-            except SkipTest:
-                pass
-
-            # verification
-            test_func.assert_not_called()
-
-    @given(st.sets(st.text(), min_size=1), st.sets(st.text()))
-    def test_tagged_test_gets_run_if_tag_is_not_forbidden(
+    @given(
+        st.sets(CustomStrategies.tags, min_size=1),
+        st.sets(CustomStrategies.tags, min_size=1),
+    )
+    def test_tagged_test_gets_run_if_tag_is_not_forbidden_and_no_tags_are_requested(
         self, test_tags: typing.Set[str], forbidden_tags: typing.Set[str]
     ):
-        # setup
         forbidden_tags.difference_update(test_tags)
-        TestTestCase, test_func = self._getTestTestCase()
-        decorator = self._initDecorator(
-            test_tags, set([f"!{t}" for t in forbidden_tags] + list(test_tags))
-        )
+        assume(len(forbidden_tags) > 0)
 
-        with self.subTest("decorated function"):
-            # execution
-            try:
-                decorator(test_func)()
-            except SkipTest:
-                pass
+        self._run(test_tags, set([f"!{t}" for t in forbidden_tags]), True)
 
-            # verification
-            test_func.assert_called_once()
-
-        test_func.reset_mock()
-        with self.subTest("decorated class"):
-            # execution
-            try:
-                decorator(TestTestCase)().test_func()
-            except SkipTest as e:
-                print(e)
-                pass
-
-            # verification
-            test_func.assert_called_once()
-
-    @given(st.sets(st.text(), min_size=1), st.sets(st.text()))
-    def test_tagged_test_func_does_not_get_run_if_tag_is_forbidden(
+    @given(st.sets(CustomStrategies.tags, min_size=1), st.sets(CustomStrategies.tags))
+    def test_tagged_test_does_not_get_run_if_tag_is_forbidden_and_no_tags_are_requested(
         self, test_tags: typing.Set[str], forbidden_tags: typing.Set[str]
     ):
-        # setup
         forbidden_tags.add(list(test_tags)[0])
-        TestTestCase, test_func = self._getTestTestCase()
-        decorator = self._initDecorator(
-            test_tags, set([f"!{t}" for t in forbidden_tags])
+
+        self._run(test_tags, set([f"!{t}" for t in forbidden_tags]), False)
+
+    @given(
+        st.sets(CustomStrategies.tags, min_size=1),
+        st.sets(CustomStrategies.tags),
+        st.sets(CustomStrategies.tags),
+    )
+    def test_tagged_test_does_not_get_run_if_tag_is_forbidden_and_requested(
+        self,
+        test_tags: typing.Set[str],
+        requested_tags: typing.Set[str],
+        forbidden_tags: typing.Set[str],
+    ):
+        forbidden_tags.add(list(test_tags)[0])
+        requested_tags.add(list(test_tags)[0])
+
+        self._run(
+            test_tags,
+            set([f"!{t}" for t in forbidden_tags] + list(requested_tags)),
+            False,
         )
+
+    @given(st.sets(CustomStrategies.tags, min_size=1))
+    def test_tags_get_merged(self, tags: typing.Set[str]):
+        # setup
+        TestTestCase, test_func = self._getTestTestCase()
 
         with self.subTest("decorated function"):
             # execution
-            try:
-                decorator(test_func)()
-            except SkipTest:
+            decorated = test_func
+            for t in tags:
+                decorated = tag(t)(decorated)
                 pass
 
             # verification
-            test_func.assert_not_called()
+            self.assertEqual(test_func._test_tags_, tags)
 
         test_func.reset_mock()
         with self.subTest("decorated class"):
             # execution
-            try:
-                decorator(TestTestCase)().test_func()
-            except SkipTest:
-                pass
+            decorated = TestTestCase
+            for t in tags:
+                decorated = tag(t)(TestTestCase)
 
             # verification
-            test_func.assert_not_called()
+            self.assertEqual(test_func._test_tags_, tags)
+
 
 
 if __name__ == "__main__":
