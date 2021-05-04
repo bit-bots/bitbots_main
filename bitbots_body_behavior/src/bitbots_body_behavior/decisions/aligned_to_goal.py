87561b1aaf3dd5b611578c99d927ec2dbe0645ad
Niklas Fiedler <5fiedler@informatik.uni-hamburg.de>
2021-05-04 14:36:09 +0200

    new decision deciding whether robot is aligned to goal

--- bitbots_body_behavior/src/bitbots_body_behavior/decisions/aligned_to_goal.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/aligned_to_goal.py
@@ -1,11 +1,44 @@
 from dynamic_stack_decider.abstract_decision_element import AbstractDecisionElement
 from tf.transformations import euler_from_quaternion
 import math
+import rospy
 
 
 class AlignedToGoal(AbstractDecisionElement):
     def __init__(self, blackboard, dsd, parameters=None):
         super(AlignedToGoal, self).__init__(blackboard, dsd, parameters)
+        self.goalpost_safety_distance = self.blackboard.config['goalpost_safety_distance']
+        self.field_length = rospy.get_param("field_length")
+        self.goal_width = rospy.get_param("goal_width")
+
+    def perform(self, reevaluate=False):
+        """
+        It is determined if the robot is correctly aligned to kick the ball into the goal.
+        """
+        current_pose = self.blackboard.world_model.get_current_position()
+        current_ball = self.blackboard.world_model.get_ball_position_xy()
+        if current_pose is None or current_ball is None:
+            return 'NO'
+
+        # calculate the distance of the intersection of a line from the robot throu the ball and the goal line.
+        dist = abs(
+            current_pose[1] +
+            (current_pose[1] - current_ball[1]) *
+            ((- current_pose[0] + self.field_length / 2) / (current_pose[0] - current_ball[0])))
+
+        self.publish_debug_data("projected_distance_to_goal_center", dist)
+        if dist < (self.goal_width / 2) - self.goalpost_safety_distance:
+            return 'YES'
+        else:
+            return 'NO'
+
+    def get_reevaluate(self):
+        return True
+
+
+class AlignedToMoveBaseGoal(AbstractDecisionElement):
+    def __init__(self, blackboard, dsd, parameters=None):
+        super(AlignedToMoveBaseGoal, self).__init__(blackboard, dsd, parameters)
         self.orientation_threshold = self.blackboard.config['goal_alignment_orientation_threshold']  # [deg]
 
     def perform(self, reevaluate=False):
@@ -34,3 +67,4 @@ class AlignedToGoal(AbstractDecisionElement):
 
     def get_reevaluate(self):
         return True
+
