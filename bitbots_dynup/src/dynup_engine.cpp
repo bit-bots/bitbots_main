b154cf41cad2df6650a0fa77c52b1043b707f45a
Florian Vahl <git@flova.de>
2021-05-06 18:24:46 +0200

    Add parameter for hand position

--- bitbots_dynup/src/dynup_engine.cpp
+++ bitbots_dynup/src/dynup_engine.cpp
@@ -562,13 +562,13 @@ double DynupEngine::calcRiseSplines(double time) {
 
   l_hand_spline_.x()->addPoint(time, 0);
   l_hand_spline_.y()->addPoint(time, 0);
-  l_hand_spline_.z()->addPoint(time, -0.35);
+  l_hand_spline_.z()->addPoint(time, params_.hand_walkready_height);
   l_hand_spline_.roll()->addPoint(time, 0);
   l_hand_spline_.pitch()->addPoint(time, params_.hand_walkready_pitch * M_PI/180);
   l_hand_spline_.yaw()->addPoint(time, 0);
   r_hand_spline_.x()->addPoint(time, 0);
   r_hand_spline_.y()->addPoint(time, 0);
-  r_hand_spline_.z()->addPoint(time, -0.35);
+  r_hand_spline_.z()->addPoint(time, params_.hand_walkready_height);
   r_hand_spline_.roll()->addPoint(time, 0);
   r_hand_spline_.pitch()->addPoint(time, params_.hand_walkready_pitch * M_PI/180);
   r_hand_spline_.yaw()->addPoint(time, 0);
