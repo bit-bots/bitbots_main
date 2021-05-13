a6f31f577cde584963c9ccd5dba0225a0208b6fc
Florian Vahl <git@flova.de>
2021-05-14 00:04:01 +0200

    Add avoid ball action and desisions

---
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/avoid_ball.py
@@ -0,0 +1,17 @@
+from std_msgs.msg import Bool
+
+from dynamic_stack_decider.abstract_action_element import AbstractActionElement
+
+
+class AvoidBall(AbstractActionElement):
+    def __init__(self, blackboard, dsd, parameters={"active": True}):
+        super(AvoidBall, self).__init__(blackboard, dsd, parameters)
+
+        self.active = parameters['active']
+
+    def perform(self, reevaluate=False):
+        self.publish_debug_data("avoid_ball", self.blackboard.pathfinding.avoid_ball)
+
+        self.blackboard.pathfinding.ball_obstacle_active_pub.publish(Bool(self.active))
+
+        self.blackboard.pathfinding.avoid_ball = self.active
---
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/avoid_ball.py
@@ -0,0 +1,21 @@
+from dynamic_stack_decider.abstract_decision_element import AbstractDecisionElement
+
+
+class AvoidBall(AbstractDecisionElement):
+    def __init__(self, blackboard, dsd, parameters=None):
+        super(AvoidBall, self).__init__(blackboard, dsd, parameters)
+
+    def perform(self, reevaluate=False):
+        """
+        Determines whether we currently avoid the ball or not
+        :param reevaluate:
+        :return:
+        """
+        self.publish_debug_data("avoid_ball", self.blackboard.pathfinding.avoid_ball)
+
+        if self.blackboard.pathfinding.avoid_ball:
+            return 'YES'
+        return 'NO'
+
+    def get_reevaluate(self):
+        return True
