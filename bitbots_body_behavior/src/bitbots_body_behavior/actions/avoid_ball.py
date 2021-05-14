767f4be16bcd2143a274086b4f2ed0cd6f44696e
Florian Vahl <git@flova.de>
2021-05-14 15:56:53 +0200

    Fix debug

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/avoid_ball.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/avoid_ball.py
@@ -10,10 +10,7 @@ class AvoidBall(AbstractActionElement):
         self.active = parameters.get('active', True)
 
     def perform(self, reevaluate=False):
-        self.publish_debug_data("avoid_ball", self.blackboard.pathfinding.avoid_ball)
-
         self.blackboard.pathfinding.ball_obstacle_active_pub.publish(Bool(self.active))
-
         self.blackboard.pathfinding.avoid_ball = self.active
-
+        self.publish_debug_data("avoid_ball", self.blackboard.pathfinding.avoid_ball)
         self.pop()
