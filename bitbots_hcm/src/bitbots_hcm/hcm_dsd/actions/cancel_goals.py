d9fe207d0965ee3e406c5d86a1b1354c554ece76
Timon Engelke <timon.engelke@online.de>
2021-05-24 12:40:11 +0200

    fix super() call

--- bitbots_hcm/src/bitbots_hcm/hcm_dsd/actions/cancel_goals.py
+++ bitbots_hcm/src/bitbots_hcm/hcm_dsd/actions/cancel_goals.py
@@ -5,7 +5,7 @@ from dynamic_stack_decider.abstract_action_element import AbstractActionElement
 class CancelGoals(AbstractActionElement):
 
     def __init__(self, blackboard, dsd, parameters=None):
-        super(AbstractActionElement, self).__init__(blackboard, dsd, parameters)
+        super(CancelGoals, self).__init__(blackboard, dsd, parameters)
 
     def perform(self):
         self.blackboard.dynup_action_client.cancel_all_goals()
