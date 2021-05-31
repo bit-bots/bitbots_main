d22d7efe507fdffb7db2b51224c6497ef39a3e64
Timon Engelke <timon.engelke@online.de>
2021-05-31 09:22:24 +0200

    change default node name to the one we use in launch files

--- bitbots_localization/src/localization.cpp
+++ bitbots_localization/src/localization.cpp
@@ -6,7 +6,7 @@
 
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "localization");
+  ros::init(argc, argv, "bitbots_localization");
   Localization localization;
 
   // dynamic reconfigure
