7af2b00afc9179764eede962b279347bedcce5d9
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2021-06-01 10:36:55 +0200

    change to new rate implementation

--- bitbots_hcm/scripts/test_publisher.py
+++ bitbots_hcm/scripts/test_publisher.py
@@ -3,13 +3,14 @@ import rospy
 import time
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Bool
+from bitbots_ros_patches.rate import Rate
 
 if __name__ == "__main__":
     rospy.init_node("test_pub")
     pub = rospy.Publisher("test", Imu, queue_size=1)
 
     msg = Imu()
-    rate = rospy.Rate(100)
+    rate = Rate(100)
 
     while not rospy.is_shutdown():
         msg.header.stamp = rospy.Time.now()
