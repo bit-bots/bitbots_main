8007af7810854f7dec78cdab952847a2ad66cf92
Florian Vahl <git@flova.de>
2021-06-03 02:22:55 +0200

    Dont kick in the center of the goal

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to_ball.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to_ball.py
@@ -24,6 +24,7 @@ class GoToBall(AbstractActionElement):
 
         self.blocking = parameters.get('blocking', True)
         self.distance = parameters.get('distance', self.blackboard.config['ball_approach_dist'])
+        self.goal_width = rospy.get_param("goal_width", 2)
 
 
     def perform(self, reevaluate=False):
@@ -32,6 +33,10 @@ class GoToBall(AbstractActionElement):
             goal_angle = self.blackboard.world_model.get_map_based_opp_goal_angle_from_ball()
 
             ball_x, ball_y = self.blackboard.world_model.get_ball_position_xy()
+
+            if abs(ball_y) < self.goal_width / 2:
+                goal_angle = 0
+
             goal_x = ball_x - math.cos(goal_angle) * self.distance
             goal_y = ball_y - math.sin(goal_angle) * self.distance
 
