86513b5ea4d131cdbc07a7f5851102a022e71855
Florian Vahl <git@flova.de>
2021-06-03 17:36:29 +0200

    Move make the gradient goal optional

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to_ball.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/go_to_ball.py
@@ -29,13 +29,23 @@ class GoToBall(AbstractActionElement):
 
     def perform(self, reevaluate=False):
 
-        if 'map_goal' == self.target:
+        if 'gradient_goal' == self.target:
             goal_angle = self.blackboard.world_model.get_gradient_map_goal()
 
             ball_x, ball_y = self.blackboard.world_model.get_ball_position_xy()
 
-            #if abs(ball_y) < self.goal_width / 2:
-            #    goal_angle = 0
+            goal_x = ball_x - math.cos(goal_angle) * self.distance
+            goal_y = ball_y - math.sin(goal_angle) * self.distance
+
+            ball_point = (goal_x, goal_y, goal_angle, self.blackboard.map_frame)
+
+        if 'map_goal' == self.target:
+            goal_angle = self.blackboard.world_model.get_map_based_opp_goal_angle_from_ball()
+
+            ball_x, ball_y = self.blackboard.world_model.get_ball_position_xy()
+
+            if abs(ball_y) < self.goal_width / 2:
+                goal_angle = 0
 
             goal_x = ball_x - math.cos(goal_angle) * self.distance
             goal_y = ball_y - math.sin(goal_angle) * self.distance
