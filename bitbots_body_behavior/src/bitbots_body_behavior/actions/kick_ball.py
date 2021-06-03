57ef9c73a5ed4f2090969515b2afb7d4b7cde00d
Florian Vahl <git@flova.de>
2021-06-03 02:23:49 +0200

    decide if we want to kick sideways

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/kick_ball.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/kick_ball.py
@@ -1,3 +1,5 @@
+import math
+import numpy as np
 import rospy
 from bitbots_msgs.msg import KickGoal
 from geometry_msgs.msg import Quaternion
@@ -60,7 +62,17 @@ class KickBallDynamic(AbstractKickAction):
                 goal.ball_position.x = ball_u
                 goal.ball_position.y = ball_v
                 goal.ball_position.z = 0
-                goal.kick_direction = Quaternion(*quaternion_from_euler(0, 0, 0))
+
+                #rospy.logerr((self.blackboard.world_model.obstacle_value_at_relative_xy(1, 0, 2), self.blackboard.world_model.obstacle_value_at_relative_xy(0.5, -0.5, 2), self.blackboard.world_model.obstacle_value_at_relative_xy(0.5, 0.5, 2)))
+
+                check_positions = [(1, 0), (0, -1), (0, 1)]
+                kick_directions = [0, -1.2, 1.2]
+
+                kick_direction = kick_directions[np.argmin(
+                    list(map(lambda t: self.blackboard.world_model.obstacle_value_at_relative_xy(*t, 3), check_positions)))]
+
+                goal.kick_direction = Quaternion(*quaternion_from_euler(0, 0, kick_direction))
+
                 if self.penalty_kick:
                     goal.kick_speed = 3
                 else:
