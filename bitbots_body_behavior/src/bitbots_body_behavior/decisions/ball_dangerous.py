fa8bb1672bf32cfabadec9145387bbd0744bd700
Sebastian Stelter <6stelter@informatik.uni-hamburg.de>
2021-06-05 17:38:18 +0200

    Added a parameter that determines an area in which the goalie wont fall

--- bitbots_body_behavior/src/bitbots_body_behavior/decisions/ball_dangerous.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/ball_dangerous.py
@@ -5,6 +5,7 @@ class BallDangerous(AbstractDecisionElement):
     def __init__(self, blackboard, dsd, parameters=None):
         super(BallDangerous, self).__init__(blackboard, dsd, parameters)
         self.goal_radius = self.blackboard.config['ball_dangerous_goal_radius']
+        self.nofall_radius = self.blackboard.config['ball_dangerous_nofall_radius']
 
     def perform(self, reevaluate=False):
         """"
@@ -13,9 +14,11 @@ class BallDangerous(AbstractDecisionElement):
         ball_position = self.blackboard.world_model.get_ball_position_xy()
         if self._in_dangerous_area(ball_position):
             robot_position = self.blackboard.world_model.get_current_position()
-            if ball_position[1] > robot_position[1]:
+            if ball_position[1] > robot_position[1] + self.nofall_radius:
                 return 'LEFT'
-            return 'RIGHT'
+            elif ball_position[1] < robot_position[1] - self.nofall_radius:
+                return 'RIGHT'
+            return 'MIDDLE'
         return 'NO'
 
     def _in_dangerous_area(self, position):
