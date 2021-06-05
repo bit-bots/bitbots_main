de6b9965a55b1b43ba46ff8a582edb22d11a41eb
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2021-06-05 21:26:45 +0200

    fixes

--- bitbots_body_behavior/src/bitbots_body_behavior/decisions/allowed_to_move.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/allowed_to_move.py
@@ -13,12 +13,11 @@ class AllowedToMove(AbstractDecisionElement):
         :param reevaluate:
         :return:
         """
-        if self.blackboard.gamestate.get_is_penalized() or self.blackboard.gamestate.get_gamestate() in (
-                GameState.GAMESTATE_FINISHED):
+        if self.blackboard.gamestate.get_is_penalized() or self.blackboard.gamestate.get_gamestate() == GameState.GAMESTATE_FINISHED:
             return 'NO_MOVEMENT'
         elif self.blackboard.gamestate.get_gamestate() in (GameState.GAMESTATE_INITAL, GameState.GAMESTATE_SET):
             return 'ONLY_HEAD'
-        elif self.blackboard.get_secondary_state() in (GameState.STATE_DIRECT_FREEKICK,
+        elif self.blackboard.gamestate.get_secondary_state() in (GameState.STATE_DIRECT_FREEKICK,
                                                        GameState.STATE_INDIRECT_FREEKICK,
                                                        GameState.STATE_PENALTYKICK,
                                                        GameState.STATE_CORNER_KICK,
@@ -27,7 +26,7 @@ class AllowedToMove(AbstractDecisionElement):
                 self.blackboard.get_secondary_state_mode() in (GameState.MODE_PREPARATION, GameState.MODE_END):
             return 'ONLY_HEAD'
         elif self.blackboard.gamestate.get_gamestate() == GameState.GAMESTATE_PLAYING:
-            if not self.blackboard.has_kickoff() and self.blackboard.secondary_seconds_remaining() != 0:
+            if not self.blackboard.gamestate.has_kickoff() and self.blackboard.secondary_seconds_remaining() != 0:
                 return 'ONLY_HEAD'
             else:
                 self.publish_debug_data("Seconds since unpenalized",
