99cfa20462dcf0ad080f08e06119e4153e3e6d20
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2021-06-08 10:21:15 +0200

    webots: add method to go to init

--- wolfgang_webots_sim/src/wolfgang_webots_sim/webots_supervisor_controller.py
+++ wolfgang_webots_sim/src/wolfgang_webots_sim/webots_supervisor_controller.py
@@ -122,6 +122,10 @@ class SupervisorController:
         self.supervisor.simulationResetPhysics()
         return EmptyResponse()
 
+    def reset_robot_init(self, name="amy"):
+        self.robot_nodes[name].loadState('__init__')
+        self.robot_nodes[name].resetPhysics()
+
     def set_initial_poses(self, req=None):
         self.reset_robot_pose_rpy([-1, 3, 0.42], [0, 0.24, -1.57], name="amy")
         self.reset_robot_pose_rpy([-1, -3, 0.42], [0, 0.24, 1.57], name="rory")
