9eeee181bb12bc380bb5356b584041fceb531f1b
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2021-06-14 17:17:43 +0200

    publish own role

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/change_role.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/change_role.py
@@ -7,15 +7,7 @@ class ChangeRole(AbstractActionElement):
         super().__init__(blackboard, dsd, parameters)
 
         self.role = parameters.get('role', None)
-        self.roles = {
-            'striker': Strategy.ROLE_STRIKER,
-            'supporter': Strategy.ROLE_SUPPORTER,
-            'defender': Strategy.ROLE_DEFENDER,
-            'other': Strategy.ROLE_OTHER,
-            'goalie': Strategy.ROLE_GOALIE,
-            'idle': Strategy.ROLE_IDLING
-        }
 
     def perform(self, reevaluate=False):
-        self.blackboard.team_data.set_role(self.roles[self.role])
+        self.blackboard.team_data.set_role(self.blackboard.team_data.roles[self.role])
         self.pop()
