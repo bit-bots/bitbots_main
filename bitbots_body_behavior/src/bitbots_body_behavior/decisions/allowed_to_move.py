87448eb63b225b6234b82ffb1d17b09d9a668063
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2021-06-14 14:50:38 +0200

    start moving if opponents kicked ball

--- bitbots_body_behavior/src/bitbots_body_behavior/decisions/allowed_to_move.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/allowed_to_move.py
@@ -6,6 +6,7 @@ from humanoid_league_msgs.msg import GameState
 class AllowedToMove(AbstractDecisionElement):
     def __init__(self, blackboard, dsd, parameters=None):
         super(AllowedToMove, self).__init__(blackboard, dsd, parameters)
+        self.ball_lost_time = rospy.Duration.from_sec(self.blackboard.config['ball_lost_time'])
 
     def perform(self, reevaluate=False):
         """
@@ -13,22 +14,28 @@ class AllowedToMove(AbstractDecisionElement):
         :param reevaluate:
         :return:
         """
-        if self.blackboard.gamestate.get_is_penalized() or self.blackboard.gamestate.get_gamestate() == GameState.GAMESTATE_FINISHED:
+        if self.blackboard.gamestate.get_is_penalized() \
+                or self.blackboard.gamestate.get_gamestate() == GameState.GAMESTATE_FINISHED:
             return 'NO_MOVEMENT'
         elif self.blackboard.gamestate.get_gamestate() in (GameState.GAMESTATE_INITAL, GameState.GAMESTATE_SET):
             return 'ONLY_HEAD'
-        elif self.blackboard.gamestate.get_secondary_state() in (GameState.STATE_DIRECT_FREEKICK,
-                                                       GameState.STATE_INDIRECT_FREEKICK,
-                                                       GameState.STATE_PENALTYKICK,
-                                                       GameState.STATE_CORNER_KICK,
-                                                       GameState.STATE_GOAL_KICK,
-                                                       GameState.STATE_THROW_IN) and \
-                self.blackboard.gamestate.get_secondary_state_mode() in (GameState.MODE_PREPARATION, GameState.MODE_END):
+        elif self.blackboard.gamestate.get_secondary_state() != GameState.STATE_NORMAL \
+                and self.blackboard.gamestate.get_secondary_state_mode() in (GameState.MODE_PREPARATION,
+                                                                             GameState.MODE_END):
             return 'ONLY_HEAD'
         elif self.blackboard.gamestate.get_gamestate() == GameState.GAMESTATE_READY:
             return 'NORMAL'
         elif self.blackboard.gamestate.get_gamestate() == GameState.GAMESTATE_PLAYING:
-            if not self.blackboard.gamestate.has_kickoff() and self.blackboard.gamestate.get_secondary_seconds_remaining() != 0:
+            if not self.blackboard.gamestate.has_kickoff():
+                if self.blackboard.gamestate.get_secondary_seconds_remaining() == 0:
+                    # time is up for the other team
+                    return 'NORMAL'
+                ball_pos = self.blackboard.world_model.get_ball_position_xy()
+                # if we know where the ball is and that it moved, we can play too
+                if rospy.Time.now() - self.blackboard.world_model.ball_last_seen() < self.ball_lost_time and (
+                        abs(ball_pos[0]) > 0.5 or abs(ball_pos[1]) > 0.5):
+                    return 'NORMAL'
+                # we need to wait for now
                 return 'ONLY_HEAD'
             else:
                 self.publish_debug_data("Seconds since unpenalized",
