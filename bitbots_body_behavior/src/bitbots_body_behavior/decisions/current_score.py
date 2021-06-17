9fb3fc37bf1cde3f8b1d1387ca8c642319206cd1
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2021-06-17 15:06:31 +0200

    missing files for last commit

---
+++ bitbots_body_behavior/src/bitbots_body_behavior/decisions/current_score.py
@@ -0,0 +1,22 @@
+import rospy
+
+from dynamic_stack_decider.abstract_decision_element import AbstractDecisionElement
+
+
+class CurrentScore(AbstractDecisionElement):
+    def __init__(self, blackboard, dsd, parameters=None):
+        super().__init__(blackboard, dsd, parameters)
+
+    def perform(self, reevaluate=False):
+        own_goals = self.blackboard.gamestate.get_own_goals()
+        opp_goals = self.blackboard.gamestate.get_opp_goals()
+
+        if own_goals == opp_goals:
+            return "DRAW"
+        elif own_goals > opp_goals:
+            return "AHEAD"
+        else:
+            return "BEHIND"
+
+    def get_reevaluate(self):
+        return True
