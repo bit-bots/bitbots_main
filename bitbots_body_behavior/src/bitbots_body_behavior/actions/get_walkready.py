76e43e5d2d65f9746e6d9a6a131b36449f6ef702
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2021-06-20 14:47:24 +0200

    let penalty player kick

--- bitbots_body_behavior/src/bitbots_body_behavior/actions/get_walkready.py
+++ bitbots_body_behavior/src/bitbots_body_behavior/actions/get_walkready.py
@@ -1,6 +1,8 @@
+import rospy
 from bitbots_hcm.hcm_dsd.actions.play_animation import PlayAnimationDynup
 
 
 class GetWalkready(PlayAnimationDynup):
     def __init__(self, blackboard, dsd, parameters=None):
+        rospy.logerr("dynup body action")
         super(GetWalkready, self).__init__(blackboard, dsd, parameters={'initial': True, 'direction': 'walkready'})
