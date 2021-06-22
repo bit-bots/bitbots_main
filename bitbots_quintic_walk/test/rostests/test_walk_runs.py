1fac70c98f0532503fc3ec71e46384999ae4676b
Timon Engelke <timon.engelke@online.de>
2021-06-22 22:56:23 +0200

    simple test for walking

---
+++ bitbots_quintic_walk/test/rostests/test_walk_runs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+from bitbots_test.test_case import RosNodeTestCase
+from bitbots_test.mocks import MockSubscriber
+from geometry_msgs.msg import Twist
+from bitbots_msgs.msg import JointCommand
+import rospy
+
+
+class WalkRunsTestCase(RosNodeTestCase):
+    def test_walk_runs(self):
+        sub = MockSubscriber('walking_motor_goals', JointCommand)
+        self.with_assertion_grace_period(lambda: self.assertNodeRunning("walking"), 10)
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size=1, latch=True)
+
+        goal = Twist()
+        goal.linear.x = 0.3
+        pub.publish(goal)
+        sub.wait_until_connected()
+        rospy.sleep(5)
+        self.with_assertion_grace_period(sub.assertMessageReceived, 1)
+
+if __name__ == '__main__':
+    from bitbots_test import run_rostests
+    run_rostests(WalkRunsTestCase)
