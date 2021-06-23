5b2b2e5c7bd859d8ede6f3d0fc7476523b4476b8
Jasper Güldenstein <5guelden@informatik.uni-hamburg.de>
2021-06-23 15:42:27 +0200

    another import remove

--- bitbots_blackboard/src/bitbots_blackboard/blackboard.py
+++ bitbots_blackboard/src/bitbots_blackboard/blackboard.py
@@ -8,7 +8,6 @@ from bitbots_blackboard.capsules.kick_capsule import KickCapsule
 from bitbots_blackboard.capsules.pathfinding_capsule import PathfindingCapsule
 from bitbots_blackboard.capsules.team_data_capsule import TeamDataCapsule
 from bitbots_blackboard.capsules.world_model_capsule import WorldModelCapsule
-from bitbots_blackboard.async_service import AsyncServiceProxy
 
 import actionlib
 from humanoid_league_msgs.msg import PlayAnimationAction
