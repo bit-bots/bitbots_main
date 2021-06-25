edb1c61a7ded49b63e272bd409fcbf6468173948
Niklas Fiedler <git@nfiedler.de>
2021-06-25 10:47:30 +0200

    remove comment

--- bitbots_head_behavior/src/bitbots_head_behavior/head_node.py
+++ bitbots_head_behavior/src/bitbots_head_behavior/head_node.py
@@ -46,7 +46,6 @@ def init():
     blackboard = HeadBlackboard()
 
     rospy.Subscriber('head_mode', HeadModeMsg, blackboard.head_capsule.head_mode_callback, queue_size=1)
-    # rospy.Subscriber("balls_relative", PoseWithCertaintyArray, blackboard.world_model.balls_callback)
     rospy.Subscriber("ball_position_relative_filtered", PoseWithCovarianceStamped, blackboard.world_model.ball_filtered_callback)
     rospy.Subscriber('joint_states', JointState, blackboard.head_capsule.joint_state_callback)
     blackboard.head_capsule.position_publisher = rospy.Publisher("head_motor_goals", JointCommand, queue_size=10)
