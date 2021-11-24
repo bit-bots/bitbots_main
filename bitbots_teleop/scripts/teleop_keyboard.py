2519d789dc7ccb8103e35833bbcc4e9237650bef
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2021-11-24 15:20:58 +0100

    small change for teleop script

--- bitbots_teleop/scripts/teleop_keyboard.py
+++ bitbots_teleop/scripts/teleop_keyboard.py
@@ -328,9 +328,8 @@ if __name__ == "__main__":
             sys.stdout.write("\x1b[A")
             sys.stdout.write("\x1b[A")
             sys.stdout.write("\x1b[A")
-            sys.stdout.write("\x1b[A")
             print(
-                f"x:    {x}          \ny:    {y}          \nturn: {th}          \n\nhead pan: {head_pan_pos:.2f}          \nhead tilt: {head_tilt_pos:.2f}          ")
+                f"x:    {x}          \ny:    {y}          \nturn: {th}          \n\n")
 
     except Exception as e:
         print(e)
