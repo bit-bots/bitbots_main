12533723d421d9393ad0065e940c059ccfe9c3c1
texhnolyze <joegriepen@gmail.com>
2021-12-08 16:16:52 +0000

    build(ros2): move to ros2 colcon python build process
    instead of adapting the previous catkin CMake build

--- humanoid_league_game_controller/setup.py
+++ humanoid_league_game_controller/setup.py
@@ -1,9 +1,34 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+import glob
+import os
 
-d = generate_distutils_setup(
-    packages=['humanoid_league_game_controller'],
-    package_dir={'': 'src'}
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'humanoid_league_game_controller'
+
+setup(
+    name=package_name,
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + "/config",
+            glob.glob('config/*.yaml')),
+        ('share/' + package_name + '/launch',
+            glob.glob('launch/*.launch')),
+    ],
+    install_requires=[
+        'launch',
+        'setuptools',
+        'construct',
+    ],
+    zip_safe=True,
+    keywords=['ROS'],
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'game_controller = humanoid_league_game_controller.receiver:main',
+        ],
+    }
 )
-
-setup(**d)
