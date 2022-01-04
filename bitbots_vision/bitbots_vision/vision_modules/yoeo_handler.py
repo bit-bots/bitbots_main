56eca4e43176ee759cad5e3657f2ec1a5af05569
Philipp Donn <30521025+phinik@users.noreply.github.com>
2022-01-04 21:01:05 +0100

    Applied feedback: 	- adjusted paths for yoeo_names.yaml, yoeo.pth and yoeo.cfg 	- segmentation class names are stored as list instead of dict 	- load_classes() of YOEO repo is no longer used to load yoeo_names.yaml 	- renamed get_segmentation() into get_mask() in YOEODetectorTemplate and its implementations
    Additionally:
    	- added missing type hints
    	- corrected where the YOEO outputs are processed (implementations instead of abstract base class)

--- bitbots_vision/bitbots_vision/vision_modules/yoeo_handler.py
+++ bitbots_vision/bitbots_vision/vision_modules/yoeo_handler.py
@@ -6,41 +6,44 @@ import cv2
 from numpy import where, moveaxis
 from os.path import join
 from rclpy import logging
-from typing import List
+from typing import List, Union, Dict, Any
+from yaml import load, SafeLoader
 
 logger = logging.get_logger('vision_yoeo')
 
 try:
     from yoeo import models as torch_models, detect as torch_detect
-    from yoeo.utils.utils import load_classes
 except ImportError:
     logger.error("Not able to import pytorchyolo. This might be fine if you use another method.")
 
 
 class YOEOHandlerTemplate(ABC):
-    def __init__(self, config: dict, model_path: str):
+    def __init__(self, config: Dict, model_path: str):
         self._use_caching = config['caching']
 
-        self._image = None
-        self._iou_non_max_suppression_thresh = config['yoeo_nms_threshold']
+        self._image = None  # : Union[None, numpy.ndarray]
+        self._iou_non_max_suppression_thresh: float = config['yoeo_nms_threshold']
 
-        self._det_candidates = defaultdict(list)
-        self._det_class_names = None
-        self._det_confidence_thresh = config['yoeo_conf_threshold']
+        self._det_candidates: Dict = defaultdict(list)
+        self._det_class_names: Union[None, List[str]] = None
+        self._det_confidence_thresh: float = config['yoeo_conf_threshold']
 
-        self._segmentation = None
-        self._segmentations = dict()
-        self._seg_class_idx = dict()
+        self._seg_candidates: Dict = dict()
+        self._seg_class_names: Union[None, List[str]] = None
 
         self._load_candidate_class_names(model_path)
 
     def _load_candidate_class_names(self, model_path: str) -> None:
-        path = join(model_path, "data", "yoeo_names.yaml")
-        data = load_classes(path)
+        path = join(model_path, "yoeo_names.yaml")
 
-        self._det_class_names = data['detection']
-        for key, i in zip(data['segmentation'], range(len(data['segmentation']))):
-            self._seg_class_idx[key] = i
+        with open(path, 'r', encoding="utf-8") as fp:
+            class_names = load(fp, Loader=SafeLoader)
+
+        assert "detection" in class_names.keys(), f"Missing key 'detection' in {path}"
+        assert "segmentation" in class_names.keys(), f"Missing key 'segmentation' in {path}"
+
+        self._det_class_names = class_names['detection']
+        self._seg_class_names = class_names['segmentation']
 
     def get_candidates(self, class_name: str) -> List[Candidate]:
         """
@@ -61,7 +64,7 @@ class YOEOHandlerTemplate(ABC):
             self._compute_new_prediction()
 
     def _prediction_has_to_be_updated(self) -> bool:
-        return self._det_candidates is None or self._segmentation is None or not self._use_caching
+        return not (self._det_candidates and self._seg_candidates and self._use_caching)
 
     def get_classes(self) -> List[str]:
         """
@@ -72,23 +75,19 @@ class YOEOHandlerTemplate(ABC):
     def get_detection_candidate_class_names(self) -> List[str]:
         return self._det_class_names
 
-    def get_segmentation_candidate_class_names(self):
-        return self._seg_class_idx
+    def get_segmentation_candidate_class_names(self) -> List[str]:
+        return self._seg_class_names
 
     def get_segmentation_for(self, class_name: str):
-        assert class_name in self._seg_class_idx, \
+        """
+        :rtype: numpy.ndarray(shape=(height, width, 1))
+        """
+        assert class_name in self._seg_class_names, \
             f"Class '{class_name}' ist not available for the current YOEO model (segmentation)"
 
         self.predict()
 
-        if class_name not in self._segmentations.keys():
-            self._compute_segmentation_for(class_name)
-
-        return self._segmentations[class_name]
-
-    def _compute_segmentation_for(self, class_name: str) -> None:
-        segmentation = where(self._segmentation == self._seg_class_idx[class_name], 1, 0)
-        self._segmentations[class_name] = moveaxis(segmentation, 0, -1)
+        return self._seg_candidates[class_name]
 
     def set_image(self, img) -> None:
         self._reset()
@@ -96,8 +95,7 @@ class YOEOHandlerTemplate(ABC):
 
     def _reset(self) -> None:
         self._det_candidates = defaultdict(list)
-        self._segmentation = None
-        self._segmentations = defaultdict(list)
+        self._seg_candidates = dict()
 
     def _update_image(self, img) -> None:
         self._image = img
@@ -115,8 +113,8 @@ class YOEOHandlerPytorch(YOEOHandlerTemplate):
     def __init__(self, config, model_path):
         super().__init__(config, model_path)
 
-        weights_path = join(model_path, "weights", "yoeo.pth")
-        config_path = join(model_path, "config", "yoeo-rev-7.cfg")
+        weights_path = join(model_path, "yoeo.pth")
+        config_path = join(model_path, "yoeo.cfg")
 
         self._model = torch_models.load_model(config_path, weights_path)
 
@@ -125,15 +123,21 @@ class YOEOHandlerPytorch(YOEOHandlerTemplate):
                                                              cv2.cvtColor(self._image, cv2.COLOR_BGR2RGB),
                                                              conf_thres=self._det_confidence_thresh,
                                                              nms_thres=self._iou_non_max_suppression_thresh)
+        self._set_detection_candidates(detections)
+        self._set_segmentations(segmentation)
 
+    def _set_detection_candidates(self, detections: Any) -> None:
         for detection in detections:
             c = Candidate.from_x1y1x2y2(*detection[0:4].astype(int), detection[4].astype(float))
             self._det_candidates[self._det_class_names[int(detection[5])]].append(c)
 
-        self._segmentation = segmentation
+    def _set_segmentations(self, segmentation: Any) -> None:
+        for i, class_name in enumerate(self._seg_class_names):
+            seg = where(segmentation == i, 1, 0)
+            self._seg_candidates[class_name] = moveaxis(seg, 0, -1)
 
 
-class YOEODetector(CandidateFinder):
+class YOEODetectorTemplate(CandidateFinder):
     def __init__(self, yoeo_handler: YOEOHandlerTemplate):
         super().__init__()
 
@@ -142,15 +146,15 @@ class YOEODetector(CandidateFinder):
     def set_image(self, image) -> None:
         self._yoeo_handler.set_image(image)
 
-    @abstractmethod
-    def get_candidates(self):
-        ...
-
-    def compute(self):
+    def compute(self) -> None:
         self._yoeo_handler.predict()
 
+    @abstractmethod
+    def get_candidates(self) -> List[Candidate]:
+        ...
 
-class YOEOBallDetector(YOEODetector):
+
+class YOEOBallDetector(YOEODetectorTemplate):
     def __init__(self, yoeo_handler: YOEOHandlerTemplate):
         super().__init__(yoeo_handler)
 
@@ -158,19 +162,19 @@ class YOEOBallDetector(YOEODetector):
         return self._yoeo_handler.get_detection_candidates_for("ball")
 
 
-class YOEOGoalpostDetector(YOEODetector):
+class YOEOGoalpostDetector(YOEODetectorTemplate):
     def __init__(self, yoeo_handler: YOEOHandlerTemplate):
         super().__init__(yoeo_handler)
 
-    def get_candidates(self):
+    def get_candidates(self) -> List[Candidate]:
         return self._yoeo_handler.get_detection_candidates_for("goalpost")
 
 
-class YOEORobotDetector(YOEODetector):
+class YOEORobotDetector(YOEODetectorTemplate):
     def __init__(self, yoeo_handler: YOEOHandlerTemplate):
         super().__init__(yoeo_handler)
 
-    def get_candidates(self):
+    def get_candidates(self) -> List[Candidate]:
         return self._yoeo_handler.get_detection_candidates_for("robot")
 
 
@@ -179,7 +183,10 @@ class YOEOSegmentationTemplate(ABC):
         self._yoeo_handler = yoeo_handler
 
     @abstractmethod
-    def get_segmentation(self):
+    def get_mask(self):
+        """
+        :rtype: numpy.ndarray(shape=(height, width, 1))
+        """
         ...
 
 
@@ -187,7 +194,10 @@ class YOEOBackgroundSegmentation(YOEOSegmentationTemplate):
     def __init__(self, yoeo_handler):
         super().__init__(yoeo_handler)
 
-    def get_segmentation(self):
+    def get_mask(self):
+        """
+        :rtype: numpy.ndarray(shape=(height, width, 1))
+        """
         return self._yoeo_handler.get_segmentation_for("background")
 
 
@@ -195,7 +205,10 @@ class YOEOFieldSegmentation(YOEOSegmentationTemplate):
     def __init__(self, yoeo_handler: YOEOHandlerTemplate):
         super().__init__(yoeo_handler)
 
-    def get_segmentation(self):
+    def get_mask(self):
+        """
+        :rtype: numpy.ndarray(shape=(height, width, 1))
+        """
         return self._yoeo_handler.get_segmentation_for("field")
 
 
@@ -203,5 +216,8 @@ class YOEOLineSegmentation(YOEOSegmentationTemplate):
     def __init__(self, yoeo_handler: YOEOHandlerTemplate):
         super().__init__(yoeo_handler)
 
-    def get_segmentation(self):
+    def get_mask(self):
+        """
+        :rtype: numpy.ndarray(shape=(height, width, 1))
+        """
         return self._yoeo_handler.get_segmentation_for("lines")
