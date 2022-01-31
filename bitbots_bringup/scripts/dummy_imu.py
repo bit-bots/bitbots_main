268bdeecc1e1d2739b5e1d1e8ebbad389e66bb47
Marc Bestmann <marc.bestmann@uni-hamburg.de>
2022-01-31 14:17:06 +0100

    translate dummy imu script to ros2g

--- bitbots_bringup/scripts/dummy_imu.py
+++ bitbots_bringup/scripts/dummy_imu.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python3
-import rospy
+import threading
+
+import rclpy
+from rclpy.node import Node
 from sensor_msgs.msg import Imu
 
+
+class DummyImu(Node):
+
+    def __init__(self):
+        super().__init__('DummyImu')
+        self.pub = self.create_publisher(Imu, '/imu/data', 1)
+        self.msg = Imu()
+        self.msg.header.frame_id = 'imu'
+        self.msg.orientation.w = 1.0
+
+    def loop(self):
+        r = self.create_rate(100)
+        while rclpy.ok():
+            self.msg.header.stamp = self.get_clock().now().to_msg()
+            self.pub.publish(self.msg)
+            r.sleep()
+
+
 if __name__ == '__main__':
-    rospy.init_node('dummy_imu')
+    rclpy.init(args=None)
 
-    pub = rospy.Publisher('/imu/data', Imu, queue_size=1)
+    node = DummyImu()
+    # necessary so that sleep in loop() is not blocking
+    thread = threading.Thread(target=rclpy.spin, args=(node,), daemon=True)
+    thread.start()
+    node.loop()
 
-    msg = Imu()
-    msg.header.frame_id = 'imu'
-    msg.orientation.w = 1
-
-    r = rospy.Rate(100)
-    while not rospy.is_shutdown():
-        msg.header.stamp = rospy.Time.now()
-        pub.publish(msg)
-        r.sleep()
+    node.destroy_node()
+    rclpy.shutdown()
