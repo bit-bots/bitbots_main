a5d5862d450f7544696d46218956c61b0f6fa8ba
Marc Bestmann <marc.bestmann@uni-hamburg.de>
2022-02-15 15:35:42 +0100

    working on python interface

--- bitbots_moveit_bindings/bitbots_moveit_bindings/__init__.py
+++ bitbots_moveit_bindings/bitbots_moveit_bindings/__init__.py
@@ -3,40 +3,15 @@ from moveit_msgs.srv import GetPositionFKResponse, GetPositionFKRequest
 from moveit_ros_planning_interface._moveit_roscpp_initializer import roscpp_init, roscpp_shutdown
 from io import BytesIO
 from bitbots_moveit_bindings.bitbots_moveit_bindings import *
-
-
-def to_cpp(msg):
-    """Return a serialized string from a ROS message
-
-    Parameters
-    ----------
-    - msg: a ROS message instance.
-    """
-    buf = BytesIO()
-    msg.serialize(buf)
-    value = buf.getvalue()
-    return value
-
-
-def from_cpp(str_msg, cls):
-    """Return a ROS message from a serialized string
-
-    Parameters
-    ----------
-    - str_msg: str, serialized message
-    - cls: ROS message class, e.g. sensor_msgs.msg.LaserScan.
-    """
-    msg = cls()
-    result = msg.deserialize(str_msg)
-    return result
+from rclpy.serialization import serialize_message, deserialize_message
 
 
 class _RosInitializer:
     def __init__(self):
-        roscpp_init('bitbots_moveit_bindings', [])
+        self.node = BitbotsMoveitBindings()
 
     def __del__(self):
-        roscpp_shutdown()
+        self.node.shutdown()
 
 
 # The ros initializer is used to automatically setup the ros structure on the
@@ -48,15 +23,15 @@ def get_position_ik(request: GetPositionIKRequest, approximate=False):
     global _ros_initializer
     if _ros_initializer is None:
         _ros_initializer = _RosInitializer()
-    request_str = to_cpp(request)
-    result_str = getPositionIK(request_str, approximate)
-    return from_cpp(result_str, GetPositionIKResponse)
+    request_str = serialize_message(request)
+    result_str = _ros_initializer.getPositionIK(request_str, approximate)
+    return deserialize_message(result_str, GetPositionIKResponse)
 
 
 def get_position_fk(request: GetPositionFKRequest):
     global _ros_initializer
     if _ros_initializer is None:
         _ros_initializer = _RosInitializer()
-    request_str = to_cpp(request)
-    result_str = getPositionFK(request_str)
-    return from_cpp(result_str, GetPositionFKResponse)
+    request_str = serialize_message(request)
+    result_str = _ros_initializer.getPositionFK(request_str)
+    return deserialize_message(result_str, GetPositionFKResponse)
