b6148f72aa0d0c2267fae1ec7f4876359380f768
Marc Bestmann <marc.bestmann@uni-hamburg.de>
2022-02-26 19:14:27 +0100

    team comm migration

--- humanoid_league_team_communication/setup.py
+++ humanoid_league_team_communication/setup.py
@@ -9,17 +9,17 @@ setup(
     packages=find_packages(),
     data_files=[
         ('share/' + package_name + "/config",
-        glob.glob('config/*.yaml')),
+         glob.glob('config/*.yaml')),
         ('share/' + package_name + '/launch',
          glob.glob('launch/*.launch')),
     ],
     install_requires=[
         'setuptools',
     ],
-    scripts=['scripts/send_text.py'],
+    scripts=['scripts/show_team_comm.py', 'scripts/teamcomm_test_marker.py', 'scripts/TeamCommTest.py'],
     entry_points={
         'console_scripts': [
-            'team_comm = humanoid_league_team_communication.team_comm.:main',
+            'team_comm = humanoid_league_team_communication.humanoid_league_team_communication:main',
         ],
     }
 )
