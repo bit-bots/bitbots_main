b0caa8633db63b8d4c1d757eca8f6e3ab9c9fc62
Marc <0bestman@informatik.uni-hamburg.de>
2022-05-22 13:57:39 +0200

    further fixes for ros2

--- bitbots_blackboard/bitbots_blackboard/blackboard.py
+++ bitbots_blackboard/bitbots_blackboard/blackboard.py
@@ -20,9 +20,9 @@ class BodyBlackboard:
     def __init__(self, node: Node):
         self.node = node
 
-        self.config = self.node.get_parameter("behavior/body").get_parameter_value().double_value
-        self.base_footprint_frame = self.node.get_parameter("~base_footprint_frame").get_parameter_value().double_value
-        self.map_frame = self.node.get_parameter("~map_frame").get_parameter_value().double_value
+        self.config = self.node.get_parameter_dict(node, "body")
+        self.base_footprint_frame = self.node.get_parameter("base_footprint_frame").get_parameter_value().double_value
+        self.map_frame = self.node.get_parameter("map_frame").get_parameter_value().double_value
         self.blackboard = BlackboardCapsule()
         self.gamestate = GameStatusCapsule()
         self.animation = AnimationCapsule()
@@ -34,15 +34,15 @@ class BodyBlackboard:
         # animations
         self.animation_action_client = ActionClient(self, PlayAnimation, 'animation')
         self.goalie_arms_animation = self.node.get_parameter(
-            "Animations/Goalie/goalieArms").get_parameter_value().double_value
+            "Animations.Goalie.goalieArms").get_parameter_value().double_value
         self.goalie_falling_right_animation = self.node.get_parameter(
-            "Animations/Goalie/fallRight").get_parameter_value().double_value
+            "Animations.Goalie.fallRight").get_parameter_value().double_value
         self.goalie_falling_left_animation = self.node.get_parameter(
-            "Animations/Goalie/fallLeft").get_parameter_value().double_value
+            "Animations.Goalie.fallLeft").get_parameter_value().double_value
         self.goalie_falling_center_animation = self.node.get_parameter(
-            "Animations/Goalie/fallCenter").get_parameter_value().double_value
-        self.cheering_animation = self.node.get_parameter("Animations/Misc/cheering").get_parameter_value().double_value
-        self.init_animation = self.node.get_parameter("Animations/Misc/init").get_parameter_value().double_value
+            "Animations.Goalie.fallCenter").get_parameter_value().double_value
+        self.cheering_animation = self.node.get_parameter("Animations.Misc.cheering").get_parameter_value().double_value
+        self.init_animation = self.node.get_parameter("Animations.Misc.init").get_parameter_value().double_value
 
         self.dynup_action_client = None
         self.dynup_cancel_pub = None  # type: Publisher
