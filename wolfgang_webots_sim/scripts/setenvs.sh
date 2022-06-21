6ec09e20d007d09e4931e414cb65c8953a7a7fa8
Marc Bestmann <marc.bestmann@uni-hamburg.de>
2022-06-21 13:30:37 +0200

    add python 3.10

--- wolfgang_webots_sim/scripts/setenvs.sh
+++ wolfgang_webots_sim/scripts/setenvs.sh
@@ -5,4 +5,5 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${WEBOTS_HOME}/lib/controller
 [[ -f /usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4 ]] && export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc_minimal.so.4
 [[ -d ${WEBOTS_HOME}/lib/controller/python36 ]] && export PYTHONPATH=$PYTHONPATH:${WEBOTS_HOME}/lib/controller/python36
 [[ -d ${WEBOTS_HOME}/lib/controller/python38 ]] && export PYTHONPATH=$PYTHONPATH:${WEBOTS_HOME}/lib/controller/python38
+[[ -d ${WEBOTS_HOME}/lib/controller/python310 ]] && export PYTHONPATH=$PYTHONPATH:${WEBOTS_HOME}/lib/controller/python310
 export PYTHONPATH=$PYTHONPATH:$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )
