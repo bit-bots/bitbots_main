597ddb767cd4995ef517fe1eda6d142aec4c2002
Marc Bestmann <marc.bestmann@uni-hamburg.de>
2022-07-09 05:12:14 +0700

    localization handler: put in own package; fix some dsd stuff

---
+++ bitbots_localization_handler/setup.py
@@ -0,0 +1,26 @@
+import glob
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'bitbots_localization_handler'
+
+
+setup(name=package_name,
+      packages=find_packages(exclude=['test']),
+      data_files=[('share/' + package_name, ['package.xml']),
+                  ('share/' + package_name + '/localization_dsd/actions', glob.glob(package_name + '/actions/*.py')),
+                  ('share/' + package_name + '/localization_dsd/decisions',
+                   glob.glob(package_name + '/decisions/*.py')),
+                  ('share/' + package_name, [package_name + '/localization_dsd/localization.dsd'])],
+      install_requires=[
+          'launch',
+          'setuptools',
+      ],
+      zip_safe=True,
+      keywords=['ROS'],
+      license='MIT',
+      entry_points={
+          'console_scripts': ['localization_handler = bitbots_localization_handler.localization_handler:main',],
+      })
