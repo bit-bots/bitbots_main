0c7c8d6576652b4f7ed1cf03b985e86d0ee2daec
Florian Vahl <florian@flova.de>
2022-07-08 08:00:08 +0200

    First building local planner version
    Signed-off-by: Florian Vahl <florian@flova.de>

--- bitbots_local_planner/src/bitbots_local_planner.cpp
+++ bitbots_local_planner/src/bitbots_local_planner.cpp
@@ -1,93 +1,90 @@
 #include <bitbots_local_planner/bitbots_local_planner.h>
 
-#include <pluginlib/class_list_macros.h>
+#include <pluginlib/class_list_macros.hpp>
 
-PLUGINLIB_EXPORT_CLASS(bitbots_local_planner::BBPlanner, nav_core::BaseLocalPlanner)
+PLUGINLIB_EXPORT_CLASS(bitbots_local_planner::BBPlanner, nav2_core::Controller)
 
 namespace bitbots_local_planner
 {
 
-    BBPlanner::BBPlanner() : odom_helper_("odom")
+    BBPlanner::BBPlanner()
     {
     }
 
-    void BBPlanner::initialize(std::string name, tf2_ros::Buffer *tf_buffer, costmap_2d::Costmap2DROS *costmap_ros)
+    void BBPlanner::configure(
+        const rclcpp_lifecycle::LifecycleNode::WeakPtr & parent,
+        std::string name,
+        std::shared_ptr<tf2_ros::Buffer> tf_buffer,
+        std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros)
     {
-        // Init node handles
-        ros::NodeHandle private_nh("~/" + name);
+        parent_ = parent;
+        auto node = parent_.lock();
+
+        logger_ = std::make_shared<rclcpp::Logger>(node->get_logger());
+        clock_ = node->get_clock();
+        costmap_ros_ = costmap_ros;
 
         // Register publishers
-        local_plan_publisher_ = private_nh.advertise<nav_msgs::Path>("local_plan", 1);
+        local_plan_publisher_ = node->create_publisher<nav_msgs::msg::Path>("local_plan", 1);
 
         // Link tf buffer
         tf_buffer_ = tf_buffer;
         // Link costmap
         costmap_ros_ = costmap_ros;
 
-        // Set fields to default values
-        goal_reached_ = false;
+        auto plugin_name = name;
 
-        //odom_helper_ = new base_local_planner::OdometryHelperRos("motion_odometry");
+        node->get_parameter(plugin_name + ".carrot_distance", config_carrot_distance);
+        node->get_parameter(plugin_name + ".max_rotation_vel", config_max_rotation_vel);
+        node->get_parameter(plugin_name + ".max_vel_x", config_max_vel_x);
+        node->get_parameter(plugin_name + ".max_vel_y", config_max_vel_y);
+        node->get_parameter(plugin_name + ".min_vel_x", config_min_vel_x);
+        node->get_parameter(plugin_name + ".orient_to_goal_distance", config_orient_to_goal_distance);
+        node->get_parameter(plugin_name + ".rotation_slow_down_factor", config_rotation_slow_down_factor);
+        node->get_parameter(plugin_name + ".smoothing_k", config_smoothing_k);
+        node->get_parameter(plugin_name + ".translation_slow_down_factor", config_translation_slow_down_factor);
 
-        // Setup dynamic reconfigure
-        dsrv_ = new dynamic_reconfigure::Server<BBPlannerConfig>(private_nh);
-        dynamic_reconfigure::Server<BBPlannerConfig>::CallbackType cb = boost::bind(&BBPlanner::reconfigureCB, this, _1, _2);
-        dsrv_->setCallback(cb);
-
-        ROS_DEBUG("BBPlanner: Version 2 Init.");
+        RCLCPP_DEBUG(*logger_.get(), "BBPlanner: Version 2 Init.");
     }
 
-    void BBPlanner::motionOdomCB(const nav_msgs::Odometry::ConstPtr &msg)
+    /**
+     * @brief Method to cleanup resources.
+     */
+    void BBPlanner::cleanup()
     {
-        motion_odom_ = *msg;
-        ROS_ERROR("GOT ODOM");
+
     }
 
-    void BBPlanner::reconfigureCB(BBPlannerConfig &config, uint32_t level)
+    /**
+     * @brief Method to active planner and any threads involved in execution.
+     */
+    void BBPlanner::activate()
     {
-        // Save new config values
-        config_ = config;
+
     }
 
-	bool BBPlanner::setPlan(const std::vector<geometry_msgs::PoseStamped> &plan) {
-        // Save global plan
-	    global_plan_ = plan;
+    /**
+     * @brief Method to deactive planner and any threads involved in execution.
+     */
+    void BBPlanner::deactivate()
+    {
 
-        // set carrot distance to the config value or the end of the path if it is shorter
-	    int carrot_distance = std::min((int)global_plan_.size() - 1, config_.carrot_distance);
-
-        // Querys the pose of our carrot which we want to follow
-        bitbots_local_planner::getXPose(
-            *tf_buffer_,
-            global_plan_,
-            costmap_ros_->getGlobalFrameID(),
-            goal_pose_,
-            carrot_distance);
-
-        // Query the final pose of our robot at the end of the global plan
-        bitbots_local_planner::getXPose(
-            *tf_buffer_, global_plan_,
-            costmap_ros_->getGlobalFrameID(),
-            end_pose_,
-            global_plan_.size() - 1);
-
-        // Set our old pose
-        old_goal_pose_ = goal_pose_;
-
-        // We obviously didn't reach that plan (yet)
-        goal_reached_ = false;
-
-        return true;
     }
 
-    bool BBPlanner::computeVelocityCommands(geometry_msgs::Twist &cmd_vel)
+    geometry_msgs::msg::TwistStamped BBPlanner::computeVelocityCommands(
+        const geometry_msgs::msg::PoseStamped &pose,
+        const geometry_msgs::msg::Twist &velocity,
+        nav2_core::GoalChecker *goal_checker)
     {
         // Save time for profiling
-        ros::Time begin = ros::Time::now();
+        rclcpp::Time begin = rclcpp::Clock(RCL_SYSTEM_TIME).now();
+
+        // Create velocity msg
+        auto cmd_vel = geometry_msgs::msg::Twist();
 
         // Query the current robot pose as a transform
         tf2::Stamped<tf2::Transform> current_pose;
-        geometry_msgs::PoseStamped current_pose_gm;
+        geometry_msgs::msg::PoseStamped current_pose_gm;
         costmap_ros_->getRobotPose(current_pose_gm);
         tf2::fromMsg(current_pose_gm, current_pose);
 
@@ -107,11 +104,11 @@ namespace bitbots_local_planner
             end_pose_.getOrigin().y() - current_pose.getOrigin().y());
 
         // Calculate the translational walk velocity. It considers the distance and breaks if we are close to the final position of the global plan
-        double walk_vel = std::min(distance * config_.translation_slow_down_factor, config_.max_vel_x);
+        double walk_vel = std::min(distance * config_translation_slow_down_factor, config_max_vel_x);
 
         // Check if we are so close to the final position of the global plan that we want to align us with its orientation and not the heading towards its position
         double diff = 0;
-        if (distance > config_.orient_to_goal_distance)
+        if (distance > config_orient_to_goal_distance)
         {
             // Calculate the difference between our current heading and the heading towards the final position of the global plan
             diff = final_walk_angle - tf2::getYaw(current_pose.getRotation());
@@ -126,103 +123,168 @@ namespace bitbots_local_planner
         double min_angle = std::remainder(diff, 2 * M_PI);
         // Calculate our desired rotation velocity based on the angle difference and our max velocity
         double rot_goal_vel = std::clamp(
-            config_.rotation_slow_down_factor * min_angle,
-            -config_.max_rotation_vel,
-            config_.max_rotation_vel);
+            config_rotation_slow_down_factor * min_angle,
+            -config_max_rotation_vel,
+            config_max_rotation_vel);
 
-        // Check if we reached the goal. If we did so set the velocities to 0
-        if (distance < config_.position_accuracy && abs(min_angle) < config_.rotation_accuracy)
-        {
-            cmd_vel.linear.x = 0;
-            cmd_vel.linear.y = 0;
-            cmd_vel.angular.z = 0;
-            goal_reached_ = true;
+        // Get odometry velocity vector
+        geometry_msgs::PoseStamped robot_vel;
+        //odom_helper_.getRobotVel(robot_vel); TODO implement
+        // Calc current velocity value
+        double current_vel_ = std::hypot(robot_vel.pose.position.x, robot_vel.pose.position.y);
+
+        // Calculate the x and y components of our linear velocity based on the desired heading and the desired translational velocity.
+        cmd_vel.linear.x = std::cos(walk_angle - tf2::getYaw(current_pose.getRotation())) * walk_vel;
+        cmd_vel.linear.y = std::sin(walk_angle - tf2::getYaw(current_pose.getRotation())) * walk_vel;
+
+        // Scale command accordingly if a limit is acceded
+        if (cmd_vel.linear.x > config_max_vel_x) {
+            RCLCPP_DEBUG(*logger_.get(), "X LIMIT reached: %f > %f, with y %f", cmd_vel.linear.x, config_max_vel_x, cmd_vel.linear.y);
+            cmd_vel.linear.y *= config_max_vel_x / cmd_vel.linear.x;
+            cmd_vel.linear.x = config_max_vel_x;
+            RCLCPP_DEBUG(*logger_.get(), "X LIMIT set y %f", cmd_vel.linear.y);
         }
-        else
-        {
-            // Get odometry velocity vector
-            geometry_msgs::PoseStamped robot_vel;
-            odom_helper_.getRobotVel(robot_vel);
-            // Calc current velocity value
-            double current_vel_ = std::hypot(robot_vel.pose.position.x, robot_vel.pose.position.y);
 
-            // Calculate the x and y components of our linear velocity based on the desired heading and the desired translational velocity.
-            cmd_vel.linear.x = std::cos(walk_angle - tf2::getYaw(current_pose.getRotation())) * walk_vel;
-            cmd_vel.linear.y = std::sin(walk_angle - tf2::getYaw(current_pose.getRotation())) * walk_vel;
-
-            // Scale command accordingly if a limit is acceded
-            if (cmd_vel.linear.x > config_.max_vel_x) {
-                ROS_DEBUG("X LIMIT reached: %f > %f, with y %f", cmd_vel.linear.x, config_.max_vel_x, cmd_vel.linear.y);
-                cmd_vel.linear.y *= config_.max_vel_x / cmd_vel.linear.x;
-                cmd_vel.linear.x = config_.max_vel_x;
-                ROS_DEBUG("X LIMIT set y %f", cmd_vel.linear.y);
-            }
-
-            if (cmd_vel.linear.x < config_.min_vel_x) {
-                ROS_DEBUG("X LIMIT reached: %f < %f, with y %f", cmd_vel.linear.x, config_.min_vel_x, cmd_vel.linear.y);
-                cmd_vel.linear.y *= config_.min_vel_x / cmd_vel.linear.x;
-                cmd_vel.linear.x = config_.min_vel_x;
-                ROS_DEBUG("X LIMIT set y %f", cmd_vel.linear.y);
-            }
-
-            double max_y = config_.max_vel_y;
-
-            if (std::abs(cmd_vel.linear.y) > max_y) {
-                ROS_DEBUG("Y LIMIT reached: %f > %f, with x %f", cmd_vel.linear.y, max_y, cmd_vel.linear.x);
-                cmd_vel.linear.x *= max_y / std::abs(cmd_vel.linear.y);
-                cmd_vel.linear.y *= max_y / std::abs(cmd_vel.linear.y);
-                ROS_DEBUG("Y LIMIT set x %f", cmd_vel.linear.x);
-            }
-
-            // Complementary Filter for smooting the outputs
-            cmd_vel.linear.x = cmd_vel.linear.x * config_.smoothing_k + robot_vel.pose.position.x * (1.0 - config_.smoothing_k);
-            cmd_vel.linear.y = cmd_vel.linear.y * config_.smoothing_k + robot_vel.pose.position.y * (1.0 - config_.smoothing_k);
-
-            // Apply the desired rotational velocity
-            cmd_vel.angular.z = rot_goal_vel;
-            // We didn't reached our goal if we need this step
-            goal_reached_ = false;
-
-            ROS_DEBUG("End vel %f, %f\n", std::hypot(cmd_vel.linear.x, cmd_vel.linear.y), current_vel_);
+        if (cmd_vel.linear.x < config_min_vel_x) {
+            RCLCPP_DEBUG(*logger_.get(), "X LIMIT reached: %f < %f, with y %f", cmd_vel.linear.x, config_min_vel_x, cmd_vel.linear.y);
+            cmd_vel.linear.y *= config_min_vel_x / cmd_vel.linear.x;
+            cmd_vel.linear.x = config_min_vel_x;
+            RCLCPP_DEBUG(*logger_.get(), "X LIMIT set y %f", cmd_vel.linear.y);
         }
+
+        double max_y = config_max_vel_y;
+
+        if (std::abs(cmd_vel.linear.y) > max_y) {
+            RCLCPP_DEBUG(*logger_.get(), "Y LIMIT reached: %f > %f, with x %f", cmd_vel.linear.y, max_y, cmd_vel.linear.x);
+            cmd_vel.linear.x *= max_y / std::abs(cmd_vel.linear.y);
+            cmd_vel.linear.y *= max_y / std::abs(cmd_vel.linear.y);
+            RCLCPP_DEBUG(*logger_.get(), "Y LIMIT set x %f", cmd_vel.linear.x);
+        }
+
+        // Complementary Filter for smooting the outputs
+        cmd_vel.linear.x = cmd_vel.linear.x * config_smoothing_k + robot_vel.pose.position.x * (1.0 - config_smoothing_k);
+        cmd_vel.linear.y = cmd_vel.linear.y * config_smoothing_k + robot_vel.pose.position.y * (1.0 - config_smoothing_k);
+
+        // Apply the desired rotational velocity
+        cmd_vel.angular.z = rot_goal_vel;
+
+        RCLCPP_DEBUG(*logger_.get(), "End vel %f, %f\n", std::hypot(cmd_vel.linear.x, cmd_vel.linear.y), current_vel_);
+
         // Publich our "local plan" for viz purposes
         publishPlan();
 
         // Print profiling result
-        ros::Time end = ros::Time::now();
-        ros::Duration duration = end - begin;
-        ROS_DEBUG("BBPlanner: Calculation time: %f seconds", duration.toSec());
-        return true;
+        rclcpp::Time end = rclcpp::Clock(RCL_SYSTEM_TIME).now();
+        rclcpp::Duration duration = end - begin;
+        RCLCPP_DEBUG(*logger_.get(), "BBPlanner: Calculation time: %f seconds", duration.seconds());
+
+        // Create stamped msg
+        auto cmd_vel_stamped = geometry_msgs::msg::TwistStamped();
+        cmd_vel_stamped.header.stamp = clock_->now();
+        cmd_vel_stamped.twist = cmd_vel;
+        return cmd_vel_stamped;
     }
 
-    bool BBPlanner::isGoalReached()
+    void BBPlanner::setPlan(const nav_msgs::msg::Path & path)
     {
-        if (goal_reached_)
-        {
-            ROS_DEBUG("BBPlanner: Goal reached.");
-        }
-        return goal_reached_;
+        // Save global plan
+	    global_plan_ = path.poses;
+
+        // set carrot distance to the config value or the end of the path if it is shorter
+	    int carrot_distance = std::min((int)global_plan_.size() - 1, config_carrot_distance);
+
+        // Querys the pose of our carrot which we want to follow
+        this->getXPose(
+            costmap_ros_->getGlobalFrameID(),
+            goal_pose_,
+            carrot_distance);
+
+        // Query the final pose of our robot at the end of the global plan
+        this->getXPose(
+            costmap_ros_->getGlobalFrameID(),
+            end_pose_,
+            global_plan_.size() - 1);
+
+        // Set our old pose
+        old_goal_pose_ = goal_pose_;
     }
+
+
+    void BBPlanner::getXPose(
+        const std::string &global_frame, tf2::Stamped<tf2::Transform> &goal_pose, int plan_point) {
+        if (global_plan_.empty()) {
+            RCLCPP_ERROR(*logger_.get(), "Received plan with zero length");
+        }
+        if (plan_point >= (int) global_plan_.size()) {
+            RCLCPP_ERROR(*logger_.get(),"Goal_functions: Plan_point %d to big. Plan size: %lu", plan_point, global_plan_.size());
+        }
+
+        const geometry_msgs::msg::PoseStamped &plan_goal_pose = global_plan_.at(plan_point);
+        try {
+            tf_buffer_->canTransform(global_frame, rclcpp::Time(0),
+                                plan_goal_pose.header.frame_id, plan_goal_pose.header.stamp,
+                                plan_goal_pose.header.frame_id, rclcpp::Duration(0, 0.5 * 1e9));
+
+            geometry_msgs::msg::TransformStamped tmp = tf_buffer_->lookupTransform(global_frame,
+                                                                            rclcpp::Time(0),
+                                                                            plan_goal_pose.header.frame_id,
+                                                                            plan_goal_pose.header.stamp,
+                                                                            plan_goal_pose.header.frame_id,
+                                                                            rclcpp::Duration(0, 0.5 * 1e9));
+            tf2::Stamped<tf2::Transform> transform;
+            tf2::convert(tmp, transform);
+
+            tf2::convert(plan_goal_pose, goal_pose);
+            goal_pose.setData(transform * goal_pose);
+            goal_pose.stamp_ = transform.stamp_;
+            goal_pose.frame_id_ = global_frame;
+        }
+        catch (tf2::LookupException &ex) {
+            RCLCPP_ERROR(*logger_.get(),"No Transform available Error: %s\n", ex.what());
+        }
+        catch (tf2::ConnectivityException &ex) {
+            RCLCPP_ERROR(*logger_.get(),"Connectivity Error: %s\n", ex.what());
+        }
+        catch (tf2::ExtrapolationException &ex) {
+            RCLCPP_ERROR(*logger_.get(),"Extrapolation Error: %s\n", ex.what());
+            if (!global_plan_.empty()) {
+            RCLCPP_ERROR(*logger_.get(),"Global Frame: %s Plan Frame size %d: %s\n",
+                        global_frame.c_str(),
+                        (unsigned int) global_plan_.size(),
+                        global_plan_[0].header.frame_id.c_str());
+            }
+        }
+    }
+
+    void BBPlanner::motionOdomCB(const nav_msgs::msg::Odometry::SharedPtr msg)
+    {
+        motion_odom_ = msg;
+    }
+
+    void BBPlanner::setSpeedLimit(const double & speed_limit, const bool & percentage)
+    {
+
+    };
+
 
     void BBPlanner::publishPlan()
     {
         // Create a path message
-        nav_msgs::Path gui_path;
+        nav_msgs::msg::Path gui_path;
         //Get the robot pose
-        geometry_msgs::PoseStamped robot_pose, carrot_pose;
+        geometry_msgs::msg::PoseStamped robot_pose;
         costmap_ros_->getRobotPose(robot_pose);
         // Set the path length to 2. Wow so long
         gui_path.poses.resize(2);
         // Set header stuff
         gui_path.header.frame_id = costmap_ros_->getGlobalFrameID();
-        gui_path.header.stamp = ros::Time::now();
-        // Get the carrot pose
-        tf2::toMsg(goal_pose_, carrot_pose);
+        gui_path.header.stamp = clock_->now();
         // The the positions of the two path elements (1. The robot position, 2. The carrot position)
         gui_path.poses[0].pose.position = robot_pose.pose.position;
-        gui_path.poses[1].pose.position = carrot_pose.pose.position;
+        gui_path.poses[1].pose.position.x = goal_pose_.getOrigin().x();
+        gui_path.poses[1].pose.position.y = goal_pose_.getOrigin().y();
         // Publish
-        local_plan_publisher_.publish(gui_path);
+        local_plan_publisher_->publish(gui_path);
     }
 
     BBPlanner::~BBPlanner()
