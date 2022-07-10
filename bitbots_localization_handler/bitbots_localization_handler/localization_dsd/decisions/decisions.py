f2509ec4f9deb850e2e60bc407ec14898a12900e
Florian Vahl <florian@flova.de>
2022-07-10 22:18:24 +0000

    Init after unpenelization

--- bitbots_localization_handler/bitbots_localization_handler/localization_dsd/decisions/decisions.py
+++ bitbots_localization_handler/bitbots_localization_handler/localization_dsd/decisions/decisions.py
@@ -245,23 +245,24 @@ class SecondaryStateTeamDecider(AbstractDecisionElement):
 
 
 class CheckPenalized(AbstractDecisionElement):
-    """
-    Decides if we are penalized.
-    """
-
     def __init__(self, blackboard, dsd, parameters=None):
-        super(CheckPenalized, self).__init__(blackboard, dsd, parameters)
+        super().__init__(blackboard, dsd, parameters)
 
     def perform(self, reevaluate=False):
+        """
+        Determines if the robot is penalized by the game controller.
+        """
+        self.publish_debug_data("Seconds since unpenalized",
+                                self.blackboard.gamestate.get_seconds_since_unpenalized())
         if self.blackboard.gamestate.get_is_penalized():
             return "YES"
+        elif self.blackboard.gamestate.get_seconds_since_unpenalized() < 1:
+            self.publish_debug_data("Reason", "Just unpenalized")
+            return "JUST_UNPENALIZED"
         else:
             return "NO"
 
     def get_reevaluate(self):
-        """
-        The state can change during the game
-        """
         return True
 
 
