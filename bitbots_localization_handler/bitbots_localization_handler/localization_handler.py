3ed5a9b4013f9b717d37b663773d6800a995ce02
Florian Vahl <florian@flova.de>
2022-07-10 20:35:17 +0000

    Reorder executor creation

--- bitbots_localization_handler/bitbots_localization_handler/localization_handler.py
+++ bitbots_localization_handler/bitbots_localization_handler/localization_handler.py
@@ -21,7 +21,6 @@ def init(node: Node):
     node.get_logger().warn(f"{global_params_dict}")
     global_params = [("field_length", global_params_dict["field_length"]),
                      ("team_id", global_params_dict["team_id"])]
-    node.get_logger().warn(f"{global_params}")
     node.declare_parameters(parameters=global_params, namespace="")
 
     blackboard = LocalizationBlackboard(node)
@@ -43,11 +42,11 @@ def main(args=None):
     rclpy.init(args=None)
     # needed to init rclcpp ros for moveit_bindings
     node = Node("bitbots_localization_handler", automatically_declare_parameters_from_overrides=True)
-    multi_executor = MultiThreadedExecutor()
-    multi_executor.add_node(node)
-
     dsd = init(node)
     node.create_timer(1 / 25.0, dsd.update)
+
+    multi_executor = MultiThreadedExecutor()
+    multi_executor.add_node(node)
 
     try:
         multi_executor.spin()
