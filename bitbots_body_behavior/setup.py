54031a7792c32bced793ce4068f9cbb97683fe15
Florian Vahl <git@flova.de>
2022-10-17 23:47:32 +0200

    Fix body behavior warnings

--- bitbots_body_behavior/setup.py
+++ bitbots_body_behavior/setup.py
@@ -12,6 +12,8 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
+                ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name + "/config",
             glob.glob('config/*.yaml')),
         ('share/' + package_name + '/launch',
