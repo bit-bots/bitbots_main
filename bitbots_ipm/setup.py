9b4de98e2462cd0bb942261e9283600b027247d9
Jan Gutsche <34797331+jaagut@users.noreply.github.com>
2022-11-24 17:21:39 +0100

    Cleanup setup.py

--- bitbots_ipm/setup.py
+++ bitbots_ipm/setup.py
@@ -2,9 +2,6 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    #packages=['bitbots_ipm'],
-    #scripts=['bin/myscript'],
-    #package_dir={'': 'src'}
 )
 
 setup(**d)
