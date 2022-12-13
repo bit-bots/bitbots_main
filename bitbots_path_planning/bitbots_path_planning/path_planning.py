2f5f9ace6abf261292a811fc08063f447ba26973
Florian Vahl <git@flova.de>
2022-12-13 16:17:23 +0100

    Fix tricky error

--- bitbots_path_planning/bitbots_path_planning/path_planning.py
+++ bitbots_path_planning/bitbots_path_planning/path_planning.py
@@ -1,17 +1,18 @@
 import rclpy
 import soccer_vision_3d_msgs.msg as sv3dm
 import tf2_ros as tf2
-
 from bitbots_path_planning.controller import Controller
 from bitbots_path_planning.map import Map
 from bitbots_path_planning.planner import Planner
 from geometry_msgs.msg import PoseStamped, Twist
-from humanoid_league_msgs.msg import PoseWithCertaintyStamped
 from nav_msgs.msg import OccupancyGrid, Path
+from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.duration import Duration
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from std_msgs.msg import Empty
+
+from humanoid_league_msgs.msg import PoseWithCertaintyStamped
 
 
 class PathPlanning(Node):
@@ -36,19 +37,32 @@ class PathPlanning(Node):
         self.controller = Controller(node=self, buffer=self.tf_buffer)
 
         # Subscribe
+        callback_group = ReentrantCallbackGroup()
         self.create_subscription(
             PoseWithCertaintyStamped,
             self.declare_parameter('map.ball_update_topic', 'ball_relative_filtered').value,
              self.map.set_ball,
-             5)
+             5,
+             callback_group=callback_group)
         self.create_subscription(
             sv3dm.RobotArray,
             self.declare_parameter('map.robot_update_topic', 'robots_relative_filtered').value,
             self.map.set_robots,
-            5)
+            5,
+            callback_group=callback_group)
 
-        self.create_subscription(PoseStamped, 'goal_pose', self.planner.set_goal, 5)
-        self.create_subscription(Empty, 'move_base/cancel', lambda _: self.planner.cancel, 5)
+        self.create_subscription(
+            PoseStamped,
+            'goal_pose',
+            self.planner.set_goal,
+            5,
+            callback_group=callback_group)
+        self.create_subscription(
+            Empty,
+            'move_base/cancel',
+            lambda _: self.planner.cancel(),
+            5,
+            callback_group=callback_group)
 
         # Publish cmd_vel
         self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', 1)
