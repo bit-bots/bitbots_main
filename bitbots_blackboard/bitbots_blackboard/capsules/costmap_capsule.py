c3180ca8d24755599a91142f4084f978b93292ab
Valerie Bartel <v.bartel.vb@gmail.com>
2023-01-30 14:39:25 +0100

    fix position

--- bitbots_blackboard/bitbots_blackboard/capsules/costmap_capsule.py
+++ bitbots_blackboard/bitbots_blackboard/capsules/costmap_capsule.py
@@ -69,13 +69,15 @@ class CostmapCapsule:
         """
         Callback with new obstacles
         """
+        self._blackboard.node.get_logger().warn("entering method")
         # Init a new obstacle costmap
         obstacle_map = np.zeros_like(self.costmap)
         # Iterate over all obstacles
         robot: Robot
         for robot in msg.robots:
+            self._blackboard.node.get_logger().warn(str(robot))
             # Convert position to array index
-            idx_x, idx_y = self.field_2_costmap_coord(robot.bb.center.position.x, robot.bb.center.position.x)
+            idx_x, idx_y = self.field_2_costmap_coord(robot.bb.center.position.x, robot.bb.center.position.y)
             # TODO inflate
             # Draw obstacle with smoothing independent weight on obstacle costmap
             obstacle_map[idx_x, idx_y] = \
@@ -88,6 +90,8 @@ class CostmapCapsule:
         self.costmap = self.base_costmap + obstacle_map - self.pass_map
         # Publish debug costmap
         self.publish_costmap()
+        self._blackboard.node.get_logger().warn("leaving method")
+
 
     def publish_costmap(self):
         """
