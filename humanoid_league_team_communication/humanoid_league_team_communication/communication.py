472c66ee07e932add27294963b4a4fdd67a677f9
texhnolyze <joegriepen@gmail.com>
2023-01-31 21:25:02 +0100

    refactor(team_comm): communication code review
    intoducing handling of truncated messages if a protobuf message should
    be larger than 1024 bytes, we then double the buffer size for the next
    iteration

--- humanoid_league_team_communication/humanoid_league_team_communication/communication.py
+++ humanoid_league_team_communication/humanoid_league_team_communication/communication.py
@@ -1,4 +1,6 @@
+from ipaddress import IPv4Address
 import socket
+from typing import List, Optional
 
 from rclpy.node import Node
 
@@ -8,17 +10,18 @@ class SocketCommunication:
     def __init__(self, node: Node, logger, team_id, robot_id):
         self.logger = logger
 
-        self.socket: socket.socket = None
-        self.target_host = node.get_parameter('target_host').get_parameter_value().string_value
+        self.buffer_size: int = 1024
+        self.socket: Optional[socket.socket] = None
+        self.target_ip: IPv4Address = IPv4Address(node.get_parameter('target_ip').value)
 
-        if self.target_host == '127.0.0.1':
-            # local mode, bind to port depending on bot id and team id
-            local_target_ports = node.get_parameter('local_target_ports').get_parameter_value().integer_array_value
+        if self.target_ip.is_loopback:
+            # local mode on loopback device, bind to port depending on bot id and team id
+            local_target_ports: List[int] = node.get_parameter('local_target_ports').value
             self.target_ports = [port + 10 * team_id for port in local_target_ports]
             self.receive_port = self.target_ports[robot_id - 1]
         else:
-            target_port = node.get_parameter('target_port').get_parameter_value().integer_value
-            receive_port = node.get_parameter('receive_port').get_parameter_value().integer_value
+            target_port: int = node.get_parameter('target_port').value
+            receive_port: int = node.get_parameter('receive_port').value
             self.target_ports = [target_port]
             self.receive_port = receive_port
 
@@ -44,10 +47,26 @@ class SocketCommunication:
             self.socket.close()
             self.logger.info("Connection closed.")
 
-    def receive_message(self) -> bytes:
-        return self.socket.recv(1024)
+    def receive_message(self) -> Optional[bytes]:
+        self.assert_is_setup()
+        msg, _, flags, _ = self.socket.recvmsg(self.buffer_size)
+        is_message_trucated = flags & socket.MSG_TRUNC
+        if is_message_trucated:
+            self.handle_truncated_message()
+            return None
+        else:
+            return msg
+
+    def handle_truncated_message(self):
+        self.logger.warn(f"recvmsg flag {socket.MSG_TRUNC} signaling a packet larger than buffer size {self.buffer_size}")
+        self.buffer_size *= 2
+        self.logger.info(f"doubled buffer size to {self.buffer_size}")
 
     def send_message(self, message):
+        self.assert_is_setup()
         for port in self.target_ports:
-            self.logger.debug(f'Sending message to {port} on {self.target_host}')
-            self.socket.sendto(message, (self.target_host, port))
+            self.logger.debug(f'Sending message to {port} on {self.target_ip}')
+            self.socket.sendto(message, (str(self.target_ip), port))
+
+    def assert_is_setup(self):
+        assert self.is_setup(), "Socket is not yet initialized"
