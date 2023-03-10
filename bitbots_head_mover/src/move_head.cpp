2dc54dab5e2787d876dc7b342bb3e24c53c1e2da
valerie <v.bartel.vb@gmail.com>
2023-03-10 14:05:28 +0100

    add a subscriber

--- bitbots_head_mover/src/move_head.cpp
+++ bitbots_head_mover/src/move_head.cpp
@@ -5,6 +5,9 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+
+using std::placeholders::_1;
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
@@ -19,6 +22,8 @@ public:
     publisher_ = this->create_publisher<bitbots_msgs::msg::JointCommand>("head_motor_goals", 10); 
     timer_ = this->create_wall_timer(
       500ms, std::bind(&HeadMover::timer_callback, this));
+      subscription_ = this->create_subscription<std_msgs::msg::String>( // here we want to call world_model.ball_filtered_callback
+      "head_mode", 10, std::bind(&HeadMover::head_mode_callback, this, _1)); // should be callback group 1
   }
 
 private:
@@ -28,6 +33,11 @@ private:
     RCLCPP_INFO(this->get_logger(), "Publishing..");
     publisher_->publish(message);
   }
+    void head_mode_callback(const sensor_msgs::msg::JointState::SharedPtr msg) const // Why SharedPtr?
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard something..");
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<bitbots_msgs::msg::JointCommand>::SharedPtr publisher_;
   size_t count_;
