a91eb0aed6ee06d265cdc46ea860b9c2fa20fa29
Valerie Bartel <v.bartel.vb@gmail.com>
2023-04-14 18:44:01 +0200

    add usage of generate_parameter_library

--- bitbots_head_mover/src/move_head.cpp
+++ bitbots_head_mover/src/move_head.cpp
@@ -6,7 +6,7 @@
 #include "std_msgs/msg/string.hpp"
 #include <string>
 #include <iostream>
-
+#include "head_parameters.hpp"
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
@@ -17,14 +17,12 @@ class HeadMover : public rclcpp::Node
 {
   //declare publisher and timer
   rclcpp::Publisher<bitbots_msgs::msg::JointCommand>::SharedPtr publisher_;
-  rclcpp::TimerBase::SharedPtr timer_;
+
  //declare subscriber
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
   std::string head_mode_decision_;
 
 
-
- float tilt_speed_;
 
 
 public:
@@ -32,15 +30,16 @@ public:
   : Node("head_mover")
   {
     publisher_ = this->create_publisher<bitbots_msgs::msg::JointCommand>("head_motor_goals", 10); 
-      subscription_ = this->create_subscription<std_msgs::msg::String>( // here we want to call world_model.ball_filtered_callback
+    subscription_ = this->create_subscription<std_msgs::msg::String>( // here we want to call world_model.ball_filtered_callback
       "head_mode", 10, std::bind(&HeadMover::head_mode_callback, this, _1)); // should be callback group 1
       std::cout << "Hello World" << std::endl;
-
       // load parameters from config
-      this->declare_parameter("head.look_at.pan_speed", 2.0);
-this->get_parameter("head.look_at.tilt_speed", tilt_speed_);
+       auto param_listener = std::make_shared<move_head::ParamListener>(rclcpp::Node::make_shared("head_mover")); //is this a problem?
+       auto params = param_listener->get_params();
+       auto pan_speed = params.look_at.pan_speed;
+
       //print the param value
-      std::cout << "Head pan max speed is: " << tilt_speed_ << std::endl;
+      std::cout << "Head pan max speed is: " << pan_speed << std::endl;
   }
 
 
