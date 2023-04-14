262ee2ef29d68f2c3ddc39dfe2adfe3137d0913e
Valerie Bartel <v.bartel.vb@gmail.com>
2023-04-14 12:41:07 +0200

    start modeling behavior tree

--- bitbots_head_mover/src/move_head.cpp
+++ bitbots_head_mover/src/move_head.cpp
@@ -2,11 +2,10 @@
 #include <chrono>
 #include <memory>
 #include <bitbots_msgs/msg/joint_command.hpp>
-
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "sensor_msgs/msg/joint_state.hpp"
-
+#include <string>
+#include <iostream>
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
@@ -15,38 +14,45 @@ using namespace std::chrono_literals;
 
 class HeadMover : public rclcpp::Node
 {
+  //declare publisher and timer
+  rclcpp::Publisher<bitbots_msgs::msg::JointCommand>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  //declare head_mode_decision_
+  std::string head_mode_decision_;
+
 public:
   HeadMover()
-  : Node("head_mover"), count_(0)
+  : Node("head_mover")
   {
     publisher_ = this->create_publisher<bitbots_msgs::msg::JointCommand>("head_motor_goals", 10); 
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&HeadMover::timer_callback, this));
       subscription_ = this->create_subscription<std_msgs::msg::String>( // here we want to call world_model.ball_filtered_callback
       "head_mode", 10, std::bind(&HeadMover::head_mode_callback, this, _1)); // should be callback group 1
+      std::cout << "Hello World" << std::endl;
   }
 
+
 private:
-  void timer_callback()
-  {
-    bitbots_msgs::msg::JointCommand message = bitbots_msgs::msg::JointCommand(); // now fill the data somehow?
-    RCLCPP_INFO(this->get_logger(), "Publishing..");
-    publisher_->publish(message);
-  }
-    void head_mode_callback(const sensor_msgs::msg::JointState::SharedPtr msg) const // Why SharedPtr?
-  {
-    RCLCPP_INFO(this->get_logger(), "I heard something..");
-  }
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<bitbots_msgs::msg::JointCommand>::SharedPtr publisher_;
-  size_t count_;
-};
+//write head_mode_callback
+void head_mode_callback(const std_msgs::msg::String::SharedPtr msg)
+{
+  head_mode_decision_ = msg->data.c_str();
+  std::cout << "I heard: [" << head_mode_decision_ << "]" << std::endl;
+}
+//write timer_callback
+
+void head_behavior()
+{
+//print head_mode_decision_
+std::cout << "Head mode is: " << head_mode_decision_ << std::endl;
+}};
+  
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<HeadMover>());
   rclcpp::shutdown();
+  
   return 0;
 }
