0a3e9f9e69cf52fdb5d9286df87d609f18f1ffbe
Valerie Bartel <v.bartel.vb@gmail.com>
2023-04-14 17:31:12 +0200

    add config

--- bitbots_head_mover/src/move_head.cpp
+++ bitbots_head_mover/src/move_head.cpp
@@ -6,6 +6,7 @@
 #include "std_msgs/msg/string.hpp"
 #include <string>
 #include <iostream>
+
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
@@ -17,9 +18,14 @@ class HeadMover : public rclcpp::Node
   //declare publisher and timer
   rclcpp::Publisher<bitbots_msgs::msg::JointCommand>::SharedPtr publisher_;
   rclcpp::TimerBase::SharedPtr timer_;
+ //declare subscriber
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-  //declare head_mode_decision_
   std::string head_mode_decision_;
+
+
+
+ float tilt_speed_;
+
 
 public:
   HeadMover()
@@ -29,6 +35,12 @@ public:
       subscription_ = this->create_subscription<std_msgs::msg::String>( // here we want to call world_model.ball_filtered_callback
       "head_mode", 10, std::bind(&HeadMover::head_mode_callback, this, _1)); // should be callback group 1
       std::cout << "Hello World" << std::endl;
+
+      // load parameters from config
+      this->declare_parameter("head.look_at.pan_speed", 2.0);
+this->get_parameter("head.look_at.tilt_speed", tilt_speed_);
+      //print the param value
+      std::cout << "Head pan max speed is: " << tilt_speed_ << std::endl;
   }
 
 
@@ -45,8 +57,11 @@ void head_behavior()
 {
 //print head_mode_decision_
 std::cout << "Head mode is: " << head_mode_decision_ << std::endl;
+if (head_mode_decision_ == "LOOK_UP")
+{
+
 }};
-  
+};
 
 int main(int argc, char * argv[])
 {
