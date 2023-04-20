c5ef0f37390ba53a9e0de595913e8cc95170f0e7
Timon Engelke <timon.engelke@online.de>
2023-04-20 18:31:31 +0200

    Cleanup

--- bitbots_head_mover/src/move_head.cpp
+++ bitbots_head_mover/src/move_head.cpp
@@ -21,35 +21,20 @@
 
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
-#include <geometry_msgs/msg/transform_stamped.hpp>
-#include <geometry_msgs/msg/twist_stamped.hpp>
-#include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
 
 #include "rclcpp/clock.hpp"
-#include <rclcpp/duration.hpp>
-
-#include <std_msgs/msg/header.hpp>
-#include <std_srvs/srv/trigger.hpp>
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-//#include <tf_transformations/euler_from_quaternion.h>
 
 
 #include <bio_ik/bio_ik.h>
 #include <moveit/robot_state/conversions.h>
 #include <tf2/convert.h>
 
-#include <bio_ik_msgs/msg/ik_request.hpp>
 #include <bio_ik_msgs/msg/ik_response.hpp>
-#include <moveit_msgs/msg/move_it_error_codes.hpp>
-#include <moveit_msgs/msg/robot_state.hpp>
-#include <moveit_msgs/srv/get_position_fk.hpp>
-#include <moveit_msgs/srv/get_position_ik.hpp>
 #include <rclcpp/logger.hpp>
-#include <tf2_eigen/tf2_eigen.hpp>
 #include <rclcpp/executors/events_executor/events_executor.hpp>
 
-#include "rcl_interfaces/srv/get_parameters.hpp"
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 
@@ -106,10 +91,8 @@ class HeadMover : public rclcpp::Node {
   HeadMover()
       : Node("head_mover", rclcpp::NodeOptions().allow_undeclared_parameters(true)) {
 
-    RCLCPP_INFO(this->get_logger(), "Hello World 1");
     timer_ = this->create_wall_timer(
         500ms, std::bind(&HeadMover::behave, this));
-    RCLCPP_INFO(this->get_logger(), "Hello World 1");
     position_publisher_ = this->create_publisher<bitbots_msgs::msg::JointCommand>("head_motor_goals", 10);
     head_mode_subscriber_ =
         this->create_subscription<humanoid_league_msgs::msg::HeadMode>( // here we want to call world_model.ball_filtered_callback
@@ -196,27 +179,8 @@ class HeadMover : public rclcpp::Node {
     ball_teammate_ = geometry_msgs::msg::PointStamped();
     ball_teammate_.header.frame_id = map_frame_;
     ball_teammate_.header.stamp = this->now();
-    RCLCPP_INFO(this->get_logger(), "Hello World 1.5");
-
-    RCLCPP_INFO(this->get_logger(), "Hello World 2");
-  }
-  static tf2::Vector3 p(const geometry_msgs::msg::Point &p) {
-    return tf2::Vector3(p.x, p.y, p.z);
   }
 
-  static tf2::Vector3 p(const geometry_msgs::msg::Vector3 &p) {
-    return tf2::Vector3(p.x, p.y, p.z);
-  }
-
-  static tf2::Quaternion q(const geometry_msgs::msg::Quaternion &q) {
-    return tf2::Quaternion(q.x, q.y, q.z, q.w);
-  }
-
-  static double w(double w, double def = 1.0) {
-    if (w == 0 || !std::isfinite(w))
-      w = def;
-    return w;
-  }
   void head_mode_callback(const humanoid_league_msgs::msg::HeadMode::SharedPtr msg) {
     /**
      *ROS Subscriber callback for /head_mode message.
@@ -228,7 +192,6 @@ class HeadMover : public rclcpp::Node {
   void joint_state_callback(const sensor_msgs::msg::JointState::SharedPtr msg) {
     current_joint_state_ = *msg;
     position_publisher_->publish(pos_msg_);
-
   }
 
 /*
@@ -467,26 +430,29 @@ class HeadMover : public rclcpp::Node {
 
 // LookAt
   std::pair<double, double> get_motor_goals_from_point(geometry_msgs::msg::Point point) {
-    geometry_msgs::msg::Point target;
-    target.x = point.x;
-    target.y = point.y;
-    target.z = point.z;
     // use bio ik
-    bio_ik_msgs::msg::IKRequest::SharedPtr request = std::make_shared<bio_ik_msgs::msg::IKRequest>();
-    request->group_name = "Head";
-    request->timeout.sec = 1;
-    request->approximate = true;
-    // append look_at_goals
-    bio_ik_msgs::msg::LookAtGoal goal;
-    goal.target = target;
-    goal.weight = 1.0;
-    goal.axis.x = 1.0;
-    goal.link_name = "camera";
+    bio_ik::BioIKKinematicsQueryOptions ik_options;
+    ik_options.return_approximate_solution = true;
+    ik_options.replace = true;
 
-    request->look_at_goals.push_back(goal);
+    bio_ik::LookAtGoal goal;
+    goal.setTarget({point.x, point.y, point.z});
+    goal.setWeight(1.0);
+    goal.setAxis({1.0, 0.0, 0.0});
+    goal.setLinkName("camera");
+    ik_options.goals.emplace_back(new bio_ik::LookAtGoal(goal));
 
-    bio_ik_msgs::msg::IKResponse response = getBioIKIK(request);
-    if (response.error_code.val == moveit_msgs::msg::MoveItErrorCodes::SUCCESS) {
+    auto joint_model_group = robot_model_->getJointModelGroup("Head");
+
+    float timeout_seconds = 1.0;
+    bool success = robot_state_->setFromIK(joint_model_group, EigenSTL::vector_Isometry3d(), std::vector<std::string>(),
+                                           timeout_seconds, moveit::core::GroupStateValidityCallbackFn(), ik_options);
+    robot_state_->update();
+
+    bio_ik_msgs::msg::IKResponse response;
+    moveit::core::robotStateToRobotStateMsg(*robot_state_, response.solution);
+    response.solution_fitness = ik_options.solution_fitness;
+    if (success) {
       std::pair<double, double>
           states = {response.solution.joint_state.position[0], response.solution.joint_state.position[1]};
       return states;
@@ -496,118 +462,6 @@ class HeadMover : public rclcpp::Node {
       return states;
     }
 
-  }
-
-  bio_ik_msgs::msg::IKResponse getBioIKIK(bio_ik_msgs::msg::IKRequest::SharedPtr msg) {
-    // extra method to use BioIK specific goals
-    bio_ik_msgs::msg::IKRequest request = *msg;
-
-    bio_ik::BioIKKinematicsQueryOptions ik_options;
-    ik_options.return_approximate_solution = request.approximate;
-    ik_options.fixed_joints = request.fixed_joints;
-    ik_options.replace = true;
-
-    convertGoals(request, ik_options);
-
-    moveit::core::GroupStateValidityCallbackFn callback;
-    if (request.avoid_collisions) {
-      std::cout << "Avoid collisions not implemented in bitbots_moveit_bindings";
-      exit(1);
-    }
-    auto joint_model_group = robot_model_->getJointModelGroup(request.group_name);
-    //print joint model group
-    std::cout << "Joint model group: " << request.group_name << std::endl;
-    if (!joint_model_group) {
-      std::cout << "Group name in IK call not specified";
-      exit(1);
-    }
-
-    float timeout_seconds = request.timeout.sec + request.timeout.nanosec * 1e9;
-    bool success = robot_state_->setFromIK(joint_model_group, EigenSTL::vector_Isometry3d(), std::vector<std::string>(),
-                                           timeout_seconds, callback, ik_options);
-
-    robot_state_->update();
-
-    bio_ik_msgs::msg::IKResponse response;
-    moveit::core::robotStateToRobotStateMsg(*robot_state_, response.solution);
-    response.solution_fitness = ik_options.solution_fitness;
-    if (success) {
-      response.error_code.val = moveit_msgs::msg::MoveItErrorCodes::SUCCESS;
-    } else {
-      response.error_code.val = moveit_msgs::msg::MoveItErrorCodes::NO_IK_SOLUTION;
-    }
-    return response;
-  }
-
-  static void convertGoals(const bio_ik_msgs::msg::IKRequest &ik_request,
-                           bio_ik::BioIKKinematicsQueryOptions &ik_options) {
-    for (auto &m: ik_request.position_goals) {
-      ik_options.goals.emplace_back(new bio_ik::PositionGoal(m.link_name, p(m.position), w(m.weight)));
-    }
-
-    for (auto &m: ik_request.orientation_goals) {
-      ik_options.goals.emplace_back(new bio_ik::OrientationGoal(m.link_name, q(m.orientation), w(m.weight)));
-    }
-
-    for (auto &m: ik_request.pose_goals) {
-      auto *g = new bio_ik::PoseGoal(m.link_name, p(m.pose.position), q(m.pose.orientation), w(m.weight));
-      g->setRotationScale(w(m.rotation_scale, 0.5));
-      ik_options.goals.emplace_back(g);
-    }
-
-    for (auto &m: ik_request.look_at_goals) {
-      ik_options.goals.emplace_back(new bio_ik::LookAtGoal(m.link_name, p(m.axis), p(m.target), w(m.weight)));
-    }
-
-    for (auto &m: ik_request.min_distance_goals) {
-      ik_options.goals.emplace_back(new bio_ik::MinDistanceGoal(m.link_name, p(m.target), m.distance, w(m.weight)));
-    }
-
-    for (auto &m: ik_request.max_distance_goals) {
-      ik_options.goals.emplace_back(new bio_ik::MaxDistanceGoal(m.link_name, p(m.target), m.distance, w(m.weight)));
-    }
-
-    for (auto &m: ik_request.line_goals) {
-      ik_options.goals.emplace_back(new bio_ik::LineGoal(m.link_name, p(m.position), p(m.direction), w(m.weight)));
-    }
-
-    for (auto &m: ik_request.avoid_joint_limits_goals) {
-      ik_options.goals.emplace_back(new bio_ik::AvoidJointLimitsGoal(w(m.weight), !m.primary));
-    }
-
-    for (auto &m: ik_request.minimal_displacement_goals) {
-      ik_options.goals.emplace_back(new bio_ik::MinimalDisplacementGoal(w(m.weight), !m.primary));
-    }
-
-    for (auto &m: ik_request.center_joints_goals) {
-      ik_options.goals.emplace_back(new bio_ik::CenterJointsGoal(w(m.weight), !m.primary));
-    }
-
-    for (auto &m: ik_request.joint_variable_goals) {
-      ik_options.goals.emplace_back(
-          new bio_ik::JointVariableGoal(m.variable_name, m.variable_position, w(m.weight), m.secondary));
-    }
-
-    for (auto &m: ik_request.balance_goals) {
-      auto *g = new bio_ik::BalanceGoal(p(m.target), w(m.weight));
-      if (m.axis.x || m.axis.y || m.axis.z) {
-        g->setAxis(p(m.axis));
-      }
-      ik_options.goals.emplace_back(g);
-    }
-
-    for (auto &m: ik_request.side_goals) {
-      ik_options.goals.emplace_back(new bio_ik::SideGoal(m.link_name, p(m.axis), p(m.direction), w(m.weight)));
-    }
-
-    for (auto &m: ik_request.direction_goals) {
-      ik_options.goals.emplace_back(new bio_ik::DirectionGoal(m.link_name, p(m.axis), p(m.direction), w(m.weight)));
-    }
-
-    for (auto &m: ik_request.cone_goals) {
-      ik_options.goals.emplace_back(new bio_ik::ConeGoal(m.link_name, p(m.position), w(m.position_weight), p(m.axis),
-                                                         p(m.direction), m.angle, w(m.weight)));
-    }
   }
 
   void look_at(geometry_msgs::msg::PointStamped point, double min_pan_delta = 0.0, double min_tilt_delta = 0.0) {
@@ -635,8 +489,6 @@ class HeadMover : public rclcpp::Node {
 
   void behave() {
     geometry_msgs::msg::PointStamped point;
-    // print hello world
-    RCLCPP_INFO(this->get_logger(), "Hello World");
     point.header.frame_id = "base_link";
     point.point.x = 1.0;
     point.point.y = 0.0;
