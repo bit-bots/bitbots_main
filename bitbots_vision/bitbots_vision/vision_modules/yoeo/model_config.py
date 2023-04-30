d4b6a0f4678f5024ed8d8bde15968bb5037d40af
Philipp Donn <30521025+phinik@users.noreply.github.com>
2023-04-30 10:40:35 +0200

    Added ModelConfigLoader class

--- bitbots_vision/bitbots_vision/vision_modules/yoeo/model_config.py
+++ bitbots_vision/bitbots_vision/vision_modules/yoeo/model_config.py
@@ -3,16 +3,19 @@ from __future__ import annotations
 import yaml
 
 from os.path import join
-from typing import Dict, List
+from typing import Dict, List, Optional
+
+
+class ModelConfigLoader:
+    @staticmethod
+    def load_from(model_path: str) -> ModelConfig:
+        with open(join(model_path, "model_config.yaml"), 'r') as f:
+            return ModelConfig(yaml.safe_load(f))
 
 
 class ModelConfig:
-    def __init__(self):
-        self._config: Dict = {}
-
-    def load(self, model_path: str) -> None:
-        with open(join(model_path, "model_config.yaml"), 'r') as f:
-            self._config = yaml.safe_load(f)
+    def __init__(self, config: Optional[Dict] = None):
+        self._config: Dict = config if config else {}
 
     def get_detection_classes(self) -> List[str]:
         return self._config['detection']['classes']
@@ -20,8 +23,8 @@ class ModelConfig:
     def get_segmentation_classes(self) -> List[str]:
         return self._config['segmentation']['classes']
 
-    def team_color_detection_supported(self) -> bool:
-        return self._config['detection']['team_colors'] if 'team_colors' in self._config['detection'].keys() else False
+    def team_colors_are_provided(self) -> bool:
+        return self._config['detection'].get('team_colors', False)
 
     def get_robot_classes(self) -> List[str]:
         robot_classes = []
