f5fa0743a7def19e8756a8a57ca795a04d95c7e2
Valerie Bartel <v.bartel.vb@gmail.com>
2023-05-24 13:44:09 +0200

    remove unused actions

--- bitbots_body_behavior/bitbots_body_behavior/actions/head_modes.py
+++ bitbots_body_behavior/bitbots_body_behavior/actions/head_modes.py
@@ -15,41 +15,11 @@ class LookAtBall(AbstractHeadModeElement):
         return self.pop()
 
 
-class LookAtGoalPosts(AbstractHeadModeElement):
-    """Search for goal posts, mainly to locate the robot on the field"""
-    def perform(self):
-        self.blackboard.blackboard.set_head_duty(HeadMode.POST_MODE)
-        return self.pop()
-
-
-class LookAtBallAndGoalPosts(AbstractHeadModeElement):
-    """Track ball and goal by constantly switching between both"""
-    def perform(self):
-        self.blackboard.blackboard.set_head_duty(HeadMode.BALL_GOAL_TRACKING)
-        return self.pop()
-
-
 class LookAtFieldFeatures(AbstractHeadModeElement):
     """Look generally for all features on the field (ball, goals, corners, center point)"""
     def perform(self):
         self.blackboard.blackboard.set_head_duty(HeadMode.FIELD_FEATURES)
         return self.pop()
-
-
-class LookAtNonFieldFeatures(AbstractHeadModeElement):
-    """Look for features outside of the field (perimeter advertising, walls, etc).
-    Can be used for localization using features on the ceiling."""
-    def perform(self):
-        self.blackboard.blackboard.set_head_duty(HeadMode.NON_FIELD_FEATURES)
-        return self.pop()
-
-
-class LookDown(AbstractHeadModeElement):
-    """Simply look down to its feet."""
-    def perform(self):
-        self.blackboard.blackboard.set_head_duty(HeadMode.LOOK_DOWN)
-        return self.pop()
-
 
 class LookForward(AbstractHeadModeElement):
     """Simply look directly forward"""
@@ -65,35 +35,14 @@ class DontMoveHead(AbstractHeadModeElement):
         return self.pop()
 
 
-class LookUp(AbstractHeadModeElement):
-    """Look to the ceiling, for example for visual compass"""
-    def perform(self):
-        self.blackboard.blackboard.set_head_duty(HeadMode.LOOK_UP)
-        return self.pop()
-
-
-class HeadRecordVisualCompass(AbstractHeadModeElement):
-    """Record ground truth for the visual compass"""
-    def perform(self):
-        self.blackboard.blackboard.set_head_duty(HeadMode.RECORD_VISUAL_COMPASS)
-        return self.pop()
-
-
 class LookAtBallPenalty(AbstractHeadModeElement):
     """Ball Mode adapted for Penalty Kick"""
     def perform(self):
         self.blackboard.blackboard.set_head_duty(HeadMode.BALL_MODE_PENALTY)
         return self.pop()
 
-
-class LookForVisualCompassFeatures(AbstractHeadModeElement):
-    """Look for visual compass features"""
-    def perform(self):
-        self.blackboard.blackboard.set_head_duty(HeadMode.VISUAL_COMPASS_FEATURES)
-        return self.pop()
-
 class LookAtFront(AbstractHeadModeElement):
-    """Search in the front of the robot"""
+    """Search in front of the robot"""
     def perform(self):
         self.blackboard.blackboard.set_head_duty(HeadMode.LOOK_FRONT)
         return self.pop()
