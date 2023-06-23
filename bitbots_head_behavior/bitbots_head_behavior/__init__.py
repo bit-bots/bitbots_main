e9eb317e934f179d6f65f5fb17a9f2a64c9f6ad5
Valerie Bartel <v.bartel.vb@gmail.com>
2023-06-23 13:54:37 +0200

    remove head_behavior files

--- bitbots_head_behavior/bitbots_head_behavior/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-BitBots Headbehavior uses the dynamic_stack_decider package to achieve dynamic but also easy to maintain
-behaviour definitions.
-
-In general the Head acts independently of the Body. It can be configured to look for different field features like
-ball, goalposts, players or all at once. Each configuration brings with it different search patterns but does not
-inhibit other parts of the robot (like vision) from detecting additional field features.
-"""
