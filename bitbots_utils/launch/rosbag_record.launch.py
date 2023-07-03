777ec3b10373d17237fe4f426f8379543970e2d2
Timon Engelke <timon.engelke@online.de>
2023-07-03 22:59:59 +0200

    Fix python launch file for rosbag launch

--- bitbots_utils/launch/rosbag_record.launch.py
+++ bitbots_utils/launch/rosbag_record.launch.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, OpaqueFunction
 from launch.conditions import IfCondition
 from launch.substitutions import EnvironmentVariable, EqualsSubstitution, LaunchConfiguration, NotEqualsSubstitution, PathJoinSubstitution
 from launch_ros.actions import Node
@@ -67,150 +67,97 @@ TOPICS_TO_RECORD: List[str] = [
 ]
 
 
+def generate_launch_arguments():
+    return [
+        DeclareLaunchArgument(
+            'sim',
+            default_value='false',
+            description='true: Use simulation time',
+            choices=['true', 'false']
+        ),
+        DeclareLaunchArgument(
+            'max_image_frequency',
+            default_value='1.0',
+            description='Max frequency [hz] for recording images'
+        ),
+        DeclareLaunchArgument(
+            'max_pointcloud_frequency',
+            default_value='1.0',
+            description='Max frequency [hz] for recording pointclouds'
+        ),
+    ]
+
+
+def generate_nodes():
+    return [
+        Node(
+            package='topic_tools',
+            executable='throttle',
+            output='screen',
+            name='record_rosbag_drop_images',
+            arguments=[
+                'messages',
+                '/camera/image_proc',
+                LaunchConfiguration('max_image_frequency'),
+                '/camera/image_to_record'
+            ]
+        ),
+        Node(
+            package='topic_tools',
+            executable='throttle',
+            output='screen',
+            name='record_rosbag_drop_pointclouds',
+            arguments=[
+                'messages',
+                '/line_mask_relative_pc',
+                LaunchConfiguration('max_pointcloud_frequency'),
+                '/line_mask_relative_pc_to_record'
+            ]
+        ),
+    ]
+
+
+def generate_action(context):
+    robot_name = os.getenv('ROBOCUP_ROBOT_ID', default=os.getenv('ROBOT_NAME', default='unknown_robot'))
+
+    # Set output directory
+    # ~/rosbags/ID_<robot_id>_<datetime>
+    output_directory = PathJoinSubstitution([
+        EnvironmentVariable('HOME'),
+        'rosbags',
+        'ID_' + robot_name + '_' + datetime.now().isoformat(timespec='seconds')
+    ])
+
+    sim_value = LaunchConfiguration('sim').perform(context)
+    sim_time = ['--use-sim-time'] if sim_value == 'true' else []
+
+    main_process = ExecuteProcess(
+        # Constructing the complete command
+        cmd=[
+            # Main command to start recording ros2 bags
+            'ros2',
+            'bag',
+            'record',
+            '-o', output_directory,
+
+            # Other options
+            '--node-name', 'ros2_bag_record',
+            '--include-hidden-topics',
+            '--include-unpublished-topics',
+            '--polling-interval', '1000',
+        ] + sim_time + TOPICS_TO_RECORD,
+        output='screen',
+        name='ros2_bag_record',
+        shell=True
+    )
+    return [main_process]
+
+
 def generate_launch_description():
-    configuration_sim = LaunchConfiguration('sim')
+    launch_arguments = generate_launch_arguments()
+    nodes = generate_nodes()
 
-    arg_sim = DeclareLaunchArgument(
-        'sim',
-        default_value='false',
-        description='true: Use simulation time',
-        choices=['true', 'false']
-    )
-
-    arg_max_image_frequency = DeclareLaunchArgument(
-        'max_image_frequency',
-        default_value='1.0',
-        description='Max frequency [hz] for recording images'
-    )
-
-    arg_max_pointcloud_frequency = DeclareLaunchArgument(
-        'max_pointcloud_frequency',
-        default_value='1.0',
-        description='Max frequency [hz] for recording pointclouds'
-    )
-
-    node_drop_images = Node(
-        package='topic_tools',
-        executable='throttle',
-        output='screen',
-        name='record_rosbag_drop_images',
-        arguments=[
-            'messages',
-            '/camera/image_proc',
-            LaunchConfiguration('max_image_frequency'),
-            '/camera/image_to_record'
-        ]
-    )
-    
-    node_drop_pointclouds = Node(
-        package='topic_tools',
-        executable='throttle',
-        output='screen',
-        name='record_rosbag_drop_pointclouds',
-        arguments=[
-            'messages',
-            '/line_mask_relative_pc',
-            LaunchConfiguration('max_pointcloud_frequency'),
-            '/line_mask_relative_pc_to_record'
-        ]
-    )
-
-    ####################################################
-    # NOTE:
-    # The following main process code is duplicated with a minor change in
-    # the condition (sim vs. !sim) and the use of the --use-sim-time option.
-    # IF YOU CHANGE SOMETHING, CHANGE IT IN BOTH PLACES!
-    # 
-    # The duplication is due to the fact that is seems impossible to
-    # set the --use-sim-time option inplace conditionally on the launch argument 'sim'.
-    ####################################################
-
-    main_process_unless_sim = ExecuteProcess(
-        condition=IfCondition(NotEqualsSubstitution(configuration_sim, 'true')),
-        # Constructing the complete command
-        cmd=[
-                # Main command to start recording ros2 bags
-                'ros2',
-                'bag',
-                'record',
-
-                # Set output directory
-                # ~/rosbags/ID_<robot_id>_<datetime>
-                '-o', PathJoinSubstitution([
-                    EnvironmentVariable('HOME'),
-                    'rosbags',
-                    'ID_' + 
-                    os.getenv(
-                        'ROBOCUP_ROBOT_ID',
-                        default=os.getenv(
-                            'ROBOT_NAME',
-                            default='unknown_robot')) +
-                    '_' +
-                    datetime.now().isoformat(timespec='seconds')
-                ]),
-
-                # Other options
-                '--node-name', 'ros2_bag_record',
-                '--include-hidden-topics',
-                '--include-unpublished-topics',
-                '--polling-interval', '1000',
-                [f'{topic} ' for topic in TOPICS_TO_RECORD]
-        ],
-        output='screen',
-        name='launch_ros2_bag_record_unless_sim',
-        shell=True
-    )
-
-    main_process_if_sim = ExecuteProcess(
-        condition=IfCondition(EqualsSubstitution(configuration_sim, 'true')),
-        # Constructing the complete command
-        cmd=[
-                # Main command to start recording ros2 bags
-                'ros2',
-                'bag',
-                'record',
-
-                # Set output directory
-                # ~/rosbags/ID_<robot_id>_<datetime>
-                '-o', PathJoinSubstitution([
-                    EnvironmentVariable('HOME'),
-                    'rosbags',
-                    'ID_' + 
-                    os.getenv('ROBOCUP_ROBOT_ID', default=os.getenv('ROBOT_NAME', default='unknown_robot')) +
-                    '_' +
-                    datetime.now().isoformat(timespec='seconds')
-                ]),
-
-                # Other options
-                '--node-name', 'ros2_bag_record',
-                '--include-hidden-topics',
-                '--include-unpublished-topics',
-                '--polling-interval', '1000',
-                '--use-sim-time',
-                [f'{topic} ' for topic in TOPICS_TO_RECORD]
-        ],
-        output='screen',
-        name='launch_ros2_bag_record_if_sim',
-        shell=True
-    )
-
-    ####################################################
-    # NOTE:
-    # The above main process code is duplicated with a minor change in
-    # the condition (sim vs. !sim) and the use of the --use-sim-time option.
-    # IF YOU CHANGE SOMETHING, CHANGE IT IN BOTH PLACES!
-    # 
-    # The duplication is due to the fact that is seems impossible to
-    # set the --use-sim-time option inplace conditionally on the launch argument 'sim'.
-    ####################################################
+    action = OpaqueFunction(function=generate_action)
 
     # Construct LaunchDescription from parts
-    return LaunchDescription([
-        arg_sim,
-        arg_max_image_frequency,
-        arg_max_pointcloud_frequency,
-        node_drop_images,
-        node_drop_pointclouds,
-        main_process_unless_sim,
-        main_process_if_sim
-    ])
+    return LaunchDescription(launch_arguments + nodes + [action])
