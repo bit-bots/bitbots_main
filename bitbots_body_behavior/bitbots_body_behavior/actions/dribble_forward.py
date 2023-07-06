ab297be9631bc5b74a64bbc6dacc358ae10ca95b
Florian Vahl <florian@flova.de>
2023-07-06 08:26:05 +0200

    Simplify x speed for dribbling

--- bitbots_body_behavior/bitbots_body_behavior/actions/dribble_forward.py
+++ bitbots_body_behavior/bitbots_body_behavior/actions/dribble_forward.py
@@ -13,9 +13,10 @@ class DribbleForward(AbstractActionElement):
         self.max_speed_x = self.blackboard.config['dribble_max_speed_x']
         self.min_speed_x = -0.1
         self.max_speed_y = self.blackboard.config['dribble_max_speed_y']
-        self.walk_backward_angle = math.radians(45)
+        self.ball_heading_x_vel_zero_point = self.blackboard.config['dribble_ball_heading_x_vel_zero_point']
         self.p = self.blackboard.config['dribble_p']
         self.max_accel_x = self.blackboard.config['dribble_accel_x']
+        self.max_accel_y = self.blackboard.config['dribble_accel_y']
 
         self.current_speed_x = self.blackboard.pathfinding.current_cmd_vel.linear.x
         self.current_speed_y = self.blackboard.pathfinding.current_cmd_vel.linear.y
@@ -28,22 +29,25 @@ class DribbleForward(AbstractActionElement):
         :return:
         """
         # Get the ball relative to the base fottprint
-        ball_u, ball_v = self.blackboard.world_model.get_ball_position_uv()
+        _, ball_v = self.blackboard.world_model.get_ball_position_uv()
         # Get the relative angle from us to the ball
         ball_angle = self.blackboard.world_model.get_ball_angle()
 
         # todo compute yaw speed based on how we are aligned to the goal
 
-        adaptive_acceleration_x =  1 - (abs(ball_angle) / self.walk_backward_angle)
+        adaptive_acceleration_x =  1 - (abs(ball_angle) / self.ball_heading_x_vel_zero_point)
         x_speed = self.max_speed_x * adaptive_acceleration_x
 
         self.current_speed_x = \
-            self.max_accel_x * np.clip(x_speed, self.min_speed_x, self.max_speed_x) + \
+            self.max_accel_x * self.max_speed_x + \
             self.current_speed_x * (1 - self.max_accel_x)
 
         # give more speed in y direction based on ball position
         y_speed = ball_v * self.p
-        self.current_speed_y = np.clip(y_speed, -self.max_speed_y, self.max_speed_y)
+
+        self.current_speed_y = \
+            self.max_accel_y * np.clip(y_speed, -self.max_speed_y, self.max_speed_y) + \
+            self.current_speed_y * (1 - self.max_accel_y)
 
         cmd_vel = Twist()
         cmd_vel.linear.x = self.current_speed_x
