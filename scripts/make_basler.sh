6d1f85a67e399022e44633328892886a8ad07294
Florian Vahl <7vahl@informatik.uni-hamburg.de>
2023-10-25 20:39:47 +0200

    Add direct basler driver download

--- scripts/make_basler.sh
+++ scripts/make_basler.sh
@@ -1,8 +1,64 @@
-#!/bin/sh
+#!/bin/bash 
 
-# Install Basler drivers
-# Check if basler_drivers is already installed
-# If not, clone the repository
-# Run the setup script to install the drivers
-test -d basler_drivers || git clone git@git.mafiasi.de:Bit-Bots/basler_drivers.git
-cd basler_drivers && ./setup.sh
+# You need to fill out a form to download the pylon driver. 
+# The pylon driver can be found in the download section of the following link:
+# https://www.baslerweb.com/en/downloads/software-downloads/
+# Go to the download button and copy the link address.
+PYLON_DOWNLOAD_URL="https://www2.baslerweb.com/media/downloads/software/pylon_software/pylon_7_4_0_14900_linux_x86_64_debs.tar.gz"
+PYLON_VERSION="7.4.0"
+
+# Similar to the pylon driver we also need to download the blaze supplementary package.
+BLAZE_DOWNLOAD_URL="https://www2.baslerweb.com/media/downloads/software/tof_software/pylon-supplementary-package-for-blaze-1.5.0.def07388_amd64.deb"
+BLAZE_VERSION="1.5.0"
+
+# Check let the user confirm that they read the license agreement on the basler website and are fine with it.
+echo "You need to confirm that you read the license agreements for pylon $PYLON_VERSION and the blaze supplementary package $BLAZE_VERSION on the basler download page (https://www.baslerweb.com/en/downloads/software-downloads/) and are fine with it."
+
+# Ask the user if they want to continue and break if they don't
+read -p "Do you want to continue? [y/N] " -n 1 -r
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Aborting..."
+    exit 1
+fi
+
+# Check if we have an internet connection
+if ! ping -q -c 1 -W 1 google.com >/dev/null; then
+    echo "No internet connection. Please check your internet connection to install the basler drivers."
+    exit 1
+fi
+
+# Check if the urls exist
+if ! curl --output /dev/null --silent --head --fail "$PYLON_DOWNLOAD_URL"; then
+    echo "Pylon download url does not exist. Please check the url. The website might have changed."
+    exit 1
+fi
+
+if ! curl --output /dev/null --silent --head --fail "$BLAZE_DOWNLOAD_URL"; then
+    echo "Blaze download url does not exist. Please check the url and update the 'BLAZE_DOWNLOAD_URL' variable in the 'make_basler.sh' script. The website might have changed."
+    exit 1
+fi
+
+# Check if the correct pylon driver PYLON_VERSION is installed (apt)
+if apt list pylon --installed | grep -q $PYLON_VERSION; then
+    echo "Pylon driver $PYLON_VERSION is already installed."
+else
+    echo "Pylon driver $PYLON_VERSION is not installed. Installing..."
+    # Download the pylon driver to temp folder
+    wget --no-verbose --show-progress $PYLON_DOWNLOAD_URL -O /tmp/pylon_${PYLON_VERSION}.tar.gz
+    # Extract the pylon driver
+    tar -xzf /tmp/pylon_${PYLON_VERSION}.tar.gz -C /tmp
+    # Install the pylon driver
+    sudo apt install /tmp/pylon_${PYLON_VERSION}*.deb
+fi
+
+# Check if the correct blaze supplementary package BLAZE_VERSION is installed (apt)
+if apt list pylon-supplementary-package-for-blaze --installed | grep -q $BLAZE_VERSION; then
+    echo "Blaze supplementary package $BLAZE_VERSION is already installed."
+else
+    echo "Blaze supplementary package $BLAZE_VERSION is not installed. Installing..."
+    # Download the blaze supplementary package to temp folder
+    wget --no-verbose --show-progress $BLAZE_DOWNLOAD_URL -O /tmp/pylon-blaze-supplementary-package_${BLAZE_VERSION}.deb
+    # Install the blaze supplementary package
+    sudo apt install /tmp/pylon-blaze-supplementary-package_${BLAZE_VERSION}*.deb
+fi
--- scripts/pull_all.sh
+++ scripts/pull_all.sh
@@ -3,8 +3,3 @@ set -e
 
 git submodule foreach git pull
 git -C humanoid_league_misc submodule update
-if [[ -d basler_drivers ]]; then
-    git -C basler_drivers pull
-else
-    echo -e "\n\n\033[91m\033[1mNo basler drivers found! Use 'make basler' to install them and restart this script afterwards\033[0m\n"
-fi
