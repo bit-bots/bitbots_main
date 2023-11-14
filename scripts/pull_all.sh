ee6d3dc9967d6411e6ef774fc4943a6391156445
Jan Gutsche <gitcommit@jaagut.de>
2023-11-14 19:56:32 +0100

    Pull_all: Add temporary hack to remove unused repos

--- scripts/pull_all.sh
+++ scripts/pull_all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+# This is a temporary hack, so it is not necessary that every user removes these dirs themselfs
+rm -rf basler_drivers
+rm -rf humanoid_league_msgs
+
 git submodule foreach git pull
 git -C humanoid_league_misc submodule update
