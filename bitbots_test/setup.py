c97bb4f4250d35ce22c6d31ea56fed65a8ec9c31
Jan Gutsche <gitcommit@jaagut.de>
2023-11-23 15:57:55 +0100

    Remove bitbots_test

--- bitbots_test/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup(
-    **generate_distutils_setup(
-        packages=["bitbots_test"],
-        package_dir={"": "src"},
-    )
-)
