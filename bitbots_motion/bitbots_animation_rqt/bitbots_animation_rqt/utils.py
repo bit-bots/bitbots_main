d37bbea9663db65157b7a20f25a152ae8ff0ea81
Florian Vahl <7vahl@informatik.uni-hamburg.de>
2024-02-16 16:56:32 +0100

    Add utility functions for dicts

--- bitbots_motion/bitbots_animation_rqt/bitbots_animation_rqt/utils.py
+++ bitbots_motion/bitbots_animation_rqt/bitbots_animation_rqt/utils.py
@@ -34,3 +34,28 @@ class DragDropList(QListWidget):
             self.setCurrentRow(self.currentRow() - 1)
         elif event.key() == Qt.Key_Down and self.currentRow() + 1 < self.count():
             self.setCurrentRow(self.currentRow() + 1)
+
+
+def flatten_dict(input_dict: dict, parent_key: str = "", sep: str = ".") -> dict:
+    """Flatten a nested dictionary."""
+    items = []
+    for k, v in input_dict.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+
+def flatten_dict_of_lists(input_dict: dict, sep: str = ".") -> dict:
+    """Flatten a nested dictionary and convert lists to separate keys."""
+    flat_dict = flatten_dict(input_dict, sep=sep)
+    new_dict = {}
+    for key, value in flat_dict.items():
+        if isinstance(value, list):
+            for i, list_value in enumerate(value):
+                new_dict[f"{key}{sep}{i}"] = list_value
+        else:
+            new_dict[key] = value
+    return new_dict
