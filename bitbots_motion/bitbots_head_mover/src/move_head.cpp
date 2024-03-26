760cf4ba7d98e525cd4f09084004145ba66067de
Florian Vahl <git@flova.de>
2024-03-26 23:59:16 +0100

    Tune params

--- bitbots_motion/bitbots_head_mover/src/move_head.cpp
+++ bitbots_motion/bitbots_head_mover/src/move_head.cpp
@@ -56,7 +56,7 @@ class HeadMover {
   std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
   // Declare variables
-  uint head_mode_ = bitbots_msgs::msg::HeadMode::LOOK_FORWARD;
+  uint head_mode_ = bitbots_msgs::msg::HeadMode::FIELD_FEATURES;
   std::optional<sensor_msgs::msg::JointState> current_joint_state_;
   bitbots_msgs::msg::JointCommand pos_msg_;
   geometry_msgs::msg::PoseWithCovarianceStamped tf_precision_pose_;
