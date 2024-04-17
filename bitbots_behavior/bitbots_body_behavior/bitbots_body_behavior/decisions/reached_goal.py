4fae1bee427cea97b162e0914dae0342b64fc19a
JanNiklasFeld <janniklasfeldhusen@gmail.com>
2024-04-17 20:36:59 +0200

    add alignment to goalie behavior

--- bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/decisions/reached_goal.py
+++ bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/decisions/reached_goal.py
@@ -49,7 +49,7 @@ class AlignedToPathPlanningGoal(AbstractDecisionElement):
 
     def __init__(self, blackboard, dsd, parameters):
         super().__init__(blackboard, dsd, parameters)
-        self.orientation_threshold = self.blackboard.config["goal_alignment_orientation_threshold"]  # [deg]
+        self.orientation_threshold = math.radians(self.blackboard.config["goal_alignment_orientation_threshold"])
         self.frame_id = parameters.get("frame_id", self.blackboard.map_frame)
 
     def perform(self, reevaluate=False):
@@ -60,6 +60,7 @@ class AlignedToPathPlanningGoal(AbstractDecisionElement):
         """
         current_pose = self.blackboard.world_model.get_current_position_pose_stamped(self.frame_id)
         current_goal = self.blackboard.pathfinding.get_goal()
+
         if current_pose is None or current_goal is None:
             # When the path planning did not received a goal yet, no current position on the map is known.
             # If it is not known if the robot is aligned correctly to, e.g., the goal the robot
@@ -67,10 +68,58 @@ class AlignedToPathPlanningGoal(AbstractDecisionElement):
             return "NO"
         current_orientation = euler_from_quaternion(numpify(current_pose.pose.orientation))
         goal_orientation = euler_from_quaternion(numpify(current_goal.pose.orientation))
-        if math.degrees(abs(current_orientation[2] - goal_orientation[2])) < self.orientation_threshold:
+        min_angle = abs(math.remainder(current_orientation[2] - goal_orientation[2], math.tau))
+
+        if min_angle < self.orientation_threshold:
             return "YES"
         else:
             return "NO"
+
+    def get_reevaluate(self):
+        return True
+
+
+class ReachedAndAlignedToPathPlanningGoalPosition(AbstractDecisionElement):
+    blackboard: BodyBlackboard
+
+    def __init__(self, blackboard, dsd, parameters):
+        super().__init__(blackboard, dsd, parameters)
+        self.frame_id = parameters.get("frame_id", self.blackboard.map_frame)
+        self.threshold = parameters.get("threshold")
+        self.orientation_threshold = math.radians(self.blackboard.config["goal_alignment_orientation_threshold"])
+        self.frame_id = parameters.get("frame_id", self.blackboard.map_frame)
+        self.latch = parameters.get("latch", False)
+        self.latched = False
+
+    def perform(self, reevaluate=False):
+        """
+        Determines whether we are near the path planning goal
+        :param reevaluate:
+        :return:
+        """
+        # We return that reached it forever if we reached it once if latching is enabled
+        if self.latched:
+            return "YES"
+
+        current_pose = self.blackboard.world_model.get_current_position_pose_stamped(self.frame_id)
+        goal_pose = self.blackboard.pathfinding.get_goal()
+
+        if current_pose is None or goal_pose is None:
+            return "NO"
+
+        current_orientation = euler_from_quaternion(numpify(current_pose.pose.orientation))
+        goal_orientation = euler_from_quaternion(numpify(goal_pose.pose.orientation))
+        min_angle = abs(math.remainder(current_orientation[2] - goal_orientation[2], math.tau))
+        self.publish_debug_data("current_orientation", current_orientation[2])
+        self.publish_debug_data("goal_orientation", goal_orientation[2])
+        self.publish_debug_data("min_angle", min_angle)
+
+        distance = np.linalg.norm(numpify(goal_pose.pose.position) - numpify(current_pose.pose.position))
+        self.publish_debug_data("distance", distance)
+        if distance < self.threshold and min_angle < self.orientation_threshold:
+            self.latched = self.latch  # Set it to true if we always want to return YES in the future
+            return "YES"
+        return "NO"
 
     def get_reevaluate(self):
         return True
