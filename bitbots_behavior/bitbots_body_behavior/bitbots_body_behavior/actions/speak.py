e15732279e470c4e1f50a1606fed165b6d439c4e
Timon Engelke <timon.engelke@online.de>
2024-04-19 20:38:40 +0200

    Play sounds after goal and at the end of the game

--- bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/actions/speak.py
+++ bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/actions/speak.py
@@ -1,3 +1,6 @@
+import os
+
+from ament_index_python import get_package_share_directory
 from bitbots_blackboard.blackboard import BodyBlackboard
 from bitbots_tts.tts import speak
 from dynamic_stack_decider.abstract_action_element import AbstractActionElement
@@ -11,4 +14,20 @@ class Speak(AbstractActionElement):
 
     def perform(self, reevaluate=False):
         speak(self.text, self.blackboard.misc.speak_pub, priority=50)
+        return self.pop()
+
+
+class PlaySound(AbstractActionElement):
+    def __init__(self, blackboard, dsd, parameters):
+        super().__init__(blackboard, dsd, parameters)
+        self.audio_file = os.path.join(
+            get_package_share_directory("bitbots_body_behavior"), "config", parameters.get("file")
+        )
+
+    def perform(self, reevaluate=False):
+        try:
+            from playsound import playsound
+            playsound(self.audio_file, block=False)
+        except ImportError:
+            pass
         return self.pop()
