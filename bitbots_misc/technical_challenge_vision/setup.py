5c2fa8e4ad95b6c9169c3a05fe6637a94eb31500
Pär <paer-wiegmann@gmx.de>
2024-05-20 20:53:51 +0200

    add a bunch of stuff to setup

--- bitbots_misc/technical_challenge_vision/setup.py
+++ bitbots_misc/technical_challenge_vision/setup.py
@@ -1,3 +1,6 @@
+import glob
+
+from generate_parameter_library_py.setup_helper import generate_parameter_module
 from setuptools import find_packages, setup
 
 package_name = "technical_challenge_vision"
@@ -9,15 +12,23 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/config", glob.glob("config/*.yaml")),
+        ("share/" + package_name + "/launch", glob.glob("launch/*.launch")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="par",
     maintainer_email="paer-wiegmann@gmx.de",
-    description="TODO: Package description",
-    license="TODO: License declaration",
-    tests_require=["pytest"],
+    description="This Package provides a simple vision to detect the obstacles for the obstacle avoidance challenge.",
+    license="MIT",
     entry_points={
-        "console_scripts": [],
+        "console_scripts": [
+            "technical_challenge_vision = technical_challenge_vision.technical_challenge_vision:main",
+        ],
     },
+)
+
+generate_parameter_module(
+    "technical_challenge_vision_params",
+    "config/range.yaml",
 )
