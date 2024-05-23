97a62eb1dd56364824ff3318a8569002a7227d25
Valerie <v.bartel.vb@gmail.com>
2024-05-23 14:34:17 +0200

    remove timer for loop

--- bitbots_motion/bitbots_dynup/src/dynup_node.cpp
+++ bitbots_motion/bitbots_dynup/src/dynup_node.cpp
@@ -380,10 +380,6 @@ int main(int argc, char **argv) {
   rclcpp::experimental::executors::EventsExecutor exec;
   exec.add_node(node);
 
-rclcpp::Duration timer_duration = rclcpp::Duration::from_seconds(1.0 / 200.0);
-    rclcpp::TimerBase::SharedPtr timer =
-      rclcpp::create_timer(node, node->get_clock(), timer_duration, [node]() -> void { node->loop(); });
-
 
   exec.spin();
   rclcpp::shutdown();
