6f5bcead44916302692bd1bd9cdcdc63347d6874
Lea <wedmann.lea@gmail.com>
2024-06-18 16:25:47 +0200

    reduce states and new color mapping of LEDs

--- bitbots_motion/bitbots_hcm/scripts/hcm_led.py
+++ bitbots_motion/bitbots_hcm/scripts/hcm_led.py
@@ -4,7 +4,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import ColorRGBA
 
-from bitbots_msgs.msg import RobotControlState
+from bitbots_msgs.msg import RobotControlState, Strategy
 
 BLINK_DURATION = 0.2
 ERROR_TIMEOUT = 1
@@ -12,31 +12,30 @@ ERROR_TIMEOUT = 1
 
 class HCMLedNode(Node):
     def __init__(self):
-        rclpy.init(args=None)
-
+        super().__init__("hcm_led")
         self.pub = self.create_publisher(ColorRGBA, "/led1", 1)
 
         self.color_mapping = {
-            RobotControlState.CONTROLLABLE: (0.3, 0.3, 0.3),
-            RobotControlState.FALLING: (0.3, 0.1, 0),
-            RobotControlState.FALLEN: (0.3, 0.3, 0),
             RobotControlState.HARDWARE_PROBLEM: (1, 0, 0),
-            RobotControlState.ANIMATION_RUNNING: (0, 0, 0.3),
-            RobotControlState.WALKING: (0, 0.3, 0),
-            RobotControlState.GETTING_UP: (0, 0.3, 0.3),
-            RobotControlState.HCM_OFF: (0, 0, 0),
-            RobotControlState.MOTOR_OFF: (0.03, 0.03, 0.03),
-            RobotControlState.KICKING: (0, 0, 0.1),
-            RobotControlState.PENALTY: (0.3, 0, 0.3),
-            RobotControlState.PENALTY_ANIMATION: (0.3, 0, 0.3),
-            RobotControlState.PICKED_UP: (0, 0.03, 0),
-            RobotControlState.RECORD: (0, 0.1, 0),
+            # RobotControlState.ANIMATION_RUNNING: (0, 0, 0.3),
+            RobotControlState.PENALTY: (0.5, 0.5, 0),
+            Strategy.ROLE_STRIKER: (0, 1, 0),
+            Strategy.ROLE_SUPPORTER: (0.3, 0.3, 0.3),
+            Strategy.ROLE_DEFENDER: (0, 0, 1),
+            Strategy.ROLE_GOALIE: (0, 0, 0),
+            Strategy.ACTION_POSITIONING: (0, 0, 1),
+            Strategy.ACTION_GOING_TO_BALL: (0, 1, 0),
+            Strategy.ACTION_WAITING: (0, 0, 1),
+            Strategy.ACTION_SEARCHING: (0.3, 0.3, 0.3),
         }
 
         self.last_state = -1
 
-        self.sub = self.create_subscription(RobotControlState, "robot_state", self.hcm_state_cb, 1)
-        rclpy.spin(self)
+        self.sub_hcm = self.create_subscription(RobotControlState, "robot_state", self.hcm_state_cb, 1)
+        self.sub_strategies = self.create_subscription(Strategy, "strategy", self.strategy_state_cb, 1)
+
+    def strategy_state_cb(self, msg: Strategy):
+        pass
 
     def hcm_state_cb(self, msg: RobotControlState):
         state = msg.state
@@ -50,8 +49,19 @@ class HCMLedNode(Node):
         led.a = 1.0
 
         if state in self.color_mapping:
-            led.r, led.g, led.b = self.color_mapping[state]
+            r, g, b = self.color_mapping[state]
+            led.r, led.g, led.b = float(r), float(g), float(b)
         else:
             self.get_logger().warn(f"Unknown state: {state}")
 
         self.pub.publish(led)
+
+
+if __name__ == "__main__":
+    rclpy.init(args=None)
+    node = HCMLedNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    node.destroy_node()
