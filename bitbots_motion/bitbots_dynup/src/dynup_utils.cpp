c23d49db9f137ec076842a70113c0d98a2eac806
Florian Vahl <git@flova.de>
2024-06-27 19:14:11 +0200

    Use better sting comparison

--- bitbots_motion/bitbots_dynup/src/dynup_utils.cpp
+++ bitbots_motion/bitbots_dynup/src/dynup_utils.cpp
@@ -3,11 +3,15 @@
 namespace bitbots_dynup {
 
 DynupDirection getDynupDirection(const std::string& direction) {
-  std::map<const char*, DynupDirection> mapping = {
+  std::map<const std::string, DynupDirection> mapping = {
       {"front", FRONT}, {"back", BACK},       {"front_only", FRONT_ONLY}, {"back_only", BACK_ONLY},
       {"rise", RISE},   {"descend", DESCEND}, {"walkready", WALKREADY},
   };
-  return mapping.at(direction.c_str());
+  try {
+    return mapping.at(direction);
+  } catch (const std::out_of_range& e) {
+    throw std::invalid_argument("Invalid direction: '" + direction + "'");
+  }
 }
 
 }  // namespace bitbots_dynup
