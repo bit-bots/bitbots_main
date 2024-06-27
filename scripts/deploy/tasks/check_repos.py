2802ddcdca436df9810a3dbb1f56081ccd4d955b
Jan Gutsche <github@jaagut.de>
2024-06-27 19:12:08 +0000

    Deploy: Check all workspace repos

---
+++ scripts/deploy/tasks/check_repos.py
@@ -0,0 +1,354 @@
+import os
+from hashlib import md5
+
+from deploy.misc import be_quiet, print_debug, print_success, print_warning
+from deploy.tasks.abstract_task import AbstractTask
+from fabric import Group, GroupResult, Result
+from git import Repo
+from git.exc import GitCommandError
+from rich.console import Group as RichGroup
+from rich.table import Table
+from yaml import safe_load
+
+
+class OurRepo(Repo):
+    def __init__(self, path: str, specified_branch: str) -> None:
+        """
+        Custom class for a GIT-repository.
+
+        :param path: The path to the repository.
+        :param specified_branch: The branch this repository should be in (e.g. as defined in the workspace.repo file).
+        """
+        super().__init__(path)
+        assert not self.bare, f"Repo {self}: Repository is bare. Is it a real GIT-repository?"
+
+        self.specified_branch = specified_branch
+
+        self.warnings: list[str] = []
+
+    def check(self, quick: bool = False) -> list[str]:
+        """
+        Run all checks for the repository and return the occurred warnings.
+
+        :param quick: If True, only check if the repository is dirty and on the specified branch. Don't check if the repository is ahead or behind of the remote repository.
+        :return: The warnings.
+        """
+        self.check_dirty()
+        self.check_branch()
+        if not quick:
+            self.check_ahead_behind()
+        return self.warnings
+
+    def get_commit_hash(self) -> str:
+        """
+        Get the current commit hash of the repository.
+
+        :return: The commit hash.
+        """
+        print_debug(f"Repo {self}: Getting current commit hash of the repository...")
+        commit_hash: str = self.head.commit.hexsha
+        print_debug(f"Repo {self}: Got commit hash: '{commit_hash}'.")
+        return commit_hash
+
+    def check_dirty(self) -> bool:
+        """
+        Check if the repository is dirty (uncommitted changes).
+
+        :return: True if the repository is dirty.
+        """
+        print_debug(f"Repo {self}: Checking if the repository is dirty...")
+        dirty: bool = self.is_dirty(untracked_files=True)
+        if dirty:
+            print_debug(f"Repo {self}: Repository is dirty (uncommitted changes)!")
+            self.warnings.append("Repository is dirty (uncommitted changes)!")
+        return dirty
+
+    def check_branch(self) -> bool:
+        """
+        Check if the repository is on the specified branch.
+
+        :return: True if the repository is on the specified branch.
+        """
+        print_debug(f"Repo {self}: Checking if the repository is on the specified branch '{self.specified_branch}'...")
+        try:
+            branch_is_correct: bool = self.active_branch.name == self.specified_branch
+            print_debug(f"Repo {self}: Is on branch: '{self.active_branch.name}'.")
+            if not branch_is_correct:
+                print_debug(f"Repo {self}: Is not on the {self.specified_branch} branch, instead {self.active_branch}!")
+                self.warnings.append(f"Is not on the {self.specified_branch} branch, instead {self.active_branch}!")
+        except TypeError:
+            return False
+        return branch_is_correct
+
+    def check_ahead_behind(self, timeout: float = 5) -> bool:
+        """
+        Check if the repository is ahead or behind of the remote repository.
+
+        :param timeout: The timeout for the fetch command.
+        :return: True if the repository is ahead or behind of the remote repository.
+        """
+        print_debug(f"Repo {self}: Checking if the repository is ahead or behind of the remote repository.")
+        try:
+            print_debug(f"Repo {self}: Trying to get remote repository.")
+            remote = self.remote()
+        except ValueError:
+            print_debug(
+                f"Repo {self}: No remote repository found. Could not check if the local repository is ahead or behind of the remote repository."
+            )
+            self.warnings.append(
+                "No remote repository found. Could not check if the local repository is ahead or behind of the remote repository."
+            )
+            return True
+
+        print_debug(f"Repo {self}: Fetching remote repository.")
+        try:
+            remote.fetch(kill_after_timeout=timeout, verbose=not be_quiet())
+        except GitCommandError as e:
+            err: str = e.stderr.strip()
+            print_debug(
+                f"Repo {self}: Fetching remote repository failed: {err}. Could not check if the repository is ahead or behind of the remote repository."
+            )
+            self.warnings.append(
+                f"Fetching remote repository failed: {err}. Could not check if the repository is ahead or behind of the remote repository."
+            )
+            return True
+
+        print_debug(f"Repo {self}: Checking if behind: Comparing local and remote repository.")
+        ahead = False
+        for _ in self.iter_commits(f"{self.specified_branch}..{remote.name}/{self.specified_branch}"):
+            ahead = True
+        if ahead:
+            print_debug(f"Repo {self}: The local repository is ahead of the remote repository.")
+            self.warnings.append("The local repository is ahead of the remote repository.")
+
+        print_debug(f"Repo {self}: Checking if ahead: Comparing remote and local repository.")
+        behind = False
+        for _ in self.iter_commits(f"{remote.name}/{self.specified_branch}..{self.specified_branch}"):
+            behind = True
+        if behind:
+            print_debug(f"Repo {self}: The local repository is behind of the remote repository.")
+            self.warnings.append("The local repository is behind of the remote repository.")
+
+        return ahead or behind
+
+    def __str__(self) -> str:
+        return os.path.basename(os.path.dirname(self.git_dir))
+
+
+class CheckReposTask(AbstractTask):
+    def __init__(self) -> None:
+        """
+        Task to check the local repositories (bitbots_main and others as specified in workspace.repos file).
+        Displays the current commit status (with a friendly name) to compare with other people in a hurry.
+        Also displays warnings and requires confirmation before proceeding when:
+        - A repository is dirty (uncommitted changes).
+        - A repository is not on the specified branch.
+        - A repository is ahead or behind of the remote repository.
+        """
+        super().__init__()
+        self._show_status = False
+
+        self.main_repo_path: str = os.path.join(
+            os.path.dirname(__file__), "..", "..", ".."
+        )  # bitbots_main/scripts/deploy/tasks
+        self.main_repo: OurRepo = OurRepo(self.main_repo_path, "main")
+        self.repos: dict[str, OurRepo] = {"bitbots_main": self.main_repo, **self._get_workspace_repos()}
+
+        self.warnings: dict[str, list[str]] = {}
+
+    def _get_workspace_repos(self) -> dict[str, OurRepo]:
+        """
+        Get the paths of the workspace repositories.
+
+        :return: List of workspace repository paths.
+        """
+        result: dict[str, OurRepo] = {}
+        with open(os.path.join(self.main_repo_path, "workspace.repos")) as file:
+            workspace_repos: dict = safe_load(file)
+            for path, repo in workspace_repos["repositories"].items():
+                if repo["type"] == "git":
+                    our_repo = OurRepo(os.path.join(self.main_repo_path, path), repo.get("version", "main"))
+                    result[str(our_repo)] = our_repo
+        return result
+
+    def _run(self, connections: Group) -> GroupResult:
+        """
+        For each repo, get the current commit hash and display warnings if necessary.
+
+        :param connections: The connections to the targets.
+        :return: The result of the check.
+        """
+
+        def success(connections: Group) -> GroupResult:
+            group_result = GroupResult()
+            group_result._successes = {connection: Result(connection=connection) for connection in connections}
+            return group_result
+
+        def failure(
+            connections: Group,
+            warnings: str,
+            do_exit: bool,
+        ) -> GroupResult:
+            group_result = GroupResult()
+            results = {
+                connection: Result(connection=connection, exited=int(not do_exit), stdout=warnings)
+                for connection in connections
+            }
+            if do_exit:
+                group_result._failures = results
+            else:
+                group_result._successes = results
+            return group_result
+
+        # Check and handle all repositories
+        commit_hashes: dict[str, str] = {}
+        for repo_name, repo in self.repos.items():
+            commit_hash: str = repo.get_commit_hash()
+            commit_hashes[repo_name] = commit_hash  # Collect commit hashes for the friendly commit name
+
+            # Run the checks
+            self.warnings[repo_name] = repo.check()
+
+        workspace_hash: str = self._get_workspace_hash(commit_hashes)
+        workspace_friendly_name: str = self._get_friendly_name(workspace_hash)
+
+        # Display results
+        # Do we have any warnings? If not, all checks for all repos were successful
+        if not any([*self.warnings.values()]):  # No warnings = Success
+            print_success(
+                f"Current commit: [bold]{workspace_friendly_name}[default] ({workspace_hash[:8]})\nYour local workspace is clean and up-to-date."
+            )
+            return success(connections)
+
+        # Display warnings and ask for confirmation
+        else:  # Warnings = Failure
+            warning_table = Table()
+            warning_table.add_column("Repository")
+            warning_table.add_column("Commit")
+            warning_table.add_column("Warnings")
+            for repo_name, repo_warnings in self.warnings.items():
+                if not repo_warnings:
+                    continue
+                commit_hash: str = commit_hashes[repo_name]
+                commit_name: str = self._get_friendly_name(commit_hash)
+                warnings: str = ""
+                for i, warning in enumerate(repo_warnings):
+                    warnings += f"{i+1}. {warning}\n"
+                warning_table.add_row(repo_name, f"{commit_name} ({commit_hash[:8]})", warnings)
+            print_warning(
+                RichGroup(
+                    f"Current commit: [bold]{workspace_friendly_name}[default] ({workspace_hash[:8]})",
+                    "Your local workspace is [bold]BAD!",
+                    warning_table,
+                    "Please check the warnings and decide if you want to proceed!",
+                )
+            )
+            answer: str = input("Do you still want to proceed with the deployment? [y/N] ").lower()
+            proceed = answer and "y" in answer[0]
+            if proceed:
+                print_debug("Proceeding despite warnings because of user choice.")
+                return failure(connections, warnings, do_exit=False)
+            else:
+                print_debug("Aborting because of user choice.")
+                return failure(connections, warnings, do_exit=True)
+
+    def _get_workspace_hash(self, commit_hashes: dict[str, str]) -> str:
+        """
+        Generate a hash for the workspace based on the commit hashes of the repositories using MD5.
+
+        :param commit_hashes: The commit hashes of the repositories.
+        :return: The hash of the workspace.
+        """
+        return md5("".join(commit_hashes.values()).encode()).hexdigest()
+
+    def _get_friendly_name(self, hash: str) -> str:
+        """
+        Get a human readable name.
+        The string is used to generate a name using friendly adjectives and animals.
+
+        :param hash: The commit hash in hexadecimal characters.
+        :return: The human readable commit name.
+        """
+        friendly_adjectives: list[str] = [
+            "Adorable",
+            "Beautiful",
+            "Caring",
+            "Charming",
+            "Cheerful",
+            "Clean",
+            "Compassionate",
+            "Content",
+            "Delightful",
+            "Elegant",
+            "Enthusiastic",
+            "Fancy",
+            "Good-natured",
+            "Gorgeous",
+            "Gracious",
+            "Handsome",
+            "Heartwarming",
+            "Incredible",
+            "Joyful",
+            "Kind",
+            "Lovely",
+            "Magnificent",
+            "Nice",
+            "Optimistic",
+            "Outstanding",
+            "Perfect",
+            "Quick",
+            "Smiling",
+            "Sunny",
+            "Superb",
+            "Uplifting",
+            "Vibrant",
+            "Warmhearted",
+            "Whimsical",
+        ]
+
+        friendly_animals: list[str] = [
+            "Antelope",
+            "Badger",
+            "Bear",
+            "Beaver",
+            "Bison",
+            "Buffalo",
+            "Bull",
+            "Camel",
+            "Cheetah",
+            "Coyote",
+            "Deer",
+            "Dolphin",
+            "Eagle",
+            "Elephant",
+            "Fox",
+            "Gazelle",
+            "Giraffe",
+            "Goat",
+            "Hawk",
+            "Horse",
+            "Jaguar",
+            "Kangaroo",
+            "Leopard",
+            "Lion",
+            "Llama",
+            "Moose",
+            "Owl",
+            "Panther",
+            "Penguin",
+            "Puma",
+            "Raccoon",
+            "Rhino",
+            "Seal",
+            "Shark",
+            "Squirrel",
+            "Tiger",
+            "Walrus",
+            "Whale",
+            "Wolf",
+            "Zebra",
+        ]
+
+        friendly_name: str = f"{friendly_adjectives[int(hash[:len(hash)//2], 16) % len(friendly_adjectives)]} {friendly_animals[int(hash[len(hash)//2:], 16) % len(friendly_animals)]}"
+        print_debug(f"Generated friendly commit name: '{friendly_name}'.")
+        return friendly_name
