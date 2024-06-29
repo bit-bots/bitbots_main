d25d017b6bd1e3c0d5fd0752f902afae51e5a4fa
Florian Vahl <7vahl@informatik.uni-hamburg.de>
2024-06-29 15:54:57 +0200

    Add falling to rise portion of dynup

---
+++ bitbots_motion/bitbots_hcm/bitbots_hcm/hcm_dsd/actions/squat.py
@@ -0,0 +1,21 @@
+
+from bitbots_hcm.hcm_dsd.actions import AbstractHCMActionElement
+
+class SetSquat(AbstractHCMActionElement):
+    """
+    Tells the blackboard that we are squatting or not.
+    """
+
+    def __init__(self, blackboard, dsd, parameters):
+        super().__init__(blackboard, dsd, parameters)
+
+        # Check if parameters are valid
+        assert "squat" in parameters, "No squat parameter given"
+        assert type(parameters["squat"]) == bool, "Squat parameter is not a bool"
+
+        # Set state in blackboard
+        self.blackboard.in_squat = parameters["squat"]
+
+    def perform(self, reevaluate=False):
+        # Our job is done, we can pop
+        self.pop()
