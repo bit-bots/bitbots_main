05c15fad1c8edd5b3cc1ff9d08861f700ffb0922
Florian Vahl <florian@flova.de>
2024-07-07 17:42:15 +0000

    Tune behavior

--- bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/behavior_dsd/actions/turn.py
+++ bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/behavior_dsd/actions/turn.py
@@ -56,7 +56,7 @@ class Turn(AbstractActionElement):
         super().__init__(blackboard, dsd, parameters)
         self.blackboard: BodyBlackboard
         self.current_rotation_vel = 0.0
-        self.max_speed = parameters.get("max_speed", 0.3)
+        self.max_speed = parameters.get("max_speed", 0.6)
 
         # Check if the have a duration
         self.duration: Optional[float] = parameters.get("duration", None)
