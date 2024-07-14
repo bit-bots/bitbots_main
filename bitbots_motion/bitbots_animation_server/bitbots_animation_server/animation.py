16de41350a6bf643e3905ed77748f85a6f8ea8ac
Florian Vahl <florian@flova.de>
2024-07-14 11:03:19 +0000

    Fix animation doc strings

--- bitbots_motion/bitbots_animation_server/bitbots_animation_server/animation.py
+++ bitbots_motion/bitbots_animation_server/bitbots_animation_server/animation.py
@@ -1,30 +1,45 @@
+from typing import Optional
+
+
 class Keyframe:
     """
     A pose which the robot reaches at :attr:`duration` seconds in the future.
+    It pauses there for :attr:`pause` seconds before moving to the next keyframe.
+
+    :param goals: The goal positions for each joint.
+    :param torque: The torque for each joint.
+    :param duration: The time in seconds until the robot reaches the goal.
+    :param pause: The time in seconds the robot pauses at the goal.
+    :param stabelization_functions: A string expression for each joint to stabilize the joint using e.g. IMU data.
     """
 
-    def __init__(self, goals, torque=None, duration=1.0, pause=0.0, p=None, stabelization_functions=None):
+    def __init__(
+        self,
+        goals,
+        torque: Optional[dict[str, float]] = None,
+        duration: float = 1.0,
+        pause: float = 0.0,
+        stabelization_functions: Optional[dict[str, str]] = None,
+    ):
         self.duration = float(duration)
         self.pause = float(pause)
         self.goals = goals
         self.torque = torque or {}
-        self.p = p or {}
         self.stabelization_functions: dict[str, str] = stabelization_functions or {}
 
 
 class Animation:
     """
     An animation is constructed by an array of goal positions (:class:`Keyframe`).
-    Between two keyframes, the goal positions are interpolated by an  :class:`Interpolator`.
+    Between two keyframes, the goal positions are interpolated.
     """
 
-    def __init__(self, name, keyframes, default_interpolator=None):
+    def __init__(self, name: str, keyframes: list[Keyframe]):
         self.name: str = name
         self.keyframes: list[Keyframe] = keyframes
-        self.default_interpolator = default_interpolator
 
 
-def parse(info):
+def parse(info: dict) -> Animation:
     """
     This method is parsing an animation from a :class:`dict`
     instance *info*, as created by :func:`as_dict`.
@@ -38,7 +53,6 @@ def parse(info):
             k.get("torque", {}),
             k.get("duration", 1),
             k.get("pause", 0),
-            k.get("p", {}),
             k.get("stabelization_functions", {}),
         )
         for k in keyframes
@@ -47,14 +61,13 @@ def parse(info):
     return anim
 
 
-def as_dict(anim: Animation):
+def as_dict(anim: Animation) -> dict:
     """
     Convert an animation to builtin python types to
     make it serializable to formats like ``json``.
     """
     return {
         "name": anim.name,
-        "default_interpolator": anim.default_interpolator.__name__,
         "interpolators": {n: ip.__name__ for n, ip in anim.interpolators},
         "keyframes": [
             {
@@ -62,7 +75,6 @@ def as_dict(anim: Animation):
                 "pause": k.pause,
                 "goals": k.goals,
                 "torque": k.torque,
-                "p": k.p,
                 "stabelization_functions": k.stabelization_functions,
             }
             for k in anim.keyframes
