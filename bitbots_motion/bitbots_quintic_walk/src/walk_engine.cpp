2cf7785a5cbe50492ad9fffa3d72d0d96831b41c
Lieven Petersen <petersenlieven@gmail.com>
2024-08-20 14:21:36 +0200

    5 cm toe kinematic offset, as well as new parameter for foot pitch angle

--- bitbots_motion/bitbots_quintic_walk/src/walk_engine.cpp
+++ bitbots_motion/bitbots_quintic_walk/src/walk_engine.cpp
@@ -518,8 +518,8 @@ void WalkEngine::buildTrajectories(WalkEngine::TrajectoryType type) {
   foot_spline_.pitch()->addPoint(0.0, foot_orientation_pos_at_last_foot_change_.y(),
                                  foot_orientation_vel_at_last_foot_change_.y(),
                                  foot_orientation_acc_at_foot_change_.y());
-  foot_spline_.pitch()->addPoint(double_support_length, 0.1);
-  // foot_spline_.pitch()->addPoint(double_support_length, config_.foot_pitch_angle);
+  foot_spline_.pitch()->addPoint(double_support_length, config_.foot_pitch_angle);
+  // foot_spline_.pitch()->addPoint(double_support_length, 0.1);
   // foot_spline_.pitch()->addPoint(double_support_length + single_support_length * config_.foot_apex_phase, 0.1);
   foot_spline_.pitch()->addPoint(half_period, 0.0);
 
