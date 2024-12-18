5246d3603cd95f7d9b9443a5e6b5a045c5298d92
Helena Jäger <helena.jaeger@studium.uni-hamburg.de>
2024-12-18 16:18:03 +0100

    fixed module initialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
+use map::{ObstacleMap, ObstacleMapConfig};
+use obstacle::RoundObstacle;
 use pyo3::prelude::*;
 
 mod map;
 mod obstacle;
 mod planner;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust.
 #[pymodule]
 fn bitbots_pathplanning_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<RoundObstacle>()?;
+    m.add_class::<ObstacleMap>()?;
+    m.add_class::<ObstacleMapConfig>()?;
     Ok(())
 }
