9b73d06a59a3ee87bc4436fa87e85b64975ffb7c
Florian Vahl <git@flova.de>
2024-12-20 16:57:24 +0100

    Add type checks for other python packages

--- bitbots_navigation/bitbots_localization_handler/bitbots_localization_handler/localization_dsd/actions/__init__.py
+++ bitbots_navigation/bitbots_localization_handler/bitbots_localization_handler/localization_dsd/actions/__init__.py
@@ -1,6 +1,12 @@
+# Setting up runtime type checking for this package
+# We need to do this again here because the dsd imports
+# the decisions and actions from this package in a standalone way
+from beartype.claw import beartype_this_package
 from dynamic_stack_decider.abstract_action_element import AbstractActionElement
 
 from bitbots_localization_handler.localization_dsd.localization_blackboard import LocalizationBlackboard
+
+beartype_this_package()
 
 
 class AbstractLocalizationActionElement(AbstractActionElement):
