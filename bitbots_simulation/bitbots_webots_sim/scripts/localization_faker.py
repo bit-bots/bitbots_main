4e0f4c12718a3499e8155c62b12f9035224ce891
Jasper Güldenstein <jasper.gueldenstein@uni-hamburg.de>
2025-04-16 11:31:43 +0000

    fake localization fixes

--- bitbots_simulation/bitbots_webots_sim/scripts/localization_faker.py
+++ bitbots_simulation/bitbots_webots_sim/scripts/localization_faker.py
@@ -8,6 +8,7 @@ from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import TransformStamped
 from rclpy.duration import Duration
 from rclpy.node import Node
+from rclpy.parameter import Parameter
 
 
 class LocalizationFaker(Node):
@@ -17,16 +18,24 @@ class LocalizationFaker(Node):
         self.tf_buffer = tf2_ros.Buffer(cache_time=Duration(seconds=10.0))
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
         self.br = tf2_ros.TransformBroadcaster(self)
+        use_sim_time_param = Parameter("use_sim_time", Parameter.Type.BOOL, True)
+        self.set_parameters([use_sim_time_param])
 
     def model_state_to_tf(self, model_state_msg):
         t = TransformStamped()
-        t.header.stamp = self.get_clock().now()
+        try:
+            time = self.get_clock().now() - Duration(seconds=0.5)
+        except Exception:
+            return
+        t.header.stamp = time.to_msg()
         for i, name in enumerate(model_state_msg.name):
-            t.header.frame_id = name + "/map"
-            t.child_frame_id = name + "/odom"
+            if name != "amy":
+                continue
+            t.header.frame_id = "map"
+            t.child_frame_id = "odom"
             try:
                 robot_in_odom = self.tf_buffer.lookup_transform(
-                    name + "/odom", name + "/base_link", t.header.stamp, timeout=Duration(seconds=0.1)
+                    "odom", "base_link", t.header.stamp, timeout=Duration(seconds=0.1)
                 )
             except tf2_ros.LookupException as ex:
                 self.get_logger().warn(self.get_name() + ": " + str(ex), throttle_duration_sec=5.0)
@@ -69,10 +78,10 @@ class LocalizationFaker(Node):
             transform_robot = transforms3d.affines.compose(pos_robot, rot_robot, [1, 1, 1])
 
             transform_final = np.matmul(transform_robot, np.linalg.inv(transform_odom))
-            t, r, _, _ = transforms3d.affines.decompose(transform_final)
-            t.transform.translation.x = t[0]
-            t.transform.translation.y = t[1]
-            t.transform.translation.z = t[2]
+            translation, r, _, _ = transforms3d.affines.decompose(transform_final)
+            t.transform.translation.x = translation[0]
+            t.transform.translation.y = translation[1]
+            t.transform.translation.z = translation[2]
             q = transforms3d.quaternions.mat2quat(r)
             t.transform.rotation.w = q[0]
             t.transform.rotation.x = q[1]
