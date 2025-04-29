1ea84e5baa9f1bc1cd2b3f7ada8e9a2a460869b6
Lea Wedmann <wedmann.lea@gmail.com>
2025-04-29 16:43:05 +0200

    add launch files

--- bitbots_misc/bitbots_education/setup.py
+++ bitbots_misc/bitbots_education/setup.py
@@ -1,25 +1,26 @@
+import glob
+
 from setuptools import find_packages, setup
 
-package_name = 'bitbots_education'
+package_name = "bitbots_education"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob.glob("launch/*.launch")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='wedmann.lea@gmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="root",
+    maintainer_email="wedmann.lea@gmail.com",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
