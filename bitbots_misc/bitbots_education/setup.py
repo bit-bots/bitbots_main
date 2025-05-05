3d09fca3dc83f17f30aac1811def67e25b9b3599
Florian Vahl <git@flova.de>
2025-05-05 16:49:26 +0200

    Add static templating

--- bitbots_misc/bitbots_education/setup.py
+++ bitbots_misc/bitbots_education/setup.py
@@ -1,28 +1,68 @@
 import glob
+import inspect
 import os
+import shutil
+import sys
+import tempfile
 
 from setuptools import find_packages, setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+import distutils.command.install as distutils_install
+from jinja2 import Environment, FileSystemLoader
 
 package_name = "bitbots_education"
 
 def generate_data_files(share_path, dir):
     data_files = []
-    
+
     for path, _, files in os.walk(dir):
         list_entry = (share_path + path, [os.path.join(path, f) for f in files if not f.startswith('.')])
         data_files.append(list_entry)
 
     return data_files
 
+def build_html():
+
+    output_path = "out"
+    components_path = os.path.join(output_path, "components")
+    template_path = "templates"
+
+    env = Environment(loader=FileSystemLoader(template_path))
+
+    # Render all templates excluding the ones in the components folder
+    for root, dirs, files in os.walk(template_path):
+        for file in files:
+            if os.path.abspath(root) != os.path.abspath(components_path):
+                if file.endswith(".html"):
+                    template_file = os.path.relpath(os.path.join(root, file), template_path)
+                    template = env.get_template(template_file)
+                    output = template.render()
+
+                    # Get install path
+                    output_file_path = os.path.join(output_path, template_file)
+                    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+
+                    with open(output_file_path, "w") as f:
+                        f.write(output)
+                else:
+                    # Copy all other files
+                    src = os.path.join(root, file)
+                    dst = os.path.join(output_path, os.path.relpath(src, template_path))
+                    os.makedirs(os.path.dirname(dst), exist_ok=True)
+                    shutil.copy2(src, dst)
+
+build_html()
+
 setup(
     name=package_name,
-    version="0.0.0",
-    packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/launch", glob.glob("launch/*.launch")),
-    ] + generate_data_files('share/' + package_name + '/', 'web'),
+    ] + generate_data_files("share/" + package_name + "/", "out/"),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     install_requires=["setuptools"],
     zip_safe=True,
     maintainer="root",
