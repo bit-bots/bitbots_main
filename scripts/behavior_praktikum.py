1777a34a84a0aeeada8f0265619505fb9f881d27
Lea Wedmann <wedmann.lea@gmail.com>
2025-05-15 22:15:14 +0200

    add handout file

---
+++ scripts/behavior_praktikum.py
@@ -0,0 +1,69 @@
+import enum
+
+import rclpy
+from game_controller_hl_interfaces.msg import GameState
+from geometry_msgs.msg import Twist
+from rclpy.node import Node
+from transitions.extensions import GraphMachine
+
+
+class States(enum.Enum):
+    INITIAL = 0
+    # TODO add more states here
+
+
+class StateMachine(Node):
+    def __init__(self):
+        super().__init__("behavior_praktikum")
+        self.get_logger().info("Activate sim time")
+        self.set_parameters(
+            [rclpy.parameter.Parameter("use_sim_time", rclpy.Parameter.Type.BOOL, True)],
+        )
+
+        # Create state machine
+        self.state: States
+        self.machine = GraphMachine(
+            model=self, states=States, initial=States.INITIAL, title="Behavior", show_conditions=True
+        )
+        self.machine.add_transition(trigger="initial", source="*", dest=States.INITIAL)
+        # TODO add more transitions here
+
+        # Render the state machine as a graph
+        self.get_graph().draw("behavior.png", prog="dot")
+
+        # Create publisher
+        self.velocity_publisher_ = self.create_publisher(Twist, "/cmd_vel", 10)
+
+        # Create subscriber
+        self.subscription = self.create_subscription(GameState, "gamestate", self.listener_callback, 10)
+
+        # Create timer
+        self.timer = self.create_timer(0.1, self.timer_callback)
+
+    def listener_callback(self, msg):
+        match msg.game_state:
+            case GameState.GAMESTATE_INITIAL:
+                self.initial()
+            # TODO add more cases for other gamestates and call trigger functions
+
+    def timer_callback(self):
+        match self.state:
+            case States.INITIAL:
+                velocity = Twist()
+                velocity.angular.x = -1.0  # stops the robot
+                self.velocity_publisher_.publish(velocity)
+            # TODO add more cases for your new states
+            case _:
+                self.get_logger().warn("State: Unknown state")
+
+
+if __name__ == "__main__":
+    rclpy.init()
+    node = StateMachine()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info("Node stopped by user.")
+    finally:
+        node.destroy_node()
+    rclpy.shutdown()
