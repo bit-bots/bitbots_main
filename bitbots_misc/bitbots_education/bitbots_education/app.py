c3fa05d1df62801db370385a917a4d3cad69f5a8
Florian Vahl <git@flova.de>
2025-05-26 18:48:54 +0200

    Move to server side flask rendering

---
+++ bitbots_misc/bitbots_education/bitbots_education/app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, render_template
+from ament_index_python import get_package_share_directory
+
+
+template_dir = os.path.join(
+    get_package_share_directory("bitbots_education"), "templates"
+)
+
+app = Flask(__name__, template_folder=template_dir)
+
+
+@app.route("/")
+def index():
+    return render_template("pages/dashboard.html")
+
+@app.route("/imu")
+def imu():
+    return render_template("pages/imu.html")
+
+@app.route("/vision")
+def vision():
+    return render_template("pages/vision.html")
+
+@app.route("/motors")
+def motors():
+    return render_template("pages/motors.html")
+
+@app.route("/behavior")
+def behavior():
+    return render_template("pages/behavior.html")
+
+
+def main():
+    app.run(host="0.0.0.0", port=8000)
--- bitbots_misc/bitbots_education/setup.py
+++ bitbots_misc/bitbots_education/setup.py
@@ -7,7 +7,6 @@ from setuptools import find_packages, setup
 
 package_name = "bitbots_education"
 
-
 def generate_data_files(share_path, dir):
     data_files = []
 
@@ -17,39 +16,6 @@ def generate_data_files(share_path, dir):
 
     return data_files
 
-
-def build_html():
-    output_path = "out"
-    components_path = os.path.join(output_path, "components")
-    template_path = "templates"
-
-    env = Environment(loader=FileSystemLoader(template_path))
-
-    # Render all templates excluding the ones in the components folder
-    for root, _, files in os.walk(template_path):
-        for file in files:
-            if os.path.abspath(root) != os.path.abspath(components_path):
-                if file.endswith(".html"):
-                    template_file = os.path.relpath(os.path.join(root, file), template_path)
-                    template = env.get_template(template_file)
-                    output = template.render()
-
-                    # Get install path
-                    output_file_path = os.path.join(output_path, template_file)
-                    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
-
-                    with open(output_file_path, "w") as f:
-                        f.write(output)
-                else:
-                    # Copy all other files
-                    src = os.path.join(root, file)
-                    dst = os.path.join(output_path, os.path.relpath(src, template_path))
-                    os.makedirs(os.path.dirname(dst), exist_ok=True)
-                    shutil.copy2(src, dst)
-
-
-build_html()
-
 setup(
     name=package_name,
     data_files=[
@@ -57,7 +23,7 @@ setup(
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/launch", glob.glob("launch/*.launch")),
     ]
-    + generate_data_files("share/" + package_name + "/", "out/"),
+    + generate_data_files("share/" + package_name + "/", "templates/"),
     version="0.0.0",
     packages=find_packages(exclude=["test"]),
     install_requires=["setuptools"],
@@ -68,6 +34,8 @@ setup(
     license="Apache-2.0",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": [],
+        "console_scripts": [
+            "webserver = bitbots_education.app:main",
+        ],
     },
 )
