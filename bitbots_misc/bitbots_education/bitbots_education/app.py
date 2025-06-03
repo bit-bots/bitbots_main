320fd107478a8fadde3b6e36a028c94f9d53afa3
Lea Wedmann <wedmann.lea@gmail.com>
2025-06-03 14:10:54 +0200

    add csv

--- bitbots_misc/bitbots_education/bitbots_education/app.py
+++ bitbots_misc/bitbots_education/bitbots_education/app.py
@@ -1,33 +1,48 @@
+import csv
 import os
-from flask import Flask, render_template
+import time
+
 from ament_index_python import get_package_share_directory
+from flask import Flask, render_template
 
-
-template_dir = os.path.join(
-    get_package_share_directory("bitbots_education"), "templates"
-)
-static_folder = os.path.join(
-    get_package_share_directory("bitbots_education"), "static"
-)
+template_dir = os.path.join(get_package_share_directory("bitbots_education"), "templates")
+static_folder = os.path.join(get_package_share_directory("bitbots_education"), "static")
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_folder)
 
 
-@app.route("/")
-def index():
-    return render_template("pages/dashboard.html")
+def write_csv(vp, button):
+    file_exists = os.path.isfile("logs.csv")
+
+    with open("logs.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+
+        # Kopfzeile nur schreiben, wenn die Datei neu ist
+        if not file_exists or os.stat("logs.csv").st_size == 0:
+            writer.writerow(["VP", "Button", "Timestamp"])
+        curr_time = time.strftime("%H:%M:%S", time.localtime())
+        writer.writerow([vp, button, curr_time])
+
+
+@app.route("//<logging>")
+def index(logging="false"):
+    return render_template("pages/dashboard.html", logging=logging)
+
 
 @app.route("/imu")
 def imu():
     return render_template("pages/imu.html")
 
+
 @app.route("/vision")
 def vision():
     return render_template("pages/vision.html")
 
+
 @app.route("/motors")
 def motors():
     return render_template("pages/motors.html")
+
 
 @app.route("/behavior")
 def behavior():
