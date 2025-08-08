e816f3239f1c96b1bf81e3c3e7f6bea4d49a2284
Lea <wedmann.lea@gmail.com>
2025-08-08 15:28:22 +0200

    add sequence scoring

--- bitbots_misc/bitbots_education/scripts/study_evaluation.py
+++ bitbots_misc/bitbots_education/scripts/study_evaluation.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 data = pd.read_csv("robot_quiz_responses.csv")
 quiz_data = None
+
 def calculate_gender():
     women = data[data['Zu welchem Geschlecht fühlst du dich zugehörig?']=='weiblich']
     men = data[data['Zu welchem Geschlecht fühlst du dich zugehörig?']=='männlich']
@@ -12,7 +13,71 @@ def calculate_gender():
     total_count_women = women['Zu welchem Geschlecht fühlst du dich zugehörig?'].count()
     total_count_men = men['Zu welchem Geschlecht fühlst du dich zugehörig?'].count()
     total_count_divers = divers['Zu welchem Geschlecht fühlst du dich zugehörig?'].count()
-    print ("Weiblich:", total_count_women, " Männlich:", total_count_men, " Divers:",total_count_divers)
+    return total_count_divers, total_count_men, total_count_women
+
+# A Naive recursive implementation of LCS problem
+
+# Returns length of LCS for s1[0..m-1], s2[0..n-1]
+def lcsRec(s1, s2, m, n):
+  
+    # Base case: If either string is empty, the length of LCS is 0
+    if m == 0 or n == 0:
+        return 0
+
+    # If the last characters of both substrings match
+    if s1[m - 1] == s2[n - 1]:
+
+        # Include this character in LCS and recur for remaining substrings
+        return 1 + lcsRec(s1, s2, m - 1, n - 1)
+
+    else:
+        # If the last characters do not match
+        # Recur for two cases:
+        # 1. Exclude the last character of S1 
+        # 2. Exclude the last character of S2 
+        # Take the maximum of these two recursive calls
+        return max(lcsRec(s1, s2, m, n - 1), lcsRec(s1, s2, m - 1, n))
+
+def lcs(s1,s2):
+    m = len(s1)
+    n = len(s2)
+    return lcsRec(s1,s2,m,n)
+
+def make_sequence_string(row):
+    rankMotors1 =  row['Ordne Motoren, die die längste Zeit innerhalb eines Fußballspieles bewegt werden, nach oben. [Rank 1]']
+    rankMotors2 =  row['Ordne Motoren, die die längste Zeit innerhalb eines Fußballspieles bewegt werden, nach oben. [Rank 2]']
+    rankMotors3 =  row['Ordne Motoren, die die längste Zeit innerhalb eines Fußballspieles bewegt werden, nach oben. [Rank 3]']
+
+    rankBehavior1 = row['Ordne die Aktionen nach oben die zuerst ausgeführt werden um einen Ball zu kicken. [Rank 1]']
+    rankBehavior2 = row['Ordne die Aktionen nach oben die zuerst ausgeführt werden um einen Ball zu kicken. [Rank 2]']
+    rankBehavior3 = row['Ordne die Aktionen nach oben die zuerst ausgeführt werden um einen Ball zu kicken. [Rank 3]']
+    rankBehavior4 = row['Ordne die Aktionen nach oben die zuerst ausgeführt werden um einen Ball zu kicken. [Rank 4]']
+
+    rankList = [rankMotors1, rankMotors2, rankMotors3, rankBehavior1, rankBehavior2, rankBehavior3, rankBehavior4]
+    motor_sequence = []
+    behavoir_sequence = []
+    for i in rankList:
+        match i:
+            case 'Motor im Hals':
+                motor_sequence.append('1')
+            case 'Motor in der Hüfte':
+                motor_sequence.append('2')
+            case 'Motor in der Schulter':
+                motor_sequence.append('3')
+            case 'Ball finden':
+                behavoir_sequence.append('1')
+            case 'Zum Ball gehen':
+                behavoir_sequence.append('2')
+            case 'Auf der Stelle gehen':
+                behavoir_sequence.append('3')
+            case 'Kick ausführen':
+                behavoir_sequence.append('4')
+
+    delimiter = ','   
+    motor_string = delimiter.join(motor_sequence)
+    behavoir_string = delimiter.join(behavoir_sequence)        
+    print("Motor sequence:" + motor_string, "Behavior sequence:" + behavoir_string)
+    return motor_sequence, behavoir_sequence
 
 def calculate_quiz_score(row):
     score = 0
@@ -33,6 +98,9 @@ def calculate_quiz_score(row):
     if (row['Müssen Roboter in der humanoid kid size league des Robocups aufstehen können?'] == 'ja, sie müssen im Spiel selbstständig aufstehen können um spielen zu dürfen'):
         score += 1   
 
+    motor_string, behavior_string = make_sequence_string(row)
+    score += lcs(motor_string, '123')  # Expected motor sequence
+    score += lcs(behavior_string, '1234')
     return score
  
 
@@ -45,8 +113,8 @@ def add_quiz_score():
     quiz_score = pd.Series(scores, name='Quiz Score')
     quiz_data = pd.concat([data, quiz_score], axis=1)
 
-    print(quiz_data['Quiz Score'])
+    return quiz_data
 
 
-calculate_gender()
-add_quiz_score()
+count_divers, count_men, count_women = calculate_gender()
+quiz_data = add_quiz_score()
