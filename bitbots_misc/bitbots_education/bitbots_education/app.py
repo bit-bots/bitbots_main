45a05e228c610d1773c490ee81afe2b76fdc9884
Lea <wedmann.lea@gmail.com>
2026-01-06 14:56:01 +0100

    fix code style issues

--- bitbots_misc/bitbots_education/bitbots_education/app.py
+++ bitbots_misc/bitbots_education/bitbots_education/app.py
@@ -5,69 +5,69 @@ import time
 from ament_index_python import get_package_share_directory
 from flask import Flask, render_template, request
 
-template_dir = os.path.join(get_package_share_directory('bitbots_education'), 'templates')
-static_folder = os.path.join(get_package_share_directory('bitbots_education'), 'static')
-csv_file_path = os.path.realpath('logs.csv')
+template_dir = os.path.join(get_package_share_directory("bitbots_education"), "templates")
+static_folder = os.path.join(get_package_share_directory("bitbots_education"), "static")
+csv_file_path = os.path.realpath("logs.csv")
 
 app = Flask(__name__, template_folder=template_dir, static_folder=static_folder)
 
 
 def write_csv(vp, page, button, status):
     file_exists = os.path.isfile(csv_file_path)
-    with open(csv_file_path, 'a+', newline='') as file:
+    with open(csv_file_path, "a+", newline="") as file:
         writer = csv.writer(file)
         # Kopfzeile nur schreiben, wenn die Datei neu ist
-        if not file_exists or os.stat('logs.csv').st_size == 0:
-            writer.writerow(['VP', 'Page', 'Button', 'Status', 'Timestamp'])
+        if not file_exists or os.stat("logs.csv").st_size == 0:
+            writer.writerow(["VP", "Page", "Button", "Status", "Timestamp"])
         curr_time = time.time()
         writer.writerow([vp, page, button, status, curr_time])
 
 
 def vp_track(page, button, status):
-    username = request.cookies.get('vp_number')
+    username = request.cookies.get("vp_number")
     if username is not None:
         write_csv(username, page, button, status)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    vp_track('dashboard', '', '')
-    dashboard = 'pages/dashboard.html'
-    return render_template(dashboard, logging=bool(request.args.get('logging', False)))
+    vp_track("dashboard", "", "")
+    dashboard = "pages/dashboard.html"
+    return render_template(dashboard, logging=bool(request.args.get("logging", False)))
 
 
-@app.route('/imu')
+@app.route("/imu")
 def imu():
-    vp_track('imu', '', '')
-    return render_template('pages/imu.html')
+    vp_track("imu", "", "")
+    return render_template("pages/imu.html")
 
 
-@app.route('/vision')
+@app.route("/vision")
 def vision():
-    vp_track('vision', '', '')
-    return render_template('pages/vision.html')
+    vp_track("vision", "", "")
+    return render_template("pages/vision.html")
 
 
-@app.route('/motors')
+@app.route("/motors")
 def motors():
-    vp_track('motors', '', '')
-    return render_template('pages/motors.html')
+    vp_track("motors", "", "")
+    return render_template("pages/motors.html")
 
 
-@app.route('/behavior')
+@app.route("/behavior")
 def behavior():
-    vp_track('behavior', '', '')
-    return render_template('pages/behavior.html')
+    vp_track("behavior", "", "")
+    return render_template("pages/behavior.html")
 
 
-@app.route('/logs')
+@app.route("/logs")
 def logs():
-    page = request.args.get('page')
-    button = request.args.get('button')
-    status = request.args.get('status')
+    page = request.args.get("page")
+    button = request.args.get("button")
+    status = request.args.get("status")
     vp_track(page[1:], button, status)
-    return ('', 204)
+    return ("", 204)
 
 
 def main():
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=8000)
--- bitbots_misc/bitbots_education/setup.py
+++ bitbots_misc/bitbots_education/setup.py
@@ -3,7 +3,7 @@ import os
 
 from setuptools import find_packages, setup
 
-package_name = 'bitbots_education'
+package_name = "bitbots_education"
 
 
 def generate_data_files(share_path, directory):
@@ -11,7 +11,7 @@ def generate_data_files(share_path, directory):
 
     for path, _, files in os.walk(directory):
         p = share_path + path
-        list_entry = (p, [os.path.join(path, f) for f in files if not f.startswith('.')])
+        list_entry = (p, [os.path.join(path, f) for f in files if not f.startswith(".")])
         data_files.append(list_entry)
 
     return data_files
@@ -20,24 +20,24 @@ def generate_data_files(share_path, directory):
 setup(
     name=package_name,
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob.glob('launch/*.launch')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob.glob("launch/*.launch")),
     ]
-    + generate_data_files('share/' + package_name + '/', 'templates/')
-    + generate_data_files('share/' + package_name + '/', 'static/'),
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    install_requires=['setuptools'],
+    + generate_data_files("share/" + package_name + "/", "templates/")
+    + generate_data_files("share/" + package_name + "/", "static/"),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='wedmann.lea@gmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="root",
+    maintainer_email="wedmann.lea@gmail.com",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'webserver = bitbots_education.app:main',
+        "console_scripts": [
+            "webserver = bitbots_education.app:main",
         ],
     },
 )
