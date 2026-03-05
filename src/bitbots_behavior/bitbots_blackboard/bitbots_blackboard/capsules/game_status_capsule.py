dd78ca26a78ff036752dbe20ca8037f33c4d32a4
Clemens <c.b.wulff@icloud.com>
2026-03-05 18:16:34 +0100

    game_state_capsule updated

--- src/bitbots_behavior/bitbots_blackboard/bitbots_blackboard/capsules/game_status_capsule.py
+++ src/bitbots_behavior/bitbots_blackboard/bitbots_blackboard/capsules/game_status_capsule.py
@@ -12,6 +12,7 @@ class GameStatusCapsule(AbstractBlackboardCapsule):
     def __init__(self, node, blackboard=None):
         super().__init__(node, blackboard)
         self.team_id: int = get_parameters_from_other_node(self._node, "parameter_blackboard", ["team_id"])["team_id"]
+        self.own_id: int = get_parameters_from_other_node(self._node, "parameter_blackboard", ["bot_id"])["bot_id"]
         self.gamestate = GameState()
         self.last_update: float = 0.0
         self.unpenalized_time: float = 0.0
@@ -20,25 +21,29 @@ class GameStatusCapsule(AbstractBlackboardCapsule):
         self.free_kick_kickoff_team: Optional[bool] = None
 
     def get_gamestate(self) -> int:
-        return self.gamestate.game_state
+        #Init, ready, set, playing, finished, standby
+        return self.gamestate.main_state
 
-    def get_secondary_state(self) -> int:
-        return self.gamestate.secondary_state
+    def get_gamePhase(self) -> int:
+        #Timeout, Normal, Penaltyshoot
+        return self.gamestate.gamePhase
 
-    def get_secondary_state_mode(self) -> int:
-        return self.gamestate.secondary_state_mode
+    def get_setPlay(self) -> int:
+        #None, Kick In, Goalkick, Cornerkick, Pushing Freekick, Penaltykick
+        return self.gamestate.setPlay
 
     def get_secondary_team(self) -> int:
-        return self.gamestate.secondary_state_team
+        #Team ID, wer in set Play den Baall hat
+        return self.gamestate.kickingTeam
 
     def has_kickoff(self) -> bool:
-        return self.gamestate.has_kick_off
+        #vegelcih mit eigener Teamnummer
+        return self.gamestate.kickingTeam == self.team_id
 
     def has_penalty_kick(self) -> bool:
-        return (
-            self.gamestate.secondary_state == GameState.STATE_PENALTYKICK
-            or self.gamestate.secondary_state == GameState.STATE_PENALTYSHOOT
-        ) and self.gamestate._secondary_state_team == self.team_id
+        return( 
+            self.gamestate.set_play == GameState.SET_PLAY_PENALTY_KICK
+            and self.gamestate.kickingTeam == self.team_id)
 
     def get_our_goals(self) -> int:
         return self.gamestate.own_score
@@ -55,28 +60,20 @@ class GameStatusCapsule(AbstractBlackboardCapsule):
     def get_seconds_remaining(self) -> float:
         # Time from the message minus time passed since receiving it
         return max(
-            self.gamestate.seconds_remaining - (self._node.get_clock().now().nanoseconds / 1e9 - self.last_update), 0.0
+            self.gamestate.secs_remaining - (self._node.get_clock().now().nanoseconds / 1e9 - self.last_update), 0.0
         )
 
     def get_secondary_seconds_remaining(self) -> float:
         """Seconds remaining for things like kickoff"""
         # Time from the message minus time passed since receiving it
         return max(
-            self.gamestate.secondary_seconds_remaining
+            self.gamestate.secondary_time
             - (self._node.get_clock().now().nanoseconds / 1e9 - self.last_update),
             0.0,
         )
 
-    def get_seconds_since_last_drop_ball(self) -> Optional[float]:
-        """Returns the seconds since the last drop in"""
-        if self.gamestate.drop_in_time == -1:
-            return None
-        else:
-            # Time from the message plus seconds passed since receiving it
-            return self.gamestate.drop_in_time + (self._node.get_clock().now().nanoseconds / 1e9 - self.last_update)
-
     def get_seconds_since_unpenalized(self) -> float:
-        return self._node.get_clock().now().nanoseconds / 1e9 - self.unpenalized_time
+        return self._node.get_clock().now().nanoseconds / 1e9 - self.seconds_till_unpenalized
 
     def get_is_penalized(self) -> bool:
         return self.gamestate.penalized
@@ -86,9 +83,6 @@ class GameStatusCapsule(AbstractBlackboardCapsule):
 
     def get_team_id(self) -> int:
         return self.team_id
-
-    def get_red_cards(self) -> int:
-        return self.gamestate.team_mates_with_red_card
 
     def gamestate_callback(self, gamestate_msg: GameState) -> None:
         if self.gamestate.penalized and not gamestate_msg.penalized:
@@ -101,21 +95,24 @@ class GameStatusCapsule(AbstractBlackboardCapsule):
         if gamestate_msg.rival_score > self.gamestate.rival_score:
             self.last_goal_time = self._node.get_clock().now().nanoseconds / 1e9
 
+        '''Was soll das? self.free_kick_kickoff wird nirgends weiter verwendet...
         if (
-            gamestate_msg.secondary_state_mode == 2
-            and self.gamestate.secondary_state_mode != 2
-            and gamestate_msg.game_state == GameState.GAMESTATE_PLAYING
+            gamestate_msg.main_state == GameState.STATE_SET
+            and self.gamestate.setPlay != 2
+            and gamestate_msg.state == GameState.STATE_PLAYING
         ):
             # secondary action is now executed but we will not see this in the new messages.
             # it will look like a normal kick off, but we need to remember that this is some sort of free kick
             # we set the kickoff value accordingly, then we will not be allowed to move if it is a kick for the others
-            self.free_kick_kickoff_team = gamestate_msg.secondary_state_team
-
-        if gamestate_msg.secondary_state_mode != 2 and gamestate_msg.secondary_seconds_remaining == 0:
+            self.free_kick_kickoff_team = gamestate_msg.kickingTeam
+            
+        
+        if gamestate_msg.setPlay != 2 and gamestate_msg.secondaryTime == 0:
             self.free_kick_kickoff_team = None
+        
 
         if self.free_kick_kickoff_team is not None:
             gamestate_msg.has_kick_off = self.free_kick_kickoff_team == self.team_id
-
+        '''
         self.last_update = self._node.get_clock().now().nanoseconds / 1e9
         self.gamestate = gamestate_msg
