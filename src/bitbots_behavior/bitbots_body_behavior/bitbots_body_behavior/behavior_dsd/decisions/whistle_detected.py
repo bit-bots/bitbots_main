a2bddc8cef58d0c24fea51066c11cb1b86bbca10
Valerie <v.bartel.vb@gmail.com>
2026-03-10 18:42:58 +0100

    fix wrong anming of capsule in whistle decision

--- src/bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/behavior_dsd/decisions/whistle_detected.py
+++ src/bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/behavior_dsd/decisions/whistle_detected.py
@@ -8,12 +8,12 @@ class WhistleDetected(AbstractDecisionElement):
     def __init__(self, blackboard, dsd, parameters):
         super().__init__(blackboard, dsd, parameters)
         self.bb = blackboard
-        self.bb.game_state_capsule.whistle_detected = False
+        self.bb.gamestate.whistle_detected = False
 
     def perform(self, reevaluate=False):
-        if self.bb.game_state_capsule.whistle_detected:
+        if self.bb.gamestate.whistle_detected:
             return "YES"
         return "NO"
 
     def get_reevaluate(self):
-        return False
+        return True
