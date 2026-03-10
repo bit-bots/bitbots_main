e4cd28e2323d479ba9fb7db62c237157109d9509
Valerie <v.bartel.vb@gmail.com>
2026-03-10 19:13:49 +0100

    remove lib

--- src/lib/sound_play/setup.py
+++
@@ -1,9 +0,0 @@
-from catkin_pkg.python_setup import generate_distutils_setup
-from setuptools import setup
-
-d = generate_distutils_setup(
-      packages=['sound_play'],
-      package_dir={'': 'src'}
-      )
-
-setup(**d)
