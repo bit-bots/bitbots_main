bdea7370e1cef3a13bea4d26bbfed5a87d3f042b
Lea Wedmann <wedmann.lea@gmail.com>
2026-04-05 22:53:47 +0200

    Remove packages that are not needed for the piplus

--- src/bitbots_motion/bitbots_moveit_bindings/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="bitbots_moveit_bindings",
-    packages=["bitbots_moveit_bindings"],
-    zip_safe=True,
-    keywords=["ROS"],
-    license="MIT",
-)
