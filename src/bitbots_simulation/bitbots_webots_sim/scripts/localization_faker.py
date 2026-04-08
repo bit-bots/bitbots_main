2efd1c51891455b6d9510bfd7a3d8464f598c491
Lea <wedmann.lea@gmail.com>
2026-04-08 17:06:44 +0200

    fake localization, 2 bots webots world

--- src/bitbots_simulation/bitbots_webots_sim/scripts/localization_faker.py
+++ src/bitbots_simulation/bitbots_webots_sim/scripts/localization_faker.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from enum import Enum
+from typing import Optional
+
 import numpy as np
 import rclpy
 import tf2_ros
@@ -8,6 +11,12 @@ from gazebo_msgs.msg import ModelStates
 from geometry_msgs.msg import TransformStamped
 from rclpy.duration import Duration
 from rclpy.node import Node
+from rclpy.parameter import Parameter
+
+
+class Side(Enum):
+    LEFT = 1
+    RIGHT = 2
 
 
 class LocalizationFaker(Node):
@@ -17,31 +26,32 @@ class LocalizationFaker(Node):
         self.tf_buffer = tf2_ros.Buffer(cache_time=Duration(seconds=10.0))
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
         self.br = tf2_ros.TransformBroadcaster(self)
+        use_sim_time_param = Parameter("use_sim_time", Parameter.Type.BOOL, True)
+        self.set_parameters([use_sim_time_param])
+        self.side: Optional[Side] = None
 
     def model_state_to_tf(self, model_state_msg):
         t = TransformStamped()
-        t.header.stamp = self.get_clock().now()
         for i, name in enumerate(model_state_msg.name):
-            t.header.frame_id = name + "/map"
-            t.child_frame_id = name + "/odom"
+            if name not in ["amy", "rory", "blue player 1", "red player 1"]:
+                continue
+            t.header.frame_id = "map"
+            t.child_frame_id = "odom"
             try:
-                robot_in_odom = self.tf_buffer.lookup_transform(
-                    name + "/odom", name + "/base_link", t.header.stamp, timeout=Duration(seconds=0.1)
-                )
+                robot_in_odom = self.tf_buffer.lookup_transform("odom", "base_link", rclpy.time.Time())
             except tf2_ros.LookupException as ex:
                 self.get_logger().warn(self.get_name() + ": " + str(ex), throttle_duration_sec=5.0)
                 return
             except tf2_ros.ExtrapolationException as ex:
                 self.get_logger().warn(self.get_name() + ": " + str(ex), throttle_duration_sec=5.0)
                 return
-            rot_odom = transforms3d.quaternions.quat2mat(
-                [
-                    robot_in_odom.transform.rotation.w,
-                    robot_in_odom.transform.rotation.x,
-                    robot_in_odom.transform.rotation.y,
-                    robot_in_odom.transform.rotation.z,
-                ]
-            )
+            except tf2_ros.ConnectivityException as ex:
+                self.get_logger().warn(self.get_name() + ": " + str(ex), throttle_duration_sec=5.0)
+                return
+            t.header.stamp = robot_in_odom.header.stamp
+            q = robot_in_odom.transform.rotation
+            _, _, r_odom_robot_yaw = transforms3d.euler.quat2euler([q.w, q.x, q.y, q.z])
+            r_odom_robot = transforms3d.euler.euler2mat(0, 0, r_odom_robot_yaw)
 
             transform_odom = transforms3d.affines.compose(
                 [
@@ -49,7 +59,7 @@ class LocalizationFaker(Node):
                     robot_in_odom.transform.translation.y,
                     robot_in_odom.transform.translation.z,
                 ],
-                rot_odom,
+                r_odom_robot,
                 [1, 1, 1],
             )
 
@@ -58,27 +68,43 @@ class LocalizationFaker(Node):
                 model_state_msg.pose[i].position.y,
                 model_state_msg.pose[i].position.z,
             ]
-            rot_robot = transforms3d.quaternions.quat2mat(
-                [
-                    model_state_msg.pose[i].orientation.w,
-                    model_state_msg.pose[i].orientation.x,
-                    model_state_msg.pose[i].orientation.y,
-                    model_state_msg.pose[i].orientation.z,
-                ]
-            )
+
+            q = model_state_msg.pose[i].orientation
+            _, _, rot_robot_yaw = transforms3d.euler.quat2euler([q.w, q.x, q.y, q.z])
+            rot_robot = transforms3d.euler.euler2mat(0, 0, rot_robot_yaw)
             transform_robot = transforms3d.affines.compose(pos_robot, rot_robot, [1, 1, 1])
 
+            # If the starting side is not yet set, store it
+            if self.side is None:
+                if transform_robot[0, 3] < 0:
+                    self.side = Side.RIGHT
+                else:
+                    self.side = Side.LEFT
+
+            # Rotate the robots coordinate frame in the simulation based on the side it is playing on
+            if self.side == Side.LEFT:
+                flip_side = transforms3d.affines.compose(
+                    [0, 0, 0],
+                    transforms3d.euler.euler2mat(0, 0, np.pi),
+                    [1, 1, 1],
+                )
+
+                # Rotate the coordinate frame of the robot to match the side it is playing on
+                transform_robot = flip_side @ transform_robot
+
             transform_final = np.matmul(transform_robot, np.linalg.inv(transform_odom))
-            t, r, _, _ = transforms3d.affines.decompose(transform_final)
-            t.transform.translation.x = t[0]
-            t.transform.translation.y = t[1]
-            t.transform.translation.z = t[2]
+            translation, r, _, _ = transforms3d.affines.decompose(transform_final)
+            t.transform.translation.x = translation[0]
+            t.transform.translation.y = translation[1]
+            t.transform.translation.z = translation[2]
             q = transforms3d.quaternions.mat2quat(r)
             t.transform.rotation.w = q[0]
             t.transform.rotation.x = q[1]
             t.transform.rotation.y = q[2]
             t.transform.rotation.z = q[3]
             self.br.sendTransform(t)
+
+            break
 
 
 if __name__ == "__main__":
@@ -87,3 +113,5 @@ if __name__ == "__main__":
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
+    
