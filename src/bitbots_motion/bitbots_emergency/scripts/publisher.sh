cca0893257f62d5c7092b45a9500db9de5031746
sWintermoor <swintermoor@gmail.com>
2026-05-23 17:46:01 +0200

    fixed launch script

--- src/bitbots_motion/bitbots_emergency/scripts/publisher.sh
+++ src/bitbots_motion/bitbots_emergency/scripts/publisher.sh
@@ -3,5 +3,3 @@
 ROBOT_IP=${1?"Usage: publisher.sh <robot-ip>"}
 
 export ZENOH_CONFIG_OVERRIDE="mode=\"client\";connect/endpoints=[\"tcp/${ROBOT_IP}:7447\"]"
-
-pixi run ros2 launch bitbots_emergency emergency.launch publisher:=true
