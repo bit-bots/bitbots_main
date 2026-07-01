0f19a275208ac6db71e66e2016a297f0819adc83
Cornelius Krupp <cornelius.krupp@studium.uni-hamburg.de>
2026-07-01 10:30:59 +0900

    Check if we are in sim and robot domain is set for loading custom overwrites

--- src/bitbots_misc/bitbots_parameter_blackboard/launch/parameter_blackboard.launch.py
+++ src/bitbots_misc/bitbots_parameter_blackboard/launch/parameter_blackboard.launch.py
@@ -4,9 +4,9 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
 from launch.launch_description_sources import AnyLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterFile
 
@@ -15,25 +15,36 @@ def generate_launch_description() -> LaunchDescription:
     package_name = "bitbots_parameter_blackboard"
     package_share = get_package_share_directory(package_name)
 
-    sim = LaunchConfiguration("sim")
-    field_name = PythonExpression(["'hsl_kid' if '", sim, "' == 'true' else 'labor'"])
+    def create_node(context, *args, **kwargs):
+        in_sim = LaunchConfiguration("sim").perform(context) == "true"
+        field_name = "hsl_kid" if in_sim else "labor"
 
-    parameters = [
-        {"simulation_active": sim},
-        {"use_sim_time": sim},
-        {"field.name": field_name},
-        ParameterFile(PathJoinSubstitution([package_share, "config", "fields", field_name, "config.yaml"])),
-        ParameterFile(PathJoinSubstitution([package_share, "config", "global_parameters.yaml"])),
-        ParameterFile(PathJoinSubstitution([package_share, "config", "game_settings.yaml"])),
-    ]
+        parameters = [
+            {"simulation_active": in_sim},
+            {"use_sim_time": in_sim},
+            {"field.name": field_name},
+            ParameterFile(PathJoinSubstitution([package_share, "config", "fields", field_name, "config.yaml"])),
+            ParameterFile(PathJoinSubstitution([package_share, "config", "global_parameters.yaml"])),
+            ParameterFile(PathJoinSubstitution([package_share, "config", "game_settings.yaml"])),
+        ]
 
-    robot_domain = os.environ.get("ROS_DOMAIN_ID")
-    if robot_domain is not None:
-        parameters.append(
-            ParameterFile(
-                PathJoinSubstitution([package_share, "config", f"sim_game_settings_{int(robot_domain)}.yaml"])
+        robot_domain = os.environ.get("ROS_DOMAIN_ID")
+        if in_sim and robot_domain is not None:
+            parameters.append(
+                ParameterFile(
+                    PathJoinSubstitution([package_share, "config", f"sim_game_settings_{int(robot_domain)}.yaml"])
+                )
             )
-        )
+
+        return [
+            Node(
+                package="demo_nodes_cpp",
+                executable="parameter_blackboard",
+                name="parameter_blackboard",
+                arguments=["--ros-args", "--log-level", "WARN"],
+                parameters=parameters,
+            )
+        ]
 
     return LaunchDescription(
         [
@@ -43,12 +54,6 @@ def generate_launch_description() -> LaunchDescription:
                     PathJoinSubstitution([get_package_share_directory("bitbots_utils"), "launch", "welcome.launch"])
                 )
             ),
-            Node(
-                package="demo_nodes_cpp",
-                executable="parameter_blackboard",
-                name="parameter_blackboard",
-                arguments=["--ros-args", "--log-level", "WARN"],
-                parameters=parameters,
-            ),
+            OpaqueFunction(function=create_node),
         ]
     )
