b685755ff4ddcccbb31a9d2ff1e816a855b69159
Jasper <jasper.gueldenstein@uni-hamburg.de>
2026-07-02 12:54:47 +0900

    use costmap for direction of rl kick

--- src/bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/behavior_dsd/actions/kick_ball.py
+++ src/bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/behavior_dsd/actions/kick_ball.py
@@ -1,3 +1,5 @@
+import math
+
 from bitbots_blackboard.body_blackboard import BodyBlackboard
 from bitbots_blackboard.capsules.kick_capsule import KickCapsule
 from dynamic_stack_decider.abstract_action_element import AbstractActionElement
@@ -47,5 +49,28 @@ class RLKick(AbstractKickAction):
 
         elapsed = self.blackboard.node.get_clock().now() - self._start_time
         if elapsed >= Duration(seconds=self.blackboard.config["rl_kick"]["timeout"]):
+            self.blackboard.kick.stop_rl_kick()
+            self.pop()
+
+
+class RLKickTowardsGoal(AbstractKickAction):
+
+    def __init__(self, blackboard, dsd, parameters):
+        super().__init__(blackboard, dsd, parameters)
+        self._strength = parameters.get("strength", 2.0)
+        self._start_time = None
+
+    def perform(self, reevaluate=False):
+        x_dir, y_dir = self.blackboard.costmap.get_gradient_at_field_position(self.blackboard.world_model.get_ball_position_xy())
+        costmap_direction_deg_in_world = (math.degrees(math.atan2(y_dir, x_dir)))
+        # transform map to robot relative
+        if self._start_time is None:
+            self._start_time = self.blackboard.node.get_clock().now()
+            self.blackboard.kick.start_rl_kick(costmap_direction_deg_in_world,
+                                               self._strength)
+
+        elapsed = self.blackboard.node.get_clock().now() - self._start_time
+        if elapsed >= Duration(
+                seconds=self.blackboard.config["rl_kick"]["timeout"]):
             self.blackboard.kick.stop_rl_kick()
             self.pop()
