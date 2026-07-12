b71e5c430c10f2e4b81bf82922acbe43b440f28b
Jan Gutsche <github@jaagut.de>
2026-07-12 16:13:46 +0900

    Refactor KickOrDribble decision logic and update main behavior configuration

--- src/bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/behavior_dsd/decisions/kick_or_dribble.py
+++ src/bitbots_behavior/bitbots_body_behavior/bitbots_body_behavior/behavior_dsd/decisions/kick_or_dribble.py
@@ -2,6 +2,7 @@ import math
 
 from bitbots_blackboard.body_blackboard import BodyBlackboard
 from dynamic_stack_decider.abstract_decision_element import AbstractDecisionElement
+from game_controller_hsl_interfaces.msg import GameState
 
 
 class KickOrDribble(AbstractDecisionElement):
@@ -9,8 +10,6 @@ class KickOrDribble(AbstractDecisionElement):
 
     def __init__(self, blackboard, dsd, parameters):
         super().__init__(blackboard, dsd, parameters)
-        self.target_distance = parameters.get("map_goal_target_distance", 0.5)
-        self.side_offset = parameters.get("side_offset", 0.0)
         self.threshold_front = parameters.get("threshold_front", 10)
         self.threshold_behind = parameters.get("threshold_behind", 10)
 
@@ -18,15 +17,19 @@ class KickOrDribble(AbstractDecisionElement):
         """
         Determines whether to kick or dribble based on the angle of the map goal
         """
-        map_goal = self.blackboard.pathfinding.get_map_goal(self.target_distance, self.side_offset)
-        # no other robots too close
+        # Get the map goal, so we can check the angle.
+        # distance or side offset are not relevant here, so we can just use 0.0 for both.
+        map_goal = self.blackboard.pathfinding.get_map_goal(distance=0.0, side_offset=0.0)
+
+        # Are no other robots too close?
         other_robots_close = self.blackboard.costmap.is_other_robot_close(self.threshold_front, self.threshold_behind)
-        # actual set play situation
+        # Get actual set play situation
         set_play_state = self.blackboard.gamestate.get_set_play()
 
-        if abs(map_goal[2]) > math.pi / 2 or (
-            other_robots_close and set_play_state == 0
-        ):  # point away from opponent goal, so we should dribble
+        map_goal_points_away_from_opp_goal = abs(map_goal[2]) > math.pi / 2
+        regular_play_with_nearby_robot = other_robots_close and set_play_state == GameState.SET_PLAY_NONE
+
+        if map_goal_points_away_from_opp_goal or regular_play_with_nearby_robot:
             return "DRIBBLE"
         else:
             return "KICK"
